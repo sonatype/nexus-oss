806cb7316d98fe91f83973fc2c541a41e83a021c
Tamas Cservenak <cstamas@sonatype.com>
2008-05-01 17:57:32 +0000

    Adding exceptions instead of swallowing them stupidly.
    git-svn-id: file:///opt/svn/repositories/sonatype.org/spice/trunk/plexus-restlet-bridge@17 5751e0cb-dcb7-432f-92e2-260806df54be

--- src/main/java/org/sonatype/plexus/rest/PlexusRestletUtils.java
+++ src/main/java/org/sonatype/plexus/rest/PlexusRestletUtils.java
@@ -1,22 +1,23 @@
 /**
-  * Copyright (C) 2008 Sonatype Inc. 
-  * Sonatype Inc, licenses this file to you under the Apache License,
-  * Version 2.0 (the "License"); you may not use this file except in 
-  * compliance with the License.  You may obtain a copy of the License at
-  *
-  * http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing,
-  * software distributed under the License is distributed on an
-  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-  * KIND, either express or implied.  See the License for the
-  * specific language governing permissions and limitations
-  * under the License.
-  */
+ * Copyright (C) 2008 Sonatype Inc. 
+ * Sonatype Inc, licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in 
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.sonatype.plexus.rest;
 
 import org.codehaus.plexus.PlexusConstants;
 import org.codehaus.plexus.PlexusContainer;
+import org.codehaus.plexus.component.repository.exception.ComponentLookupException;
 import org.restlet.Context;
 
 /**
@@ -28,31 +29,19 @@ public class PlexusRestletUtils
 {
 
     public static Object plexusLookup( Context context, String role )
+        throws ComponentLookupException
     {
-        try
-        {
-            PlexusContainer container = (PlexusContainer) context.getAttributes().get( PlexusConstants.PLEXUS_KEY );
+        PlexusContainer container = (PlexusContainer) context.getAttributes().get( PlexusConstants.PLEXUS_KEY );
 
-            return container.lookup( role );
-        }
-        catch ( Exception e )
-        {
-            return null;
-        }
+        return container.lookup( role );
     }
 
     public static Object plexusLookup( Context context, String role, String roleHint )
+        throws ComponentLookupException
     {
-        try
-        {
-            PlexusContainer container = (PlexusContainer) context.getAttributes().get( PlexusConstants.PLEXUS_KEY );
+        PlexusContainer container = (PlexusContainer) context.getAttributes().get( PlexusConstants.PLEXUS_KEY );
 
-            return container.lookup( role, roleHint );
-        }
-        catch ( Exception e )
-        {
-            return null;
-        }
+        return container.lookup( role, roleHint );
     }
 
 }
