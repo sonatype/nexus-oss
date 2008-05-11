85d4ef366c094d2f5ce4dc32ff44c6cbdba3f2e5
Tamas Cservenak <cstamas@sonatype.com>
2008-05-11 23:49:54 +0000

    In simple mode we must filter for one of the "simple usernames"
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@89 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-rest-api/src/main/java/org/sonatype/nexus/rest/ApplicationContentBridge.java
+++ nexus/nexus-rest-api/src/main/java/org/sonatype/nexus/rest/ApplicationContentBridge.java
@@ -25,6 +25,7 @@ import org.restlet.Context;
 import org.restlet.Restlet;
 import org.restlet.Router;
 import org.sonatype.nexus.Nexus;
+import org.sonatype.nexus.security.SimpleAuthenticationSource;
 import org.sonatype.plexus.rest.PlexusRestletUtils;
 
 /**
@@ -67,7 +68,9 @@ public class ApplicationContentBridge
             if ( nexus.isSimpleSecurityModel() )
             {
                 // with "simple" we offer RO public access, but pwd is needed (if set) for deployment
-                nexusGuard = new NexusWriteAccessAuthenticationGuard( getContext() );
+                nexusGuard = new NexusWriteAccessAuthenticationGuard(
+                    getContext(),
+                    SimpleAuthenticationSource.DEPLOYMENT_USERNAME );
             }
             else
             {
