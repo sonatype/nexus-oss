203230a8956bbc3e7cfa182d9daf08ab5aea306a
dain <dain@2aa8b3fc-8ebb-4439-a84f-95066eaea8ab>
2008-05-14 21:10:31 +0000

    Fixed formatting
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@142 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-configuration/src/test/java/org/sonatype/nexus/security/SimpleAuthenticationSourceTest.java
+++ nexus/nexus-configuration/src/test/java/org/sonatype/nexus/security/SimpleAuthenticationSourceTest.java
@@ -8,47 +8,47 @@ public class SimpleAuthenticationSourceTest
     private AuthenticationSource source;
 
     protected void setUp()
-            throws Exception
+        throws Exception
     {
         super.setUp();
         source = (AuthenticationSource) lookup( AuthenticationSource.ROLE, "simple" );
-        assertNotNull( "source is null", source);
-        assertFalse(source.isAnynonymousAllowed());
+        assertNotNull( "source is null", source );
+        assertFalse( source.isAnynonymousAllowed() );
     }
 
     public void testAdminUser()
         throws Exception
     {
-        assertTrue(source.isKnown(SimpleAuthenticationSource.ADMIN_USERNAME));
-        assertTrue(source.hasPasswordSet(SimpleAuthenticationSource.ADMIN_USERNAME));
-        User adminUser = source.authenticate(SimpleAuthenticationSource.ADMIN_USERNAME, "admin123");
-        assertNotNull(adminUser);
-        assertEquals(SimpleAuthenticationSource.ADMIN_USERNAME, adminUser.getUsername());
-        assertFalse(adminUser.isAnonymous());
+        assertTrue( source.isKnown( SimpleAuthenticationSource.ADMIN_USERNAME ) );
+        assertTrue( source.hasPasswordSet( SimpleAuthenticationSource.ADMIN_USERNAME ) );
+        User adminUser = source.authenticate( SimpleAuthenticationSource.ADMIN_USERNAME, "admin123" );
+        assertNotNull( adminUser );
+        assertEquals( SimpleAuthenticationSource.ADMIN_USERNAME, adminUser.getUsername() );
+        assertFalse( adminUser.isAnonymous() );
 
-        adminUser = source.authenticate(SimpleAuthenticationSource.ADMIN_USERNAME, "unknown");
-        assertNull(adminUser);
+        adminUser = source.authenticate( SimpleAuthenticationSource.ADMIN_USERNAME, "unknown" );
+        assertNull( adminUser );
     }
 
     public void testDeploymentUser()
         throws Exception
     {
-        assertTrue(source.isKnown(SimpleAuthenticationSource.DEPLOYMENT_USERNAME));
-        assertFalse(source.hasPasswordSet(SimpleAuthenticationSource.DEPLOYMENT_USERNAME));
-        User developmentUser = source.authenticate(SimpleAuthenticationSource.DEPLOYMENT_USERNAME, "unknown");
-        assertNotNull(developmentUser);
-        assertEquals(SimpleAuthenticationSource.DEPLOYMENT_USERNAME, developmentUser.getUsername());
-        assertFalse(developmentUser.isAnonymous());
+        assertTrue( source.isKnown( SimpleAuthenticationSource.DEPLOYMENT_USERNAME ) );
+        assertFalse( source.hasPasswordSet( SimpleAuthenticationSource.DEPLOYMENT_USERNAME ) );
+        User developmentUser = source.authenticate( SimpleAuthenticationSource.DEPLOYMENT_USERNAME, "unknown" );
+        assertNotNull( developmentUser );
+        assertEquals( SimpleAuthenticationSource.DEPLOYMENT_USERNAME, developmentUser.getUsername() );
+        assertFalse( developmentUser.isAnonymous() );
     }
 
     public void testUnknownUser()
         throws Exception
     {
-        assertFalse(source.isKnown("UNKNOWN"));
-        assertFalse(source.hasPasswordSet("UNKNOWN"));
-        User unknownUser = source.authenticate("UNKNOWN", "unknown");
-        assertNotNull(unknownUser);
-        assertEquals("UNKNOWN", unknownUser.getUsername());
-        assertFalse(unknownUser.isAnonymous());
+        assertFalse( source.isKnown( "UNKNOWN" ) );
+        assertFalse( source.hasPasswordSet( "UNKNOWN" ) );
+        User unknownUser = source.authenticate( "UNKNOWN", "unknown" );
+        assertNotNull( unknownUser );
+        assertEquals( "UNKNOWN", unknownUser.getUsername() );
+        assertFalse( unknownUser.isAnonymous() );
     }
 }
