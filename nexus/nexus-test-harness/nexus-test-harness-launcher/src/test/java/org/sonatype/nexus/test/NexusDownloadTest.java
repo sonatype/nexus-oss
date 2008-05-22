358f07fbfbb0ddc49c4ae6a2f6c7598dac5baa40
Damian Bradicich <dbradicich@sonatype.com>
2008-05-22 16:12:41 +0000

    NX-551: start and stop are both working properly, now need to look into writing the tests
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@213 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-test-harness/nexus-test-harness-launcher/src/test/java/org/sonatype/nexus/test/NexusDownloadTest.java
+++ nexus/nexus-test-harness/nexus-test-harness-launcher/src/test/java/org/sonatype/nexus/test/NexusDownloadTest.java
@@ -1,4 +1,31 @@
+/*
+ * Nexus: Maven Repository Manager
+ * Copyright (C) 2008 Sonatype Inc.                                                                                                                          
+ * 
+ * This file is part of Nexus.                                                                                                                                  
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/.
+ *
+ */
 package org.sonatype.nexus.test;
+
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
 
 import junit.framework.TestCase;
 
@@ -18,6 +45,26 @@ public class NexusDownloadTest
         super.tearDown();
     }
     public void testDownloadArtifact()
+        throws Exception
     {
+        /*URL url = new URL( "http://localhost:8081/nexus/content/groups/nexus-test/org/sonatype/nexus/release-jar/1.0.0-beta-4-SNAPSHOT/release-jar-1.0.0-beta-4-SNAPSHOT" );
+        OutputStream out = new BufferedOutputStream( new FileOutputStream( "file.jar" ) );
+        URLConnection conn = url.openConnection();
+        InputStream in = conn.getInputStream();
+        try
+        {
+            byte[] buffer = new byte[1024];
+            int numRead;
+            long numWritten = 0;
+            while ((numRead = in.read(buffer)) != -1) {
+                out.write(buffer, 0, numRead);
+                numWritten += numRead;
+            }
+        }
+        finally
+        {
+            out.close();
+            in.close();
+        }*/
     }
 }
