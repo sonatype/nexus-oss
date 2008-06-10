df883e4b7b53e6dc3523076dde07f0181128face
Damian Bradicich <dbradicich@sonatype.com>
2008-06-10 20:22:31 +0000

    Updated some help text
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@357 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-webapp/src/main/webapp/js/repoServer/repoServer.resources.js
+++ nexus/nexus-webapp/src/main/webapp/js/repoServer/repoServer.resources.js
@@ -53,7 +53,7 @@ Sonatype.repoServer.resources.help = {
     deploymentPassword : 'Field to set the new Deployment password.',
     securityConfiguration : 'The Security Model to use.  Options include Simple, Custom and Off.', 
     workingDirectory : 'The base folder where Nexus will store all of its data. For easier upgrades, it is recommended that this be in a folder outside your Nexus binary installation. Nexus must be restarted after changing this value.',
-    logDirectory : 'This is the location of your log files. It must correspond to the location specified by /etc/log4j.properties.',
+    logDirectory : 'This is the location of your log files. It must correspond to the location specified by - runtime/apps/nexus/conf/log4j.properties.',
     baseUrl : 'This is the Base URL of the Nexus web application.  i.e. http://localhost:8081/nexus',
 
     //use default nexus text
