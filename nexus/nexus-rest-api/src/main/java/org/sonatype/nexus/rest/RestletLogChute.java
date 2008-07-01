c50291a1e0ab7b4e7e8b177eea390f1cf4f481e6
Tamas Cservenak <cstamas@sonatype.com>
2008-07-01 20:39:29 +0000

    Added new LogChute implementation to manage Velocity logging.
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@562 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

---
+++ nexus/nexus-rest-api/src/main/java/org/sonatype/nexus/rest/RestletLogChute.java
@@ -0,0 +1,61 @@
+package org.sonatype.nexus.rest;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.apache.velocity.runtime.RuntimeServices;
+import org.apache.velocity.runtime.log.LogChute;
+import org.restlet.Context;
+
+public class RestletLogChute
+    implements LogChute
+{
+    private final Logger logger;
+
+    public RestletLogChute( Context context )
+    {
+        super();
+
+        this.logger = context.getLogger();
+    }
+
+    public void init( RuntimeServices srv )
+        throws Exception
+    {
+        // nothing
+    }
+
+    public boolean isLevelEnabled( int level )
+    {
+        return logger.isLoggable( convertLevel( level ) );
+    }
+
+    public void log( int level, String msg )
+    {
+        logger.log( convertLevel( level ), msg );
+    }
+
+    public void log( int level, String msg, Throwable t )
+    {
+        logger.log( convertLevel( level ), msg, t );
+    }
+
+    protected Level convertLevel( int lvl )
+    {
+        switch ( lvl )
+        {
+            case TRACE_ID:
+                return Level.FINER;
+            case LogChute.DEBUG_ID:
+                return Level.FINE;
+            case INFO_ID:
+                return Level.INFO;
+            case WARN_ID:
+                return Level.WARNING;
+            case ERROR_ID:
+                return Level.SEVERE;
+            default:
+                return Level.INFO;
+        }
+    }
+}
