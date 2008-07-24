2aec8000ef57c82080a1224f55ab66aefcc30683
Tamas Cservenak <cstamas@sonatype.com>
2008-07-24 00:11:21 +0000

    A stupid hack to avoid One ServerServlet per webapp stupid limitation. Stupid.
    git-svn-id: file:///opt/svn/repositories/sonatype.org/spice/trunk/plexus-restlet-bridge@134 5751e0cb-dcb7-432f-92e2-260806df54be

--- src/main/java/org/sonatype/plexus/rest/ServletRestletApplicationBridge.java
+++ src/main/java/org/sonatype/plexus/rest/ServletRestletApplicationBridge.java
@@ -1,18 +1,18 @@
 /**
-  * Copyright (C) 2008 Sonatype Inc. 
-  * Sonatype Inc, licenses this file to you under the Apache License,
-  * Version 2.0 (the "License"); you may not use this file except in 
-  * compliance with the License.  You may obtain a copy of the License at
-  *
-  * http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing,
-  * software distributed under the License is distributed on an
-  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-  * KIND, either express or implied.  See the License for the
-  * specific language governing permissions and limitations
-  * under the License.
-  */
+ * Copyright (C) 2008 Sonatype Inc. 
+ * Sonatype Inc, licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in 
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.sonatype.plexus.rest;
 
 import org.codehaus.plexus.PlexusConstants;
@@ -40,7 +40,7 @@ public class ServletRestletApplicationBridge
         {
             ( (PlexusRestletApplicationBridge) application ).setPlexusContainer( getPlexusContainer() );
         }
-        
+
         return application;
     }
 
@@ -49,4 +49,22 @@ public class ServletRestletApplicationBridge
         return (PlexusContainer) getServletContext().getAttribute( PlexusConstants.PLEXUS_KEY );
     }
 
+    public String getInitParameter( String name, String defaultValue )
+    {
+        String prefixedName = getServletConfig().getServletName() + "." + name;
+
+        String result = getServletConfig().getInitParameter( prefixedName );
+
+        if ( result == null )
+        {
+            result = getServletConfig().getServletContext().getInitParameter( prefixedName );
+        }
+
+        if ( result == null )
+        {
+            result = getServletConfig().getServletName() + "." + defaultValue;
+        }
+
+        return result;
+    }
 }
