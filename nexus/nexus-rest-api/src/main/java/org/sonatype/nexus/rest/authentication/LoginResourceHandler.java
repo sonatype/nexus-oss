25477ec17ada5178191e2f18c1241a07ed3269ff
Tamas Cservenak <cstamas@sonatype.com>
2008-07-31 14:14:58 +0000

    rest resource returns real mapping
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@882 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-rest-api/src/main/java/org/sonatype/nexus/rest/authentication/LoginResourceHandler.java
+++ nexus/nexus-rest-api/src/main/java/org/sonatype/nexus/rest/authentication/LoginResourceHandler.java
@@ -22,11 +22,16 @@ package org.sonatype.nexus.rest.authentication;
 
 import java.io.IOException;
 
+import org.jsecurity.SecurityUtils;
+import org.jsecurity.authz.Permission;
+import org.jsecurity.authz.permission.WildcardPermission;
+import org.jsecurity.subject.Subject;
 import org.restlet.Context;
 import org.restlet.data.Request;
 import org.restlet.data.Response;
 import org.restlet.resource.Representation;
 import org.restlet.resource.Variant;
+import org.sonatype.nexus.configuration.security.model.CPrivilege;
 import org.sonatype.nexus.rest.AbstractNexusResourceHandler;
 import org.sonatype.nexus.rest.model.AuthenticationClientPermissions;
 import org.sonatype.nexus.rest.model.AuthenticationLoginResource;
@@ -40,11 +45,15 @@ import org.sonatype.nexus.rest.model.AuthenticationLoginResourceResponse;
 public class LoginResourceHandler
     extends AbstractNexusResourceHandler
 {
+    private static final int NONE = 0;
+
     private static final int READ = 1;
 
-    private static final int EDIT = 2;
+    private static final int UPDATE = 2;
 
     private static final int DELETE = 4;
+
+    private static final int CREATE = 8;
 
     public LoginResourceHandler( Context context, Request request, Response response )
     {
@@ -56,43 +65,43 @@ public class LoginResourceHandler
     {
         AuthenticationLoginResource resource = new AuthenticationLoginResource();
 
-        resource.setAuthToken( "DEPRECATED" );
-
         AuthenticationClientPermissions perms = new AuthenticationClientPermissions();
 
-        perms.setViewSearch( READ );
+        Subject subject = SecurityUtils.getSubject();
 
-        perms.setViewUpdatedArtifacts( 0 );
+        perms.setViewSearch( getFlagsForPermission( subject, "nexus:index" ) );
 
-        perms.setViewCachedArtifacts( READ );
+        perms.setViewUpdatedArtifacts( getFlagsForPermission( subject, "nexus:feeds" ) );
 
-        perms.setViewDeployedArtifacts( READ );
+        perms.setViewCachedArtifacts( getFlagsForPermission( subject, "nexus:feeds" ) );
 
-        perms.setViewSystemChanges( READ );
+        perms.setViewDeployedArtifacts( getFlagsForPermission( subject, "nexus:feeds" ) );
 
-        perms.setMaintLogs( READ );
+        perms.setViewSystemChanges( getFlagsForPermission( subject, "nexus:feeds" ) );
 
-        perms.setMaintConfig( READ );
+        perms.setMaintLogs( getFlagsForPermission( subject, "nexus:logs" ) );
 
-        perms.setMaintRepos( READ | EDIT );
+        perms.setMaintConfig( getFlagsForPermission( subject, "nexus:configuration" ) );
 
-        perms.setConfigServer( READ | EDIT );
+        perms.setMaintRepos( getFlagsForPermission( subject, "nexus:feeds" ) );
 
-        perms.setConfigGroups( READ | EDIT | DELETE );
+        perms.setConfigServer( getFlagsForPermission( subject, "nexus:configuration" ) );
 
-        perms.setConfigRules( READ | EDIT | DELETE );
+        perms.setConfigGroups( getFlagsForPermission( subject, "nexus:repogroups" ) );
 
-        perms.setConfigRepos( READ | EDIT | DELETE );
-        
-        perms.setConfigSchedules( READ | EDIT | DELETE );
-        
-        perms.setConfigUsers( READ | EDIT | DELETE );
-        
-        perms.setConfigRoles( READ | EDIT | DELETE );
-        
-        perms.setConfigPrivileges( READ | EDIT | DELETE );
-        
-        perms.setConfigRepoTargets( READ | EDIT | DELETE );
+        perms.setConfigRules( getFlagsForPermission( subject, "nexus:routes" ) );
+
+        perms.setConfigRepos( getFlagsForPermission( subject, "nexus:repositories" ) );
+
+        perms.setConfigSchedules( getFlagsForPermission( subject, "nexus:tasks" ) );
+
+        perms.setConfigUsers( getFlagsForPermission( subject, "nexus:users" ) );
+
+        perms.setConfigRoles( getFlagsForPermission( subject, "nexus:roles" ) );
+
+        perms.setConfigPrivileges( getFlagsForPermission( subject, "nexus:privileges" ) );
+
+        perms.setConfigRepoTargets( getFlagsForPermission( subject, "nexus:targets" ) );
 
         resource.setClientPermissions( perms );
 
@@ -101,6 +110,38 @@ public class LoginResourceHandler
         response.setData( resource );
 
         return serialize( variant, response );
+    }
+
+    protected int getFlagsForPermission( Subject subject, String domain )
+    {
+        Permission readPerm = new WildcardPermission( domain + ":" + CPrivilege.METHOD_READ );
+
+        Permission createPerm = new WildcardPermission( domain + ":" + CPrivilege.METHOD_CREATE );
+
+        Permission updatePerm = new WildcardPermission( domain + ":" + CPrivilege.METHOD_UPDATE );
+
+        Permission deletePerm = new WildcardPermission( domain + ":" + CPrivilege.METHOD_DELETE );
+
+        int perm = NONE;
+
+        if ( subject.isPermitted( readPerm ) )
+        {
+            perm |= READ;
+        }
+        if ( subject.isPermitted( createPerm ) )
+        {
+            perm |= CREATE;
+        }
+        if ( subject.isPermitted( updatePerm ) )
+        {
+            perm |= UPDATE;
+        }
+        if ( subject.isPermitted( deletePerm ) )
+        {
+            perm |= DELETE;
+        }
+
+        return perm;
     }
 
 }
