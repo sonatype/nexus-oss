28c4c1631bf9f31048e492c0528b83e158383d92
Marvin Froeder <velo@sonatype.com>
2008-08-01 01:03:08 +0000

    IN PROGRESS - issue NEXUS-383: Search tests http://issues.sonatype.org/browse/NEXUS-383
    Add SHA1 tests.
    
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@897 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-test-harness/nexus-test-harness-launcher/src/test/java/org/sonatype/nexus/integrationtests/nexus383/SearchMessageUtil.java
+++ nexus/nexus-test-harness/nexus-test-harness-launcher/src/test/java/org/sonatype/nexus/integrationtests/nexus383/SearchMessageUtil.java
@@ -5,49 +5,90 @@ import java.io.InputStream;
 import java.net.URL;
 import java.util.List;
 
+import org.codehaus.plexus.util.StringUtils;
 import org.restlet.data.MediaType;
 import org.sonatype.nexus.rest.model.NexusArtifact;
 import org.sonatype.nexus.rest.model.SearchResponse;
+import org.sonatype.nexus.rest.xstream.XStreamInitializer;
 import org.sonatype.plexus.rest.representation.XStreamRepresentation;
 
 import com.thoughtworks.xstream.XStream;
 
-public class SearchMessageUtil {
+public class SearchMessageUtil
+{
 
-	private String baseNexusUrl;
+    private String baseNexusUrl;
 
-	public SearchMessageUtil(String baseNexusUrl) {
-		super();
-		this.baseNexusUrl = baseNexusUrl;
-	}
+    public SearchMessageUtil( String baseNexusUrl )
+    {
+        super();
+        this.baseNexusUrl = baseNexusUrl;
+    }
 
-	@SuppressWarnings("unchecked")
-	public List<NexusArtifact> searchFor(String query) throws Exception {
-		String serviceURI = this.baseNexusUrl + "service/local/data_index?q="
-				+ query;
-		System.out.println("serviceURI: " + serviceURI);
+    @SuppressWarnings( "unchecked" )
+    public List<NexusArtifact> searchFor( String query )
+        throws Exception
+    {
+        String serviceURI = this.baseNexusUrl + "service/local/data_index?q=" + query;
+        System.out.println( "serviceURI: " + serviceURI );
 
-		URL serviceURL = new URL(serviceURI);
+        URL serviceURL = new URL( serviceURI );
 
-		InputStream is = serviceURL.openStream();
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
+        InputStream is = serviceURL.openStream();
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
 
-		int readChar = -1;
-		while ((readChar = is.read()) != -1) {
-			out.write(readChar);
-		}
+        int readChar = -1;
+        while ( ( readChar = is.read() ) != -1 )
+        {
+            out.write( readChar );
+        }
 
-		String responseText = out.toString();
-		System.out.println("responseText: \n" + responseText);
-		XStream stream = new XStream();
-		stream.alias("artifact", NexusArtifact.class);
-		XStreamRepresentation representation = new XStreamRepresentation(
-				stream, responseText, MediaType.APPLICATION_XML);
+        String responseText = out.toString();
+        System.out.println( "responseText: \n" + responseText );
+        XStream xstream = new XStream();
+        XStreamInitializer.initialize( xstream );
+        XStreamRepresentation representation =
+            new XStreamRepresentation( xstream, responseText, MediaType.APPLICATION_XML );
 
-		SearchResponse searchResponde = (SearchResponse) representation
-				.getPayload(new SearchResponse());
+        SearchResponse searchResponde = (SearchResponse) representation.getPayload( new SearchResponse() );
 
-		return searchResponde.getData();
-	}
+        return searchResponde.getData();
+    }
+
+    public NexusArtifact searchForSHA1( String sha1 )
+        throws Exception
+    {
+        String serviceURI = this.baseNexusUrl + "service/local/identify/sha1/" + sha1;
+        System.out.println( "serviceURI: " + serviceURI );
+
+        URL serviceURL = new URL( serviceURI );
+
+        InputStream is = serviceURL.openStream();
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+        int readChar = -1;
+        while ( ( readChar = is.read() ) != -1 )
+        {
+            out.write( readChar );
+        }
+
+        String responseText = out.toString();
+        if ( StringUtils.isEmpty( responseText ) )
+        {
+            return null;
+        }
+        System.out.println( "responseText: \n" + responseText );
+        XStream xstream = new XStream();
+        XStreamInitializer.initialize( xstream );
+        // FIXME check with toby if I need to use this.
+        // XStreamRepresentation representation = new XStreamRepresentation(
+        // xstream, responseText, MediaType.APPLICATION_XML);
+        //
+        // SearchResponse searchResponde = (SearchResponse) representation
+        // .getPayload(new SearchResponse());
+        //
+        // return searchResponde.getData();
+        return (NexusArtifact) xstream.fromXML( responseText );
+    }
 
 }
