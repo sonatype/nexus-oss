21cb10ea9d393b91b7cf12f388681994fac5ffb4
Tamas Cservenak <cstamas@sonatype.com>
2008-08-04 19:39:09 +0000

    Fixed the test, now it is using locally brought up Jetty to simulate remote reposes
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@938 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-app/src/test/java/org/sonatype/nexus/maven/tasks/DefaultSnapshotRemoverTest.java
+++ nexus/nexus-app/src/test/java/org/sonatype/nexus/maven/tasks/DefaultSnapshotRemoverTest.java
@@ -1,5 +1,6 @@
 package org.sonatype.nexus.maven.tasks;
 
+import org.sonatype.jettytestsuite.ServletServer;
 import org.sonatype.nexus.AbstractNexusTestCase;
 import org.sonatype.nexus.DefaultNexus;
 import org.sonatype.nexus.Nexus;
@@ -17,90 +18,86 @@ public class DefaultSnapshotRemoverTest
 
     private MavenRepository central;
 
+    private ServletServer servetServer;
+
     protected void setUp()
         throws Exception
     {
         super.setUp();
 
+        // jetty as "remote repo"
+        servetServer = (ServletServer) lookup( ServletServer.ROLE );
+
+        servetServer.start();
+
         defaultNexus = (DefaultNexus) lookup( Nexus.ROLE );
 
         apacheSnapshots = (MavenRepository) defaultNexus.getRepository( "apache-snapshots" );
 
+        apacheSnapshots.setRemoteUrl( servetServer.getUrl( apacheSnapshots.getId() ) );
+
         central = (MavenRepository) defaultNexus.getRepository( "central" );
+
+        central.setRemoteUrl( servetServer.getUrl( central.getId() ) );
     }
 
     protected void tearDown()
         throws Exception
     {
+        servetServer.stop();
+
         super.tearDown();
     }
 
     public void fillInRepo()
         throws Exception
     {
-        // get 4 pieces of maven-core
+        // Case1: different builds of same snap artifact. Should be kept the newest (with regard to params)
+        // get 4 pieces of beta-5-snap
         ArtifactStoreRequest gavRequest = new ArtifactStoreRequest(
-            "org.apache.maven",
-            "maven-core",
-            "2.0.9-20080306.010327-7" );
+            "org.sonatype.nexus",
+            "nexus-indexer",
+            "1.0-beta-5-20080711.162119-2" );
 
         apacheSnapshots.retrieveArtifactPom( gavRequest );
 
         apacheSnapshots.retrieveArtifact( gavRequest );
 
-        gavRequest = new ArtifactStoreRequest( "org.apache.maven", "maven-core", "2.0.9-20080302.032223-6" );
-
-        apacheSnapshots.retrieveArtifactPom( gavRequest );
-
-        apacheSnapshots.retrieveArtifact( gavRequest );
-        
-        // get sources for this too
-        
-        gavRequest = new ArtifactStoreRequest( "org.apache.maven", "maven-core", "2.0.9-20080302.032223-6", "zip", "bin" );
-
-        apacheSnapshots.retrieveArtifact( gavRequest );
-
-        gavRequest = new ArtifactStoreRequest( "org.apache.maven", "maven-core", "2.0.9-20080302.014714-5" );
+        gavRequest = new ArtifactStoreRequest( "org.sonatype.nexus", "nexus-indexer", "1.0-beta-5-20080718.231118-50" );
 
         apacheSnapshots.retrieveArtifactPom( gavRequest );
 
         apacheSnapshots.retrieveArtifact( gavRequest );
 
-        gavRequest = new ArtifactStoreRequest( "org.apache.maven", "maven-core", "2.0.9-20080215.024846-4" );
+        gavRequest = new ArtifactStoreRequest( "org.sonatype.nexus", "nexus-indexer", "1.0-beta-5-20080730.002543-149" );
 
         apacheSnapshots.retrieveArtifactPom( gavRequest );
 
         apacheSnapshots.retrieveArtifact( gavRequest );
 
-        // get 2 pieces of maven-archiver
-        gavRequest = new ArtifactStoreRequest( "org.apache.maven", "maven-archiver", "2.3-20080101.212006-10" );
+        gavRequest = new ArtifactStoreRequest( "org.sonatype.nexus", "nexus-indexer", "1.0-beta-5-20080731.150252-163" );
 
         apacheSnapshots.retrieveArtifactPom( gavRequest );
 
         apacheSnapshots.retrieveArtifact( gavRequest );
 
-        gavRequest = new ArtifactStoreRequest( "org.apache.maven", "maven-archiver", "2.3-20080101.211719-9" );
-
-        apacheSnapshots.retrieveArtifactPom( gavRequest );
-
-        apacheSnapshots.retrieveArtifact( gavRequest );
-
-        // get 1 piece of maven-artifact
+        // Case2: a snap is here that has release also in other repo
+        // get 1 piece of nexus-indexer b4
         apacheSnapshots.retrieveItem( new ResourceStoreRequest(
-            "/org/apache/maven/maven-artifact/2.0.9-SNAPSHOT/maven-artifact-2.0.9-20080306.011152-9.pom",
+            "/org/sonatype/nexus/nexus-indexer/1.0-beta-4-SNAPSHOT/nexus-indexer-1.0-beta-4-SNAPSHOT.pom",
             false ) );
 
         apacheSnapshots.retrieveItem( new ResourceStoreRequest(
-            "/org/apache/maven/maven-artifact/2.0.9-SNAPSHOT/maven-artifact-2.0.9-20080306.011152-9.jar",
+            "/org/sonatype/nexus/nexus-indexer/1.0-beta-4-SNAPSHOT/nexus-indexer-1.0-beta-4-SNAPSHOT.jar",
             false ) );
 
         // and get it's release counterpart
         central.retrieveItem( new ResourceStoreRequest(
-            "/org/apache/maven/maven-artifact/2.0.9/maven-artifact-2.0.9.pom",
+            "/org/sonatype/nexus/nexus-indexer/1.0-beta-4/nexus-indexer-1.0-beta-4.pom",
             false ) );
 
         central.retrieveItem( new ResourceStoreRequest(
-            "/org/apache/maven/maven-artifact/2.0.9/maven-artifact-2.0.9.jar",
+            "/org/sonatype/nexus/nexus-indexer/1.0-beta-4/nexus-indexer-1.0-beta-4.jar",
             false ) );
     }
 
@@ -120,8 +117,8 @@ public class DefaultSnapshotRemoverTest
         ArtifactStoreRequest gavRequest = null;
 
         // test the results by their local availability
-        
-        gavRequest = new ArtifactStoreRequest( "org.apache.maven", "maven-core", "2.0.9-20080306.010327-7" );
+
+        gavRequest = new ArtifactStoreRequest( "org.sonatype.nexus", "nexus-indexer", "1.0-beta-5-20080731.150252-163" );
 
         gavRequest.setRequestLocalOnly( true );
 
@@ -143,7 +140,7 @@ public class DefaultSnapshotRemoverTest
             fail( "This is the newest, it should be left!" );
         }
 
-        gavRequest = new ArtifactStoreRequest( "org.apache.maven", "maven-core", "2.0.9-20080302.032223-6" );
+        gavRequest = new ArtifactStoreRequest( "org.sonatype.nexus", "nexus-indexer", "1.0-beta-5-20080730.002543-149" );
 
         gavRequest.setRequestLocalOnly( true );
 
@@ -158,108 +155,7 @@ public class DefaultSnapshotRemoverTest
             // good
         }
 
-        gavRequest = new ArtifactStoreRequest( "org.apache.maven", "maven-core", "2.0.9-20080302.032223-6", "zip", "bin" );
-
-        gavRequest.setRequestLocalOnly( true );
-
-        try
-        {
-            apacheSnapshots.retrieveArtifactPom( gavRequest );
-
-            fail( "This is not the newest bin distro, it should be deleted!" );
-        }
-        catch ( ItemNotFoundException e )
-        {
-            // good
-        }
-
-        try
-        {
-            apacheSnapshots.retrieveArtifact( gavRequest );
-
-            fail( "This is not the newest, it should be deleted!" );
-        }
-        catch ( ItemNotFoundException e )
-        {
-            // good
-        }
-
-        gavRequest = new ArtifactStoreRequest( "org.apache.maven", "maven-core", "2.0.9-20080302.014714-5" );
-
-        gavRequest.setRequestLocalOnly( true );
-
-        try
-        {
-            apacheSnapshots.retrieveArtifactPom( gavRequest );
-
-            fail( "This is not the newest, it should be deleted!" );
-        }
-        catch ( ItemNotFoundException e )
-        {
-            // good
-        }
-
-        try
-        {
-            apacheSnapshots.retrieveArtifact( gavRequest );
-
-            fail( "This is not the newest, it should be deleted!" );
-        }
-        catch ( ItemNotFoundException e )
-        {
-            // good
-        }
-
-        gavRequest = new ArtifactStoreRequest( "org.apache.maven", "maven-core", "2.0.9-20080215.024846-4" );
-
-        gavRequest.setRequestLocalOnly( true );
-
-        try
-        {
-            apacheSnapshots.retrieveArtifactPom( gavRequest );
-
-            fail( "This is not the newest, it should be deleted!" );
-        }
-        catch ( ItemNotFoundException e )
-        {
-            // good
-        }
-
-        try
-        {
-            apacheSnapshots.retrieveArtifact( gavRequest );
-
-            fail( "This is not the newest, it should be deleted!" );
-        }
-        catch ( ItemNotFoundException e )
-        {
-            // good
-        }
-
-        // get 2 pieces of maven-archiver
-        gavRequest = new ArtifactStoreRequest( "org.apache.maven", "maven-archiver", "2.3-20080101.212006-10" );
-
-        gavRequest.setRequestLocalOnly( true );
-
-        try
-        {
-            apacheSnapshots.retrieveArtifactPom( gavRequest );
-        }
-        catch ( ItemNotFoundException e )
-        {
-            fail( "This is the newest, it should be left!" );
-        }
-
-        try
-        {
-            apacheSnapshots.retrieveArtifact( gavRequest );
-        }
-        catch ( ItemNotFoundException e )
-        {
-            fail( "This is the newest, it should be left!" );
-        }
-
-        gavRequest = new ArtifactStoreRequest( "org.apache.maven", "maven-archiver", "2.3-20080101.211719-9" );
+        gavRequest = new ArtifactStoreRequest( "org.sonatype.nexus", "nexus-indexer", "1.0-beta-5-20080718.231118-50" );
 
         gavRequest.setRequestLocalOnly( true );
 
@@ -289,7 +185,7 @@ public class DefaultSnapshotRemoverTest
         try
         {
             apacheSnapshots.retrieveItem( new ResourceStoreRequest(
-                "/org/apache/maven/maven-artifact/2.0.9-SNAPSHOT/maven-artifact-2.0.9-20080306.011152-9.pom",
+                "/org/sonatype/nexus/nexus-indexer/1.0-beta-4-SNAPSHOT/nexus-indexer-1.0-beta-4-SNAPSHOT.pom",
                 true ) );
 
             fail( "Release exists, it should be deleted!" );
@@ -302,7 +198,7 @@ public class DefaultSnapshotRemoverTest
         try
         {
             apacheSnapshots.retrieveItem( new ResourceStoreRequest(
-                "/org/apache/maven/maven-artifact/2.0.9-SNAPSHOT/maven-artifact-2.0.9-20080306.011152-9.jar",
+                "/org/sonatype/nexus/nexus-indexer/1.0-beta-4-SNAPSHOT/nexus-indexer-1.0-beta-4-SNAPSHOT.jar",
                 true ) );
 
             fail( "Release exists, it should be deleted!" );
@@ -316,7 +212,7 @@ public class DefaultSnapshotRemoverTest
         try
         {
             central.retrieveItem( new ResourceStoreRequest(
-                "/org/apache/maven/maven-artifact/2.0.9/maven-artifact-2.0.9.pom",
+                "/org/sonatype/nexus/nexus-indexer/1.0-beta-4/nexus-indexer-1.0-beta-4.pom",
                 true ) );
         }
         catch ( ItemNotFoundException e )
@@ -327,35 +223,33 @@ public class DefaultSnapshotRemoverTest
         try
         {
             central.retrieveItem( new ResourceStoreRequest(
-                "/org/apache/maven/maven-artifact/2.0.9/maven-artifact-2.0.9.jar",
+                "/org/sonatype/nexus/nexus-indexer/1.0-beta-4/nexus-indexer-1.0-beta-4.jar",
                 true ) );
         }
         catch ( ItemNotFoundException e )
         {
             fail( "This is the release, it should be left!" );
         }
-        
+
         /*******************************************
-         * 
          * Start round 2
-         * 
          ******************************************/
-        
+
         fillInRepo();
 
         // and now setup the request
-        // process the apacheSnapshots, leave min 1 snap
+        // process the apacheSnapshots, leave min 2 snap
         request = new SnapshotRemovalRequest( apacheSnapshots.getId(), null, 2, -1, false );
 
         result = defaultNexus.removeSnapshots( request );
 
         assertEquals( 1, result.getProcessedRepositories().size() );
-        
+
         gavRequest = null;
 
         // test the results by their local availability
-        
-        gavRequest = new ArtifactStoreRequest( "org.apache.maven", "maven-core", "2.0.9-20080306.010327-7" );
+
+        gavRequest = new ArtifactStoreRequest( "org.sonatype.nexus", "nexus-indexer", "1.0-beta-5-20080731.150252-163" );
 
         gavRequest.setRequestLocalOnly( true );
 
@@ -377,7 +271,7 @@ public class DefaultSnapshotRemoverTest
             fail( "Should not be deleted!" );
         }
 
-        gavRequest = new ArtifactStoreRequest( "org.apache.maven", "maven-core", "2.0.9-20080302.032223-6" );
+        gavRequest = new ArtifactStoreRequest( "org.sonatype.nexus", "nexus-indexer", "1.0-beta-5-20080730.002543-149" );
 
         gavRequest.setRequestLocalOnly( true );
 
@@ -399,33 +293,7 @@ public class DefaultSnapshotRemoverTest
             fail( "Should not be deleted!" );
         }
 
-        gavRequest = new ArtifactStoreRequest( "org.apache.maven", "maven-core", "2.0.9-20080302.014714-5" );
-
-        gavRequest.setRequestLocalOnly( true );
-
-        try
-        {
-            apacheSnapshots.retrieveArtifactPom( gavRequest );
-
-            fail( "Should be deleted!" );
-        }
-        catch ( ItemNotFoundException e )
-        {
-            // good
-        }
-
-        try
-        {
-            apacheSnapshots.retrieveArtifact( gavRequest );
-
-            fail( "Should be deleted!" );
-        }
-        catch ( ItemNotFoundException e )
-        {
-            // good
-        }
-
-        gavRequest = new ArtifactStoreRequest( "org.apache.maven", "maven-core", "2.0.9-20080215.024846-4" );
+        gavRequest = new ArtifactStoreRequest( "org.sonatype.nexus", "nexus-indexer", "1.0-beta-5-20080718.231118-50" );
 
         gavRequest.setRequestLocalOnly( true );
 
@@ -449,58 +317,13 @@ public class DefaultSnapshotRemoverTest
         catch ( ItemNotFoundException e )
         {
             // good
-        }
-
-        // get 2 pieces of maven-archiver
-        gavRequest = new ArtifactStoreRequest( "org.apache.maven", "maven-archiver", "2.3-20080101.212006-10" );
-
-        gavRequest.setRequestLocalOnly( true );
-
-        try
-        {
-            apacheSnapshots.retrieveArtifactPom( gavRequest );
-        }
-        catch ( ItemNotFoundException e )
-        {
-            fail( "Should not be deleted!" );
-        }
-
-        try
-        {
-            apacheSnapshots.retrieveArtifact( gavRequest );
-        }
-        catch ( ItemNotFoundException e )
-        {
-            fail( "Should not be deleted!" );
-        }
-
-        gavRequest = new ArtifactStoreRequest( "org.apache.maven", "maven-archiver", "2.3-20080101.211719-9" );
-
-        gavRequest.setRequestLocalOnly( true );
-
-        try
-        {
-            apacheSnapshots.retrieveArtifactPom( gavRequest );
-        }
-        catch ( ItemNotFoundException e )
-        {
-            fail( "Should not be deleted!" );
-        }
-
-        try
-        {
-            apacheSnapshots.retrieveArtifact( gavRequest );
-        }
-        catch ( ItemNotFoundException e )
-        {
-            fail( "Should not be deleted!" );
         }
 
         // get 1 piece of maven-artifact
         try
         {
             apacheSnapshots.retrieveItem( new ResourceStoreRequest(
-                "/org/apache/maven/maven-artifact/2.0.9-SNAPSHOT/maven-artifact-2.0.9-20080306.011152-9.pom",
+                "/org/sonatype/nexus/nexus-indexer/1.0-beta-4-SNAPSHOT/nexus-indexer-1.0-beta-4-SNAPSHOT.pom",
                 true ) );
         }
         catch ( ItemNotFoundException e )
@@ -511,7 +334,7 @@ public class DefaultSnapshotRemoverTest
         try
         {
             apacheSnapshots.retrieveItem( new ResourceStoreRequest(
-                "/org/apache/maven/maven-artifact/2.0.9-SNAPSHOT/maven-artifact-2.0.9-20080306.011152-9.jar",
+                "/org/sonatype/nexus/nexus-indexer/1.0-beta-4-SNAPSHOT/nexus-indexer-1.0-beta-4-SNAPSHOT.jar",
                 true ) );
         }
         catch ( ItemNotFoundException e )
@@ -523,7 +346,7 @@ public class DefaultSnapshotRemoverTest
         try
         {
             central.retrieveItem( new ResourceStoreRequest(
-                "/org/apache/maven/maven-artifact/2.0.9/maven-artifact-2.0.9.pom",
+                "/org/sonatype/nexus/nexus-indexer/1.0-beta-4/nexus-indexer-1.0-beta-4.pom",
                 true ) );
         }
         catch ( ItemNotFoundException e )
@@ -534,7 +357,7 @@ public class DefaultSnapshotRemoverTest
         try
         {
             central.retrieveItem( new ResourceStoreRequest(
-                "/org/apache/maven/maven-artifact/2.0.9/maven-artifact-2.0.9.jar",
+                "/org/sonatype/nexus/nexus-indexer/1.0-beta-4/nexus-indexer-1.0-beta-4.jar",
                 true ) );
         }
         catch ( ItemNotFoundException e )
