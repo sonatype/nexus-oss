c490f31f4426d3962498462b9c4c3ab0a6455604
Marvin Froeder <velo@sonatype.com>
2008-08-07 19:49:03 +0000

    IN PROGRESS - issue NEXUS-408: Create IT tests for change password http://issues.sonatype.org/browse/NEXUS-408
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@1028 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

---
+++ nexus/nexus-test-harness/nexus-test-harness-launcher/src/test/java/org/sonatype/nexus/integrationtests/nexus408/Nexus408PermissionTest.java
@@ -0,0 +1,52 @@
+package org.sonatype.nexus.integrationtests.nexus408;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.restlet.data.Status;
+import org.sonatype.nexus.integrationtests.AbstractPrivilegeTest;
+import org.sonatype.nexus.integrationtests.TestContainer;
+
+public class Nexus408PermissionTest
+    extends AbstractPrivilegeTest
+{
+
+    @Test
+    public void withPermission()
+        throws Exception
+    {
+        overwriteUserRole( TEST_USER_NAME, "anonymous-with-login-forgotuser", "1", "2" /* login */, "6", "14", "17",
+                           "19", "44", "54", "55", "56", "57", "58", "59", "64"/* change pw */, "T1", "T2" );
+
+        TestContainer.getInstance().getTestContext().setUsername( TEST_USER_NAME );
+        TestContainer.getInstance().getTestContext().setPassword( TEST_USER_PASSWORD );
+
+        // Should be able to change my own password
+        Status status = ChangePasswordUtils.recoverUsername( "test-user", "admin123", "123admin" );
+        Assert.assertTrue( status.isSuccess() );
+
+        // NOT Should be able to change my own password
+        status = ChangePasswordUtils.recoverUsername( "admin", "admin123", "123admin" );
+        Assert.assertEquals( 401, status.getCode() );
+
+    }
+
+    @Test
+    public void withoutPermission()
+        throws Exception
+    {
+        overwriteUserRole( TEST_USER_NAME, "anonymous-with-login-but-forgotuser", "1", "2" /* login */, "6", "14",
+                           "17", "19", "44", "54", "55", "56", "57", "58", "59", /* "64" change pw, */"T1", "T2" );
+
+        TestContainer.getInstance().getTestContext().setUsername( TEST_USER_NAME );
+        TestContainer.getInstance().getTestContext().setPassword( TEST_USER_PASSWORD );
+
+        // NOT Should be able to forgot my own username
+        Status status = ChangePasswordUtils.recoverUsername( "test-user", "admin123", "123admin" );
+        Assert.assertEquals( 401, status.getCode() );
+
+        // NOT Should be able to forgot anyone username
+        status = ChangePasswordUtils.recoverUsername( "admin", "admin123", "123admin" );
+        Assert.assertEquals( 401, status.getCode() );
+    }
+
+}
