a610cf41a1b28c89ef4c7fc9151570cc314c610c
Tamas Cservenak <cstamas@sonatype.com>
2008-08-12 21:38:01 +0000

    NEXUS-518: unflush
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@1098 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-app/src/main/java/org/sonatype/nexus/configuration/application/runtime/DefaultApplicationRuntimeConfigurationBuilder.java
+++ nexus/nexus-app/src/main/java/org/sonatype/nexus/configuration/application/runtime/DefaultApplicationRuntimeConfigurationBuilder.java
@@ -402,6 +402,8 @@ public class DefaultApplicationRuntimeConfigurationBuilder
 
             shadowRepository
                 .setLocalStorage( getLocalRepositoryStorage( shadowRepository.getId(), DEFAULT_LS_PROVIDER ) );
+            
+            return shadowRepository;
         }
         catch ( MalformedURLException e )
         {
