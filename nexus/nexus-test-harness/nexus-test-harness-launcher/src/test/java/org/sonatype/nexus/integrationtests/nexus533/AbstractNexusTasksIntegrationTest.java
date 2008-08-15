3252e5500c7ebc2a2b86fb380685a983e78f45a2
Marvin Froeder <velo@sonatype.com>
2008-08-15 14:27:52 +0000

    IN PROGRESS - issue NEXUS-533: Create crud tests for Tasks http://issues.sonatype.org/browse/NEXUS-533
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@1186 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

---
+++ nexus/nexus-test-harness/nexus-test-harness-launcher/src/test/java/org/sonatype/nexus/integrationtests/nexus533/AbstractNexusTasksIntegrationTest.java
@@ -0,0 +1,98 @@
+package org.sonatype.nexus.integrationtests.nexus533;
+
+import java.io.IOException;
+import java.util.List;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.restlet.data.Status;
+import org.sonatype.nexus.configuration.model.CScheduledTask;
+import org.sonatype.nexus.configuration.model.Configuration;
+import org.sonatype.nexus.integrationtests.AbstractNexusIntegrationTest;
+import org.sonatype.nexus.rest.model.ScheduledServiceBaseResource;
+import org.sonatype.nexus.rest.model.ScheduledServiceListResource;
+import org.sonatype.nexus.test.utils.NexusConfigUtil;
+
+public abstract class AbstractNexusTasksIntegrationTest<E extends ScheduledServiceBaseResource>
+    extends AbstractNexusIntegrationTest
+{
+
+    public abstract E getTaskScheduled();
+
+    @Test
+    public void scheduleTasks()
+        throws Exception
+    {
+        Status status = TaskScheduleUtil.create( getTaskScheduled() );
+        Assert.assertTrue( status.isSuccess() );
+
+        assertTasks();
+    }
+
+    @SuppressWarnings( "unchecked" )
+    private void assertTasks()
+        throws IOException
+    {
+        Configuration nexusConfig = NexusConfigUtil.getNexusConfig();
+
+        List<CScheduledTask> tasks = nexusConfig.getTasks();
+        Assert.assertEquals( 1, tasks.size() );
+
+        CScheduledTask task = tasks.get( 0 );
+        E scheduledTask = getTaskScheduled();
+
+        Assert.assertEquals( scheduledTask.getName(), task.getName() );
+        Assert.assertEquals( scheduledTask.getTypeId(), task.getType() );
+    }
+
+    @Test
+    public void updateTasks()
+        throws Exception
+    {
+        E scheduledTask = getTaskScheduled();
+        ScheduledServiceListResource task = TaskScheduleUtil.getTask( scheduledTask.getName() );
+        scheduledTask.setId( task.getId() );
+        updateTask( scheduledTask );
+        Status status = TaskScheduleUtil.update( scheduledTask );
+        Assert.assertTrue( status.isSuccess() );
+
+        assertTasks();
+    }
+
+    public abstract void updateTask( E scheduledTask );
+
+    @Test
+    public void changeScheduling()
+        throws Exception
+    {
+        E scheduledTask = getTaskScheduled();
+        ScheduledServiceListResource task = TaskScheduleUtil.getTask( scheduledTask.getName() );
+
+        ScheduledServiceBaseResource taskManual = new ScheduledServiceBaseResource();
+        taskManual.setId( task.getId() );
+        taskManual.setName( scheduledTask.getName() );
+        taskManual.setEnabled( true );
+        taskManual.setTypeId( scheduledTask.getTypeId() );
+        taskManual.setProperties( scheduledTask.getProperties() );
+
+        Status status = TaskScheduleUtil.update( taskManual );
+        Assert.assertTrue( status.isSuccess() );
+
+        assertTasks();
+    }
+
+    @Test
+    public void deleteTasks()
+        throws Exception
+    {
+        ScheduledServiceListResource task = TaskScheduleUtil.getTask( getTaskScheduled().getName() );
+        Status status = TaskScheduleUtil.deleteTask( task.getId() );
+        Assert.assertTrue( status.isSuccess() );
+
+        // delete is not working, see NEXUS-572
+        // Configuration nexusConfig = NexusConfigUtil.getNexusConfig();
+        // Assert.assertTrue( nexusConfig.getTasks().isEmpty() );
+    }
+
+}
---
+++ nexus/nexus-test-harness/nexus-test-harness-launcher/src/test/java/org/sonatype/nexus/integrationtests/nexus533/Nexus533TaskOnceTest.java
@@ -0,0 +1,47 @@
+package org.sonatype.nexus.integrationtests.nexus533;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import org.apache.commons.lang.time.DateUtils;
+import org.sonatype.nexus.rest.model.ScheduledServiceOnceResource;
+import org.sonatype.nexus.rest.model.ScheduledServicePropertyResource;
+
+public class Nexus533TaskOnceTest
+    extends AbstractNexusTasksIntegrationTest<ScheduledServiceOnceResource>
+{
+
+    private static ScheduledServiceOnceResource scheduledTask;
+
+    @Override
+    public ScheduledServiceOnceResource getTaskScheduled()
+    {
+        if ( scheduledTask == null )
+        {
+            scheduledTask = new ScheduledServiceOnceResource();
+            scheduledTask.setEnabled( true );
+            scheduledTask.setId( null );
+            scheduledTask.setName( "taskOnce" );
+            // A future date
+            Date startDate = DateUtils.addDays( new Date(), 10 );
+            startDate = DateUtils.round( startDate, Calendar.DAY_OF_MONTH );
+            scheduledTask.setStartDate( String.valueOf( startDate.getTime() ) );
+            scheduledTask.setStartTime( "03:30" );
+
+            scheduledTask.setTypeId( "org.sonatype.nexus.tasks.ReindexTask" );
+
+            ScheduledServicePropertyResource prop = new ScheduledServicePropertyResource();
+            prop.setId( "repositoryOrGroupId" );
+            prop.setValue( "all_repo" );
+            scheduledTask.addProperty( prop );
+        }
+        return scheduledTask;
+    }
+
+    @Override
+    public void updateTask( ScheduledServiceOnceResource scheduledTask )
+    {
+        scheduledTask.setStartTime( "00:00" );
+    }
+
+}
