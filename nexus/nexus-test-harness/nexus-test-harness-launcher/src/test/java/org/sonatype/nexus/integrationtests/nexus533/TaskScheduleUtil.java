f122ae957c33f45b10533090e889be2a293e1f7e
Marvin Froeder <velo@sonatype.com>
2008-08-15 01:12:17 +0000

    IN PROGRESS - issue NEXUS-533: Create crud tests for Tasks http://issues.sonatype.org/browse/NEXUS-533
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@1181 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-test-harness/nexus-test-harness-launcher/src/test/java/org/sonatype/nexus/integrationtests/nexus533/TaskScheduleUtil.java
+++ nexus/nexus-test-harness/nexus-test-harness-launcher/src/test/java/org/sonatype/nexus/integrationtests/nexus533/TaskScheduleUtil.java
@@ -1,13 +1,16 @@
 package org.sonatype.nexus.integrationtests.nexus533;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.restlet.data.MediaType;
 import org.restlet.data.Method;
 import org.restlet.data.Response;
 import org.restlet.data.Status;
 import org.sonatype.nexus.integrationtests.RequestFacade;
-import org.sonatype.nexus.rest.model.ScheduledServiceOnceResource;
+import org.sonatype.nexus.rest.model.ScheduledServiceBaseResource;
+import org.sonatype.nexus.rest.model.ScheduledServiceListResource;
+import org.sonatype.nexus.rest.model.ScheduledServiceListResourceResponse;
 import org.sonatype.nexus.rest.model.ScheduledServiceResourceResponse;
 import org.sonatype.nexus.rest.xstream.XStreamInitializer;
 import org.sonatype.plexus.rest.representation.XStreamRepresentation;
@@ -24,7 +27,7 @@ public class TaskScheduleUtil
         xstream = XStreamInitializer.initialize( new XStream() );
     }
 
-    public static Status create( ScheduledServiceOnceResource task )
+    public static Status create( ScheduledServiceBaseResource task )
         throws IOException
     {
         ScheduledServiceResourceResponse request = new ScheduledServiceResourceResponse();
@@ -35,6 +38,60 @@ public class TaskScheduleUtil
 
         String serviceURI = "service/local/schedules";
         Response response = RequestFacade.sendMessage( serviceURI, Method.POST, representation );
+
+        return response.getStatus();
+    }
+
+    public static ScheduledServiceListResource getTask( String name )
+        throws Exception
+    {
+        List<ScheduledServiceListResource> list = getTasks();
+        for ( ScheduledServiceListResource task : list )
+        {
+            if ( name.equals( task.getName() ) )
+            {
+                return task;
+            }
+        }
+
+        return null;
+    }
+
+    @SuppressWarnings( "unchecked" )
+    public static List<ScheduledServiceListResource> getTasks()
+        throws IOException
+    {
+        String serviceURI = "service/local/schedules";
+        Response response = RequestFacade.doGetRequest( serviceURI );
+        XStreamRepresentation representation =
+            new XStreamRepresentation( xstream, response.getEntity().getText(), MediaType.APPLICATION_XML );
+
+        ScheduledServiceListResourceResponse scheduleResponse =
+            (ScheduledServiceListResourceResponse) representation.getPayload( new ScheduledServiceListResourceResponse() );
+
+        return scheduleResponse.getData();
+    }
+
+    public static Status update( ScheduledServiceBaseResource task )
+        throws IOException
+    {
+        ScheduledServiceResourceResponse request = new ScheduledServiceResourceResponse();
+        request.setData( task );
+
+        XStreamRepresentation representation = new XStreamRepresentation( xstream, "", MediaType.APPLICATION_XML );
+        representation.setPayload( request );
+
+        String serviceURI = "service/local/schedules/" + task.getId();
+        Response response = RequestFacade.sendMessage( serviceURI, Method.PUT, representation );
+
+        return response.getStatus();
+    }
+
+    public static Status deleteTask( String id )
+        throws IOException
+    {
+        String serviceURI = "service/local/schedules/" + id;
+        Response response = RequestFacade.sendMessage( serviceURI, Method.DELETE );
 
         return response.getStatus();
     }
