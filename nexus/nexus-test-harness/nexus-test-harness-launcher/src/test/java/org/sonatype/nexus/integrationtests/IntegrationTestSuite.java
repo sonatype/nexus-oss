d7af761fee892143e53f03779f1bbc40272b23b6
Brian Demers <bdemers@sonatype.com>
2008-09-17 17:20:11 +0000

    Added a call to NexusClient.isNexusStarted(true) after nexus is started. This will increase the total wait to a max of 16 seconds, but still keeps the min the same at 6 seconds.  The client polls the server every half second.
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@1559 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-test-harness/nexus-test-harness-launcher/src/test/java/org/sonatype/nexus/integrationtests/IntegrationTestSuite.java
+++ nexus/nexus-test-harness/nexus-test-harness-launcher/src/test/java/org/sonatype/nexus/integrationtests/IntegrationTestSuite.java
@@ -6,7 +6,10 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 import org.sonatype.appbooter.ForkedAppBooter;
+import org.sonatype.nexus.client.NexusClient;
+import org.sonatype.nexus.client.rest.NexusRestClient;
 import org.sonatype.nexus.test.utils.NexusStateUtil;
+import org.sonatype.nexus.test.utils.TestProperties;
 
 
 @RunWith( Suite.class )
@@ -19,10 +22,21 @@ public class IntegrationTestSuite
     public static void beforeSuite()
         throws Exception
     {
+        TestContainer testContainer = TestContainer.getInstance();
+        
         ForkedAppBooter appBooter =
-            (ForkedAppBooter) TestContainer.getInstance().lookup( ForkedAppBooter.ROLE, "TestForkedAppBooter" );
+            (ForkedAppBooter) testContainer.lookup( ForkedAppBooter.ROLE, "TestForkedAppBooter" );
         appBooter.start();
-
+        
+        // use the java client to wait for nexus to start 
+        // the ForkedAppBooter waits 6 seconds, and that is configurable
+        // The client will wait 10 seconds by default, but will poll every half second.
+        NexusClient client = new NexusRestClient();
+        // at this point security should not be turned on, but you never know...
+        client.connect( TestProperties.getString( "nexus.base.url" ), testContainer.getTestContext().getAdminUsername(), testContainer.getTestContext().getAdminPassword() );
+        client.isNexusStarted( true );
+        client.disconnect();
+        
         // now to make everything work correctly we actually have to "soft-stop" nexus
         NexusStateUtil.doSoftStop();
 
