2d6c6531bb887d4d03284012033733c2a35fd70d
Jason van Zyl <jvanzyl@sonatype.com>
2008-09-25 19:37:41 +0000

    o changing resources to assume the base URI has been applied by the application router
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@1647 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-rest-api/src/main/java/org/sonatype/nexus/rest/configurations/ConfigurationPlexusResource.java
+++ nexus/nexus-rest-api/src/main/java/org/sonatype/nexus/rest/configurations/ConfigurationPlexusResource.java
@@ -43,8 +43,8 @@ public class ConfigurationPlexusResource
     @Override
     public String getResourceUri()
     {
-        return "/{" + NEXUS_INSTANCE_KEY + "}/configs/{" + CONFIG_NAME_KEY + "}";
-        //return "/configs/{" + CONFIG_NAME_KEY + "}";
+        //return "/{" + NEXUS_INSTANCE_KEY + "}/configs/{" + CONFIG_NAME_KEY + "}";
+        return "/configs/{" + CONFIG_NAME_KEY + "}";
     }
 
     @Override
