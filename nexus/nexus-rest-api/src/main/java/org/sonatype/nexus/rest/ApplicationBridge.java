e170196ce0cfbc062b46d0f16e1d00751a7ddc6f
Jason van Zyl <jvanzyl@sonatype.com>
2008-09-25 09:06:47 +0000

    o making changes for access to the layered routers. i have to test by checking in because all the ITs fail on the mac while they work in hudson.
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@1632 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-rest-api/src/main/java/org/sonatype/nexus/rest/ApplicationBridge.java
+++ nexus/nexus-rest-api/src/main/java/org/sonatype/nexus/rest/ApplicationBridge.java
@@ -161,12 +161,7 @@ public class ApplicationBridge
         return XStreamInitializer.initialize( xstream );
     }
 
-    /**
-     * "Decorating" the root with our resources.
-     * 
-     * @TODO Move this to PlexusResources, except Status (see isStarted usage below!)
-     */
-    protected void doCreateRoot( Router root, boolean isStarted )
+    protected Router initializeRouter( Router root )
     {
         // instance filter, that injects proper Nexus instance into request attributes
         NexusInstanceFilter nif = new NexusInstanceFilter( getContext() );
@@ -175,212 +170,202 @@ public class ApplicationBridge
         attach( root, false, "/{" + NexusInstanceFilter.NEXUS_INSTANCE_KEY + "}", nif );
 
         // creating _another_ router, that will be next isntance called after filtering
-        Router router = new Router( getContext() );
-
+        Router applicationRouter = new Router( getContext() );
+               
         // attaching it after nif
-        nif.setNext( router );
-
-        // -----
+        nif.setNext( applicationRouter );
+        
+        return applicationRouter;
+    }
+    
+    /**
+     * "Decorating" the root with our resources.
+     * 
+     * @TODO Move this to PlexusResources, except Status (see isStarted usage below!)
+     */
+    @SuppressWarnings("deprecation")
+    protected void doCreateRoot( Router applicationRouter, boolean isStarted )
+    {
         // a little digression here. if !isStarted, we are shutting down everything except /status and /status/command
 
-        attach( router, false, "/status", StatusResourceHandler.class );
+        attach( applicationRouter, false, "/status", StatusResourceHandler.class );
 
-        attach( router, false, "/status/command", CommandResourceHandler.class );
+        attach( applicationRouter, false, "/status/command", CommandResourceHandler.class );
 
+        System.out.println( "isStart: " + isStarted );
+        
         if ( !isStarted )
         {
             return;
         }
-
+        
         // ==========================================================
         // now we are playing with the two router: unprotectedResources for not protected
         // and protectedResources for protected ones
 
         // attaching the restlets to scond router
-        attach( router, false, "/feeds", FeedsListResourceHandler.class );
+        attach( applicationRouter, false, "/feeds", FeedsListResourceHandler.class );
 
-        attach( router, false, "/feeds/{" + FeedResourceHandler.FEED_KEY + "}", FeedResourceHandler.class );
+        attach( applicationRouter, false, "/feeds/{" + FeedResourceHandler.FEED_KEY + "}", FeedResourceHandler.class );
 
-        attach( router, false, "/authentication/login", LoginResourceHandler.class );
+        attach( applicationRouter, false, "/authentication/login", LoginResourceHandler.class );
 
-        attach( router, false, "/authentication/logout", LogoutResourceHandler.class );
+        attach( applicationRouter, false, "/authentication/logout", LogoutResourceHandler.class );
 
-        attach( router, false, "/identify/{" + IdentifyHashResourceHandler.ALGORITHM_KEY + "}/{"
+        attach( applicationRouter, false, "/identify/{" + IdentifyHashResourceHandler.ALGORITHM_KEY + "}/{"
             + IdentifyHashResourceHandler.HASH_KEY + "}", IdentifyHashResourceHandler.class );
 
-        attach( router, false, "/artifact/maven", ArtifactResourceHandler.class );
+        attach( applicationRouter, false, "/artifact/maven", ArtifactResourceHandler.class );
 
-        attach( router, false, "/artifact/maven/redirect", ArtifactResourceRedirectHandler.class );
+        attach( applicationRouter, false, "/artifact/maven/redirect", ArtifactResourceRedirectHandler.class );
 
-        attach( router, false, "/artifact/maven/content", ArtifactResourceContentHandler.class );
+        attach( applicationRouter, false, "/artifact/maven/content", ArtifactResourceContentHandler.class );
 
         // protected resources
 
-        attach( router, false, "/data_index", IndexResourceHandler.class );
+        attach( applicationRouter, false, "/data_index", IndexResourceHandler.class );
 
-        attach( router, false, "/data_index/{" + IndexResourceHandler.DOMAIN + "}/{" + IndexResourceHandler.TARGET_ID
+        attach( applicationRouter, false, "/data_index/{" + IndexResourceHandler.DOMAIN + "}/{" + IndexResourceHandler.TARGET_ID
             + "}", IndexResourceHandler.class );
 
-        attach( router, false, "/data_index/{" + IndexResourceHandler.DOMAIN + "}/{" + IndexResourceHandler.TARGET_ID
+        attach( applicationRouter, false, "/data_index/{" + IndexResourceHandler.DOMAIN + "}/{" + IndexResourceHandler.TARGET_ID
             + "}/content", IndexResourceHandler.class );
 
-        attach( router, false, "/data_cache/{" + CacheResourceHandler.DOMAIN + "}/{" + CacheResourceHandler.TARGET_ID
+        attach( applicationRouter, false, "/data_cache/{" + CacheResourceHandler.DOMAIN + "}/{" + CacheResourceHandler.TARGET_ID
             + "}/content", CacheResourceHandler.class );
 
-        attach( router, false, "/wastebasket", WastebasketResourceHandler.class );
+        attach( applicationRouter, false, "/wastebasket", WastebasketResourceHandler.class );
 
-        attach( router, false, "/attributes", AttributesResourceHandler.class );
+        attach( applicationRouter, false, "/attributes", AttributesResourceHandler.class );
 
-        attach( router, false, "/attributes/{" + AttributesResourceHandler.DOMAIN + "}/{"
+        attach( applicationRouter, false, "/attributes/{" + AttributesResourceHandler.DOMAIN + "}/{"
             + AttributesResourceHandler.TARGET_ID + "}", AttributesResourceHandler.class );
 
-        attach( router, false, "/attributes/{" + AttributesResourceHandler.DOMAIN + "}/{"
+        attach( applicationRouter, false, "/attributes/{" + AttributesResourceHandler.DOMAIN + "}/{"
             + AttributesResourceHandler.TARGET_ID + "}/content", AttributesResourceHandler.class );
 
-        attach( router, false, "/repository_statuses", RepositoryStatusesListResourceHandler.class );
+        attach( applicationRouter, false, "/repository_statuses", RepositoryStatusesListResourceHandler.class );
 
-        attach( router, false, "/repositories", RepositoryListResourceHandler.class );
+        attach( applicationRouter, false, "/repositories", RepositoryListResourceHandler.class );
 
         attach(
-            router,
+            applicationRouter,
             false,
             "/repositories/{" + RepositoryResourceHandler.REPOSITORY_ID_KEY + "}",
             RepositoryResourceHandler.class );
 
         attach(
-            router,
+            applicationRouter,
             false,
             "/repositories/{" + RepositoryResourceHandler.REPOSITORY_ID_KEY + "}/status",
             RepositoryStatusResourceHandler.class );
 
         attach(
-            router,
+            applicationRouter,
             false,
             "/repositories/{" + RepositoryResourceHandler.REPOSITORY_ID_KEY + "}/meta",
             RepositoryMetaResourceHandler.class );
 
         attach(
-            router,
+            applicationRouter,
             false,
             "/repositories/{" + RepositoryResourceHandler.REPOSITORY_ID_KEY + "}/content",
             RepositoryContentResourceHandler.class );
 
-        attach( router, false, "/repo_groups", RepositoryGroupListResourceHandler.class );
+        attach( applicationRouter, false, "/repo_groups", RepositoryGroupListResourceHandler.class );
 
         attach(
-            router,
+            applicationRouter,
             false,
             "/repo_groups/{" + RepositoryGroupResourceHandler.GROUP_ID_KEY + "}",
             RepositoryGroupResourceHandler.class );
 
         attach(
-            router,
+            applicationRouter,
             false,
             "/repo_groups/{" + RepositoryGroupResourceHandler.GROUP_ID_KEY + "}/content",
             RepositoryGroupContentResourceHandler.class );
 
-        attach( router, false, "/logs", LogsListResourceHandler.class );
+        attach( applicationRouter, false, "/logs", LogsListResourceHandler.class );
 
-        attach( router, false, "/logs/{" + LogsResourceHandler.FILE_NAME_KEY + "}", LogsResourceHandler.class );
+        attach( applicationRouter, false, "/logs/{" + LogsResourceHandler.FILE_NAME_KEY + "}", LogsResourceHandler.class );
 
-        attach( router, false, "/global_settings", GlobalConfigurationListResourceHandler.class );
+        attach( applicationRouter, false, "/global_settings", GlobalConfigurationListResourceHandler.class );
 
         attach(
-            router,
+            applicationRouter,
             false,
             "/global_settings/{" + GlobalConfigurationResourceHandler.CONFIG_NAME_KEY + "}",
             GlobalConfigurationResourceHandler.class );
 
-        attach( router, false, "/repo_routes", RepositoryRouteListResourceHandler.class );
+        attach( applicationRouter, false, "/repo_routes", RepositoryRouteListResourceHandler.class );
 
         attach(
-            router,
+            applicationRouter,
             false,
             "/repo_routes/{" + RepositoryRouteResourceHandler.ROUTE_ID_KEY + "}",
             RepositoryRouteResourceHandler.class );
 
-        attach( router, false, "/templates/repositories", RepositoryTemplateListResourceHandler.class );
+        attach( applicationRouter, false, "/templates/repositories", RepositoryTemplateListResourceHandler.class );
 
         attach(
-            router,
+            applicationRouter,
             false,
             "/templates/repositories/{" + RepositoryTemplateResourceHandler.REPOSITORY_ID_KEY + "}",
             RepositoryTemplateResourceHandler.class );
 
-        attach( router, false, "/schedules", ScheduledServiceListResourceHandler.class );
+        attach( applicationRouter, false, "/schedules", ScheduledServiceListResourceHandler.class );
 
-        attach( router, false, "/schedule_types", ScheduledServiceTypeResourceHandler.class );
+        attach( applicationRouter, false, "/schedule_types", ScheduledServiceTypeResourceHandler.class );
 
         attach(
-            router,
+            applicationRouter,
             false,
             "/schedule_run/{" + ScheduledServiceRunResourceHandler.SCHEDULED_SERVICE_ID_KEY + "}",
             ScheduledServiceRunResourceHandler.class );
 
         attach(
-            router,
+            applicationRouter,
             false,
             "/schedules/{" + ScheduledServiceResourceHandler.SCHEDULED_SERVICE_ID_KEY + "}",
             ScheduledServiceResourceHandler.class );
 
-        attach( router, false, "/users", UserListResourceHandler.class );
+        attach( applicationRouter, false, "/users", UserListResourceHandler.class );
 
-        attach( router, false, "/users/{" + UserResourceHandler.USER_ID_KEY + "}", UserResourceHandler.class );
+        attach( applicationRouter, false, "/users/{" + UserResourceHandler.USER_ID_KEY + "}", UserResourceHandler.class );
 
-        attach( router, false, "/users_reset/{" + UserResourceHandler.USER_ID_KEY + "}", UserResetResourceHandler.class );
+        attach( applicationRouter, false, "/users_reset/{" + UserResourceHandler.USER_ID_KEY + "}", UserResetResourceHandler.class );
 
         attach(
-            router,
+            applicationRouter,
             false,
             "/users_forgotid/{" + UserResourceHandler.USER_EMAIL_KEY + "}",
             UserForgotIdResourceHandler.class );
 
-        attach( router, false, "/users_forgotpw", UserForgotPasswordResourceHandler.class );
+        attach( applicationRouter, false, "/users_forgotpw", UserForgotPasswordResourceHandler.class );
 
-        attach( router, false, "/users_changepw", UserChangePasswordResourceHandler.class );
+        attach( applicationRouter, false, "/users_changepw", UserChangePasswordResourceHandler.class );
 
-        attach( router, false, "/roles", RoleListResourceHandler.class );
+        attach( applicationRouter, false, "/roles", RoleListResourceHandler.class );
 
-        attach( router, false, "/roles/{" + RoleResourceHandler.ROLE_ID_KEY + "}", RoleResourceHandler.class );
+        attach( applicationRouter, false, "/roles/{" + RoleResourceHandler.ROLE_ID_KEY + "}", RoleResourceHandler.class );
 
-        attach( router, false, "/privileges", PrivilegeListResourceHandler.class );
+        attach( applicationRouter, false, "/privileges", PrivilegeListResourceHandler.class );
 
         attach(
-            router,
+            applicationRouter,
             false,
             "/privileges/{" + PrivilegeResourceHandler.PRIVILEGE_ID_KEY + "}",
             PrivilegeResourceHandler.class );
 
-        attach( router, false, "/repo_targets", RepositoryTargetListResourceHandler.class );
+        attach( applicationRouter, false, "/repo_targets", RepositoryTargetListResourceHandler.class );
 
         attach(
-            router,
+            applicationRouter,
             false,
             "/repo_targets/{" + RepositoryTargetResourceHandler.REPO_TARGET_ID_KEY + "}",
             RepositoryTargetResourceHandler.class );
 
-        attach( router, false, "/repo_content_classes", ContentClassesListResourceHandler.class );
-    }
-
-    @Deprecated
-    protected void attach( Router router, boolean strict, String uriPattern, Class<? extends Resource> targetClass )
-    {
-        Route route = router.attach( uriPattern, targetClass );
-
-        if ( strict )
-        {
-            route.getTemplate().setMatchingMode( Template.MODE_EQUALS );
-        }
-    }
-
-    @Deprecated
-    protected void attach( Router router, boolean strict, String uriPattern, Restlet target )
-    {
-        Route route = router.attach( uriPattern, target );
-
-        if ( strict )
-        {
-            route.getTemplate().setMatchingMode( Template.MODE_EQUALS );
-        }
+        attach( applicationRouter, false, "/repo_content_classes", ContentClassesListResourceHandler.class );
     }
 }
