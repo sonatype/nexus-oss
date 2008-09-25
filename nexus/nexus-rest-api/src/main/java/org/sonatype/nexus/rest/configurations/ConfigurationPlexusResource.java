aa7b8c1b42755beeb51548af40c00c39f1c1a57c
Jason van Zyl <jvanzyl@sonatype.com>
2008-09-25 09:35:20 +0000

    o the URIs can now be mounted relative to the application route
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@1634 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-rest-api/src/main/java/org/sonatype/nexus/rest/configurations/ConfigurationPlexusResource.java
+++ nexus/nexus-rest-api/src/main/java/org/sonatype/nexus/rest/configurations/ConfigurationPlexusResource.java
@@ -43,7 +43,7 @@ public class ConfigurationPlexusResource
     @Override
     public String getResourceUri()
     {
-        return "/{" + NEXUS_INSTANCE_KEY + "}/configs/{" + CONFIG_NAME_KEY + "}";
+        return "/configs/{" + CONFIG_NAME_KEY + "}";
     }
 
     @Override
