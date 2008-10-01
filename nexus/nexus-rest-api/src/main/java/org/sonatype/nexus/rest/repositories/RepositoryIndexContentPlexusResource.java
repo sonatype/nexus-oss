cbb51e63d1941f9c419d856d541b31166afffc4a
dip <dip@2aa8b3fc-8ebb-4439-a84f-95066eaea8ab>
2008-10-01 03:34:50 +0000

    NEXUS-689: group (folder) browsing implemented
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@1694 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-rest-api/src/main/java/org/sonatype/nexus/rest/repositories/RepositoryIndexContentPlexusResource.java
+++ nexus/nexus-rest-api/src/main/java/org/sonatype/nexus/rest/repositories/RepositoryIndexContentPlexusResource.java
@@ -20,24 +20,41 @@
  */
 package org.sonatype.nexus.rest.repositories;
 
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.codehaus.plexus.util.StringUtils;
+import org.restlet.Context;
 import org.restlet.data.Request;
+import org.restlet.data.Response;
+import org.restlet.data.Status;
 import org.restlet.resource.ResourceException;
+import org.restlet.resource.Variant;
+import org.sonatype.nexus.index.IndexerManager;
+import org.sonatype.nexus.index.NexusIndexer;
+import org.sonatype.nexus.index.context.IndexingContext;
 import org.sonatype.nexus.proxy.NoSuchRepositoryException;
-import org.sonatype.nexus.proxy.ResourceStore;
-import org.sonatype.nexus.rest.AbstractResourceStoreContentPlexusResource;
-import org.sonatype.nexus.rest.ApplicationBridge;
+import org.sonatype.nexus.rest.AbstractNexusPlexusResource;
+import org.sonatype.nexus.rest.model.ContentListResource;
+import org.sonatype.nexus.rest.model.ContentListResourceResponse;
 
 /**
- * A simple testing resource. Will "publish" itself on passed in token URI
- * (/token) and will emit "token" for GETs and respond with HTTP 200 to all
- * other HTTP methods (PUT, POST) only if the token equals to entity passed in.
+ * Index content resource.
  * 
  * @author dip
- * @plexus.component role-hint="repoIndexBrowser"
+ * @plexus.component role-hint="repoIndexResource"
  */
-public class RepositoryIndexContentPlexusResource extends AbstractResourceStoreContentPlexusResource {
+public class RepositoryIndexContentPlexusResource extends AbstractNexusPlexusResource {
 
     public static final String REPOSITORY_ID_KEY = "repositoryId";
+
+    /**
+     * @plexus.requirement
+     */
+    private IndexerManager indexerManager;
     
     @Override
     public Object getPayloadInstance() {
@@ -49,16 +66,101 @@ public class RepositoryIndexContentPlexusResource extends AbstractResourceStoreC
         return "/repositories/{" + REPOSITORY_ID_KEY + "}/index_content";
     }
     
-    /**
-     * The RepositoryContentResourceHandler simply returns the repository by ID that is in request attributes. It is
-     * actually coming from router path mapping.
-     * 
-     * @see ApplicationBridge
-     */
-    protected ResourceStore getResourceStore( Request request )
-        throws ResourceException, NoSuchRepositoryException
-    {
-        return getNexusInstance(request).getRepository(
-            request.getAttributes().get( REPOSITORY_ID_KEY ).toString() );
+    @Override
+    public Object get(Context context, Request request, Response response, Variant variant)
+            throws ResourceException {
+        String path = parsePathFromUri( request.getResourceRef().toString() );
+        if ( ! path.endsWith( "/" ) ) {
+            response.redirectPermanent( path + "/" );
+            return null;
+        }
+
+        String repositoryId = String.valueOf( request.getAttributes().get( REPOSITORY_ID_KEY ) );
+        try {
+            IndexingContext indexingContext =
+                indexerManager.getRepositoryRemoteIndexContext( repositoryId );
+            
+            return createResponse( request, indexingContext );
+        }
+        catch ( NoSuchRepositoryException e ) {
+            throw new ResourceException( Status.CLIENT_ERROR_NOT_FOUND, e );
+        }
+    }
+    
+    protected Object createResponse( Request request, IndexingContext indexingContext )
+            throws ResourceException {
+        String path = parsePathFromUri( request.getResourceRef().getRemainingPart() );
+        Collection<String> groups = getGroups( path, indexingContext ); 
+        
+        ContentListResourceResponse response = new ContentListResourceResponse();
+
+        for ( String group : groups )
+        {
+            ContentListResource resource = new ContentListResource();
+
+            resource.setText( group );
+            resource.setLeaf( false );
+            resource.setResourceURI( createChildReference( request, group ).toString() + "/" );
+            resource.setRelativePath( path + group + "/" );
+            resource.setLastModified( new Date() );
+            resource.setSizeOnDisk( -1 );
+
+            response.addData( resource );
+        }
+        
+        return response;
+    }
+    
+    protected Collection<String> getGroups( String path, IndexingContext indexingContext )
+            throws ResourceException {
+        NexusIndexer indexer = indexerManager.getNexusIndexer();
+        path = path.substring( 1 ); // strip the leading slash
+
+        try {
+            if ( path.length() == 0 ) {
+              return indexer.getRootGroups( indexingContext );
+            }
+            else {
+                path = path.replace( '/', '.' );
+                int n = path.length();
+                Set<String> result = new HashSet<String>();
+                Set<String> groups = indexer.getAllGroups( indexingContext );
+                for ( String group : groups ) {
+                    if ( group.startsWith( path ) ) {
+                        group = group.substring( n );
+                        int nextDot = group.indexOf( '.' );
+                        if ( nextDot > -1 ) {
+                            group = group.substring( 0, nextDot );
+                        }
+                        if ( ! result.contains( group ) ) {
+                            result.add( group );
+                        }
+                    }
+                }
+                return result;
+            }
+        }
+        catch ( IOException e ) {
+            throw new ResourceException( Status.SERVER_ERROR_INTERNAL, e );
+        }
+    }
+    
+    protected String parsePathFromUri(String parsedPath) {
+
+        // get rid of query part
+        if (parsedPath.contains("?")) {
+            parsedPath = parsedPath.substring(0, parsedPath.indexOf('?'));
+        }
+
+        // get rid of reference part
+        if (parsedPath.contains("#")) {
+            parsedPath = parsedPath.substring(0, parsedPath.indexOf('#'));
+        }
+
+        if (StringUtils.isEmpty(parsedPath)) {
+            parsedPath = "/";
+        }
+
+        return parsedPath;
     }
 }
