096153be8e6814624f2da2c760bdb4562126fb69
Damian Bradicich <dbradicich@sonatype.com>
2008-10-08 15:38:16 +0000

    NEXUS-874: Authorization now tested as well
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@1772 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-test-harness/nexus-test-harness-launcher/src/test/java/org/sonatype/nexus/integrationtests/nexus874/Nexus874SecurityRealmReplacementTest.java
+++ nexus/nexus-test-harness/nexus-test-harness-launcher/src/test/java/org/sonatype/nexus/integrationtests/nexus874/Nexus874SecurityRealmReplacementTest.java
@@ -5,9 +5,16 @@ import java.net.ConnectException;
 import junit.framework.Assert;
 
 import org.junit.Test;
+import org.restlet.data.MediaType;
 import org.sonatype.nexus.integrationtests.AbstractNexusIntegrationTest;
 import org.sonatype.nexus.integrationtests.TestContainer;
+import org.sonatype.nexus.test.utils.GroupMessageUtil;
 import org.sonatype.nexus.test.utils.NexusStateUtil;
+import org.sonatype.nexus.test.utils.RepositoryMessageUtil;
+import org.sonatype.nexus.test.utils.RoleMessageUtil;
+import org.sonatype.nexus.test.utils.TargetMessageUtil;
+import org.sonatype.nexus.test.utils.TaskScheduleUtil;
+import org.sonatype.nexus.test.utils.UserMessageUtil;
 
 /**
  * Validate the MemoryRealm that replaces default nexus security
@@ -15,9 +22,20 @@ import org.sonatype.nexus.test.utils.NexusStateUtil;
 public class Nexus874SecurityRealmReplacementTest
     extends AbstractNexusIntegrationTest
 {    
+    private GroupMessageUtil groupUtil;
+    private RepositoryMessageUtil repoUtil;
+    private TargetMessageUtil targetUtil;
+    private RoleMessageUtil roleUtil;
+    private UserMessageUtil userUtil;
+    
     public Nexus874SecurityRealmReplacementTest()
     {
         TestContainer.getInstance().getTestContext().setSecureTest( true );
+        groupUtil = new GroupMessageUtil( this.getJsonXStream(), MediaType.APPLICATION_JSON );
+        repoUtil = new RepositoryMessageUtil( this.getJsonXStream(), MediaType.APPLICATION_JSON );
+        targetUtil = new TargetMessageUtil( this.getJsonXStream(), MediaType.APPLICATION_JSON );
+        roleUtil = new RoleMessageUtil( this.getJsonXStream(), MediaType.APPLICATION_JSON );
+        userUtil = new UserMessageUtil( this.getJsonXStream(), MediaType.APPLICATION_JSON );
     }
         
     @Test
@@ -38,7 +56,12 @@ public class Nexus874SecurityRealmReplacementTest
         TestContainer.getInstance().getTestContext().setPassword( "anonymous" );
         
         NexusStateUtil.getNexusStatus();
-        
+    }
+    
+    @Test
+    public void negativeAuthentication()
+        throws Exception
+    {           
         TestContainer.getInstance().getTestContext().setUsername( "admin" );
         TestContainer.getInstance().getTestContext().setPassword( "badpassword" );
         
@@ -79,12 +102,105 @@ public class Nexus874SecurityRealmReplacementTest
         }
     }
     
+    @Test
     public void authorization()
         throws Exception
+    {        
+        TestContainer.getInstance().getTestContext().setUsername( "admin" );
+        TestContainer.getInstance().getTestContext().setPassword( "admin123" );
+        
+        NexusStateUtil.getNexusStatus();
+        groupUtil.getList();
+        repoUtil.getList();
+        targetUtil.getList();
+        TaskScheduleUtil.getTasks();
+        
+        TestContainer.getInstance().getTestContext().setUsername( "deployment" );
+        TestContainer.getInstance().getTestContext().setPassword( "deployment123" );
+        
+        NexusStateUtil.getNexusStatus();
+        groupUtil.getList();
+        repoUtil.getList();
+        
+        TestContainer.getInstance().getTestContext().setUsername( "anonymous" );
+        TestContainer.getInstance().getTestContext().setPassword( "anonymous" );
+        
+        NexusStateUtil.getNexusStatus();
+        groupUtil.getList();
+        repoUtil.getList();
+    }
+    
+    @Test
+    public void negativeAuthorization()
+        throws Exception
     {
-        if( printKnownErrorButDoNotFail( Nexus874SecurityRealmReplacementTest.class, "authorization" ))
+        TestContainer.getInstance().getTestContext().setUsername( "deployment" );
+        TestContainer.getInstance().getTestContext().setPassword( "deployment123" );
+        
+        try
         {
-            return;
+            targetUtil.getList();
+            Assert.fail();
+        }
+        catch ( Exception e )
+        {
+            // OK
+        }
+        
+        try
+        {
+            TaskScheduleUtil.getTasks();
+            Assert.fail();
+        }
+        catch ( Exception e )
+        {
+            // OK
+        }
+        
+        TestContainer.getInstance().getTestContext().setUsername( "anonymous" );
+        TestContainer.getInstance().getTestContext().setPassword( "anonymous" );
+        
+        try
+        {
+            targetUtil.getList();
+            Assert.fail();
+        }
+        catch ( Exception e )
+        {
+            // OK
+        }
+        
+        try
+        {
+            TaskScheduleUtil.getTasks();
+            Assert.fail();
+        }
+        catch ( Exception e )
+        {
+            // OK
+        }
+        
+        TestContainer.getInstance().getTestContext().setUsername( "admin" );
+        TestContainer.getInstance().getTestContext().setPassword( "admin123" );
+        
+        try
+        {
+            userUtil.getList();
+            Assert.fail();
+        }
+        catch ( Exception e )
+        {
+            // OK
+        }
+        
+        try
+        {
+            roleUtil.getList();
+            Assert.fail();
+        }
+        catch ( Exception e )
+        {
+            // OK
         }
     }
 }
