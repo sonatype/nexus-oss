8834b839f99f5fbcbbad696d9a97e7df21230838
Damian Bradicich <dbradicich@sonatype.com>
2008-10-08 13:59:57 +0000

    NEXUS-874: Authentication tested, now need to complete authorization test
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@1770 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-test-harness/nexus-test-harness-launcher/src/test/java/org/sonatype/nexus/integrationtests/nexus874/Nexus874SecurityRealmReplacementTest.java
+++ nexus/nexus-test-harness/nexus-test-harness-launcher/src/test/java/org/sonatype/nexus/integrationtests/nexus874/Nexus874SecurityRealmReplacementTest.java
@@ -1,56 +1,29 @@
 package org.sonatype.nexus.integrationtests.nexus874;
 
-import java.io.File;
 import java.net.ConnectException;
 
 import junit.framework.Assert;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.sonatype.nexus.integrationtests.AbstractNexusIntegrationTest;
-import org.sonatype.nexus.integrationtests.AbstractPrivilegeTest;
 import org.sonatype.nexus.integrationtests.TestContainer;
-import org.sonatype.nexus.test.utils.FileTestingUtils;
 import org.sonatype.nexus.test.utils.NexusStateUtil;
 
 /**
  * Validate the MemoryRealm that replaces default nexus security
  */
 public class Nexus874SecurityRealmReplacementTest
-    extends AbstractPrivilegeTest
+    extends AbstractNexusIntegrationTest
 {    
     public Nexus874SecurityRealmReplacementTest()
     {
+        TestContainer.getInstance().getTestContext().setSecureTest( true );
     }
-    
-    @Before
-    public void copyFile()
-        throws Exception
-    {
-        File memoryRealmJar = new File( "target/copied-dependencies/nexus-simple-memory-realm.jar" );
         
-        File destinationJar = new File( AbstractNexusIntegrationTest.nexusBaseDir + "/runtime/apps/nexus/lib/nexus-simple-memory-realm.jar" );
-        
-        FileTestingUtils.fileCopy( memoryRealmJar, destinationJar );
-    }
-
-    @After
-    public void deleteFile()
-        throws Exception
-    {
-        new File( AbstractNexusIntegrationTest.nexusBaseDir + "/runtime/apps/nexus/lib/nexus-simple-memory-realm.jar" ).delete();
-    }
-    
     @Test
     public void authentication()
         throws Exception
-    {   
-        if( printKnownErrorButDoNotFail( Nexus874SecurityRealmReplacementTest.class, "authorization" ))
-        {
-            return;
-        }
-        
+    {           
         TestContainer.getInstance().getTestContext().setUsername( "admin" );
         TestContainer.getInstance().getTestContext().setPassword( "admin123" );
         
