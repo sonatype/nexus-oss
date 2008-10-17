9e88bd714e133797a1544151cb64bfc4dcd14187
Tamas Cservenak <cstamas@sonatype.com>
2008-10-17 18:08:10 +0000

    Fixed comments to avoid misunderstanding.
    git-svn-id: file:///opt/svn/repositories/sonatype.org/spice/trunk/plexus-task-scheduler@378 5751e0cb-dcb7-432f-92e2-260806df54be

--- src/main/java/org/sonatype/scheduling/TaskState.java
+++ src/main/java/org/sonatype/scheduling/TaskState.java
@@ -21,7 +21,7 @@
 package org.sonatype.scheduling;
 
 /**
- * Enum for describing task state. It is a state machine: starting state is SCHEDULED, finishing states are FINISHED,
+ * Enum for describing task state. It is a state machine: starting state is SUBMITTED, finishing states are FINISHED,
  * BROKEN and CANCELLED. Scheduled tasks are jumping between RUNNING and WAITING until finished, cancelled or error
  * (broken).
  * 
