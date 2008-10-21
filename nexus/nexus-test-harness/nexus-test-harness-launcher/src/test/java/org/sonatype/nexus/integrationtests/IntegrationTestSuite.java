ef5657e4d0d925118e563a397ae8a114ed401232
Marvin Froeder <velo@sonatype.com>
2008-10-21 12:45:24 +0000

    IN PROGRESS - issue NEXUS-979: IT's fail on CI http://issues.sonatype.org/browse/NEXUS-979
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@1911 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-test-harness/nexus-test-harness-launcher/src/test/java/org/sonatype/nexus/integrationtests/IntegrationTestSuite.java
+++ nexus/nexus-test-harness/nexus-test-harness-launcher/src/test/java/org/sonatype/nexus/integrationtests/IntegrationTestSuite.java
@@ -1,16 +1,18 @@
 package org.sonatype.nexus.integrationtests;
 
+import junit.framework.Assert;
+
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
+import org.restlet.data.Status;
 import org.sonatype.appbooter.ForkedAppBooter;
 import org.sonatype.nexus.client.NexusClient;
 import org.sonatype.nexus.client.rest.NexusRestClient;
 import org.sonatype.nexus.test.utils.NexusStateUtil;
 import org.sonatype.nexus.test.utils.TestProperties;
-
 
 @RunWith( Suite.class )
 @SuiteClasses( { IntegrationTestSuiteClasses.class, IntegrationTestSuiteClassesSecurity.class } )
@@ -22,24 +24,45 @@ public class IntegrationTestSuite
     public static void beforeSuite()
         throws Exception
     {
+        checkPreviousInstance();
+
         TestContainer testContainer = TestContainer.getInstance();
-        
+
         ForkedAppBooter appBooter =
             (ForkedAppBooter) testContainer.lookup( ForkedAppBooter.ROLE, "TestForkedAppBooter" );
         appBooter.start();
-        
-        // use the java client to wait for nexus to start 
+
+        // use the java client to wait for nexus to start
         // the ForkedAppBooter waits 6 seconds, and that is configurable
         // The client will wait 10 seconds by default, but will poll every half second.
         NexusClient client = new NexusRestClient();
         // at this point security should not be turned on, but you never know...
-        client.connect( TestProperties.getString( "nexus.base.url" ), testContainer.getTestContext().getAdminUsername(), testContainer.getTestContext().getAdminPassword() );
+        client.connect( TestProperties.getString( "nexus.base.url" ),
+                        testContainer.getTestContext().getAdminUsername(),
+                        testContainer.getTestContext().getAdminPassword() );
         client.isNexusStarted( true );
         client.disconnect();
-        
+
         // now to make everything work correctly we actually have to "soft-stop" nexus
         NexusStateUtil.doSoftStop();
 
+    }
+
+    private static void checkPreviousInstance()
+        throws Exception
+    {
+        try
+        {
+            Status status = RequestFacade.doGetRequest( "service/local/status " ).getStatus();
+            if ( status.isSuccess() )
+            {
+                Assert.fail( "Nexus already started.  Stop it first!" );
+            }
+        }
+        catch ( Exception e )
+        {
+            e.printStackTrace();
+        }
     }
 
     @AfterClass
