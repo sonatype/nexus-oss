97df120fbd77450ada1d0019c8631dabb5471b3c
Marvin Froeder <velo@sonatype.com>
2008-10-30 13:54:31 +0000

    Change mail server to use port allocator plugin too
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@2080 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-test-harness/nexus-test-harness-launcher/src/test/java/org/sonatype/nexus/integrationtests/AbstractEmailServerNexusIT.java
+++ nexus/nexus-test-harness/nexus-test-harness-launcher/src/test/java/org/sonatype/nexus/integrationtests/AbstractEmailServerNexusIT.java
@@ -3,23 +3,34 @@ package org.sonatype.nexus.integrationtests;
 import org.apache.log4j.Logger;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.sonatype.nexus.test.utils.TestProperties;
 
 import com.icegreen.greenmail.util.GreenMail;
-import com.icegreen.greenmail.util.ServerSetupTest;
+import com.icegreen.greenmail.util.ServerSetup;
 
 public abstract class AbstractEmailServerNexusIT
     extends AbstractNexusIntegrationTest
 {
 
     private static final Logger LOG = Logger.getLogger( AbstractEmailServerNexusIT.class );
-    
+
+    private static final int emailServerPort;
+
+    static
+    {
+        String port = TestProperties.getString( "email.server.port" );
+        emailServerPort = new Integer( port );
+    }
+
     protected static GreenMail server;
 
     @BeforeClass
     public static void startEmailServer()
     {
         // ServerSetup smtp = new ServerSetup( 1234, null, ServerSetup.PROTOCOL_SMTP );
-        server = new GreenMail( ServerSetupTest.SMTP );
+        ServerSetup smtp = new ServerSetup( emailServerPort, null, ServerSetup.PROTOCOL_SMTP );
+
+        server = new GreenMail( smtp );
         server.setUser( "system@nexus.org", "smtp-username", "smtp-password" );
         LOG.debug( "Starting e-mail server" );
         server.start();
