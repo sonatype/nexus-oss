fa10f09ead14ccf18078def0db33a8bb5a8e055f
Marvin Froeder <velo@sonatype.com>
2008-10-31 18:11:38 +0000

    IN PROGRESS - issue NEXUS-979: IT's fail on CI http://issues.sonatype.org/browse/NEXUS-979
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@2130 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-test-harness/nexus-test-harness-launcher/src/test/java/org/sonatype/nexus/integrationtests/nexus1022/Nexus1022RebuildRepositoryMavenMetadataTaskTest.java
+++ nexus/nexus-test-harness/nexus-test-harness-launcher/src/test/java/org/sonatype/nexus/integrationtests/nexus1022/Nexus1022RebuildRepositoryMavenMetadataTaskTest.java
@@ -1,19 +1,19 @@
 /*
  * Nexus: Maven Repository Manager
- * Copyright (C) 2008 Sonatype Inc.                                                                                                                          
- * 
- * This file is part of Nexus.                                                                                                                                  
- * 
+ * Copyright (C) 2008 Sonatype Inc.
+ *
+ * This file is part of Nexus.
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see http://www.gnu.org/licenses/.
  *
@@ -28,6 +28,7 @@ import junit.framework.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.sonatype.nexus.integrationtests.AbstractNexusIntegrationTest;
+import org.sonatype.nexus.rest.model.ScheduledServiceListResource;
 import org.sonatype.nexus.rest.model.ScheduledServicePropertyResource;
 import org.sonatype.nexus.tasks.descriptors.RebuildMavenMetadataTaskDescriptor;
 import org.sonatype.nexus.test.utils.TaskScheduleUtil;
@@ -42,20 +43,25 @@ public class Nexus1022RebuildRepositoryMavenMetadataTaskTest
         String releaseRepoPath = "runtime/work/storage/nexus-test-harness-repo/";
 
         ScheduledServicePropertyResource repo = new ScheduledServicePropertyResource();
-        
+
         repo.setId( "repositoryOrGroupId" );
-        
+
         repo.setValue( "repo_" + REPO_TEST_HARNESS_REPO );
-        
-        TaskScheduleUtil.runTask( RebuildMavenMetadataTaskDescriptor.ID, repo );
+
+        ScheduledServiceListResource task = TaskScheduleUtil.runTask("RebuildMavenMetadata-Nexus1022", RebuildMavenMetadataTaskDescriptor.ID, 300, repo );
+        Assert.assertNotNull( "The ScheduledServicePropertyResource task didn't run", task );
 
         File artifactDirMd = new File( nexusBaseDir, releaseRepoPath + "nexus1022/foo/bar/artifact/maven-metadata.xml" );
         Assert.assertTrue( "Maven metadata file should be generated after rebuild", artifactDirMd.exists() );
-      
+
         File groupPluginMd = new File( nexusBaseDir, releaseRepoPath + "nexus1022/foo/bar/plugins/maven-metadata.xml" );
         Assert.assertTrue( "Maven metadata file should be generated after rebuild", groupPluginMd.exists() );
 
     }
-    
-    
+
+    @BeforeClass
+    public static void cleanWorkFolder() throws Exception {
+        cleanWorkDir();
+    }
+
 }
