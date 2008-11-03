1f4f376704f291421b1c4caa119d9844eac0423c
Marvin Froeder <velo@sonatype.com>
2008-11-03 13:49:53 +0000

    IN PROGRESS - issue NEXUS-979: IT's fail on CI http://issues.sonatype.org/browse/NEXUS-979
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@2159 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-test-harness/nexus-test-harness-launcher/src/test/java/org/sonatype/nexus/test/utils/ServiceStatusUtil.java
+++ nexus/nexus-test-harness/nexus-test-harness-launcher/src/test/java/org/sonatype/nexus/test/utils/ServiceStatusUtil.java
@@ -26,7 +26,7 @@ public class ServiceStatusUtil
         throws NexusClientException, NexusConnectionException
     {
         System.setProperty( NexusRestClient.WAIT_FOR_START_TIMEOUT_KEY, "1000" );
-        for ( int i = 0; i < 40; i++ )
+        for ( int i = 0; i < 120; i++ )
         {
             if ( !client.isNexusStarted( true ) )
             {
