825c91c7365b365a7a45e25e5b44e7524e1085ef
Juven Xu <juven@sonatype.com>
2008-11-05 04:56:27 +0000

    Nexus970 while deleting repo storage folders, index files shouldn't be removed by this task
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@2187 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-test-harness/nexus-test-harness-launcher/src/test/java/org/sonatype/nexus/integrationtests/nexus970/Nexus970DeleteRepositoryTest.java
+++ nexus/nexus-test-harness/nexus-test-harness-launcher/src/test/java/org/sonatype/nexus/integrationtests/nexus970/Nexus970DeleteRepositoryTest.java
@@ -28,11 +28,9 @@ public class Nexus970DeleteRepositoryTest
     {
         File storageDir = new File( nexusBaseDir, "runtime/work/storage/nexus970-default" );
         File artifactFile = new File( storageDir, "nexus970/artifact/1.0.0/artifact-1.0.0.jar" );
-        File index = new File( nexusBaseDir, "runtime/work/indexer/nexus970-default-local" );
 
         // sanity check
         Assert.assertTrue( storageDir.isDirectory() );
-        Assert.assertTrue( index.isDirectory() );
         Assert.assertTrue( artifactFile.isFile() );
 
         String uri = "service/local/repositories/nexus970-default";
@@ -43,14 +41,11 @@ public class Nexus970DeleteRepositoryTest
 
         Assert.assertFalse( "Artifacts shouldn't exists on deleted repo", artifactFile.exists() );
         Assert.assertFalse( "Storage dir should be deleted", storageDir.exists() );
-        Assert.assertFalse( "Index dir should be deleted", index.exists() );
 
         File trashStorageDir = new File( nexusBaseDir, "runtime/work/trash/nexus970-default" );
         File trashArtifactFile = new File( trashStorageDir, "nexus970/artifact/1.0.0/artifact-1.0.0.jar" );
-        File trashIndex = new File( nexusBaseDir, "runtime/work/trash/nexus970-default-local" );
 
         Assert.assertTrue( "Storage should be moved to trash", trashStorageDir.isDirectory() );
-        Assert.assertTrue( "Index should be moved to trash", trashIndex.isDirectory() );
         Assert.assertTrue( "Artifacts should be moved to trash", trashArtifactFile.isFile() );
     }
 
@@ -63,11 +58,9 @@ public class Nexus970DeleteRepositoryTest
     {
         File storageDir = getTestFile( "overwrote-repo" );
         File artifactFile = new File( storageDir, "nexus970/artifact/1.0.0/artifact-1.0.0.jar" );
-        File index = new File( nexusBaseDir, "runtime/work/indexer/nexus970-overwrote-local" );
 
         // sanity check
         Assert.assertTrue( storageDir.isDirectory() );
-        Assert.assertTrue( index.isDirectory() );
         Assert.assertTrue( artifactFile.isFile() );
 
         String uri = "service/local/repositories/nexus970-overwrote";
@@ -78,14 +71,11 @@ public class Nexus970DeleteRepositoryTest
 
         Assert.assertTrue( "Artifacts should exists on deleted repo", artifactFile.isFile() );
         Assert.assertTrue( "Storage dir shouldn't be deleted", storageDir.isDirectory() );
-        Assert.assertTrue( "Index dir shouldn't be deleted", index.isDirectory() );
 
         File trashStorageDir = new File( nexusBaseDir, "runtime/work/trash/nexus970-overwrote" );
         File trashArtifactFile = new File( trashStorageDir, "nexus970/artifact/1.0.0/artifact-1.0.0.jar" );
-        File trashIndex = new File( nexusBaseDir, "runtime/work/trash/nexus970-overwrote-local" );
 
         Assert.assertFalse( "Storage shouldn't be moved to trash", trashStorageDir.exists() );
-        Assert.assertFalse( "Index shouldn't be moved to trash", trashIndex.exists() );
         Assert.assertFalse( "Artifacts shouldn't be moved to trash", trashArtifactFile.exists() );
     }
 
