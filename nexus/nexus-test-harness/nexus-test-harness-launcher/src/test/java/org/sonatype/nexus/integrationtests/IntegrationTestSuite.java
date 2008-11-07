daa6e462f9e8c20042f456c8c8f16fb2a0a6ca39
Marvin Froeder <velo@sonatype.com>
2008-11-07 01:47:25 +0000

    IN PROGRESS - issue NEXUS-1076: Change test harness to allow plugins testing http://issues.sonatype.org/browse/NEXUS-1076
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@2229 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-test-harness/nexus-test-harness-launcher/src/test/java/org/sonatype/nexus/integrationtests/IntegrationTestSuite.java
+++ nexus/nexus-test-harness/nexus-test-harness-launcher/src/test/java/org/sonatype/nexus/integrationtests/IntegrationTestSuite.java
@@ -1,21 +1,11 @@
 package org.sonatype.nexus.integrationtests;
 
-import java.io.File;
-
-import junit.framework.Assert;
-
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
-import org.sonatype.appbooter.ForkedAppBooter;
-import org.sonatype.nexus.client.NexusClient;
-import org.sonatype.nexus.client.rest.NexusRestClient;
-import org.sonatype.nexus.test.utils.FileTestingUtils;
-import org.sonatype.nexus.test.utils.NexusStateUtil;
-import org.sonatype.nexus.test.utils.ServiceStatusUtil;
-import org.sonatype.nexus.test.utils.TestProperties;
+import org.sonatype.nexus.test.utils.TestSuiteUtil;
 
 @RunWith( Suite.class )
 @SuiteClasses( { IntegrationTestSuiteClasses.class, IntegrationTestSuiteClassesSecurity.class } )
@@ -27,47 +17,14 @@ public class IntegrationTestSuite
     public static void beforeSuite()
         throws Exception
     {
-        // checkPreviousInstance(); r1931 no longer required, port allocator put each new instance in a new port
-
-        TestContainer testContainer = TestContainer.getInstance();
-
-        // copy default nexus.xml
-        File testConfigFile = AbstractNexusIntegrationTest.getResource( "default-config/nexus.xml" );
-        File outputFile =
-            new File( AbstractNexusIntegrationTest.nexusBaseDir + "/"
-                + AbstractNexusIntegrationTest.RELATIVE_WORK_CONF_DIR, "nexus.xml" );
-        FileTestingUtils.fileCopy( testConfigFile, outputFile );
-
-        // start nexus
-        ForkedAppBooter appBooter =
-            (ForkedAppBooter) testContainer.lookup( ForkedAppBooter.ROLE, "TestForkedAppBooter" );
-        appBooter.start();
-
-        // use the java client to wait for nexus to start
-        // the ForkedAppBooter waits 6 seconds, and that is configurable
-        // The client will wait 10 seconds by default, but will poll every half second.
-        NexusClient client = new NexusRestClient();
-        // at this point security should not be turned on, but you never know...
-        client.connect( TestProperties.getString( "nexus.base.url" ),
-                        testContainer.getTestContext().getAdminUsername(),
-                        testContainer.getTestContext().getAdminPassword() );
-
-        Assert.assertTrue( ServiceStatusUtil.waitForStart( client ) );
-
-        // now to make everything work correctly we actually have to "soft-stop" nexus
-        NexusStateUtil.doSoftStop();
-
-        Assert.assertTrue( ServiceStatusUtil.waitForStop( client ) );
-        client.disconnect();
+        TestSuiteUtil.startNexus();
     }
 
     @AfterClass
     public static void afterSuite()
         throws Exception
     {
-        ForkedAppBooter appBooter =
-            (ForkedAppBooter) TestContainer.getInstance().lookup( ForkedAppBooter.ROLE, "TestForkedAppBooter" );
-        appBooter.shutdown();
+        TestSuiteUtil.stopNexus();
     }
 
 }
