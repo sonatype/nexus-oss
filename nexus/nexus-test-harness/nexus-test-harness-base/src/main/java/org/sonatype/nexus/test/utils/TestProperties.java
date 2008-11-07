061c83e04bc4ecc46e7493e4c2190dc8584eecb5
Marvin Froeder <velo@sonatype.com>
2008-11-07 18:35:55 +0000

    Allocating a port to web proxy
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@2237 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-test-harness/nexus-test-harness-base/src/main/java/org/sonatype/nexus/test/utils/TestProperties.java
+++ nexus/nexus-test-harness/nexus-test-harness-base/src/main/java/org/sonatype/nexus/test/utils/TestProperties.java
@@ -6,17 +6,24 @@ public class TestProperties
 {
 
     private static ResourceBundle bundle;
-    
+
     // i hate doing this, but its really easy, and this is for tests. this class can be replaced easy, if we need to...
     static
     {
         bundle = ResourceBundle.getBundle( "baseTest" );
     }
-    
-    
+
+
     public static String getString( String key )
     {
         return bundle.getString( key );
     }
-    
+
+
+    public static Integer getInteger( String key)
+    {
+        String value = bundle.getString( key );
+        return new Integer(value);
+    }
+
 }
