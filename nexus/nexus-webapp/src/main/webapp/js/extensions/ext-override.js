de334bfca40abceb2a80180d7802fd81794e2548
Damian Bradicich <dbradicich@sonatype.com>
2008-11-24 22:41:26 +0000

    Added an override so show/hide/destroy will also affect labels in formsets
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@2408 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-webapp/src/main/webapp/js/extensions/ext-override.js
+++ nexus/nexus-webapp/src/main/webapp/js/extensions/ext-override.js
@@ -657,3 +657,31 @@ Ext.override(Ext.tree.TreeNodeUI, {
     }
   }
 });
+
+Ext.override(Ext.layout.FormLayout, {
+    renderItem : function(c, position, target){
+        if(c && !c.rendered && c.isFormField && c.inputType != 'hidden'){
+            var args = [
+                   c.id, c.fieldLabel,
+                   c.labelStyle||this.labelStyle||'',
+                   this.elementStyle||'',
+                   typeof c.labelSeparator == 'undefined' ? this.labelSeparator : c.labelSeparator,
+                   (c.itemCls||this.container.itemCls||'') + (c.hideLabel ? ' x-hide-label' : ''),
+                   c.clearCls || 'x-form-clear-left' 
+            ];
+            if(typeof position == 'number'){
+                position = target.dom.childNodes[position] || null;
+            }
+            if(position){
+                c.formItem = this.fieldTpl.insertBefore(position, args, true);
+            }else{
+                c.formItem = this.fieldTpl.append(target, args, true);
+            }
+            c.render('x-form-el-'+c.id);
+            c.container = c.formItem; // must set after render, because render sets it.
+            c.actionMode = 'container';
+        }else {
+            Ext.layout.FormLayout.superclass.renderItem.apply(this, arguments);
+        }
+    }
+});
