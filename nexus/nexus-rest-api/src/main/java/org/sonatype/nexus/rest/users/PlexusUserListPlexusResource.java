e2d610600893b8a653f7341f66d11e0bc47ff218
Damian Bradicich <dbradicich@sonatype.com>
2008-12-01 16:38:09 +0000

    NXCM-200: Added a new plexus_users resource to allow retrieval of users from any realm in the UI
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@2482 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

---
+++ nexus/nexus-rest-api/src/main/java/org/sonatype/nexus/rest/users/PlexusUserListPlexusResource.java
@@ -0,0 +1,77 @@
+package org.sonatype.nexus.rest.users;
+
+import org.codehaus.plexus.component.annotations.Component;
+import org.codehaus.plexus.component.annotations.Requirement;
+import org.restlet.Context;
+import org.restlet.data.Request;
+import org.restlet.data.Response;
+import org.restlet.resource.ResourceException;
+import org.restlet.resource.Variant;
+import org.sonatype.jsecurity.locators.users.PlexusUser;
+import org.sonatype.jsecurity.locators.users.PlexusUserManager;
+import org.sonatype.nexus.rest.AbstractNexusPlexusResource;
+import org.sonatype.nexus.rest.model.PlexusUserListResourceResponse;
+import org.sonatype.nexus.rest.model.PlexusUserResource;
+import org.sonatype.plexus.rest.resource.PathProtectionDescriptor;
+import org.sonatype.plexus.rest.resource.PlexusResource;
+
+@Component( role = PlexusResource.class, hint = "PlexusUserListPlexusResource" )
+public class PlexusUserListPlexusResource
+    extends AbstractNexusPlexusResource
+{
+    @Requirement( role = PlexusUserManager.class )
+    private PlexusUserManager userManager;
+    
+    public PlexusUserListPlexusResource()
+    {
+        setModifiable( false );
+    }
+    
+    @Override
+    public Object getPayloadInstance()
+    {
+        return null;
+    }
+
+    @Override
+    public PathProtectionDescriptor getResourceProtection()
+    {
+        return new PathProtectionDescriptor( getResourceUri(), "authcBasic,perms[nexus:users]" );
+    }
+
+    @Override
+    public String getResourceUri()
+    {
+        return "/plexus_users";
+    }
+    
+    @Override
+    public Object get( Context context, Request request, Response response, Variant variant )
+        throws ResourceException
+    {
+        PlexusUserListResourceResponse result = new PlexusUserListResourceResponse();
+        
+        for ( PlexusUser user : userManager.listUsers() )
+        {
+            PlexusUserResource res = nexusToRestModel( user, request );
+
+            if ( res != null )
+            {
+                result.addData( res );
+            }
+        }
+
+        return result;
+    }
+    
+    protected PlexusUserResource nexusToRestModel( PlexusUser user, Request request )
+    {
+        PlexusUserResource resource = new PlexusUserResource();
+        
+        resource.setUserId( user.getUserId() );
+        resource.setName( user.getName() );
+        resource.setEmail( user.getEmailAddress() );
+        
+        return resource;
+    }
+}
