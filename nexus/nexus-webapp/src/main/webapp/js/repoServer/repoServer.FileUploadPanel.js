ed58fdd0650c0fd994b2efe1cd49143ad080557e
dip <dip@2aa8b3fc-8ebb-4439-a84f-95066eaea8ab>
2008-12-09 16:37:37 +0000

    NEXUS-1220: "Browse" button fix
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@2598 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-webapp/src/main/webapp/js/repoServer/repoServer.FileUploadPanel.js
+++ nexus/nexus-webapp/src/main/webapp/js/repoServer/repoServer.FileUploadPanel.js
@@ -625,6 +625,10 @@ Sonatype.Events.addListener( 'repositoryViewInit', function( cardPanel, rec ) {
               
               uploadPanel.on( 'show', function( p ) {
                 p.doLayout();
+                var b = p.find( 'xtype', 'browsebutton' );
+                for ( var i = 0; i < b.length; i++ ) {
+                  b[i].setClipSize();
+                }
               } );
             }
             else {
