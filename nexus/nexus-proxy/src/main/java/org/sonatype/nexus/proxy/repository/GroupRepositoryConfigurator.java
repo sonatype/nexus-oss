8f87bb02ae61c33c6c367c02b23c1e1951631233
Damian Bradicich <dbradicich@sonatype.com>
2008-12-18 19:03:08 +0000

    updated headers
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@2783 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-proxy/src/main/java/org/sonatype/nexus/proxy/repository/GroupRepositoryConfigurator.java
+++ nexus/nexus-proxy/src/main/java/org/sonatype/nexus/proxy/repository/GroupRepositoryConfigurator.java
@@ -1,3 +1,19 @@
+/**
+ * Sonatype Nexus (TM) [Open Source Version].
+ * Copyright (c) 2008 Sonatype, Inc. All rights reserved.
+ * Includes the third-party code listed at ${thirdPartyUrl}.
+ *
+ * This program is licensed to you under Version 3 only of the GNU
+ * General Public License as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License Version 3 for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * Version 3 along with this program. If not, see http://www.gnu.org/licenses/.
+ */
 package org.sonatype.nexus.proxy.repository;
 
 import org.sonatype.nexus.configuration.application.ApplicationConfiguration;
--- nexus/nexus-proxy/src/test/java/org/sonatype/nexus/proxy/registry/DefaultRepositoryRegistryTest.java
+++ nexus/nexus-proxy/src/test/java/org/sonatype/nexus/proxy/registry/DefaultRepositoryRegistryTest.java
@@ -1,22 +1,18 @@
-/*
- * Nexus: Maven Repository Manager
- * Copyright (C) 2008 Sonatype Inc.                                                                                                                          
- * 
- * This file is part of Nexus.                                                                                                                                  
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
+/**
+ * Sonatype Nexus (TM) [Open Source Version].
+ * Copyright (c) 2008 Sonatype, Inc. All rights reserved.
+ * Includes the third-party code listed at ${thirdPartyUrl}.
+ *
+ * This program is licensed to you under Version 3 only of the GNU
+ * General Public License as published by the Free Software Foundation.
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see http://www.gnu.org/licenses/.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License Version 3 for more details.
  *
+ * You should have received a copy of the GNU General Public License
+ * Version 3 along with this program. If not, see http://www.gnu.org/licenses/.
  */
 package org.sonatype.nexus.proxy.registry;
 
