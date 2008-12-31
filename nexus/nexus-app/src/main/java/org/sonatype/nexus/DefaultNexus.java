181af87411fb609a25213ccd71e46736c906de01
eu <eu@2aa8b3fc-8ebb-4439-a84f-95066eaea8ab>
2008-12-31 03:02:07 +0000

    unused imports
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@2845 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-app/src/main/java/org/sonatype/nexus/DefaultNexus.java
+++ nexus/nexus-app/src/main/java/org/sonatype/nexus/DefaultNexus.java
@@ -23,7 +23,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
