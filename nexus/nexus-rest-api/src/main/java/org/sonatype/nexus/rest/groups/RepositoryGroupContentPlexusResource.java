2badfeb202b79fba33cf9a284f0906050827d199
Tamas Cservenak <cstamas@sonatype.com>
2009-01-05 13:14:53 +0000

    Removed old code remnants from resource.
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@2862 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-rest-api/src/main/java/org/sonatype/nexus/rest/groups/RepositoryGroupContentPlexusResource.java
+++ nexus/nexus-rest-api/src/main/java/org/sonatype/nexus/rest/groups/RepositoryGroupContentPlexusResource.java
@@ -17,12 +17,11 @@
 package org.sonatype.nexus.rest.groups;
 
 import org.codehaus.plexus.component.annotations.Component;
-import org.codehaus.plexus.component.annotations.Requirement;
 import org.restlet.data.Request;
 import org.restlet.resource.ResourceException;
 import org.sonatype.nexus.proxy.NoSuchRepositoryException;
 import org.sonatype.nexus.proxy.ResourceStore;
-import org.sonatype.nexus.proxy.router.RepositoryRouter;
+import org.sonatype.nexus.proxy.repository.GroupRepository;
 import org.sonatype.nexus.rest.AbstractResourceStoreContentPlexusResource;
 import org.sonatype.plexus.rest.resource.PathProtectionDescriptor;
 import org.sonatype.plexus.rest.resource.PlexusResource;
@@ -34,14 +33,7 @@ import org.sonatype.plexus.rest.resource.PlexusResource;
 public class RepositoryGroupContentPlexusResource
     extends AbstractResourceStoreContentPlexusResource
 {
-
     public static final String GROUP_ID_KEY = "groupId";
-
-    /**
-     * TODO: THIS IS BAD! Dynamic router needed!
-     */
-    @Requirement( role = RepositoryRouter.class, hint = "groups" )
-    private ResourceStore resourceStore;
 
     @Override
     public Object getPayloadInstance()
@@ -63,18 +55,13 @@ public class RepositoryGroupContentPlexusResource
     }
 
     @Override
-    protected String getResourceStorePath( Request request )
-    {
-        String groupId = request.getAttributes().get( GROUP_ID_KEY ).toString();
-        return "/" + groupId + super.getResourceStorePath( request );
-    }
-
-    @Override
     protected ResourceStore getResourceStore( Request request )
         throws NoSuchRepositoryException,
             ResourceException
     {
-        return resourceStore;
+        String groupId = request.getAttributes().get( GROUP_ID_KEY ).toString();
+
+        return getNexus().getRepositoryWithFacet( groupId, GroupRepository.class );
     }
 
 }
