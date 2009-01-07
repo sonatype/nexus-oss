591b33f5cd327fe4727ce0474d243c7b9a498279
Juven Xu <juven@sonatype.com>
2009-01-07 15:35:24 +0000

    [NXCM-326] update EPL headers for nexus-indexer, link it up with the cool maven-license-plugin
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@2913 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus-indexer/src/main/java/org/sonatype/nexus/index/updater/IndexUpdater.java
+++ nexus-indexer/src/main/java/org/sonatype/nexus/index/updater/IndexUpdater.java
@@ -1,16 +1,15 @@
-/*******************************************************************************
- * Copyright (c) 2007-2008 Sonatype Inc
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+/**
+ * Copyright Â© 2007-2008 Sonatype, Inc. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
  *
  * Contributors:
- *    Eugene Kuleshov (Sonatype)
- *    Tamás Cservenák (Sonatype)
- *    Brian Fox (Sonatype)
- *    Jason Van Zyl (Sonatype)
- *******************************************************************************/
+ * Eugene Kuleshov (Sonatype)
+ * Tamas Cservenak (Sonatype)
+ * Brian Fox (Sonatype)
+ * Jason Van Zyl (Sonatype)
+ */
 package org.sonatype.nexus.index.updater;
 
 import java.io.IOException;
--- nexus-indexer/src/test/java/org/sonatype/nexus/index/Nexus687NexusIndexerTest.java
+++ nexus-indexer/src/test/java/org/sonatype/nexus/index/Nexus687NexusIndexerTest.java
@@ -1,3 +1,15 @@
+/**
+ * Copyright © 2007-2008 Sonatype, Inc. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Contributors:
+ * Eugene Kuleshov (Sonatype)
+ * Tamas Cservenak (Sonatype)
+ * Brian Fox (Sonatype)
+ * Jason Van Zyl (Sonatype)
+ */
 package org.sonatype.nexus.index;
 
 import java.io.File;
