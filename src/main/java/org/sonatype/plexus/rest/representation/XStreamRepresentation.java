511bc079e6da6f77313d71736a2265b8458972b1
Juven Xu <juven@sonatype.com>
2009-01-07 15:22:34 +0000

    [NXCM-328] update ASF headers for spice, link it up with the cool maven-license-plugin
    git-svn-id: file:///opt/svn/repositories/sonatype.org/spice/trunk/plexus-restlet-bridge@696 5751e0cb-dcb7-432f-92e2-260806df54be

--- src/main/java/org/sonatype/plexus/rest/representation/XStreamRepresentation.java
+++ src/main/java/org/sonatype/plexus/rest/representation/XStreamRepresentation.java
@@ -1,18 +1,15 @@
 /**
-  * Copyright (C) 2008 Sonatype Inc. 
-  * Sonatype Inc, licenses this file to you under the Apache License,
-  * Version 2.0 (the "License"); you may not use this file except in 
-  * compliance with the License.  You may obtain a copy of the License at
-  *
-  * http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing,
-  * software distributed under the License is distributed on an
-  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-  * KIND, either express or implied.  See the License for the
-  * specific language governing permissions and limitations
-  * under the License.
-  */
+ * Copyright © 2008 Sonatype, Inc. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
 package org.sonatype.plexus.rest.representation;
 
 import org.restlet.data.CharacterSet;
