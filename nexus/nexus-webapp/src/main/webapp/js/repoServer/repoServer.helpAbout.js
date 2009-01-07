4ea5cfd31f5dc336710514ba7bcf5196796ef19b
Damian Bradicich <dbradicich@sonatype.com>
2009-01-07 21:25:09 +0000

    added line breaks
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@2927 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-webapp/src/main/webapp/js/repoServer/repoServer.helpAbout.js
+++ nexus/nexus-webapp/src/main/webapp/js/repoServer/repoServer.helpAbout.js
@@ -46,9 +46,9 @@ Ext.extend(Sonatype.repoServer.HelpAboutPanel, Ext.Panel, {
     + ' Version'
     + '<br>Copyright&copy; 2008 Sonatype, Inc.  All rights reserved.'
     + '<br>Includes the third-party code listed at <a href="http://nexus.sonatype.org/dev/attributions.html" target="_new">http://nexus.sonatype.org/dev/attributions.html</a>'
-    + '<br>This program is licensed to you under Version 3 only of the GNU General Public License as published by the Free Software Foundation.'
+    + '<br><br>This program is licensed to you under Version 3 only of the GNU General Public License as published by the Free Software Foundation.'
     + '<br>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License Version 3 for more details.'
-    + '<br>You should have received a copy of the GNU General Public License Version 3 along with this program. If not, see <a href="http://www.gnu.org/licenses/" target="_new">http://www.gnu.org/licenses/</a>'
+    + '<br><br>You should have received a copy of the GNU General Public License Version 3 along with this program. If not, see <a href="http://www.gnu.org/licenses/" target="_new">http://www.gnu.org/licenses/</a>'
     + '<br>Sonatype Nexus&trade; '
     + Sonatype.utils.edition
     + ' Version is available from Sonatype, Inc.'
