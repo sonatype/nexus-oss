eaa9224d37784cac63809b8523c32b173783162f
Juven Xu <juven@sonatype.com>
2009-01-07 18:14:26 +0000

    remove the attribution list from the head
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@2920 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus-indexer/src/main/java/org/sonatype/nexus/DefaultNexusEnforcer.java
+++ nexus-indexer/src/main/java/org/sonatype/nexus/DefaultNexusEnforcer.java
@@ -3,12 +3,6 @@
  *
  * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
  * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
- *
- * Contributors:
- * Eugene Kuleshov (Sonatype)
- * Tamas Cservenak (Sonatype)
- * Brian Fox (Sonatype)
- * Jason Van Zyl (Sonatype)
  */
 package org.sonatype.nexus;
 
