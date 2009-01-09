b7be03938cecf3d8b4fe3aed5db2524c78db317c
Brian Demers <bdemers@sonatype.com>
2009-01-09 17:05:09 +0000

    NEXUS-1239, fixed test, added work around for in-memory user locator kicking around
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@2969 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-test-harness/nexus-test-harness-launcher/src/test/java/org/sonatype/nexus/integrationtests/nexus1239/Nexus1239PlexusUserResourceTest.java
+++ nexus/nexus-test-harness/nexus-test-harness-launcher/src/test/java/org/sonatype/nexus/integrationtests/nexus1239/Nexus1239PlexusUserResourceTest.java
@@ -13,122 +13,133 @@ import org.sonatype.nexus.rest.model.PlexusRoleResource;
 import org.sonatype.nexus.rest.model.PlexusUserResource;
 import org.sonatype.nexus.test.utils.UserMessageUtil;
 
-public class Nexus1239PlexusUserResourceTest extends AbstractNexusIntegrationTest
+public class Nexus1239PlexusUserResourceTest
+    extends AbstractNexusIntegrationTest
 {
 
-    @SuppressWarnings("unchecked")
+    @SuppressWarnings( "unchecked" )
     @Test
-    public void getUserTestWithSource() throws IOException
+    public void getUserTestWithSource()
+        throws IOException
     {
-        
-        UserMessageUtil userUtil = new UserMessageUtil(this.getJsonXStream(), MediaType.APPLICATION_JSON);
+
+        UserMessageUtil userUtil = new UserMessageUtil( this.getJsonXStream(), MediaType.APPLICATION_JSON );
         PlexusUserResource user = userUtil.getPlexusUser( "default", "admin" );
         Assert.assertEquals( "admin", user.getUserId() );
         Assert.assertEquals( "changeme@yourcompany.com", user.getEmail() );
         Assert.assertEquals( "Administrator", user.getName() );
         Assert.assertEquals( "default", user.getSource() );
-        
+
         List<PlexusRoleResource> roles = user.getRoles();
         Assert.assertEquals( 1, roles.size() );
-        
+
         PlexusRoleResource role = roles.get( 0 );
         Assert.assertEquals( "Nexus Administrator Role", role.getName() );
         Assert.assertEquals( "admin", role.getRoleId() );
         Assert.assertEquals( "default", role.getSource() );
     }
-    
-    @SuppressWarnings("unchecked")
+
+    @SuppressWarnings( "unchecked" )
     @Test
-    public void getUserTestWithOutSource() throws IOException
+    public void getUserTestWithOutSource()
+        throws IOException
     {
-        
-        UserMessageUtil userUtil = new UserMessageUtil(this.getJsonXStream(), MediaType.APPLICATION_JSON);
+
+        UserMessageUtil userUtil = new UserMessageUtil( this.getJsonXStream(), MediaType.APPLICATION_JSON );
         PlexusUserResource user = userUtil.getPlexusUser( null, "admin" );
         Assert.assertEquals( "admin", user.getUserId() );
         Assert.assertEquals( "changeme@yourcompany.com", user.getEmail() );
         Assert.assertEquals( "Administrator", user.getName() );
         Assert.assertEquals( "default", user.getSource() );
-        
+
         List<PlexusRoleResource> roles = user.getRoles();
         Assert.assertEquals( 1, roles.size() );
-        
+
         PlexusRoleResource role = roles.get( 0 );
         Assert.assertEquals( "Nexus Administrator Role", role.getName() );
         Assert.assertEquals( "admin", role.getRoleId() );
         Assert.assertEquals( "default", role.getSource() );
     }
-    
-    @SuppressWarnings("unchecked")
+
+    @SuppressWarnings( "unchecked" )
     @Test
-    public void getUserTestWithAllSource() throws IOException
+    public void getUserTestWithAllSource()
+        throws IOException
     {
-        
-        UserMessageUtil userUtil = new UserMessageUtil(this.getJsonXStream(), MediaType.APPLICATION_JSON);
+
+        UserMessageUtil userUtil = new UserMessageUtil( this.getJsonXStream(), MediaType.APPLICATION_JSON );
         PlexusUserResource user = userUtil.getPlexusUser( "all", "admin" );
         Assert.assertEquals( "admin", user.getUserId() );
         Assert.assertEquals( "changeme@yourcompany.com", user.getEmail() );
         Assert.assertEquals( "Administrator", user.getName() );
         Assert.assertEquals( "default", user.getSource() );
-        
+
         List<PlexusRoleResource> roles = user.getRoles();
         Assert.assertEquals( 1, roles.size() );
-        
+
         PlexusRoleResource role = roles.get( 0 );
         Assert.assertEquals( "Nexus Administrator Role", role.getName() );
         Assert.assertEquals( "admin", role.getRoleId() );
         Assert.assertEquals( "default", role.getSource() );
     }
-    
-    @SuppressWarnings("unchecked")
+
+    @SuppressWarnings( "unchecked" )
     @Test
-    public void getUsersTest() throws IOException
+    public void getUsersTest()
+        throws IOException
     {
-        UserMessageUtil userUtil = new UserMessageUtil(this.getJsonXStream(), MediaType.APPLICATION_JSON);
+        UserMessageUtil userUtil = new UserMessageUtil( this.getJsonXStream(), MediaType.APPLICATION_JSON );
         List<PlexusUserResource> users = userUtil.getPlexusUsers( "default" );
-        
+
         List<String> userIds = new ArrayList<String>();
-        
+
         for ( PlexusUserResource plexusUserResource : users )
         {
             userIds.add( plexusUserResource.getUserId() );
         }
-        
+
         Assert.assertTrue( userIds.contains( "admin" ) );
         Assert.assertTrue( userIds.contains( "anonymous" ) );
         Assert.assertTrue( userIds.contains( "deployment" ) );
         Assert.assertTrue( userIds.contains( "test-user" ) );
-        Assert.assertEquals( "Users: "+ userIds, 4, users.size() );
+        Assert.assertEquals( "Users: " + userIds, 4, users.size() );
     }
-    
-    @SuppressWarnings("unchecked")
+
+    @SuppressWarnings( "unchecked" )
     @Test
-    public void getUsersTestAllSource() throws IOException
+    public void getUsersTestAllSource()
+        throws IOException
     {
-        UserMessageUtil userUtil = new UserMessageUtil(this.getJsonXStream(), MediaType.APPLICATION_JSON);
+        UserMessageUtil userUtil = new UserMessageUtil( this.getJsonXStream(), MediaType.APPLICATION_JSON );
         List<PlexusUserResource> users = userUtil.getPlexusUsers( "all" );
-        
+
         List<String> userIds = new ArrayList<String>();
-        
+
         for ( PlexusUserResource plexusUserResource : users )
         {
             userIds.add( plexusUserResource.getUserId() );
         }
-        
+
         Assert.assertTrue( userIds.contains( "admin" ) );
         Assert.assertTrue( userIds.contains( "anonymous" ) );
         Assert.assertTrue( userIds.contains( "deployment" ) );
         Assert.assertTrue( userIds.contains( "test-user" ) );
-        Assert.assertEquals( "Users: "+ userIds, 4, users.size() );
+        // Assert.assertEquals( "Users: "+ userIds, 4, users.size() );
+
+        // NOTE: this needs to be at least the number of users expected in the default realm, the In-Memory realm add
+        // another user locator, and there is no way to disable it.
+        Assert.assertTrue( "Users: " + userIds, users.size() >= 4 );
     }
-    
-    @SuppressWarnings("unchecked")
+
+    @SuppressWarnings( "unchecked" )
     @Test
-    public void junkSourceTest() throws IOException
+    public void junkSourceTest()
+        throws IOException
     {
-        
-        UserMessageUtil userUtil = new UserMessageUtil(this.getJsonXStream(), MediaType.APPLICATION_JSON);
+
+        UserMessageUtil userUtil = new UserMessageUtil( this.getJsonXStream(), MediaType.APPLICATION_JSON );
         List<PlexusUserResource> users = userUtil.getPlexusUsers( "VOID" );
         Assert.assertEquals( 0, users.size() );
     }
-    
+
 }
