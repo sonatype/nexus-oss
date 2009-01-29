0a2bf1bb9b9380802a28123d09c19def64eaf2e8
Marvin Froeder <velo@sonatype.com>
2009-01-29 22:30:51 +0000

    OPEN - issue NEXUS-1329: Setup mirror support ITs https://issues.sonatype.org/browse/NEXUS-1329
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@3184 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

---
+++ nexus/nexus-test-harness/nexus-test-harness-launcher/src/test/java/org/sonatype/nexus/integrationtests/nexus1329/AbstractMirrorTest.java
@@ -0,0 +1,47 @@
+package org.sonatype.nexus.integrationtests.nexus1329;
+
+import org.junit.After;
+import org.junit.Before;
+import org.restlet.data.MediaType;
+import org.sonatype.jettytestsuite.ControlledServer;
+import org.sonatype.nexus.integrationtests.AbstractNexusIntegrationTest;
+import org.sonatype.nexus.test.utils.MirrorMessageUtils;
+import org.sonatype.nexus.test.utils.TestProperties;
+
+public class AbstractMirrorTest
+    extends AbstractNexusIntegrationTest
+{
+
+    public static final String REPO = "nexus1329-repo";
+    protected static final int webProxyPort;
+
+    static
+    {
+        webProxyPort = TestProperties.getInteger( "webproxy.server.port" );
+    }
+
+    protected ControlledServer server;
+
+    protected MirrorMessageUtils messageUtil;
+
+    public AbstractMirrorTest()
+    {
+        super();
+        this.messageUtil = new MirrorMessageUtils( this.getJsonXStream(), MediaType.APPLICATION_JSON );
+    }
+
+    @Before
+    public void start()
+        throws Exception
+    {
+        server = (ControlledServer) lookup( ControlledServer.ROLE );
+    }
+
+    @After
+    public void stop()
+        throws Exception
+    {
+        server.stop();
+    }
+
+}
---
+++ nexus/nexus-test-harness/nexus-test-harness-launcher/src/test/java/org/sonatype/nexus/integrationtests/nexus1329/Nexus1329MirrorOnlyTest.java
@@ -0,0 +1,57 @@
+/**
+ * Sonatype Nexus (TM) Open Source Version.
+ * Copyright (c) 2008 Sonatype, Inc. All rights reserved.
+ * Includes the third-party code listed at http://nexus.sonatype.org/dev/attributions.html
+ * This program is licensed to you under Version 3 only of the GNU General Public License as published by the Free Software Foundation.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License Version 3 for more details.
+ * You should have received a copy of the GNU General Public License Version 3 along with this program.
+ * If not, see http://www.gnu.org/licenses/.
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc.
+ * "Sonatype" and "Sonatype Nexus" are trademarks of Sonatype, Inc.
+ */
+package org.sonatype.nexus.integrationtests.nexus1329;
+
+import java.io.File;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.sonatype.nexus.artifact.Gav;
+import org.sonatype.nexus.test.utils.FileTestingUtils;
+
+public class Nexus1329MirrorOnlyTest
+    extends AbstractMirrorTest
+{
+
+    /**
+     * 1. download from mirror, no repository access
+     */
+    @Test
+    public void downloadFileThatIsOnlyInMirrorTest()
+        throws Exception
+    {
+        File content = getTestFile( "basic" );
+
+        List<String> repoUrls = server.addServer( "repository", HttpServletResponse.SC_NOT_FOUND );
+        server.addServer( "mirror1", content );
+        List<String> mirror2Urls = server.addServer( "mirror2", 500 );
+
+        server.start();
+
+        Gav gav =
+            new Gav( "nexus1329", "sample", "1.0.0", null, "xml", null, null, null, false, false, null, false, null );
+
+        File artifactFile = this.downloadArtifactFromRepository( REPO, gav, "./target/downloads/nexus1329" );
+
+        File originalFile = this.getTestFile( "basic/nexus1329/sample/1.0.0/sample-1.0.0.xml" );
+        Assert.assertTrue( FileTestingUtils.compareFileSHA1s( originalFile, artifactFile ) );
+
+        Assert.assertTrue( "Nexus should not access repository canonical url " + repoUrls, repoUrls.isEmpty() );
+        Assert.assertTrue( "Nexus should not access second mirror " + mirror2Urls, mirror2Urls.isEmpty() );
+    }
+
+}
