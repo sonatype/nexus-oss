d45a32548a75983949e98533d4a497fc5346eac3
Marvin Froeder <velo@sonatype.com>
2009-01-29 21:09:43 +0000

    OPEN - issue NEXUS-1329: Setup mirror support ITs https://issues.sonatype.org/browse/NEXUS-1329
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@3179 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-test-harness/nexus-test-harness-launcher/src/test/java/org/sonatype/nexus/integrationtests/nexus1329/Nexus1329MirrorTest.java
+++ nexus/nexus-test-harness/nexus-test-harness-launcher/src/test/java/org/sonatype/nexus/integrationtests/nexus1329/Nexus1329MirrorTest.java
@@ -14,101 +14,88 @@
 package org.sonatype.nexus.integrationtests.nexus1329;
 
 import java.io.File;
-import java.io.IOException;
-import java.util.Date;
 
-import junit.framework.Assert;
+import javax.servlet.http.HttpServletResponse;
 
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.sonatype.jettytestsuite.ControlledServer;
 import org.sonatype.nexus.artifact.Gav;
-import org.sonatype.nexus.integrationtests.proxy.AbstractNexusProxyIntegrationTest;
+import org.sonatype.nexus.integrationtests.AbstractNexusIntegrationTest;
 import org.sonatype.nexus.test.utils.FileTestingUtils;
 
 /**
- *
- * The nexus.xml for this test is located: /nexus-test-harness-launcher/src/test/resources/nexus1329/test-config/nexus.xml
- * 
- * The mirrors are located: /nexus-test-harness-launcher/src/test/resources/proxyRepo/nexus1329
- * They are located on the same server with different URLs.
- *
+ * The nexus.xml for this test is located:
+ * /nexus-test-harness-launcher/src/test/resources/nexus1329/test-config/nexus.xml The mirrors are located:
+ * /nexus-test-harness-launcher/src/test/resources/proxyRepo/nexus1329 They are located on the same server with
+ * different URLs.
  */
 public class Nexus1329MirrorTest
-    extends AbstractNexusProxyIntegrationTest // AbstractNexusProxyIntegrationTest should be moved out of proxy package when build is stable
+    extends AbstractNexusIntegrationTest // AbstractNexusProxyIntegrationTest should be moved out of proxy package when
+// build is stable
 {
+
+    private ControlledServer server;
+
+    @Before
+    public void start()
+        throws Exception
+    {
+        server = (ControlledServer) lookup( ControlledServer.ROLE );
+    }
 
     @Test
     public void downloadFromMirrorTest()
-        throws IOException
+        throws Exception
     {
-        // add a mirror
-        // TODO: add REST call
+        File content = getTestFile( "basic" );
 
-        // build a GAV for the artifact
-        Gav gav = new Gav(
-            this.getTestId(),
-            "sample",
-            "1.0.0",
-            null,
-            "xml",
-            0,
-            new Date().getTime(),
-            "Artifact Name",
-            false,
-            false,
-            null,
-            false,
-            null );
+        server.addServer( "repository", content );
+        server.addServer( "mirror1", 500 );
+        server.addServer( "mirror2", 500 );
+
+        server.start();
+
+        Gav gav =
+            new Gav( "nexus1329", "sample", "1.0.0", null, "xml", null, null, null, false, false, null, false, null );
 
         String repositoryId = "nexus1329-repo";
+        File artifactFile = this.downloadArtifactFromRepository( repositoryId, gav, "./target/downloads/nexus1329" );
 
-        // download file from repo
-        File artifactFile = this.downloadArtifactFromRepository( repositoryId, gav, "./target/downloadedFiles/repo" );
-        Assert.assertNotNull( artifactFile );
-        Assert.assertTrue( artifactFile.exists() );
-
-        // if you want to check the downloaded file against a known file you can compare the SHA1's
-        Assert.assertTrue( FileTestingUtils.compareFileSHA1s( this.getLocalFile(
-            this.getTestId() + "/repository",
-            gav ), artifactFile ) );
-
-        // download file from group
-        artifactFile = this.downloadArtifactFromGroup( "nexus1329-group", gav, "./target/downloadedFiles/group" );
-
+        Assert.assertTrue( FileTestingUtils.compareFileSHA1s(
+                                                              this.getTestFile( "basic/nexus1329/sample/1.0.0/sample-1.0.0.xml" ),
+                                                              artifactFile ) );
     }
 
     @Test
-    public void downloadFileThatIsOnlyInMirrorTest() throws IOException
+    public void downloadFileThatIsOnlyInMirrorTest()
+        throws Exception
     {
-        // build a GAV for the artifact
-        Gav gav = new Gav(
-            this.getTestId(),
-            "sample",
-            "1.0.1", // Different version then above
-            null,
-            "xml",
-            0,
-            new Date().getTime(),
-            "Artifact Name",
-            false,
-            false,
-            null,
-            false,
-            null );
+        File content = getTestFile( "basic" );
+
+        server.addServer( "repository", HttpServletResponse.SC_NOT_FOUND );
+        server.addServer( "mirror1", content );
+        server.addServer( "mirror2", 500 );
+
+        server.start();
+
+        Gav gav =
+            new Gav( "nexus1329", "sample", "1.0.0", null, "xml", null, null, null, false, false, null, false, null );
 
         String repositoryId = "nexus1329-repo";
+        File artifactFile = this.downloadArtifactFromRepository( repositoryId, gav, "./target/downloads/nexus1329" );
 
-        // download file from repo
-        File artifactFile = this.downloadArtifactFromRepository( repositoryId, gav, "./target/downloadedFiles/repo" );
-        Assert.assertNotNull( artifactFile );
-        Assert.assertTrue( artifactFile.exists() );
-
-        // if you want to check the downloaded file against a known file you can compare the SHA1's
-        Assert.assertTrue( FileTestingUtils.compareFileSHA1s( this.getLocalFile(
-            this.getTestId() + "/nexus1329-repo",
-            gav ), artifactFile ) );
-
-        // download file from group
-        artifactFile = this.downloadArtifactFromGroup( "nexus1329-group", gav, "./target/downloadedFiles/group" );
+        Assert.assertTrue( FileTestingUtils.compareFileSHA1s(
+                                                              this.getTestFile( "basic/nexus1329/sample/1.0.0/sample-1.0.0.xml" ),
+                                                              artifactFile ) );
     }
 
+    @After
+    public void stop()
+        throws Exception
+    {
+        server.stop();
+    }
 }
