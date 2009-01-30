bee368c398c75103a95879c5e83b16bf0a440d40
Tamas Cservenak <cstamas@sonatype.com>
2009-01-30 21:12:28 +0000

    NEXUS-1500: added content generator feature
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@3203 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

---
+++ nexus/nexus-api/src/main/java/org/sonatype/nexus/proxy/item/ContentGenerator.java
@@ -0,0 +1,22 @@
+package org.sonatype.nexus.proxy.item;
+
+import org.sonatype.nexus.proxy.IllegalOperationException;
+import org.sonatype.nexus.proxy.ItemNotFoundException;
+import org.sonatype.nexus.proxy.StorageException;
+import org.sonatype.nexus.proxy.repository.Repository;
+
+/**
+ * A content generator is a special component, that is able to generate content on-the-fly, that will be substituted
+ * with the content coming from the Local Storage.
+ * 
+ * @author cstamas
+ */
+public interface ContentGenerator
+{
+    public static final String CONTENT_GENERATOR_ID = "contentGenerator";
+
+    ContentLocator generateContent( Repository repository, String path, StorageFileItem item )
+        throws IllegalOperationException,
+            ItemNotFoundException,
+            StorageException;
+}
