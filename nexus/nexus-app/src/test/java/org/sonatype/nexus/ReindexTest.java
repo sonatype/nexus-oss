96fd284344cf3668b18fd39956a56c11414377a7
Tamas Cservenak <cstamas@sonatype.com>
2009-02-06 16:31:39 +0000

    Restored V1 indexes for tests, and added new "v1" test using those. Now, we have "v1" tests and "current indexer" test.
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@3279 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-app/src/test/java/org/sonatype/nexus/ReindexTest.java
+++ nexus/nexus-app/src/test/java/org/sonatype/nexus/ReindexTest.java
@@ -15,8 +15,15 @@ package org.sonatype.nexus;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Date;
 
-import org.apache.lucene.store.RAMDirectory;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.codehaus.plexus.util.FileUtils;
 import org.sonatype.jettytestsuite.ServletServer;
 import org.sonatype.nexus.configuration.model.CRepository;
 import org.sonatype.nexus.index.ArtifactInfo;
@@ -28,6 +35,8 @@ import org.sonatype.nexus.index.packer.IndexPackingRequest;
 public class ReindexTest
     extends AbstractMavenRepoContentTests
 {
+    public static final long A_DAY_MILLIS = 24 * 60 * 60 * 1000;
+
     private ServletServer servletServer;
 
     private NexusIndexer nexusIndexer;
@@ -59,7 +68,6 @@ public class ReindexTest
     protected void makeCentralPointTo( String url )
         throws Exception
     {
-
         CRepository repoConfig = defaultNexus.readRepository( "central" );
 
         // redirect it to our "sppof" jetty (see ReindexTest.xml in src/test/resources....
@@ -73,23 +81,106 @@ public class ReindexTest
         defaultNexus.updateRepository( repoConfig );
     }
 
-    protected void reindexRemoteRepositoryAndPublish( String repositoryId )
+    protected File getIndexFamilyDirectory( String path )
+    {
+        File indexDirectory = new File( new File( getBasedir() ), "target/indexFamily/" + path );
+
+        return indexDirectory;
+    }
+
+    protected File getRemoteRepositoryRoot( String path )
+    {
+        // Be aware, that "name" != "repoId"! For example, "central-inc1", "central-inc2"... are all "slices" of
+        // "central" repo in different time!
+        File root = new File( new File( getBasedir() ), "target/test-classes/reposes-remote/" + path );
+
+        return root;
+    }
+
+    protected void shiftContextInTime( IndexingContext ctx, int shiftDays )
         throws IOException
     {
-        File root = new File( new File( getBasedir() ), "target/test-classes/reposes-remote/" + repositoryId );
-        
+        if ( shiftDays != 0 )
+        {
+            IndexWriter iw = ctx.getIndexWriter();
+
+            for ( int docNum = 0; docNum < ctx.getIndexReader().maxDoc(); docNum++ )
+            {
+                if ( !ctx.getIndexReader().isDeleted( docNum ) )
+                {
+                    Document doc = ctx.getIndexReader().document( docNum );
+
+                    String lastModified = doc.get( ArtifactInfo.LAST_MODIFIED );
+
+                    if ( lastModified != null )
+                    {
+                        long lm = Long.parseLong( lastModified );
+
+                        lm = lm + ( shiftDays * A_DAY_MILLIS );
+
+                        doc.removeFields( ArtifactInfo.LAST_MODIFIED );
+
+                        doc.add( new Field(
+                            ArtifactInfo.LAST_MODIFIED,
+                            Long.toString( lm ),
+                            Field.Store.YES,
+                            Field.Index.NO ) );
+
+                        iw.updateDocument( new Term( ArtifactInfo.UINFO, doc.get( ArtifactInfo.UINFO ) ), doc );
+                    }
+                }
+            }
+
+            iw.optimize();
+
+            iw.close();
+
+            // shift timestamp too
+            if ( ctx.getTimestamp() != null )
+            {
+                ctx.updateTimestamp( true, new Date( ctx.getTimestamp().getTime() + ( shiftDays * A_DAY_MILLIS ) ) );
+            }
+            else
+            {
+                ctx.updateTimestamp( true, new Date( System.currentTimeMillis() + ( shiftDays * A_DAY_MILLIS ) ) );
+            }
+        }
+    }
+
+    /**
+     * Will reindex, shift if needed and publish indexes for a "remote" repository (published over jetty component).
+     * 
+     * @param repositoryRoot
+     * @param repositoryId
+     * @param deleteIndexFiles
+     * @param shiftDays
+     * @throws IOException
+     */
+    protected void reindexRemoteRepositoryAndPublish( File repositoryRoot, String repositoryId,
+        boolean deleteIndexFiles, int shiftDays )
+        throws IOException
+    {
+        File indexDirectory = getIndexFamilyDirectory( repositoryId );
+
+        Directory directory = FSDirectory.getDirectory( indexDirectory );
+
         IndexingContext ctx = nexusIndexer.addIndexingContextForced(
-            repositoryId + "-temp",
             repositoryId,
-            root,
-            new RAMDirectory(),
+            repositoryId,
+            repositoryRoot,
+            directory,
             null,
             null,
             NexusIndexer.FULL_INDEX );
 
-        nexusIndexer.scan( ctx );
+        // shifting if needed (very crude way to do it, but heh)
+        shiftContextInTime( ctx, shiftDays );
 
-        File targetDir = new File( root, ".index" );
+        // and scan "today"
+        nexusIndexer.scan( ctx, null, true );
+
+        // pack it up
+        File targetDir = new File( repositoryRoot, ".index" );
 
         targetDir.mkdirs();
 
@@ -97,7 +188,7 @@ public class ReindexTest
 
         indexPacker.packIndex( ipr );
 
-        nexusIndexer.removeIndexingContext( ctx, true );
+        nexusIndexer.removeIndexingContext( ctx, deleteIndexFiles );
     }
 
     protected void validateIndexWithIdentify( boolean shouldBePresent, String sha1Hash, String gid, String aid,
@@ -140,8 +231,8 @@ public class ReindexTest
     {
         fillInRepo();
 
-        reindexRemoteRepositoryAndPublish( "central" );
-        
+        reindexRemoteRepositoryAndPublish( getRemoteRepositoryRoot( "central" ), "central", true, 0 );
+
         makeCentralPointTo( "http://localhost:12345/central/" );
 
         defaultNexus.reindexRepository( null, "central" );
@@ -154,8 +245,8 @@ public class ReindexTest
     {
         fillInRepo();
 
-        reindexRemoteRepositoryAndPublish( "central" );
-        
+        reindexRemoteRepositoryAndPublish( getRemoteRepositoryRoot( "central" ), "central", true, 0 );
+
         makeCentralPointTo( "http://localhost:12345/central/" );
 
         // central is member of public group
@@ -164,14 +255,17 @@ public class ReindexTest
         validateIndexWithIdentify( true, "057b8740427ee6d7b0b60792751356cad17dc0d9", "log4j", "log4j", "1.2.12" );
     }
 
-    public void testIncrementalIndexes()
+    public void testCurrentIncrementalIndexes()
         throws Exception
     {
+        // clean up 1st
+        FileUtils.deleteDirectory( getIndexFamilyDirectory( "central" ) );
+
         // day 1
-        reindexRemoteRepositoryAndPublish( "central-inc1" );
+        reindexRemoteRepositoryAndPublish( getRemoteRepositoryRoot( "central-inc1" ), "central", false, 0 );
 
         makeCentralPointTo( "http://localhost:12345/central-inc1/" );
-        
+
         defaultNexus.reindexRepository( null, "central" );
 
         // validation
@@ -182,8 +276,9 @@ public class ReindexTest
         validateIndexWithIdentify( false, "057b8740427ee6d7b0b60792751356cad17dc0d9", "log4j", "log4j", "1.2.12" );
         validateIndexWithIdentify( false, "f0a0d2e29ed910808c33135a3a5a51bba6358f7b", "log4j", "log4j", "1.2.15" );
 
-        // day 2
-        reindexRemoteRepositoryAndPublish( "central-inc2" );
+        // day 2 (1 day passed), so shift both ctxes "in time"
+        reindexRemoteRepositoryAndPublish( getRemoteRepositoryRoot( "central-inc2" ), "central", false, -1 );
+        shiftContextInTime( defaultNexus.getRepositoryRemoteIndexContext( "central" ), -1 );
 
         makeCentralPointTo( "http://localhost:12345/central-inc2/" );
 
@@ -198,9 +293,53 @@ public class ReindexTest
         validateIndexWithIdentify( false, "f0a0d2e29ed910808c33135a3a5a51bba6358f7b", "log4j", "log4j", "1.2.15" );
 
         // day 3
-        reindexRemoteRepositoryAndPublish( "central-inc3" );
+        reindexRemoteRepositoryAndPublish( getRemoteRepositoryRoot( "central-inc3" ), "central", false, -1 );
+        shiftContextInTime( defaultNexus.getRepositoryRemoteIndexContext( "central" ), -1 );
 
         makeCentralPointTo( "http://localhost:12345/central-inc3/" );
+
+        defaultNexus.reindexRepository( null, "central" );
+
+        // validation
+        validateIndexWithIdentify( true, "cf4f67dae5df4f9932ae7810f4548ef3e14dd35e", "antlr", "antlr", "2.7.6" );
+        validateIndexWithIdentify( true, "83cd2cd674a217ade95a4bb83a8a14f351f48bd0", "antlr", "antlr", "2.7.7" );
+
+        validateIndexWithIdentify( true, "3640dd71069d7986c9a14d333519216f4ca5c094", "log4j", "log4j", "1.2.8" );
+        validateIndexWithIdentify( true, "057b8740427ee6d7b0b60792751356cad17dc0d9", "log4j", "log4j", "1.2.12" );
+        validateIndexWithIdentify( true, "f0a0d2e29ed910808c33135a3a5a51bba6358f7b", "log4j", "log4j", "1.2.15" );
+    }
+
+    public void testV1IncrementalIndexes()
+        throws Exception
+    {
+        // day 1
+        makeCentralPointTo( "http://localhost:12345/central-inc1-v1/" );
+
+        defaultNexus.reindexRepository( null, "central" );
+
+        // validation
+        validateIndexWithIdentify( true, "cf4f67dae5df4f9932ae7810f4548ef3e14dd35e", "antlr", "antlr", "2.7.6" );
+        validateIndexWithIdentify( false, "83cd2cd674a217ade95a4bb83a8a14f351f48bd0", "antlr", "antlr", "2.7.7" );
+
+        validateIndexWithIdentify( true, "3640dd71069d7986c9a14d333519216f4ca5c094", "log4j", "log4j", "1.2.8" );
+        validateIndexWithIdentify( false, "057b8740427ee6d7b0b60792751356cad17dc0d9", "log4j", "log4j", "1.2.12" );
+        validateIndexWithIdentify( false, "f0a0d2e29ed910808c33135a3a5a51bba6358f7b", "log4j", "log4j", "1.2.15" );
+
+        // day 2
+        makeCentralPointTo( "http://localhost:12345/central-inc2-v1/" );
+
+        defaultNexus.reindexRepository( null, "central" );
+
+        // validation
+        validateIndexWithIdentify( true, "cf4f67dae5df4f9932ae7810f4548ef3e14dd35e", "antlr", "antlr", "2.7.6" );
+        validateIndexWithIdentify( true, "83cd2cd674a217ade95a4bb83a8a14f351f48bd0", "antlr", "antlr", "2.7.7" );
+
+        validateIndexWithIdentify( true, "3640dd71069d7986c9a14d333519216f4ca5c094", "log4j", "log4j", "1.2.8" );
+        validateIndexWithIdentify( true, "057b8740427ee6d7b0b60792751356cad17dc0d9", "log4j", "log4j", "1.2.12" );
+        validateIndexWithIdentify( false, "f0a0d2e29ed910808c33135a3a5a51bba6358f7b", "log4j", "log4j", "1.2.15" );
+
+        // day 3
+        makeCentralPointTo( "http://localhost:12345/central-inc3-v1/" );
 
         defaultNexus.reindexRepository( null, "central" );
 
