f61d0790bdceca6223eb47639a07e7c8d4a245d5
Damian Bradicich <dbradicich@sonatype.com>
2009-02-10 19:36:19 +0000

    NEXUS-1417: completed task, repository metadata module is failing w/ NPE though
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@3315 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-rest-api/src/main/java/org/sonatype/nexus/rest/mirrors/RepositoryPredefinedMirrorListPlexusResource.java
+++ nexus/nexus-rest-api/src/main/java/org/sonatype/nexus/rest/mirrors/RepositoryPredefinedMirrorListPlexusResource.java
@@ -1,14 +1,23 @@
 package org.sonatype.nexus.rest.mirrors;
 
+import java.io.IOException;
+import java.util.List;
+
 import org.codehaus.plexus.component.annotations.Component;
+import org.codehaus.plexus.component.annotations.Requirement;
 import org.restlet.Context;
 import org.restlet.data.Request;
 import org.restlet.data.Response;
+import org.restlet.data.Status;
 import org.restlet.resource.ResourceException;
 import org.restlet.resource.Variant;
+import org.sonatype.nexus.proxy.NoSuchRepositoryException;
+import org.sonatype.nexus.proxy.repository.metadata.NexusRepositoryMetadataHandler;
+import org.sonatype.nexus.repository.metadata.MetadataHandlerException;
+import org.sonatype.nexus.repository.metadata.model.RepositoryMetadata;
+import org.sonatype.nexus.repository.metadata.model.RepositoryMirrorMetadata;
 import org.sonatype.nexus.rest.model.MirrorResource;
 import org.sonatype.nexus.rest.model.MirrorResourceListResponse;
-import org.sonatype.nexus.util.StringDigester;
 import org.sonatype.plexus.rest.resource.PathProtectionDescriptor;
 import org.sonatype.plexus.rest.resource.PlexusResource;
 
@@ -16,6 +25,9 @@ import org.sonatype.plexus.rest.resource.PlexusResource;
 public class RepositoryPredefinedMirrorListPlexusResource
     extends AbstractRepositoryMirrorPlexusResource
 {
+    @Requirement
+    NexusRepositoryMetadataHandler repoMetadata;
+    
     public RepositoryPredefinedMirrorListPlexusResource()
     {
         setModifiable( false );
@@ -44,23 +56,33 @@ public class RepositoryPredefinedMirrorListPlexusResource
         throws ResourceException
     {
         MirrorResourceListResponse dto = new MirrorResourceListResponse();
-        
-        //TODO: retrieve a list of mirrors from somewhere
-        
-        MirrorResource resource = new MirrorResource();
-        resource.setUrl( "http://someurl" );
-        resource.setId( StringDigester.getSha1Digest( resource.getUrl() ) );
-        dto.addData( resource );
-        
-        resource = new MirrorResource();
-        resource.setUrl( "http://someurl2" );
-        resource.setId( StringDigester.getSha1Digest( resource.getUrl() ) );
-        dto.addData( resource );
-        
-        resource = new MirrorResource();
-        resource.setUrl( "http://someurl3" );
-        resource.setId( StringDigester.getSha1Digest( resource.getUrl() ) );
-        dto.addData( resource );
+    
+        try
+        {
+            RepositoryMetadata metadata = repoMetadata.readRepositoryMetadata( getRepositoryId( request ) );
+            
+            for ( RepositoryMirrorMetadata mirror : ( List<RepositoryMirrorMetadata> ) metadata.getMirrors() )
+            {
+                MirrorResource resource = new MirrorResource();
+                resource.setId( mirror.getId() );
+                resource.setUrl( mirror.getUrl() );
+                dto.addData( resource );
+            }
+        }
+        catch ( NoSuchRepositoryException e )
+        {
+            getLogger().error( "Unable to retrieve metadata", e );
+            throw new ResourceException( Status.CLIENT_ERROR_BAD_REQUEST, "Invalid repository ID", e );
+        }
+        catch ( MetadataHandlerException e )
+        {
+            getLogger().error( "Unable to retrieve metadata, returning no items", e );
+        }
+        catch ( IOException e )
+        {
+            getLogger().error( "Unable to retrieve metadata", e );
+            throw new ResourceException( Status.SERVER_ERROR_INTERNAL, "Metadata handling error", e );
+        }
         
         return dto;
     }
