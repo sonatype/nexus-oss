1d0b3bccea305a2aac01f5ace23ce3a3725ccfb7
Brian Demers <bdemers@sonatype.com>
2009-02-11 20:25:42 +0000

    rolling back
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@3335 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-app/src/test/java/org/sonatype/nexus/AbstractNexusTestCase.java
+++ nexus/nexus-app/src/test/java/org/sonatype/nexus/AbstractNexusTestCase.java
@@ -27,7 +27,6 @@ import org.codehaus.plexus.logging.LoggerManager;
 import org.codehaus.plexus.util.FileUtils;
 import org.codehaus.plexus.util.IOUtil;
 import org.sonatype.nexus.configuration.application.NexusConfiguration;
-import org.sonatype.nexus.proxy.cache.CacheManager;
 
 public abstract class AbstractNexusTestCase
     extends PlexusTestCase
@@ -47,8 +46,6 @@ public abstract class AbstractNexusTestCase
     protected static final File CONF_HOME = new File( WORK_HOME, "conf" );
 
     protected NexusConfiguration nexusConfiguration;
-    
-    protected CacheManager cacheManager;
 
     protected void customizeContext( Context ctx )
     {
@@ -106,8 +103,6 @@ public abstract class AbstractNexusTestCase
         throws Exception
     {
         super.setUp();
-        
-        this.cacheManager = this.lookup( CacheManager.class );
 
         FileUtils.deleteDirectory( PLEXUS_HOME );
 
@@ -132,7 +127,6 @@ public abstract class AbstractNexusTestCase
         throws Exception
     {
         super.tearDown();
-        this.cacheManager.stopService();
     }
 
     protected LoggerManager getLoggerManager() throws ComponentLookupException
