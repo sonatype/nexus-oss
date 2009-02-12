70969fa74c7136422bb01c5a20ee72878a349221
Brian Demers <bdemers@sonatype.com>
2009-02-12 04:27:25 +0000

    NEXUS-1542, removed Startable interface
    git-svn-id: file:///opt/svn/repositories/sonatype.org/spice/trunk/plexus-ehcache@878 5751e0cb-dcb7-432f-92e2-260806df54be

--- src/main/java/org/sonatype/plexus/components/ehcache/PlexusEhCacheWrapper.java
+++ src/main/java/org/sonatype/plexus/components/ehcache/PlexusEhCacheWrapper.java
@@ -2,10 +2,17 @@ package org.sonatype.plexus.components.ehcache;
 
 import net.sf.ehcache.CacheManager;
 
-import org.codehaus.plexus.personality.plexus.lifecycle.phase.Startable;
+import org.codehaus.plexus.personality.plexus.lifecycle.phase.StartingException;
+import org.codehaus.plexus.personality.plexus.lifecycle.phase.StoppingException;
 
-public interface PlexusEhCacheWrapper extends Startable
+public interface PlexusEhCacheWrapper
 {
     
     CacheManager getEhCacheManager();
+    
+    public void start()
+    throws StartingException;
+    
+    public void stop()
+    throws StoppingException;
 }
