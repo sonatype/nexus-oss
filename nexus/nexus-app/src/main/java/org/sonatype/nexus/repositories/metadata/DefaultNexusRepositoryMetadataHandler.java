5a9f43856675c3bc08c3fb63b417be890d96a032
Tamas Cservenak <cstamas@sonatype.com>
2009-02-19 21:02:50 +0000

    Fixed transparent proxying for repo md
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@3416 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-app/src/main/java/org/sonatype/nexus/repositories/metadata/DefaultNexusRepositoryMetadataHandler.java
+++ nexus/nexus-app/src/main/java/org/sonatype/nexus/repositories/metadata/DefaultNexusRepositoryMetadataHandler.java
@@ -41,7 +41,7 @@ public class DefaultNexusRepositoryMetadataHandler
     {
         Repository repository = repositoryRegistry.getRepository( repositoryId );
 
-        NexusRawTransport nrt = new NexusRawTransport( repository, true, false );
+        NexusRawTransport nrt = new NexusRawTransport( repository, false, false );
 
         return repositoryMetadataHandler.readRepositoryMetadata( nrt );
     }
