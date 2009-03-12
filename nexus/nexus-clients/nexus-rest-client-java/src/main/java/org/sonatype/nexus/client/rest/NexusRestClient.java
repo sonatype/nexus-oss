0ec6a9bfc6a16df222c5c25ceea08e266ba4f114
Juven Xu <juven@sonatype.com>
2009-03-12 13:12:26 +0000

    [NEXUS-1648] kill annoying JUL in rest-client
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@3633 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-clients/nexus-rest-client-java/src/main/java/org/sonatype/nexus/client/rest/NexusRestClient.java
+++ nexus/nexus-clients/nexus-rest-client-java/src/main/java/org/sonatype/nexus/client/rest/NexusRestClient.java
@@ -16,6 +16,8 @@ package org.sonatype.nexus.client.rest;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Handler;
+import java.util.logging.LogManager;
 
 import org.apache.log4j.Logger;
 import org.codehaus.plexus.component.annotations.Component;
@@ -45,6 +47,19 @@ public class NexusRestClient
     private Logger logger = Logger.getLogger( getClass() );
 
     public static final String WAIT_FOR_START_TIMEOUT_KEY = "nexus.client.started.timeout";
+    
+    public NexusRestClient()
+    {
+        // kill all JUL 
+        java.util.logging.Logger julLogger = LogManager.getLogManager().getLogger( "" );
+        
+        Handler[] handlers = julLogger.getHandlers();
+        
+        for ( Handler handler : handlers)
+        {
+            julLogger.removeHandler( handler );
+        }
+    }
 
     public void connect( String baseUrl, String username, String password )
     {
