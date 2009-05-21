c1d749c56d18bf0accdf7e5a68967fd0d237a6bf
Tamas Cservenak <cstamas@sonatype.com>
2009-05-21 13:58:42 +0000

    Marked extPoint
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@4216 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-rest-api/src/main/java/org/sonatype/nexus/rest/NexusApplicationCustomizer.java
+++ nexus/nexus-rest-api/src/main/java/org/sonatype/nexus/rest/NexusApplicationCustomizer.java
@@ -1,7 +1,9 @@
 package org.sonatype.nexus.rest;
 
+import org.sonatype.nexus.plugins.ExtensionPoint;
 import org.sonatype.plexus.rest.RetargetableRestlet;
 
+@ExtensionPoint
 public interface NexusApplicationCustomizer
 {
     void customize( NexusApplication nexusApplication, RetargetableRestlet root );
