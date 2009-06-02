3d66d76ad5fe597394f7eec8680102b24b1d03bf
Tamas Cservenak <cstamas@sonatype.com>
2009-06-02 11:48:09 +0000

    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@4302 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-plugins/nexus-plugin-api/src/main/java/org/sonatype/nexus/plugins/RepositoryType.java
+++ nexus/nexus-plugins/nexus-plugin-api/src/main/java/org/sonatype/nexus/plugins/RepositoryType.java
@@ -2,7 +2,6 @@ package org.sonatype.nexus.plugins;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
-import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -14,7 +13,6 @@ import java.lang.annotation.Target;
  * @author cstamas
  */
 @Documented
-@Inherited
 @Target( ElementType.TYPE )
 @Retention( RetentionPolicy.RUNTIME )
 public @interface RepositoryType
