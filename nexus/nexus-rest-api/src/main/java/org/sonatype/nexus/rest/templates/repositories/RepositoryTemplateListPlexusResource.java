53587595f017536d6a976650ffb731f86970c487
Tamas Cservenak <cstamas@sonatype.com>
2009-06-16 21:16:35 +0000

    NEXUS-1993: tied into rest-api
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@4412 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-rest-api/src/main/java/org/sonatype/nexus/rest/templates/repositories/RepositoryTemplateListPlexusResource.java
+++ nexus/nexus-rest-api/src/main/java/org/sonatype/nexus/rest/templates/repositories/RepositoryTemplateListPlexusResource.java
@@ -13,27 +13,24 @@
  */
 package org.sonatype.nexus.rest.templates.repositories;
 
+import java.util.List;
+
 import org.codehaus.plexus.component.annotations.Component;
 import org.restlet.Context;
 import org.restlet.data.Request;
 import org.restlet.data.Response;
-import org.restlet.data.Status;
 import org.restlet.resource.ResourceException;
 import org.restlet.resource.Variant;
-import org.sonatype.nexus.configuration.model.CRepository;
+import org.sonatype.nexus.proxy.maven.AbstractMavenRepositoryConfiguration;
 import org.sonatype.nexus.proxy.repository.GroupRepository;
 import org.sonatype.nexus.proxy.repository.HostedRepository;
 import org.sonatype.nexus.proxy.repository.ProxyRepository;
 import org.sonatype.nexus.proxy.repository.Repository;
 import org.sonatype.nexus.proxy.repository.ShadowRepository;
 import org.sonatype.nexus.rest.AbstractNexusPlexusResource;
-import org.sonatype.nexus.rest.model.RepositoryBaseResource;
-import org.sonatype.nexus.rest.model.RepositoryGroupResource;
-import org.sonatype.nexus.rest.model.RepositoryProxyResource;
-import org.sonatype.nexus.rest.model.RepositoryResource;
+import org.sonatype.nexus.rest.model.RepositoryListResource;
+import org.sonatype.nexus.rest.model.RepositoryListResourceResponse;
 import org.sonatype.nexus.rest.model.RepositoryResourceResponse;
-import org.sonatype.nexus.rest.model.RepositoryShadowResource;
-import org.sonatype.nexus.templates.NoSuchTemplateIdException;
 import org.sonatype.nexus.templates.Template;
 import org.sonatype.nexus.templates.repository.RepositoryTemplate;
 import org.sonatype.plexus.rest.resource.PathProtectionDescriptor;
@@ -42,13 +39,10 @@ import org.sonatype.plexus.rest.resource.PlexusResource;
 /**
  * @author tstevens
  */
-@Component( role = PlexusResource.class, hint = "RepositoryTemplatePlexusResource" )
-public class RepositoryTemplatePlexusResource
+@Component( role = PlexusResource.class, hint = "RepositoryTemplateListPlexusResource" )
+public class RepositoryTemplateListPlexusResource
     extends AbstractNexusPlexusResource
 {
-    /* * Key to store Repo with which we work against. */
-    public static final String REPOSITORY_ID_KEY = "repositoryId";
-
     @Override
     public Object getPayloadInstance()
     {
@@ -58,80 +52,83 @@ public class RepositoryTemplatePlexusResource
     @Override
     public String getResourceUri()
     {
-        return "/templates/repositories/{" + REPOSITORY_ID_KEY + "}";
+        return "/templates/repositories";
     }
 
     @Override
     public PathProtectionDescriptor getResourceProtection()
     {
-        return new PathProtectionDescriptor( "/templates/repositories/*", "authcBasic,perms[nexus:repotemplates]" );
-    }
-
-    protected String getRepositoryId( Request request )
-    {
-        return request.getAttributes().get( REPOSITORY_ID_KEY ).toString();
+        return new PathProtectionDescriptor( getResourceUri(), "authcBasic,perms[nexus:repotemplates]" );
     }
 
     @Override
     public Object get( Context context, Request request, Response response, Variant variant )
         throws ResourceException
     {
-        RepositoryResourceResponse result = new RepositoryResourceResponse();
+        RepositoryListResourceResponse result = new RepositoryListResourceResponse();
 
-        try
+        RepositoryListResource repoRes;
+
+        List<Template<Repository>> repoTemplates = getNexus().getRepositoryTemplates();
+
+        for ( Template<Repository> genTemplate : repoTemplates )
         {
-            Template<Repository> genTemplate = getNexus().getRepositoryTemplateById( getRepositoryId( request ) );
-
             // TODO: a hack, fix it
             RepositoryTemplate template = (RepositoryTemplate) genTemplate;
 
-            RepositoryBaseResource repoRes = null;
+            repoRes = new RepositoryListResource();
 
-            if ( ProxyRepository.class.isAssignableFrom( template.getMainFacet() ) )
-            {
-                repoRes = new RepositoryProxyResource();
-
-                repoRes.setRepoType( "proxy" );
-            }
-            else if ( HostedRepository.class.isAssignableFrom( template.getMainFacet() ) )
-            {
-                repoRes = new RepositoryResource();
-
-                repoRes.setRepoType( "hosted" );
-            }
-            else if ( ShadowRepository.class.isAssignableFrom( template.getMainFacet() ) )
-            {
-                repoRes = new RepositoryShadowResource();
-
-                repoRes.setRepoType( "virtual" );
-            }
-            else if ( GroupRepository.class.isAssignableFrom( template.getMainFacet() ) )
-            {
-                repoRes = new RepositoryGroupResource();
-
-                repoRes.setRepoType( "group" );
-            }
-            else
-            {
-                // huh?
-                throw new ResourceException( Status.SERVER_ERROR_INTERNAL, "Unrecognized repository template with ID='"
-                    + template.getId() + "' and mainFacet='" + template.getMainFacet().getName() + "'!" );
-            }
+            repoRes.setResourceURI( createChildReference( request, this, template.getId() ).toString() );
 
             repoRes.setId( template.getId() );
 
             repoRes.setName( template.getDescription() );
 
-            repoRes.setProvider( ( (CRepository) template.getCoreConfiguration().getConfiguration( false ) )
-                .getProviderHint() );
+            if ( ProxyRepository.class.isAssignableFrom( template.getMainFacet() ) )
+            {
+                repoRes.setRepoType( "proxy" );
+            }
+            else if ( HostedRepository.class.isAssignableFrom( template.getMainFacet() ) )
+            {
+                repoRes.setRepoType( "hosted" );
+            }
+            else if ( ShadowRepository.class.isAssignableFrom( template.getMainFacet() ) )
+            {
+                repoRes.setRepoType( "virtual" );
+            }
+            else if ( GroupRepository.class.isAssignableFrom( template.getMainFacet() ) )
+            {
+                repoRes.setRepoType( "group" );
+            }
+            else
+            {
+                // huh?
+                repoRes.setRepoType( template.getMainFacet().getName() );
+            }
 
+            // policy
+            // another hack
+            if ( template.getExternalConfiguration() instanceof AbstractMavenRepositoryConfiguration )
+            {
+                repoRes.setRepoPolicy( ( (AbstractMavenRepositoryConfiguration) template.getExternalConfiguration() )
+                    .getRepositoryPolicy().toString() );
+            }
+
+            // format
             repoRes.setFormat( template.getContentClass().getId() );
 
-            result.setData( repoRes );
-        }
-        catch ( NoSuchTemplateIdException e )
-        {
-            throw new ResourceException( Status.CLIENT_ERROR_NOT_FOUND, e.getMessage() );
+            // userManaged
+            repoRes.setUserManaged( template.getConfigurableRepository().isUserManaged() );
+
+            // exposed
+            repoRes.setExposed( template.getConfigurableRepository().isExposed() );
+
+            // ==
+            // below are not used for templates (and does not make any sense)
+            // effectiveLocalStorageUrl
+            // remoteUri
+
+            result.addData( repoRes );
         }
 
         return result;
