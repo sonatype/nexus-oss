de2bef31fd96c8c874f06722dc9f2cd0f2c8b128
Tamas Cservenak <cstamas@sonatype.com>
2009-06-28 17:17:05 +0000

    Removed old test plugin, added new ones, and fixed test case to "mimic" the bundle
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@4506 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-plugins/nexus-plugin-manager/src/test/java/org/sonatype/nexus/plugins/AbstractNexusPluginManagerTest.java
+++ nexus/nexus-plugins/nexus-plugin-manager/src/test/java/org/sonatype/nexus/plugins/AbstractNexusPluginManagerTest.java
@@ -1,17 +1,47 @@
 package org.sonatype.nexus.plugins;
 
+import org.codehaus.plexus.ContainerConfiguration;
 import org.codehaus.plexus.PlexusTestCase;
+import org.codehaus.plexus.classworlds.ClassWorld;
+import org.codehaus.plexus.classworlds.realm.ClassRealm;
+import org.codehaus.plexus.classworlds.realm.DuplicateRealmException;
 import org.codehaus.plexus.context.Context;
 
 public abstract class AbstractNexusPluginManagerTest
     extends PlexusTestCase
 {
+    protected ClassWorld classWorld;
+    
     protected DefaultNexusPluginManager nexusPluginManager;
 
     @Override
     protected void customizeContext( Context context )
     {
         context.put( "nexus-work", getTestFile( "src/test" ).getAbsolutePath() );
+    }
+
+    @Override
+    protected void customizeContainerConfiguration( ContainerConfiguration config )
+    {
+        classWorld = new ClassWorld();
+        
+        try
+        {
+            ClassRealm realm = classWorld.newRealm( "plexus.core", Thread.currentThread().getContextClassLoader() );
+
+            Thread.currentThread().setContextClassLoader( realm );
+        }
+        catch ( DuplicateRealmException e )
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            
+            fail();
+        }
+
+        config.setClassWorld( classWorld );
+        
+        config.setComponentRepository( new NexusPluginsComponentRepository() );
     }
 
     protected void setUp()
--- nexus/nexus-plugins/nexus-plugin-manager/src/test/java/org/sonatype/nexus/plugins/DefaultNexusPluginManagerTest.java
+++ nexus/nexus-plugins/nexus-plugin-manager/src/test/java/org/sonatype/nexus/plugins/DefaultNexusPluginManagerTest.java
@@ -17,7 +17,7 @@ public class DefaultNexusPluginManagerTest
         // do discovery, both recorded value should be +1
         PluginManagerResponse pmresponse = nexusPluginManager.activateInstalledPlugins();
 
-        assertEquals( "One plugin should be discovered!", 1, pmresponse.getProcessedPluginResponses().size() );
+        assertEquals( "Two plugins should be discovered!", 2, pmresponse.getProcessedPluginResponses().size() );
         assertEquals( "Should be okay!", RequestResult.COMPLETELY_EXECUTED, pmresponse.getResult() );
 
         // record post-discovery state
@@ -29,16 +29,26 @@ public class DefaultNexusPluginManagerTest
         assertTrue( "The map should grow!", processorsPre < processorsPost );
 
         // lookup the virus customizer from plugin
-        Object virusScanner =
-            lookup( "org.sonatype.nexus.plugins.RepositoryCustomizer",
-                    "org.sonatype.nexus.plugins.sample.virusscanner.VirusScannerRepositoryCustomizer" );
+        Object virusScanner = lookup( "org.sonatype.nexus.plugin.samples.kungfu.VirusScanner", "XY" );
         assertNotNull( virusScanner );
 
-        // now destroy
-        PluginResponse presponse =
-            nexusPluginManager.deactivatePlugin( pmresponse.getProcessedPluginCoordinates().get( 0 ) );
+        // lookup the collector from other plugin
+        Object infectedFilesCollector = lookup( "org.sonatype.nexus.plugin.samples.interdep.InfectedFilesCollector" );
+        assertNotNull( infectedFilesCollector );
 
-        assertEquals( "Should be succesful!", true, presponse.isSuccesful() );
+        // lookup the collector from other plugin
+        Object infectedFilesCollectorFeedSource =
+            lookup( "org.sonatype.nexus.rest.feeds.sources.FeedSource",
+                    "org.sonatype.nexus.plugin.samples.interdep.InfectedItemsFeedSource" );
+
+        // now destroy
+        PluginResponse presponse1 =
+            nexusPluginManager.deactivatePlugin( pmresponse.getProcessedPluginCoordinates().get( 0 ) );
+        PluginResponse presponse2 =
+            nexusPluginManager.deactivatePlugin( pmresponse.getProcessedPluginCoordinates().get( 1 ) );
+
+        assertEquals( "Should be succesful!", true, presponse1.isSuccesful() );
+        assertEquals( "Should be succesful!", true, presponse2.isSuccesful() );
 
         // record post-destroy state
         int customizersDestroy = mc.getCustomizers().size();
