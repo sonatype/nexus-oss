2ec18a1d7762048820f674e3aa4d36dad40789ae
Marvin Froeder <velo@sonatype.com>
2009-07-02 20:08:02 +0000

    OPEN - issue NEXUS-2239: Include firebug lite https://issues.sonatype.org/browse/NEXUS-2239
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@4521 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

---
+++ nexus/nexus-test-harness/nexus-test-harness-selenium/src/main/java/org/sonatype/nexus/mock/firebug/FirebugNexusResourceBundle.java
@@ -0,0 +1,21 @@
+package org.sonatype.nexus.mock.firebug;
+
+import java.util.Map;
+
+import org.codehaus.plexus.component.annotations.Component;
+import org.sonatype.nexus.plugins.rest.AbstractNexusResourceBundle;
+import org.sonatype.nexus.plugins.rest.NexusResourceBundle;
+
+@Component( role = NexusResourceBundle.class, hint = "FirebugNexusResourceBundle" )
+public class FirebugNexusResourceBundle
+    extends AbstractNexusResourceBundle
+    implements NexusResourceBundle
+{
+
+    @Override
+    public String getPreHeadContribution( Map<String, Object> context )
+    {
+        return "<script src=\"firebug-lite-1.2-compressed.js?\" type=\"text/javascript\" charset=\"utf-8\"></script>";
+    }
+
+}
