85e8a5961b6a54455f0b632a9fa979b4166e53e3
Tamas Cservenak <cstamas@sonatype.com>
2009-07-16 17:19:30 +0000

    NXCM-970: fixed
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@4627 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

---
+++ nexus/nexus-test-harness/nexus-test-harness-launcher/src/test/java/org/sonatype/nexus/integrationtests/nxcm970/Nxcm970SimultaneousUploadDownloadTest.java
@@ -0,0 +1,88 @@
+package org.sonatype.nexus.integrationtests.nxcm970;
+
+import java.io.IOException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.sonatype.nexus.integrationtests.AbstractNexusIntegrationTest;
+
+public class Nxcm970SimultaneousUploadDownloadTest
+    extends AbstractNexusIntegrationTest
+{
+    private Executor executor = Executors.newSingleThreadExecutor();
+
+    @Test
+    public void testSimultaneousUploadDownload()
+        throws Exception
+    {
+        // preparations
+        String baseUrl = getRepositoryUrl( "nexus-test-harness-repo" );
+        // add path
+        String targetUrl = baseUrl + "nxcm970/artifact/1.0/artifact-1.0.pom";
+
+        // create deployer that we will control how long to "deploy"
+        ContinuousDeployer continuousDeployer = new ContinuousDeployer( targetUrl );
+
+        // download the subjectArtifact -- should result in 404
+        downloadSubjectArtifact( false, baseUrl );
+
+        // start deploying the subjectArtifact -- should work on it
+        executor.execute( continuousDeployer );
+
+        // download the subjectArtifact -- should result in 404
+        downloadSubjectArtifact( false, baseUrl );
+
+        // let it work a lil'
+        Thread.sleep( 1000 );
+
+        // download the subjectArtifact -- should result in 404
+        downloadSubjectArtifact( false, baseUrl );
+
+        // let it work a lil'
+        Thread.sleep( 1000 );
+
+        // download the subjectArtifact -- should result in 404
+        downloadSubjectArtifact( false, baseUrl );
+
+        // finish deploying the subjectArtifaft -- should finish succesfully
+        continuousDeployer.finishDeploying();
+
+        // wait to finish the HTTP tx, check result
+        while ( !continuousDeployer.isFinished() )
+        {
+            Thread.sleep( 200 );
+        }
+
+        Assert.assertTrue( "Deployment failed: " + continuousDeployer.getResult(),
+                           continuousDeployer.getResult() == 201 );
+
+        // download the subjectArtifact -- should result in 200, found
+        downloadSubjectArtifact( true, baseUrl );
+    }
+
+    // ==
+
+    protected void downloadSubjectArtifact( boolean shouldSucceed, String baseUrl )
+    {
+        try
+        {
+            downloadArtifact( baseUrl, "nxcm970", "artifact", "1.0", "pom", null, "./target/downloaded-jars" );
+
+            if ( !shouldSucceed )
+            {
+                Assert.fail( "Should not succeed the retrieval!" );
+            }
+        }
+        catch ( IOException e )
+        {
+            if ( shouldSucceed )
+            {
+                Assert.fail( "Should succeed the retrieval!" );
+            }
+        }
+    }
+
+}
