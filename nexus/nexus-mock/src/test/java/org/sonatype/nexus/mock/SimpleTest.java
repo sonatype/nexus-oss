4e901969e3edbc3f939feed4b2cf76e763b309e1
Marvin Froeder <velo@sonatype.com>
2009-08-31 13:42:00 +0000

    OPEN - issue NEXUS-2561: nexus-mock tests are using port 8081 https://issues.sonatype.org/browse/NEXUS-2561
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@4925 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-mock/src/test/java/org/sonatype/nexus/mock/SimpleTest.java
+++ nexus/nexus-mock/src/test/java/org/sonatype/nexus/mock/SimpleTest.java
@@ -2,10 +2,21 @@ package org.sonatype.nexus.mock;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.util.Collection;
+import java.util.Properties;
 
 import junit.framework.TestCase;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.NameFileFilter;
+import org.apache.commons.io.filefilter.TrueFileFilter;
 import org.codehaus.classworlds.Launcher;
+import org.codehaus.plexus.util.IOUtil;
 import org.restlet.Application;
 import org.restlet.Client;
 import org.restlet.data.Protocol;
@@ -26,10 +37,32 @@ public class SimpleTest
 {
     protected MockNexusEnvironment mockNexusEnvironment;
 
+    private File bundleRoot;
+
+    private static final int port;
+
+    static
+    {
+        ServerSocket ss;
+        try
+        {
+            ss = new ServerSocket( 0 );
+            port = ss.getLocalPort();
+            ss.close();
+        }
+        catch ( IOException e )
+        {
+            throw new RuntimeException( e );
+        }
+    }
+
     @Override
     protected void setUp()
         throws Exception
     {
+        bundleRoot = MockNexusEnvironment.getBundleRoot( new File( "target/nexus-ui" ) );
+        tamperPlexusProperties( bundleRoot );
+
         MockHelper.clearMocks();
 
         super.setUp();
@@ -39,10 +72,31 @@ public class SimpleTest
         mockNexusEnvironment.start();
     }
 
+    private void tamperPlexusProperties( File basedir )
+        throws Exception
+    {
+        Collection<File> files =
+            FileUtils.listFiles( basedir, new NameFileFilter( "plexus.properties" ), TrueFileFilter.TRUE );
+        assertEquals( 1, files.size() );
+
+        File pp = files.iterator().next();
+        assertTrue( pp.exists() );
+
+        Properties p = new Properties();
+        InputStream in = new FileInputStream( pp );
+        p.load( in );
+        IOUtil.close( in );
+
+        p.setProperty( "application-port", String.valueOf( port ) );
+
+        OutputStream out = new FileOutputStream( pp );
+        p.store( out, "" );
+        IOUtil.close( out );
+    }
+
     private PlexusAppBooter getAppBooter()
         throws Exception
     {
-        File bundleRoot = MockNexusEnvironment.getBundleRoot( new File( "target/nexus-ui" ) );
         System.setProperty( "basedir", bundleRoot.getAbsolutePath() );
 
         System.setProperty( "plexus.appbooter.customizers", "org.sonatype.nexus.NexusBooterCustomizer,"
@@ -89,7 +143,7 @@ public class SimpleTest
 
     /**
      * Here, we don't mock anything, we are relying on _real_ response from real Nexus
-     *
+     * 
      * @throws Exception
      */
     public void testStatusFine()
@@ -97,14 +151,14 @@ public class SimpleTest
     {
         Client client = new Client( Protocol.HTTP );
 
-        Response response = client.get( new Reference( "http://localhost:8081/nexus/service/local/status" ) );
+        Response response = client.get( new Reference( "http://localhost:" + port + "/nexus/service/local/status" ) );
 
         assertEquals( "We just started Nexus withount any tampering", 200, response.getStatus().getCode() );
     }
 
     /**
      * We mock the status resource to be unavailable.
-     *
+     * 
      * @throws Exception
      */
     public void testStatusUnavailable()
@@ -114,7 +168,7 @@ public class SimpleTest
 
         Client client = new Client( Protocol.HTTP );
 
-        Response response = client.get( new Reference( "http://localhost:8081/nexus/service/local/status" ) );
+        Response response = client.get( new Reference( "http://localhost:" + port + "/nexus/service/local/status" ) );
 
         assertEquals( "The status resource should be mocked", Status.SERVER_ERROR_SERVICE_UNAVAILABLE.getCode(),
                       response.getStatus().getCode() );
@@ -124,7 +178,7 @@ public class SimpleTest
 
     /**
      * We mock status response.
-     *
+     * 
      * @throws Exception
      */
     public void testStatusCustomContent()
@@ -142,7 +196,7 @@ public class SimpleTest
 
         Client client = new Client( Protocol.HTTP );
 
-        Response response = client.get( new Reference( "http://localhost:8081/nexus/service/local/status" ) );
+        Response response = client.get( new Reference( "http://localhost:" + port + "/nexus/service/local/status" ) );
 
         assertEquals( 200, response.getStatus().getCode() );
 
@@ -162,7 +216,7 @@ public class SimpleTest
 
     /**
      * Here, we don't mock anything, we are just listening the _real_ response from real Nexus
-     *
+     * 
      * @throws Exception
      */
     public void testListenStatusFine()
@@ -173,7 +227,7 @@ public class SimpleTest
 
         Client client = new Client( Protocol.HTTP );
 
-        Response response = client.get( new Reference( "http://localhost:8081/nexus/service/local/status" ) );
+        Response response = client.get( new Reference( "http://localhost:" + port + "/nexus/service/local/status" ) );
 
         assertEquals( "We just started Nexus withount any tampering", 200, response.getStatus().getCode() );
 
