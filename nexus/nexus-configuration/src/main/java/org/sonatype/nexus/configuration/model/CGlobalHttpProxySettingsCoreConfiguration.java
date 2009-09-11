f75ba6351d66740da7309a67f0b40b62e6571fb4
Juven Xu <juven@sonatype.com>
2009-09-11 16:10:58 +0000

    [NEXUS-2617] add a 'nullifiled' flag to CGlobalHttpProxySettingsCoreConfiguration, so that even the changedConfig is null, the change will still be committed
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@4983 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-configuration/src/main/java/org/sonatype/nexus/configuration/model/CGlobalHttpProxySettingsCoreConfiguration.java
+++ nexus/nexus-configuration/src/main/java/org/sonatype/nexus/configuration/model/CGlobalHttpProxySettingsCoreConfiguration.java
@@ -1,11 +1,14 @@
 package org.sonatype.nexus.configuration.model;
 
+import org.sonatype.configuration.ConfigurationException;
 import org.sonatype.configuration.validation.ValidationResponse;
 import org.sonatype.nexus.configuration.application.ApplicationConfiguration;
 
 public class CGlobalHttpProxySettingsCoreConfiguration
     extends AbstractCoreConfiguration
 {
+    private boolean nullified;
+
     public CGlobalHttpProxySettingsCoreConfiguration( ApplicationConfiguration applicationConfiguration )
     {
         super( applicationConfiguration );
@@ -34,14 +37,45 @@ public class CGlobalHttpProxySettingsCoreConfiguration
 
     public void nullifyConfig()
     {
-        setOriginalConfiguration( null );
-
         setChangedConfiguration( null );
+
+        nullified = true;
     }
 
     @Override
     public ValidationResponse doValidateChanges( Object changedConfiguration )
     {
         return new ValidationResponse();
+    }
+
+    @Override
+    public boolean isDirty()
+    {
+        return super.isDirty() || nullified;
+    }
+
+    @Override
+    public void commitChanges()
+        throws ConfigurationException
+    {
+        if ( nullified )
+        {
+            // nullified, nothing to validate and the super.commitChanges() will not work
+            getApplicationConfiguration().getConfigurationModel().setGlobalHttpProxySettings( null );
+        }
+        else
+        {
+            super.commitChanges();
+        }
+
+        nullified = false;
+    }
+
+    @Override
+    public void rollbackChanges()
+    {
+        super.rollbackChanges();
+
+        nullified = false;
     }
 }
