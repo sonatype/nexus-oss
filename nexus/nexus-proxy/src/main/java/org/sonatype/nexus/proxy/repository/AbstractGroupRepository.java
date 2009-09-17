e3c6abd111fd751571516d9bd5b71613d8acaed8
Tamas Cservenak <cstamas@sonatype.com>
2009-09-17 15:20:13 +0000

    Removed unused member
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@5023 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-proxy/src/main/java/org/sonatype/nexus/proxy/repository/AbstractGroupRepository.java
+++ nexus/nexus-proxy/src/main/java/org/sonatype/nexus/proxy/repository/AbstractGroupRepository.java
@@ -18,15 +18,8 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.CancellationException;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
 import org.codehaus.plexus.component.annotations.Requirement;
-import org.codehaus.plexus.logging.Logger;
 import org.sonatype.nexus.configuration.ConfigurationPrepareForSaveEvent;
 import org.sonatype.nexus.proxy.IllegalOperationException;
 import org.sonatype.nexus.proxy.ItemNotFoundException;
@@ -57,8 +50,6 @@ public abstract class AbstractGroupRepository
 
     @Requirement
     private RequestRepositoryMapper requestRepositoryMapper;
-
-    private ExecutorService executorService = Executors.newCachedThreadPool();
 
     @Override
     protected AbstractGroupRepositoryConfiguration getExternalConfiguration( boolean forWrite )
