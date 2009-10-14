1dd02254dcb10d34e819799c763272146fd41ea0
Tamas Cservenak <cstamas@sonatype.com>
2009-10-14 12:11:23 +0000

    SPICE-21: fixed
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@5273 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

---
+++ nexus/nexus-plugins/nexus-plugin-manager/src/test/java/org/sonatype/nexus/plugins/Spice21Test.java
@@ -0,0 +1,40 @@
+package org.sonatype.nexus.plugins;
+
+import java.util.Collection;
+
+import junit.framework.Assert;
+
+import org.codehaus.plexus.component.repository.ComponentDescriptor;
+
+public class Spice21Test
+    extends AbstractNexusPluginManagerTest
+{
+    public void testSpice21InnerClasses()
+        throws Exception
+    {
+        DefaultNexusPluginManager nexusPluginManager =
+            (DefaultNexusPluginManager) getContainer().lookup( NexusPluginManager.class );
+
+        // do discovery
+        Collection<PluginManagerResponse> activationResponses = nexusPluginManager.activateInstalledPlugins();
+
+        Assert.assertEquals( "Wroung number of plugins discovered!", Assertions.INSTALLED_PLUGINS, activationResponses
+            .size() );
+
+        // now we have the nexus-test-plugin with a doubled component (see the plugin)
+        ComponentDescriptor<?> outer =
+            getContainer().getComponentDescriptor( "org.sonatype.nexus.plugins.spice21.SomeComponent", "A" );
+
+        ComponentDescriptor<?> inner =
+            getContainer().getComponentDescriptor( "org.sonatype.nexus.plugins.spice21.SomeComponent", "B" );
+
+        // the plexus handled one should remain
+        assertTrue( "Outer class should be gleaned!", outer != null );
+        assertEquals( "org.sonatype.nexus.plugins.spice21.ASomeComponent", outer.getImplementation() );
+
+        // runtime gleaning should not happen
+        assertTrue( "Inner class should be gleaned!", inner != null );
+        assertEquals( "org.sonatype.nexus.plugins.spice21.ASomeComponent$BSomeComponent", inner.getImplementation() );
+    }
+
+}
