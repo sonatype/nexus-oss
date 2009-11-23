f29fc8beab27032dde76dc4f5f6ad21ce97bf436
Tamas Cservenak <cstamas@sonatype.com>
2009-11-23 13:36:17 +0000

    NEXUS-3032: commented the helper methods.
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@5609 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- sandbox/wrapper-playground/src/main/java/org/sonatype/sample/wrapper/WrapperHelper.java
+++ sandbox/wrapper-playground/src/main/java/org/sonatype/sample/wrapper/WrapperHelper.java
@@ -5,16 +5,37 @@ import java.io.IOException;
 
 import org.codehaus.plexus.util.FileUtils;
 
+/**
+ * This is a JSW Wrapper helper class to manipulate wrapper.conf. It has only one "dependency": to have set the
+ * "basedir" property on System properties with path pointing to the root of the Application bundle. This is the case
+ * with Nexus bundle.
+ * 
+ * @author cstamas
+ */
 public class WrapperHelper
 {
+    /**
+     * The File pointing to basedir. Lazily inited, see getBasedir() method.
+     */
     private static File basedir;
 
+    /**
+     * Backups wrapper.conf if not backed up already.
+     * 
+     * @throws IOException
+     */
     public static void backupWrapperConf()
         throws IOException
     {
         backupWrapperConf( false );
     }
 
+    /**
+     * Backups wrapper.conf. It may be forced to do so, potentionally overwriting the backup file.
+     * 
+     * @param overwrite true, if you want to overwrite the backup file even if it exists.
+     * @throws IOException
+     */
     public static void backupWrapperConf( boolean overwrite )
         throws IOException
     {
@@ -26,27 +47,55 @@ public class WrapperHelper
         }
     }
 
+    /**
+     * Replaces the wrapper.conf file with the provided one.
+     * 
+     * @param file
+     * @throws IOException
+     */
     public static void swapInWrapperConf( File file )
         throws IOException
     {
         FileUtils.copyFile( file, getWrapperConfFile() );
     }
 
+    /**
+     * Return the File that points to wrapper.conf.
+     * 
+     * @return
+     */
     public static File getWrapperConfFile()
     {
         return getConfFile( "wrapper.conf" );
     }
 
+    /**
+     * Returns any configuration file from /conf directory of bundle.
+     * 
+     * @param name for example "wrapper.conf", but better use getWrapperConfFile() method for that!
+     * @return
+     */
     public static File getConfFile( String name )
     {
         return new File( getConfDir(), name );
     }
 
+    /**
+     * Returns the File pointing to /conf directory of the bundle.
+     * 
+     * @return
+     */
     public static File getConfDir()
     {
         return new File( getBasedir(), "conf" );
     }
 
+    /**
+     * Returns the File pointing to the basedir. If it is not set in System properties, it will try to "guess" (and
+     * probably give wrong results, as this method is copied from PlexusTestCase class).
+     * 
+     * @return
+     */
     public static File getBasedir()
     {
         if ( basedir != null )
