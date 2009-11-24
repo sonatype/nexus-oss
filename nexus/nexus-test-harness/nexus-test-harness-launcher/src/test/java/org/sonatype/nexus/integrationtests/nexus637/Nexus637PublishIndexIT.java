bd5368c86750cf599869d79a956bdc0315e21e83
Marvin Froeder <velo@sonatype.com>
2009-11-24 15:07:46 +0000

    IN PROGRESS - issue NXCM-1509: Nexus637PublishIndexIT fails on nexus PRO https://issues.sonatype.org/browse/NXCM-1509
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@5627 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-test-harness/nexus-test-harness-launcher/src/test/java/org/sonatype/nexus/integrationtests/nexus637/Nexus637PublishIndexIT.java
+++ nexus/nexus-test-harness/nexus-test-harness-launcher/src/test/java/org/sonatype/nexus/integrationtests/nexus637/Nexus637PublishIndexIT.java
@@ -14,11 +14,13 @@
 package org.sonatype.nexus.integrationtests.nexus637;
 
 import java.io.File;
+import java.util.Arrays;
 
-import junit.framework.Assert;
-
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.internal.matchers.IsCollectionContaining;
 import org.sonatype.nexus.integrationtests.AbstractNexusIntegrationTest;
 import org.sonatype.nexus.rest.model.ScheduledServicePropertyResource;
 import org.sonatype.nexus.tasks.descriptors.PublishIndexesTaskDescriptor;
@@ -52,7 +54,22 @@ public class Nexus637PublishIndexIT
         File repositoryPath = new File( nexusWorkDir, "storage/nexus-test-harness-repo" );
         File index = new File( repositoryPath, ".index" );
 
-        Assert.assertFalse( ".index shouldn't exists before publish index task is run.", index.exists() );
+        if ( index.exists() )
+        {
+            // can't contain the OSS index
+            Assert.assertThat(
+                               Arrays.asList( index.list() ),
+                               CoreMatchers.not( IsCollectionContaining.hasItems(
+                                                                                  "nexus-maven-repository-index.gz",
+                                                                                  "nexus-maven-repository-index.gz.md5",
+                                                                                  "nexus-maven-repository-index.gz.sha1",
+                                                                                  "nexus-maven-repository-index.properties",
+                                                                                  "nexus-maven-repository-index.properties.md5",
+                                                                                  "nexus-maven-repository-index.properties.sha1",
+                                                                                  "nexus-maven-repository-index.zip",
+                                                                                  "nexus-maven-repository-index.zip.md5",
+                                                                                  "nexus-maven-repository-index.zip.sha1" ) ) );
+        }
 
         ScheduledServicePropertyResource prop = new ScheduledServicePropertyResource();
         prop.setId( "repositoryOrGroupId" );
