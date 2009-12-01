815b9d55346e8331b3950f55c786677ee511dbb1
Tamas Cservenak <cstamas@sonatype.com>
2009-12-01 12:39:36 +0000

    NEXUS-3056: added comments to how the passed in input should be formatted
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@5658 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-core-plugins/nexus-buup-plugin/src/main/java/org/sonatype/nexus/buup/invoke/NexusBuupInvocationRequest.java
+++ nexus/nexus-core-plugins/nexus-buup-plugin/src/main/java/org/sonatype/nexus/buup/invoke/NexusBuupInvocationRequest.java
@@ -9,10 +9,21 @@ import java.io.File;
  */
 public class NexusBuupInvocationRequest
 {
+    /**
+     * The directory where the downloaded upgrade bundle is unzipped.
+     */
     private final File explodedUpgradeBundleDirectory;
 
+    /**
+     * The amount of memory to pass in as -Xmx parameter. Should be as JVM CLI specifies, but without the "-Xmx" prefix!
+     * Example: 512m means 512MB
+     */
     private String nexusBundleXmx;
 
+    /**
+     * The amount of memory to pass in as -Xms parameter. Should be as JVM CLI specifies, but without the "-Xms" prefix!
+     * Example: 128m means 128MB
+     */
     private String nexusBundleXms;
 
     public NexusBuupInvocationRequest( File explodedUpgradeBundleDirectory )
