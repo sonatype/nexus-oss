2ab81515d12c6a87bbc4a28bd7c8d7f99d275dad
Juven Xu <juven@sonatype.com>
2009-12-21 17:22:32 +0000

    [NEXUS-3112] finished the license page and most of the user info page
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@5745 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-core-plugins/nexus-buup-plugin/src/main/js/static/js/repoServer.UpgradePanel.js
+++ nexus/nexus-core-plugins/nexus-buup-plugin/src/main/js/static/js/repoServer.UpgradePanel.js
@@ -7,35 +7,241 @@ Sonatype.repoServer.UpgradePanel = function( config ) {
     steps: ['License Agreement', 'User Information', 'JVM Memory', 'Activation', 'Download', 'Upgrade'],
     activeStep: 0
   });
+
+  this.countryStore = new Ext.data.SimpleStore({
+    fields: ['value', 'display'],
+    data: [
+['United States', 'United States'],
+['Canada', 'Canada'],
+['Australia', 'Australia'],
+['Austria', 'Austria'],
+['Belgium', 'Belgium'],
+['Brazil', 'Brazil'],
+['Bulgaria', 'Bulgaria'],
+['China', 'China'],
+['Croatia', 'Croatia'],
+['Cyprus', 'Cyprus'],
+['Czech Republic', 'Czech Republic'],
+['Denmark', 'Denmark'],
+['Estonia', 'Estonia'],
+['Finland', 'Finland'],
+['France', 'France'],
+['Germany', 'Germany'],
+['Greece', 'Greece'],
+['Hungary', 'Hungary'],
+['Iceland', 'Iceland'],
+['India', 'India'],
+['Indonesia', 'Indonesia'],
+['Ireland', 'Ireland'],
+['Israel', 'Israel'],
+['Italy', 'Italy'],
+['Japan', 'Japan'],
+['Latvia', 'Latvia'],
+['Lithuania', 'Lithuania'],
+['Luxembourg', 'Luxembourg'],
+['Malaysia', 'Malaysia'],
+['Malta', 'Malta'],
+['Mexico', 'Mexico'],      
+['Netherlands', 'Netherlands'],
+['New Zealand', 'New Zealand'],
+['Norway', 'Norway'],
+['Phillipines', 'Phillipines'],
+['Poland', 'Poland'],                   
+['Portugal', 'Portugal'],      
+['Romania', 'Romania'],
+['Singapore', 'Singapore'],
+['Slovakia', 'Slovakia'],
+['Slovenia', 'Slovenia'],
+['South Korea', 'South Korea'],
+['Spain', 'Spain'],             
+['Sweden', 'Sweden'],
+['Switzerland', 'Switzerland'],
+['Taiwan', 'Taiwan'],
+['Thailand', 'Thailand'],
+['UK', 'UK'],
+['Vietnam', 'Vietnam']
+    ]
+  });
   
   this.stepLicense = new Ext.Panel({
       id: 'step-0',
       items: [
       {
+        id: 'eula-content',
         xtype: 'panel',
-        html: '<h2 style="font: bold 16px tahoma, arial, helvetica, sans-serif; padding: 15px 15px 15px 15px;">Sonatype Nexus Professional End User License Agreement (EULA)</h2>'
+        title: 'Sonatype Nexus Professional End User License Agreement (EULA)',
+        width: 'auto',
+        height: 290,
+        autoScroll: true,
+        style: 'margin: 15px 0px 0px 15px; background-color: white; border-width: 0px 1px 1px 1px',
+        bodyStyle: 'padding: 0px 10px 0px 10px'
       },
       {
-        xtype: 'textarea'
-      },
-      {
-        xtype: 'checkbox',
-        fieldLabel: 'I accpet',
-        name: 'acceptEULA'
+        xtype: 'panel',
+        layout: 'column',
+        style: 'margin: 10px 0px 0px 15px',
+        items: [
+        {
+          xtype: 'checkbox',
+          name: 'acceptLicense',
+          listeners: {
+            'check': function ( cmpt, checked ) {
+               var stepNextBtn = Ext.getCmp('wizardBtnNext');
+               if ( checked ) {
+                 stepNextBtn.enable();
+               }
+               else {
+                 stepNextBtn.disable();
+               }
+            }
+          }
+        },
+        {
+          html: 'I have read and agree to all terms in the above EULA',
+          style: 'margin: 3px 0px 0px 6px'
+        }
+        ]
       }
       ],
       listeners: {
         'beforeshow' : function( cmpt ){
-            var stepBackBtn = Ext.getCmp('wizardBtnBack');
-            stepBackBtn.disable();
+          var stepBackBtn = Ext.getCmp('wizardBtnBack');
+          stepBackBtn.disable();
+          var licenseCheckbox = this.find('name', 'acceptLicense')[0];
+          var stepNextBtn = Ext.getCmp('wizardBtnNext');
+          if ( licenseCheckbox.getValue() ) {
+            stepNextBtn.enable();
+          }
+          else {
+            stepNextBtn.disable();
+          }
         }
-      
       }
   });
+
+  Ext.Ajax.request({
+    method: 'GET',
+    url: Sonatype.config.resourcePath + '/html/license.html',
+    headers: {
+      accept: 'text/html' 
+    },
+    success: function ( response, options ) {
+      var licenseHtml = response.responseText;
+      Ext.getCmp('eula-content').body.dom.innerHTML = licenseHtml;
+    }
+  }); 
   
   this.stepUser = new Ext.Panel({
     id: 'step-1',
-    html: 'User Information'
+    layout: 'column',
+    bodyStyle: 'margin: 10px',
+    hideMode: 'offsets',
+    items: [
+    {
+      xtype: 'fieldset',
+      title: 'Contact Information',
+      collapsible: false,
+      autoHeight: true,
+      bodyStyle: 'margin: 10px',
+      items: [
+      {
+        xtype: 'textfield',
+        width: 160,
+        fieldLabel: 'First Name',
+        name: 'firstName'
+      },
+      {
+        xtype: 'textfield',
+        width: 160,
+        fieldLabel: 'Last Name',
+        name: 'lastName'
+      },
+      {
+        xtype: 'textfield',
+        width: 160,
+        fieldLabel: 'Title',
+        name: 'title'
+      },
+      {
+        xtype: 'textfield',
+        width: 160,
+        fieldLabel: 'Email',
+        name: 'email'
+      },
+      {
+        xtype: 'textfield',
+        width: 160,
+        fieldLabel: 'Phone',
+        name: 'phone'
+      }
+      ]
+    },
+    {
+      html: '&nbsp',
+      width: 50
+    },
+    {
+      xtype: 'fieldset',
+      title: 'Address Information',
+      collapsible: false,
+      autoHeight: true,
+      bodyStyle: 'margin: 10px',
+      items: [
+      {
+        xtype: 'textfield',
+        width: 168,
+        fieldLabel: 'Organization',
+        name: 'organization'
+      },
+      {
+        xtype: 'textfield',
+        width: 168,
+        fieldLabel: 'Street Address',
+        name: 'streetAddress'
+      }, 
+      {
+        xtype: 'textfield',
+        width: 168,
+        fieldLabel: 'City',
+        name: 'city'
+      }, 
+      {
+        xtype: 'combo',
+        width: 185,
+        fieldLabel: 'State/Province',
+        name: 'state',
+        emptyText: 'Selete One(US/Canada Only)'
+      },
+      {
+        xtype: 'textfield',
+        fieldLabel: 'Zip Code',
+        width: 168,
+        name: 'zipCode'
+      }, 
+      {
+        xtype: 'combo',
+        fieldLabel: 'Country',
+        name: 'country',
+        emptyText: 'Select One',
+        store: this.countryStore,
+        displayField: 'display',
+        valueField: 'value',
+        triggerAction: 'all',
+        mode: 'local',
+        width: 185,
+        editable: false
+      }
+      ]
+    } 
+    ],
+    listeners: {
+      'beforeshow' : function( cmpt ){
+          var stepBackBtn = Ext.getCmp('wizardBtnBack');
+          stepBackBtn.enable();
+          var stepNextBtn = Ext.getCmp('wizardBtnNext');
+          stepNextBtn.enable();
+      }
+    }
   });
   
   this.stepJVM = new Ext.Panel({
