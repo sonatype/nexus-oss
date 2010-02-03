008acb31fe4d89233e49fbaba638bf3d178ed100
Anders Hammar <anders@hammar.net>
2010-02-03 15:06:07 +0000

    Removed old unused comment and re-formatted code.
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@5963 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-core-plugins/nexus-rrb-plugin/src/main/java/org/sonatype/nexus/plugins/rrb/MavenRepositoryReader.java
+++ nexus/nexus-core-plugins/nexus-rrb-plugin/src/main/java/org/sonatype/nexus/plugins/rrb/MavenRepositoryReader.java
@@ -19,124 +19,153 @@ import org.sonatype.nexus.plugins.rrb.parsers.S3RemoteRepositoryParser;
 import org.sonatype.nexus.proxy.repository.ProxyRepository;
 import org.sonatype.nexus.proxy.storage.remote.RemoteStorageContext;
 import org.sonatype.nexus.proxy.storage.remote.commonshttpclient.HttpClientProxyUtil;
-/**
- * Class for retrieving directory data from remote repository. This class is not
- * thread-safe!
- */
-public class MavenRepositoryReader {
 
-	private final Logger logger = LoggerFactory.getLogger(MavenRepositoryReader.class);
-    
+/**
+ * Class for retrieving directory data from remote repository. This class is not thread-safe!
+ */
+public class MavenRepositoryReader
+{
+
+    private final Logger logger = LoggerFactory.getLogger( MavenRepositoryReader.class );
+
     private String remoteUrl;
+
     private String localUrl;
+
     private ProxyRepository proxyRepository;
 
-	private String id;
-    
-//    @Requirement
-//    private NexusConfiguration nexusConfig;
+    private String id;
 
-    
     /**
-     * 
      * @param remoteUrl url to the remote repository
      * @param localUrl url to the local resource service
-     * @return a json array containing the remote data
+     * @return a list containing the remote data
      */
-    public List<RepositoryDirectory> extract(String remoteUrl, String localUrl,ProxyRepository proxyRepository,String id) {
-    	logger.debug("remoteUrl={}",remoteUrl);
-    	this.remoteUrl = remoteUrl;
+    public List<RepositoryDirectory> extract( String remoteUrl, String localUrl, ProxyRepository proxyRepository,
+                                              String id )
+    {
+        logger.debug( "remoteUrl={}", remoteUrl );
+        this.remoteUrl = remoteUrl;
         this.localUrl = localUrl;
-        this.proxyRepository=proxyRepository;
-        this.id=id;
+        this.proxyRepository = proxyRepository;
+        this.id = id;
         StringBuilder html = getContent();
-        if (logger.isDebugEnabled()) {
-            logger.trace(html.toString());
+        if ( logger.isDebugEnabled() )
+        {
+            logger.trace( html.toString() );
         }
-        return parseResult(html);
+        return parseResult( html );
     }
 
-    private ArrayList<RepositoryDirectory> parseResult(StringBuilder indata) {
+    private ArrayList<RepositoryDirectory> parseResult( StringBuilder indata )
+    {
         RemoteRepositoryParser parser = null;
-        if (indata.indexOf("<html ") != -1) {
-        	logger.debug("is html repository");
-            parser = new HtmlRemoteRepositoryParser(remoteUrl, localUrl,id);
+        if ( indata.indexOf( "<html " ) != -1 )
+        {
+            logger.debug( "is html repository" );
+            parser = new HtmlRemoteRepositoryParser( remoteUrl, localUrl, id );
         }
-        if (indata.indexOf("xmlns=\"http://s3.amazonaws.com/doc/2006-03-01/\"") != -1 || (indata.indexOf("<?xml") != -1&& responseContainsError(indata))) {
-        	logger.debug("is S3 repository");
-            if (responseContainsError(indata)) {
-            	logger.debug("response from S3 repository contains error, need to find rootUrl");
-                remoteUrl = findRootUrl(indata);
+        if ( indata.indexOf( "xmlns=\"http://s3.amazonaws.com/doc/2006-03-01/\"" ) != -1
+            || ( indata.indexOf( "<?xml" ) != -1 && responseContainsError( indata ) ) )
+        {
+            logger.debug( "is S3 repository" );
+            if ( responseContainsError( indata ) )
+            {
+                logger.debug( "response from S3 repository contains error, need to find rootUrl" );
+                remoteUrl = findRootUrl( indata );
                 indata = getContent();
             }
-            parser = new S3RemoteRepositoryParser(remoteUrl, localUrl, id);
-        } else {
-        	logger.debug("Found no matching parser, using default html parser");
-            parser = new HtmlRemoteRepositoryParser(remoteUrl, localUrl, id);
+            parser = new S3RemoteRepositoryParser( remoteUrl, localUrl, id );
         }
-        return parser.extractLinks(indata);
+        else
+        {
+            logger.debug( "Found no matching parser, using default html parser" );
+            parser = new HtmlRemoteRepositoryParser( remoteUrl, localUrl, id );
+        }
+        return parser.extractLinks( indata );
     }
 
-    private String findRootUrl(StringBuilder indata) {
-    	logger.debug("indata={}", indata.toString());
+    private String findRootUrl( StringBuilder indata )
+    {
+        logger.debug( "indata={}", indata.toString() );
         String key = "";
         String newUrl = "";
-        int start = indata.indexOf("<Key>");
-        int end = indata.indexOf("</Key>");
-        if (start > 0 && end > start) {
-            key = indata.substring(start + 5, end);
-            newUrl = remoteUrl.substring(0, remoteUrl.indexOf(key));
+        int start = indata.indexOf( "<Key>" );
+        int end = indata.indexOf( "</Key>" );
+        if ( start > 0 && end > start )
+        {
+            key = indata.substring( start + 5, end );
+            newUrl = remoteUrl.substring( 0, remoteUrl.indexOf( key ) );
             newUrl += "?prefix=" + key;
         }
-        logger.debug("newUrl={}", newUrl);
+        logger.debug( "newUrl={}", newUrl );
         return newUrl;
     }
 
-    private boolean responseContainsError(StringBuilder indata) {
-        if (indata.indexOf("<Error>") != -1 || indata.indexOf("<error>") != -1) {
+    private boolean responseContainsError( StringBuilder indata )
+    {
+        if ( indata.indexOf( "<Error>" ) != -1 || indata.indexOf( "<error>" ) != -1 )
+        {
             return true;
         }
         return false;
     }
 
-    private StringBuilder getContent() {
+    private StringBuilder getContent()
+    {
         GetMethod method = null;
-        
+
         HttpClient client = new HttpClient();
-        
-        if(proxyRepository!=null){
-			RemoteStorageContext rctx = proxyRepository.getRemoteStorageContext();
-			HttpClientProxyUtil.applyProxyToHttpClient( client, rctx, null ); // no logger to pass in
-        }
-        
-        if (remoteUrl.indexOf("?prefix") != -1) {
-            method = new GetMethod(remoteUrl + "&delimiter=/");
-            method.setFollowRedirects(true);
-        } else {
-            method = new GetMethod(remoteUrl + "?delimiter=/");
-            method.setFollowRedirects(true);
+
+        if ( proxyRepository != null )
+        {
+            RemoteStorageContext rctx = proxyRepository.getRemoteStorageContext();
+            HttpClientProxyUtil.applyProxyToHttpClient( client, rctx, null ); // no logger to pass in
         }
 
-        method.getParams().setParameter(HttpMethodParams.RETRY_HANDLER, new DefaultHttpMethodRetryHandler(3, false) );
-        client.getParams().setParameter("http.protocol.max-redirects", new Integer(1));
+        if ( remoteUrl.indexOf( "?prefix" ) != -1 )
+        {
+            method = new GetMethod( remoteUrl + "&delimiter=/" );
+            method.setFollowRedirects( true );
+        }
+        else
+        {
+            method = new GetMethod( remoteUrl + "?delimiter=/" );
+            method.setFollowRedirects( true );
+        }
+
+        method.getParams().setParameter( HttpMethodParams.RETRY_HANDLER, new DefaultHttpMethodRetryHandler( 3, false ) );
+        client.getParams().setParameter( "http.protocol.max-redirects", new Integer( 1 ) );
 
         StringBuilder result = new StringBuilder();
 
-        try {
-        	doCall(method, client, result);
-        } catch (HttpException e) {
-        	method.setPath(method.getPath()+"/");
+        try
+        {
+            doCall( method, client, result );
+        }
+        catch ( HttpException e )
+        {
+            method.setPath( method.getPath() + "/" );
 
-        	try {
-				doCall(method, client, result);
-			} catch (HttpException e1) {
-			    logger.error(e.getMessage(), e);
-			} catch (IOException e1) {
-			    logger.error(e.getMessage(), e);
-			}
-        } catch (IOException e) {
-        	logger.error(e.getMessage(), e);
-        } finally {
+            try
+            {
+                doCall( method, client, result );
+            }
+            catch ( HttpException e1 )
+            {
+                logger.error( e.getMessage(), e );
+            }
+            catch ( IOException e1 )
+            {
+                logger.error( e.getMessage(), e );
+            }
+        }
+        catch ( IOException e )
+        {
+            logger.error( e.getMessage(), e );
+        }
+        finally
+        {
             // Release the connection.
             method.releaseConnection();
         }
@@ -144,23 +173,19 @@ public class MavenRepositoryReader {
         return result;
     }
 
-	private void doCall(GetMethod method, HttpClient client,
-			StringBuilder result) throws IOException, HttpException {
-		int responseCode;
-		responseCode = client.executeMethod(method);
+    private void doCall( GetMethod method, HttpClient client, StringBuilder result )
+        throws IOException, HttpException
+    {
+        int responseCode;
+        responseCode = client.executeMethod( method );
 
-		logger.debug("responseCode={}",responseCode);
-		BufferedReader reader = new BufferedReader(new InputStreamReader(method.getResponseBodyAsStream()));
+        logger.debug( "responseCode={}", responseCode );
+        BufferedReader reader = new BufferedReader( new InputStreamReader( method.getResponseBodyAsStream() ) );
 
-		String line = null;
-		while ((line = reader.readLine()) != null) {
-			result.append(line + "\n");
-		}
-	}
-
-    /**
-     * Inner class used to imitate the for Nexus expected structure for the json
-     * reply.
-     */
-   
+        String line = null;
+        while ( ( line = reader.readLine() ) != null )
+        {
+            result.append( line + "\n" );
+        }
+    }
 }
