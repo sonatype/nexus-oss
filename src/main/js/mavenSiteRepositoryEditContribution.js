aa8324bfd61dff9484bd4bc995043b0395c751a4
Marvin Froeder <velo@sonatype.com>
2010-03-25 11:52:48 +0000

    Formatted all javascript files https://docs.sonatype.com/display/ENG/Spket+License+%28JS+IDE%29
    git-svn-id: https://svn.sonatype.com/repos/code/products/nexus-enterprise/trunk@13016 839f2357-842e-4662-a642-f9b39ea64065

--- src/main/js/mavenSiteRepositoryEditContribution.js
+++ src/main/js/mavenSiteRepositoryEditContribution.js
@@ -1,12 +1,14 @@
 Ext.override(Sonatype.repoServer.HostedRepositoryEditor, {
-  afterProviderSelectHandler: function( combo, rec, index ) {
-    this.updateIndexableCombo(rec.data.format);
-    
-    if ( rec.data.provider == 'maven-site' ){
-      this.find('name','writePolicy')[0].setValue('ALLOW_WRITE');
-    }
-    else {
-      this.find('name','writePolicy')[0].setValue('ALLOW_WRITE_ONCE');
-    }
-  }
-});
+      afterProviderSelectHandler : function(combo, rec, index) {
+        this.updateIndexableCombo(rec.data.format);
+
+        if (rec.data.provider == 'maven-site')
+        {
+          this.find('name', 'writePolicy')[0].setValue('ALLOW_WRITE');
+        }
+        else
+        {
+          this.find('name', 'writePolicy')[0].setValue('ALLOW_WRITE_ONCE');
+        }
+      }
+    });
