849814824487da089cd41b5087930b4e43c3398f
Marvin Froeder <velo@sonatype.com>
2010-04-06 20:48:29 +0000

    OPEN - issue NXCM-1952: Default OSGI and P2 repository roles/permissions are wrong https://issues.sonatype.org/browse/NXCM-1952
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@6281 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-app/src/main/java/org/sonatype/nexus/security/NexusViewSecurityResource.java
+++ nexus/nexus-app/src/main/java/org/sonatype/nexus/security/NexusViewSecurityResource.java
@@ -1,24 +1,36 @@
 package org.sonatype.nexus.security;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+import java.util.Map.Entry;
+
 import org.codehaus.plexus.component.annotations.Component;
 import org.codehaus.plexus.component.annotations.Requirement;
+import org.codehaus.plexus.interpolation.util.StringUtils;
 import org.codehaus.plexus.personality.plexus.lifecycle.phase.Initializable;
 import org.codehaus.plexus.personality.plexus.lifecycle.phase.InitializationException;
 import org.sonatype.nexus.proxy.events.RepositoryRegistryEventAdd;
 import org.sonatype.nexus.proxy.events.RepositoryRegistryEventRemove;
+import org.sonatype.nexus.proxy.registry.ContentClass;
 import org.sonatype.nexus.proxy.registry.RepositoryRegistry;
+import org.sonatype.nexus.proxy.registry.RepositoryTypeRegistry;
 import org.sonatype.nexus.proxy.repository.Repository;
 import org.sonatype.plexus.appevents.ApplicationEventMulticaster;
 import org.sonatype.plexus.appevents.Event;
 import org.sonatype.plexus.appevents.EventListener;
 import org.sonatype.security.model.CPrivilege;
 import org.sonatype.security.model.CProperty;
+import org.sonatype.security.model.CRole;
 import org.sonatype.security.model.Configuration;
 import org.sonatype.security.realms.tools.AbstractStaticSecurityResource;
 import org.sonatype.security.realms.tools.StaticSecurityResource;
 
 @Component( role = StaticSecurityResource.class, hint = "NexusViewSecurityResource" )
-public class NexusViewSecurityResource extends AbstractStaticSecurityResource implements EventListener, Initializable
+public class NexusViewSecurityResource
+    extends AbstractStaticSecurityResource
+    implements EventListener, Initializable
 {
     @Requirement
     private RepositoryRegistry repoRegistry;
@@ -26,50 +38,95 @@ public class NexusViewSecurityResource extends AbstractStaticSecurityResource im
     @Requirement
     private ApplicationEventMulticaster eventMulticaster;
 
+    @Requirement
+    private RepositoryTypeRegistry repoTypeRegistry;
+
+    @Override
     public String getResourcePath()
     {
         return null;
     }
 
+    @Override
     public Configuration getConfiguration()
     {
         Configuration configuration = new Configuration();
 
-        configuration.addPrivilege( buildPrivilege(
-            "All Repositories - (view)",
-            "Privilege that gives view access to all repositories.",
-            "*" ) );
+        configuration.addPrivilege( buildPrivilege( "All Repositories - (view)",
+                                                    "Privilege that gives view access to all repositories.", "*" ) );
 
         for ( Repository repo : repoRegistry.getRepositories() )
         {
-            configuration.addPrivilege( buildPrivilege(
-                repo.getName() + " - (view)",
-                "Privilege that gives view access to the " + repo.getName() + " repository.",
-                repo.getId() ) );
+            configuration.addPrivilege( buildPrivilege( repo.getName() + " - (view)",
+                                                        "Privilege that gives view access to the " + repo.getName()
+                                                            + " repository.", repo.getId() ) );
+        }
+
+        Set<Entry<String, ContentClass>> contents = repoTypeRegistry.getContentClasses().entrySet();
+
+        for ( Entry<String, ContentClass> entry : contents )
+        {
+            // TODO in the future we can create CRUD privs / roles here
+            configuration.addRole( buildRole( entry, "view" ) );
         }
 
         setDirty( false );
 
         return configuration;
     }
-    
+
+    private CRole buildRole( Entry<String, ContentClass> entry, String method )
+    {
+        String content = entry.getKey();
+        CRole view = new CRole();
+        view.setId( content + "-all-" + method );
+
+        content = StringUtils.capitalizeFirstLetter( content );
+        view.setDescription( "Gives access to " + method + " ALL " + content + " Repositories in Nexus." );
+
+        method = StringUtils.capitalizeFirstLetter( method );
+        view.setName( "Repo: All Eclipse " + content + " Repositories (" + method + ")" );
+        view.setSessionTimeout( 60 );
+
+        List<? extends Repository> repos = getRepositoriesWithContentClass( entry.getValue() );
+        for ( Repository repo : repos )
+        {
+            view.addPrivilege( "repository-" + repo.getId() );
+        }
+        return view;
+    }
+
+    private List<? extends Repository> getRepositoriesWithContentClass( ContentClass content )
+    {
+        List<Repository> filtered = new ArrayList<Repository>();
+        Collection<Repository> repos = repoRegistry.getRepositories();
+        for ( Repository repository : repos )
+        {
+            if ( content.equals( repository.getRepositoryContentClass() ) )
+            {
+                filtered.add( repository );
+            }
+        }
+        return filtered;
+    }
+
     protected CPrivilege buildPrivilege( String name, String description, String repoId )
     {
         CPrivilege priv = new CPrivilege();
-        
+
         priv.setId( "repository-" + ( repoId.equals( "*" ) ? "all" : repoId ) );
         priv.setName( name );
         priv.setDescription( description );
         priv.setType( RepositoryViewPrivilegeDescriptor.TYPE );
-        
+
         CProperty prop = new CProperty();
         prop.setKey( RepositoryPropertyDescriptor.ID );
-        prop.setValue( repoId );        
+        prop.setValue( repoId );
         priv.addProperty( prop );
-        
+
         return priv;
     }
-    
+
     public void onEvent( Event<?> event )
     {
         if ( RepositoryRegistryEventAdd.class.isAssignableFrom( event.getClass() )
