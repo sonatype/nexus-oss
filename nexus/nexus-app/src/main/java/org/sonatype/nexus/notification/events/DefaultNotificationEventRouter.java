ce8e957837b9c066ffb9a584d0ac3b2a467ae99e
Tamas Cservenak <cstamas@sonatype.com>
2010-04-13 16:21:31 +0000

    Changed notifications to send only one mail about blocked repository if it was blocked for at least 60 seconds. And send another mail, about ublocking, only if 1st mail was sent at all.
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@6323 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-app/src/main/java/org/sonatype/nexus/notification/events/DefaultNotificationEventRouter.java
+++ nexus/nexus-app/src/main/java/org/sonatype/nexus/notification/events/DefaultNotificationEventRouter.java
@@ -1,16 +1,19 @@
 package org.sonatype.nexus.notification.events;
 
-import java.util.HashSet;
-
 import org.codehaus.plexus.component.annotations.Component;
 import org.codehaus.plexus.component.annotations.Requirement;
 import org.sonatype.nexus.notification.NotificationCheat;
 import org.sonatype.nexus.notification.NotificationManager;
 import org.sonatype.nexus.notification.NotificationRequest;
 import org.sonatype.nexus.notification.NotificationTarget;
+import org.sonatype.nexus.proxy.events.RepositoryEventProxyModeChanged;
 import org.sonatype.nexus.proxy.events.RepositoryEventProxyModeSet;
 import org.sonatype.nexus.proxy.repository.ProxyMode;
 import org.sonatype.plexus.appevents.Event;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 
 /**
  * This component routes based on Application event. Currently it is hardwired, but we would have some "mediation"
@@ -28,26 +31,37 @@ public class DefaultNotificationEventRouter
 
     public NotificationRequest getRequestForEvent( Event<?> evt )
     {
+        // this is for notifying about repository being blocked
         if ( evt instanceof RepositoryEventProxyModeSet )
         {
-            // this event is _always_ fired! Do not mix it with RepositoryEventProxyModeChanged event! Read their JavaDoc!
+            // this event is _always_ fired! Do not mix it with RepositoryEventProxyModeChanged event! Read their
+            // JavaDoc!
             RepositoryEventProxyModeSet rpmevt = (RepositoryEventProxyModeSet) evt;
 
+            // targets to be notified
             HashSet<NotificationTarget> targets = new HashSet<NotificationTarget>();
 
-            // currently we "hardwire" this one only
-            // later we should back this with real routing configuration
-            NotificationTarget autoBlockTarget =
-                notificationManager.readNotificationTarget( NotificationCheat.AUTO_BLOCK_NOTIFICATION_GROUP_ID );
-
-            if ( autoBlockTarget != null )
+            // send out email notification only if event is AutoBlocked and repo is blocked for at least
+            // 60secs
+            if ( ProxyMode.BLOCKED_AUTO.equals( rpmevt.getNewProxyMode() )
+                && rpmevt.getRepository().getRepositoryStatusCheckPeriod() >= 60000 )
             {
-                // do NOT send out email notification if event is AutoBlocked and repo is not blocked for at least
-                // 60secs
-                if ( !( ProxyMode.BLOCKED_AUTO.equals( rpmevt.getNewProxyMode() ) && rpmevt.getRepository()
-                    .getRepositoryStatusCheckPeriod() < 60000 ) )
+                // notify only once
+                if ( !getAutoBlockedRepositoryIds().contains( rpmevt.getRepository().getId() ) )
                 {
-                    targets.add( autoBlockTarget );
+                    // currently we "hardwire" this one only
+                    // later we should back this with real routing configuration
+                    NotificationTarget autoBlockTarget =
+                        notificationManager.readNotificationTarget( NotificationCheat.AUTO_BLOCK_NOTIFICATION_GROUP_ID );
+
+                    if ( autoBlockTarget != null )
+                    {
+                        // add this target to set of targets
+                        targets.add( autoBlockTarget );
+
+                        // add this repo's ID to the "remembered" ones, since it is notified
+                        getAutoBlockedRepositoryIds().add( rpmevt.getRepository().getId() );
+                    }
                 }
             }
 
@@ -57,8 +71,50 @@ public class DefaultNotificationEventRouter
 
             if ( !targets.isEmpty() )
             {
-                RepositoryEventProxyModeMessage message =
-                    new RepositoryEventProxyModeMessage( rpmevt, null );
+                RepositoryEventProxyModeMessage message = new RepositoryEventProxyModeMessage( rpmevt, null );
+
+                return new NotificationRequest( message, targets );
+            }
+            else
+            {
+                return NotificationRequest.EMPTY;
+            }
+        }
+        // this is for notifying about repository being unblocked
+        else if ( evt instanceof RepositoryEventProxyModeChanged )
+        {
+            // this event is fired _only_ on transition! Do not mix it with RepositoryEventProxyModeSet event! Read
+            // their JavaDoc!
+            RepositoryEventProxyModeChanged rpmevt = (RepositoryEventProxyModeChanged) evt;
+
+            // targets to be notified
+            HashSet<NotificationTarget> targets = new HashSet<NotificationTarget>();
+
+            // notify about unblock only if it was notified about block
+            if ( getAutoBlockedRepositoryIds().contains( rpmevt.getRepository().getId() ) )
+            {
+                // currently we "hardwire" this one only
+                // later we should back this with real routing configuration
+                NotificationTarget autoBlockTarget =
+                    notificationManager.readNotificationTarget( NotificationCheat.AUTO_BLOCK_NOTIFICATION_GROUP_ID );
+
+                if ( autoBlockTarget != null )
+                {
+                    // add this target to set of targets
+                    targets.add( autoBlockTarget );
+
+                    // add this repo's ID to the "remembered" ones, since it is notified
+                    getAutoBlockedRepositoryIds().remove( rpmevt.getRepository().getId() );
+                }
+            }
+
+            // we could lookup other groups too, like SMS notif, and even RSS feed could be one group
+            // ...
+            // stuff below this line is "generic" - hardwired stuff is above only
+
+            if ( !targets.isEmpty() )
+            {
+                RepositoryEventProxyModeMessage message = new RepositoryEventProxyModeMessage( rpmevt, null );
 
                 return new NotificationRequest( message, targets );
             }
@@ -71,5 +127,19 @@ public class DefaultNotificationEventRouter
         {
             return NotificationRequest.EMPTY;
         }
+    }
+
+    // ==
+
+    private List<String> autoBlockedRepositoryIds;
+
+    protected List<String> getAutoBlockedRepositoryIds()
+    {
+        if ( autoBlockedRepositoryIds == null )
+        {
+            autoBlockedRepositoryIds = new ArrayList<String>();
+        }
+
+        return autoBlockedRepositoryIds;
     }
 }
