11dcdbb1ffec5780c4460cb90613ba85951a133a
Marvin Froeder <velo@sonatype.com>
2010-04-30 21:27:12 +0000

    No longer needs maven repo
    git-svn-id: https://svn.sonatype.com/repos/code/products/nexus-enterprise/trunk@13783 839f2357-842e-4662-a642-f9b39ea64065

--- src/test/java/com/sonatype/nexus/proxy/maven/site/nxcm1008/NXCM1008SiteDeployIT.java
+++ src/test/java/com/sonatype/nexus/proxy/maven/site/nxcm1008/NXCM1008SiteDeployIT.java
@@ -1,27 +1,46 @@
 package com.sonatype.nexus.proxy.maven.site.nxcm1008;
 
-import org.apache.maven.it.VerificationException;
-import org.apache.maven.it.Verifier;
-import org.junit.Test;
-import org.restlet.data.MediaType;
-import org.sonatype.nexus.integrationtests.AbstractMavenNexusIT;
-import org.sonatype.nexus.proxy.maven.ChecksumPolicy;
-import org.sonatype.nexus.proxy.maven.RepositoryPolicy;
-import org.sonatype.nexus.proxy.repository.RepositoryWritePolicy;
-import org.sonatype.nexus.proxy.repository.WebSiteRepository;
-import org.sonatype.nexus.rest.model.RepositoryResource;
-import org.sonatype.nexus.test.utils.FileTestingUtils;
-import org.sonatype.nexus.test.utils.RepositoryMessageUtil;
-import org.sonatype.nexus.test.utils.TestProperties;
-
 import java.io.File;
 import java.net.URL;
 
 import junit.framework.Assert;
 
+import org.apache.maven.it.VerificationException;
+import org.apache.maven.it.Verifier;
+import org.junit.Test;
+import org.restlet.data.MediaType;
+import org.sonatype.nexus.integrationtests.AbstractMavenNexusIT;
+import org.sonatype.nexus.maven.tasks.descriptors.RebuildMavenMetadataTaskDescriptor;
+import org.sonatype.nexus.proxy.maven.ChecksumPolicy;
+import org.sonatype.nexus.proxy.maven.RepositoryPolicy;
+import org.sonatype.nexus.proxy.repository.RepositoryWritePolicy;
+import org.sonatype.nexus.proxy.repository.WebSiteRepository;
+import org.sonatype.nexus.rest.model.RepositoryResource;
+import org.sonatype.nexus.rest.model.ScheduledServicePropertyResource;
+import org.sonatype.nexus.test.utils.FileTestingUtils;
+import org.sonatype.nexus.test.utils.RepositoryMessageUtil;
+import org.sonatype.nexus.test.utils.TaskScheduleUtil;
+import org.sonatype.nexus.test.utils.TestProperties;
+
 public class NXCM1008SiteDeployIT
     extends AbstractMavenNexusIT
 {
+
+    @Override
+    protected void runOnce()
+        throws Exception
+    {
+        super.runOnce();
+
+        ScheduledServicePropertyResource repo = new ScheduledServicePropertyResource();
+
+        repo.setId( "repositoryOrGroupId" );
+
+        repo.setValue( "repo_fake-central" );
+
+        TaskScheduleUtil.runTask( "RebuildMavenMetadata-Nexus1022", RebuildMavenMetadataTaskDescriptor.ID, repo );
+        TaskScheduleUtil.waitForAllTasksToStop();
+    }
 
     @Test
     public void testDeployAndGet()
@@ -29,9 +48,9 @@ public class NXCM1008SiteDeployIT
     {
         this.setTestRepositoryId( "nxcm1008site" );
         this.createSiteRepo();
-        
+
         // now deploy something
-        
+
         File mavenProject1 = getTestFile( "site-1" );
 
         File settings1 = getTestFile( "settings1.xml" );
@@ -46,7 +65,7 @@ public class NXCM1008SiteDeployIT
 
             verifier1.executeGoal( "site" );
             verifier1.verifyErrorFreeLog();
-            
+
             verifier1.executeGoal( "site:deploy" );
             verifier1.verifyErrorFreeLog();
         }
@@ -56,13 +75,16 @@ public class NXCM1008SiteDeployIT
             e.printStackTrace();
             failTest( verifier1 );
         }
-        
+
         // now call get and compare it with the original
-        
-        File downloadedFile = this.downloadFile( new URL( TestProperties.getString( "nexus.base.url" ) +"content/sites/nxcm1008site/site-1/index.html") , "target/site/tmp-index.html" );
-        FileTestingUtils.compareFileSHA1s( downloadedFile, new File( "target/resources/nxcm1008/files/site-1/target/site/index.html" ) );
+
+        File downloadedFile =
+            this.downloadFile( new URL( TestProperties.getString( "nexus.base.url" )
+                + "content/sites/nxcm1008site/site-1/index.html" ), "target/site/tmp-index.html" );
+        FileTestingUtils.compareFileSHA1s( downloadedFile,
+                                           new File( "target/resources/nxcm1008/files/site-1/target/site/index.html" ) );
     }
-    
+
     private void createSiteRepo()
         throws Exception
     {
@@ -74,15 +96,13 @@ public class NXCM1008SiteDeployIT
         resource.setProviderRole( WebSiteRepository.class.getName() );
         resource.setProvider( "maven-site" );
         resource.setRepoPolicy( RepositoryPolicy.MIXED.name() ); // TODO: this shouldn't be required
-        resource.setBrowseable( true ); 
+        resource.setBrowseable( true );
         resource.setExposed( true ); // sort of important for a website
         resource.setWritePolicy( RepositoryWritePolicy.ALLOW_WRITE.name() );
         resource.setChecksumPolicy( ChecksumPolicy.WARN.name() );
 
-        RepositoryMessageUtil messageUtil = new RepositoryMessageUtil(
-            this.getJsonXStream(),
-            MediaType.APPLICATION_JSON,
-            getRepositoryTypeRegistry() );
+        RepositoryMessageUtil messageUtil =
+            new RepositoryMessageUtil( this.getJsonXStream(), MediaType.APPLICATION_JSON, getRepositoryTypeRegistry() );
         // this also validates
         // TODO: No, it's not: it uses RepositoryTypeRegistry from _this_ plexus container,
         // while Nexus (where the new repo type is registered) runs in Guice!
