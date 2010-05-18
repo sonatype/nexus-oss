ebd0eb7805b7c6a2a11c8771c0825df0e87c7a0b
Marvin Froeder <velo@sonatype.com>
2010-05-18 11:55:14 +0000

    OPEN - issue NEXUS-977: Add ability to group other groups too, to make possible for a group to contain not only repositories https://issues.sonatype.org/browse/NEXUS-977
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@6463 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

---
+++ nexus/nexus-test-harness/nexus-test-harness-its/src/test/java/org/sonatype/nexus/integrationtests/nexus977browse/Nexus977GroupOfGroupsBrowsingIT.java
@@ -0,0 +1,9 @@
+package org.sonatype.nexus.integrationtests.nexus977browse;
+
+import org.sonatype.nexus.integrationtests.nexus947.Nexus947GroupBrowsingIT;
+
+public class Nexus977GroupOfGroupsBrowsingIT
+    extends Nexus947GroupBrowsingIT
+{
+
+}
