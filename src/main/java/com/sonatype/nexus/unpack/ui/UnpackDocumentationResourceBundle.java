59e45282e35055fb1378482ca9c2f88a61052e51
Marvin Froeder <velo@sonatype.com>
2010-06-14 16:57:45 +0000

    OPEN - issue NXCM-2124: Bundle Enunciate https://issues.sonatype.org/browse/NXCM-2124
    git-svn-id: https://svn.sonatype.com/repos/code/products/nexus-enterprise/trunk@14676 839f2357-842e-4662-a642-f9b39ea64065

---
+++ src/main/java/com/sonatype/nexus/unpack/ui/UnpackDocumentationResourceBundle.java
@@ -0,0 +1,18 @@
+package com.sonatype.nexus.unpack.ui;
+
+import org.codehaus.plexus.component.annotations.Component;
+import org.sonatype.nexus.plugins.rest.AbstractDocumentationNexusResourceBundle;
+import org.sonatype.nexus.plugins.rest.NexusResourceBundle;
+
+@Component( role = NexusResourceBundle.class, hint = "UnpackDocumentationResourceBundle" )
+public class UnpackDocumentationResourceBundle
+    extends AbstractDocumentationNexusResourceBundle
+{
+
+    @Override
+    public String getPluginId()
+    {
+        return "nexus-unpack-plugin";
+    }
+
+}
