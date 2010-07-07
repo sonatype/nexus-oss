4f26c96a0cd6d1b5cb8302fe7f0ff5aa40fda5d6
Marvin Froeder <velo@sonatype.com>
2010-07-07 12:44:07 +0000

    Rework Search Add ArtifactInfo rest resource
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@6713 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-indexer-lucene/nexus-indexer-lucene-rest-api/src/main/java/org/sonatype/nexus/rest/artifact/ArtifactInfoContentProvider.java
+++ nexus/nexus-indexer-lucene/nexus-indexer-lucene-rest-api/src/main/java/org/sonatype/nexus/rest/artifact/ArtifactInfoContentProvider.java
@@ -2,26 +2,33 @@ package org.sonatype.nexus.rest.artifact;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
 import org.codehaus.plexus.component.annotations.Component;
 import org.codehaus.plexus.component.annotations.Requirement;
+import org.codehaus.plexus.logging.AbstractLogEnabled;
+import org.sonatype.nexus.index.ArtifactInfo;
+import org.sonatype.nexus.index.IndexerManager;
+import org.sonatype.nexus.index.IteratorSearchResponse;
+import org.sonatype.nexus.proxy.NoSuchRepositoryException;
 import org.sonatype.nexus.proxy.access.AccessManager;
 import org.sonatype.nexus.proxy.attributes.inspectors.DigestCalculatingInspector;
 import org.sonatype.nexus.proxy.item.StorageFileItem;
 import org.sonatype.nexus.proxy.item.StorageItem;
-import org.sonatype.nexus.proxy.registry.RepositoryRegistry;
 import org.sonatype.nexus.rest.ArtifactViewProvider;
 import org.sonatype.nexus.rest.model.ArtifactInfoResource;
 import org.sonatype.nexus.rest.model.ArtifactInfoResourceResponse;
 
 @Component( role = ArtifactViewProvider.class, hint = "artifactInfo" )
 public class ArtifactInfoContentProvider
+    extends AbstractLogEnabled
     implements ArtifactViewProvider
 {
-    @Requirement( role = RepositoryRegistry.class, hint = "protected" )
-    private RepositoryRegistry repositoryRegistry;
+
+    @Requirement
+    private IndexerManager indexerManager;
 
     public Object retrieveView( StorageItem item )
         throws IOException
@@ -31,15 +38,27 @@ public class ArtifactInfoContentProvider
             return null;
         }
 
-        // for "repositories" (added below as that):
-        // notQuiteTrue #1: an artifact may be _duplicated_ in some other repository, this is neglecting that fact
-        // notQuiteTrue #2: think about repository Routes
-        // notQuitetrue #3: artifact accessed over direct repository URL is not listed here, and this is wrong
-        Set<String> groups = new LinkedHashSet<String>();
-        groups.add( item.getRepositoryId() );
-        groups.addAll( repositoryRegistry.getGroupsOfRepository( item.getRepositoryId() ) );
-
         StorageFileItem fileItem = (StorageFileItem) item;
+
+        Set<String> repositories = new LinkedHashSet<String>();
+        try
+        {
+            IteratorSearchResponse searchResponse =
+                indexerManager.searchArtifactSha1ChecksumIterator(
+                    fileItem.getAttributes().get( DigestCalculatingInspector.DIGEST_SHA1_KEY ), null, null, null, null,
+                    null );
+
+            for ( Iterator<ArtifactInfo> iterator = searchResponse.iterator(); iterator.hasNext(); )
+            {
+                ArtifactInfo info = iterator.next();
+                repositories.add( info.repository );
+            }
+        }
+        catch ( NoSuchRepositoryException e )
+        {
+            // should never trigger this exception since I'm searching on all repositories
+            getLogger().error( e.getMessage(), e );
+        }
 
         ArtifactInfoResourceResponse result = new ArtifactInfoResourceResponse();
 
@@ -49,7 +68,7 @@ public class ArtifactInfoContentProvider
         resource.setRepositoryPath( item.getRepositoryItemUid().getPath() );
         resource.setSha1Hash( fileItem.getAttributes().get( DigestCalculatingInspector.DIGEST_SHA1_KEY ) );
         resource.setLastChanged( fileItem.getModified() );
-        resource.setRepositories( new ArrayList<String>( groups ) );
+        resource.setRepositories( new ArrayList<String>( repositories ) );
         resource.setSize( fileItem.getLength() );
         resource.setUploaded( fileItem.getCreated() );
         resource.setUploader( fileItem.getAttributes().get( AccessManager.REQUEST_USER ) );
