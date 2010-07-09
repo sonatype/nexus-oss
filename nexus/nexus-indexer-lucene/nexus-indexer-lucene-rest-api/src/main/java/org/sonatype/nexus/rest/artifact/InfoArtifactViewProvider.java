0a18d76880d2dffabad55e7887624b98fcb10733
Marvin Froeder <velo@sonatype.com>
2010-07-09 17:40:23 +0000

    Incomplete - issue NEXUS-3615: New search / repo screens https://issues.sonatype.org/browse/NEXUS-3615 Get repository url for pom artifacts too
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@6742 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-indexer-lucene/nexus-indexer-lucene-rest-api/src/main/java/org/sonatype/nexus/rest/artifact/InfoArtifactViewProvider.java
+++ nexus/nexus-indexer-lucene/nexus-indexer-lucene-rest-api/src/main/java/org/sonatype/nexus/rest/artifact/InfoArtifactViewProvider.java
@@ -15,11 +15,14 @@ import org.sonatype.nexus.index.ArtifactInfo;
 import org.sonatype.nexus.index.IndexerManager;
 import org.sonatype.nexus.index.IteratorSearchResponse;
 import org.sonatype.nexus.proxy.NoSuchRepositoryException;
+import org.sonatype.nexus.proxy.ResourceStoreRequest;
 import org.sonatype.nexus.proxy.access.AccessManager;
 import org.sonatype.nexus.proxy.attributes.inspectors.DigestCalculatingInspector;
+import org.sonatype.nexus.proxy.item.RepositoryItemUid;
 import org.sonatype.nexus.proxy.item.StorageFileItem;
 import org.sonatype.nexus.proxy.item.StorageItem;
 import org.sonatype.nexus.proxy.registry.RepositoryRegistry;
+import org.sonatype.nexus.proxy.repository.Repository;
 import org.sonatype.nexus.rest.ArtifactViewProvider;
 import org.sonatype.nexus.rest.model.ArtifactInfoResource;
 import org.sonatype.nexus.rest.model.ArtifactInfoResourceResponse;
@@ -71,16 +74,27 @@ public class InfoArtifactViewProvider
             getLogger().error( e.getMessage(), e );
         }
 
+        RepositoryItemUid itemUid = item.getRepositoryItemUid();
+
+        // hosted / cache check usefull if the index is out to date or disable
+        for ( Repository repo : repositoryRegistry.getRepositories() )
+        {
+            if ( repo.getLocalStorage().containsItem( repo, new ResourceStoreRequest( itemUid.getPath() ) ) )
+            {
+                repositories.add( repo.getId() );
+            }
+        }
+
         ArtifactInfoResourceResponse result = new ArtifactInfoResourceResponse();
 
         ArtifactInfoResource resource = new ArtifactInfoResource();
-        resource.setRepositoryId( item.getRepositoryItemUid().getRepository().getId() );
-        resource.setRepositoryName( item.getRepositoryItemUid().getRepository().getName() );
-        resource.setRepositoryPath( item.getRepositoryItemUid().getPath() );
+        resource.setRepositoryId( itemUid.getRepository().getId() );
+        resource.setRepositoryName( itemUid.getRepository().getName() );
+        resource.setRepositoryPath( itemUid.getPath() );
         resource.setMd5Hash( fileItem.getAttributes().get( DigestCalculatingInspector.DIGEST_MD5_KEY ) );
         resource.setSha1Hash( fileItem.getAttributes().get( DigestCalculatingInspector.DIGEST_SHA1_KEY ) );
         resource.setLastChanged( fileItem.getModified() );
-        resource.setRepositories( createRepositoriesUrl( repositories, req, item.getRepositoryItemUid().getPath() ) );
+        resource.setRepositories( createRepositoriesUrl( repositories, req, itemUid.getPath() ) );
         resource.setSize( fileItem.getLength() );
         resource.setUploaded( fileItem.getCreated() );
         resource.setUploader( fileItem.getAttributes().get( AccessManager.REQUEST_USER ) );
