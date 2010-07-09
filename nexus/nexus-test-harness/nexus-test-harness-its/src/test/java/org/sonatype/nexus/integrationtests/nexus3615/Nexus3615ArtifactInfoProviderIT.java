9715eca17e5a043db760f259ee3d62b9b2d867b7
Marvin Froeder <velo@sonatype.com>
2010-07-09 15:38:23 +0000

    Incomplete - issue NEXUS-3615: New search / repo screens https://issues.sonatype.org/browse/NEXUS-3615 Add ArtifactInfo rest resource
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@6737 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-test-harness/nexus-test-harness-its/src/test/java/org/sonatype/nexus/integrationtests/nexus3615/Nexus3615ArtifactInfoProviderIT.java
+++ nexus/nexus-test-harness/nexus-test-harness-its/src/test/java/org/sonatype/nexus/integrationtests/nexus3615/Nexus3615ArtifactInfoProviderIT.java
@@ -1,12 +1,15 @@
 package org.sonatype.nexus.integrationtests.nexus3615;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.internal.matchers.IsCollectionContaining;
 import org.sonatype.nexus.integrationtests.AbstractNexusIntegrationTest;
 import org.sonatype.nexus.rest.model.ArtifactInfoResource;
+import org.sonatype.nexus.rest.model.RepositoryUrlResource;
 
 public class Nexus3615ArtifactInfoProviderIT
     extends AbstractNexusIntegrationTest
@@ -36,9 +39,20 @@ public class Nexus3615ArtifactInfoProviderIT
         Assert.assertEquals( "/nexus3615/artifact/1.0/artifact-1.0.jar", info.getRepositoryPath() );
         Assert.assertEquals( "b354a0022914a48daf90b5b203f90077f6852c68", info.getSha1Hash() );
         Assert.assertEquals( 3, info.getRepositories().size() );
-        Assert.assertThat( info.getRepositories(), IsCollectionContaining.hasItems( REPO_TEST_HARNESS_REPO,
-            REPO_TEST_HARNESS_REPO2, REPO_TEST_HARNESS_RELEASE_REPO ) );
+        Assert.assertThat( getRepositoryId( info.getRepositories() ), IsCollectionContaining.hasItems(
+            REPO_TEST_HARNESS_REPO, REPO_TEST_HARNESS_REPO2, REPO_TEST_HARNESS_RELEASE_REPO ) );
         Assert.assertEquals( "application/java-archive", info.getMimeType() );
         Assert.assertEquals( 1364, info.getSize() );
+    }
+
+    private Iterable<String> getRepositoryId( List<RepositoryUrlResource> repositories )
+    {
+        List<String> repoIds = new ArrayList<String>();
+        for ( RepositoryUrlResource repositoryUrlResource : repositories )
+        {
+            repoIds.add( repositoryUrlResource.getRepositoryId() );
+        }
+
+        return repoIds;
     }
 }
