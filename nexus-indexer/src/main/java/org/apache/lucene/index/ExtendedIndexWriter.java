e5cdfca00f7d33aee6dc1c9ae415c615f29bb856
Tamas Cservenak <cstamas@sonatype.com>
2010-08-16 22:17:24 +0000

    Making two classes compatible.
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@7020 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus-indexer/src/main/java/org/apache/lucene/index/ExtendedIndexWriter.java
+++ nexus-indexer/src/main/java/org/apache/lucene/index/ExtendedIndexWriter.java
@@ -6,15 +6,27 @@ import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.LockObtainFailedException;
 
+/**
+ * Hacky class.
+ * 
+ * @author cstamas
+ */
 public class ExtendedIndexWriter
     extends IndexWriter
 {
+    @Deprecated
     public ExtendedIndexWriter( Directory d, boolean autoCommit, Analyzer a, boolean create )
         throws CorruptIndexException, LockObtainFailedException, IOException
     {
         super( d, autoCommit, a, create );
     }
-    
+
+    public ExtendedIndexWriter( Directory d, Analyzer a, boolean create, MaxFieldLength mfl )
+        throws CorruptIndexException, LockObtainFailedException, IOException
+    {
+        super( d, a, create, mfl );
+    }
+
     public boolean hasUncommitedChanges()
     {
         return pendingCommit != null;
