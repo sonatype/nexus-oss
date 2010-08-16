be7a793dfbaa68cb6b3469d8a694b1b4d905bae0
Marvin Froeder <velo@sonatype.com>
2010-08-16 13:53:13 +0000

    OPEN - issue NEXUS-3699: Move capabilities plugin to nexus core https://issues.sonatype.org/browse/NEXUS-3699 add UT's ITs for it
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@7015 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-test-harness/nexus-test-harness-its/src/test/java/org/sonatype/nexus/integrationtests/nexus3699/Nexus3699CapabilityIT.java
+++ nexus/nexus-test-harness/nexus-test-harness-its/src/test/java/org/sonatype/nexus/integrationtests/nexus3699/Nexus3699CapabilityIT.java
@@ -8,6 +8,8 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.sonatype.nexus.integrationtests.AbstractNexusIntegrationTest;
 import org.sonatype.nexus.plugins.capabilities.internal.rest.dto.CapabilityListItemResource;
+import org.sonatype.nexus.plugins.capabilities.internal.rest.dto.CapabilityPropertyResource;
+import org.sonatype.nexus.plugins.capabilities.internal.rest.dto.CapabilityResource;
 import org.sonatype.nexus.test.utils.CapabilitiesMessageUtil;
 
 public class Nexus3699CapabilityIT
@@ -37,8 +39,40 @@ public class Nexus3699CapabilityIT
 
     @Test
     public void crud()
+        throws Exception
     {
+        // create
+        CapabilityResource cap = new CapabilityResource();
+        cap.setName( "crud-test" );
+        cap.setTypeId( "TouchTest" );
+        CapabilityPropertyResource prop = new CapabilityPropertyResource();
+        prop.setKey( "repoOrGroupId" );
+        prop.setValue( "repo_" + REPO_TEST_HARNESS_REPO );
+        cap.addProperty( prop );
+        prop = new CapabilityPropertyResource();
+        prop.setKey( "message" );
+        prop.setValue( "Testing CRUD" );
+        cap.addProperty( prop );
 
+        CapabilityListItemResource r = CapabilitiesMessageUtil.create( cap );
+        Assert.assertNotNull( r.getId() );
+
+        // read
+        CapabilityResource read = CapabilitiesMessageUtil.read( r.getId() );
+        Assert.assertEquals( r.getId(), read.getId() );
+        Assert.assertEquals( cap.getName(), read.getName() );
+        Assert.assertEquals( cap.getTypeId(), read.getTypeId() );
+        Assert.assertEquals( cap.getProperties().size(), read.getProperties().size() );
+
+        // update
+        read.setName( "updateCrudTest" );
+        CapabilityListItemResource updated = CapabilitiesMessageUtil.update( read );
+        Assert.assertEquals( "updateCrudTest", updated.getName() );
+        read = CapabilitiesMessageUtil.read( r.getId() );
+        Assert.assertEquals( "updateCrudTest", read.getName() );
+
+        // delete
+        CapabilitiesMessageUtil.delete( r.getId() );
     }
 
 }
--- nexus/nexus-test-harness/nexus-test-harness-launcher/src/main/java/org/sonatype/nexus/test/utils/CapabilitiesMessageUtil.java
+++ nexus/nexus-test-harness/nexus-test-harness-launcher/src/main/java/org/sonatype/nexus/test/utils/CapabilitiesMessageUtil.java
@@ -4,13 +4,20 @@ import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 
+import junit.framework.Assert;
+
 import org.apache.log4j.Logger;
 import org.restlet.data.MediaType;
+import org.restlet.data.Method;
 import org.restlet.data.Response;
 import org.sonatype.nexus.integrationtests.RequestFacade;
 import org.sonatype.nexus.plugins.capabilities.internal.rest.CapabilitiesPlexusResource;
 import org.sonatype.nexus.plugins.capabilities.internal.rest.dto.CapabilitiesListResponseResource;
 import org.sonatype.nexus.plugins.capabilities.internal.rest.dto.CapabilityListItemResource;
+import org.sonatype.nexus.plugins.capabilities.internal.rest.dto.CapabilityRequestResource;
+import org.sonatype.nexus.plugins.capabilities.internal.rest.dto.CapabilityResource;
+import org.sonatype.nexus.plugins.capabilities.internal.rest.dto.CapabilityResponseResource;
+import org.sonatype.nexus.plugins.capabilities.internal.rest.dto.CapabilityStatusResponseResource;
 import org.sonatype.plexus.rest.representation.XStreamRepresentation;
 
 import com.thoughtworks.xstream.XStream;
@@ -45,6 +52,87 @@ public class CapabilitiesMessageUtil
             (CapabilitiesListResponseResource) representation.getPayload( new CapabilitiesListResponseResource() );
 
         return scheduleResponse.getData();
+    }
+
+    public static CapabilityListItemResource create( CapabilityResource capability )
+        throws IOException
+    {
+        XStreamRepresentation representation = new XStreamRepresentation( xstream, "", MediaType.APPLICATION_XML );
+
+        CapabilityRequestResource envelope = new CapabilityRequestResource();
+        envelope.setData( capability );
+
+        representation.setPayload( envelope );
+
+        Response response = RequestFacade.sendMessage( "service/local/capabilities", Method.POST, representation );
+
+        if ( response.getStatus().isError() )
+        {
+            Assert.fail( response.getStatus().toString() );
+        }
+
+        representation = new XStreamRepresentation( xstream, response.getEntity().getText(), MediaType.APPLICATION_XML );
+
+        CapabilityStatusResponseResource scheduleResponse =
+            (CapabilityStatusResponseResource) representation.getPayload( new CapabilityStatusResponseResource() );
+
+        return scheduleResponse.getData();
+    }
+
+    public static CapabilityResource read( String id )
+        throws IOException
+    {
+        Response response = RequestFacade.doGetRequest( "service/local/capabilities/" + id );
+
+        if ( response.getStatus().isError() )
+        {
+            Assert.fail( response.getStatus().toString() );
+        }
+
+        XStreamRepresentation representation =
+            new XStreamRepresentation( xstream, response.getEntity().getText(), MediaType.APPLICATION_XML );
+
+        CapabilityResponseResource scheduleResponse =
+            (CapabilityResponseResource) representation.getPayload( new CapabilityResponseResource() );
+
+        return scheduleResponse.getData();
+    }
+
+    public static CapabilityListItemResource update( CapabilityResource resource )
+        throws IOException
+    {
+        XStreamRepresentation representation = new XStreamRepresentation( xstream, "", MediaType.APPLICATION_XML );
+
+        CapabilityRequestResource envelope = new CapabilityRequestResource();
+        envelope.setData( resource );
+
+        representation.setPayload( envelope );
+
+        Response response =
+            RequestFacade.sendMessage( "service/local/capabilities/" + resource.getId(), Method.PUT, representation );
+
+        if ( response.getStatus().isError() )
+        {
+            Assert.fail( response.getStatus().toString() );
+        }
+
+        representation = new XStreamRepresentation( xstream, response.getEntity().getText(), MediaType.APPLICATION_XML );
+
+        CapabilityStatusResponseResource scheduleResponse =
+            (CapabilityStatusResponseResource) representation.getPayload( new CapabilityStatusResponseResource() );
+
+        return scheduleResponse.getData();
+    }
+
+    public static void delete( String id )
+        throws IOException
+    {
+        Response response = RequestFacade.sendMessage( "service/local/capabilities/" + id, Method.DELETE );
+
+        if ( response.getStatus().isError() )
+        {
+            Assert.fail( response.getStatus().toString() );
+        }
     }
 
 }
