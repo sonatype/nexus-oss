74d21e338e22fec54e0eab24e6f7bf6266acd084
Marvin Froeder <velo@sonatype.com>
2010-08-26 12:33:12 +0000

    nexus plugin archetype updates testNG
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus-plugins/trunk/nexus-plugin-archetype@522 c41184c3-3edd-4803-b646-023156917d6f

---
+++ nexus-maven-plugins/nexus-plugin-archetype/src/main/resources/archetype-resources/src/test/java/VirusScannerTest.java
@@ -0,0 +1,36 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.sonatype.nexus.proxy.item.StorageFileItem;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class VirusScannerTest
+{
+
+    private VirusScanner scanner;
+
+    @BeforeMethod
+    public void createScanner()
+        throws Exception
+    {
+        scanner = new XYVirusScanner(); // TODO must lookup for it!
+    }
+
+    @Test
+    public void scan()
+    {
+        StorageFileItem file = mock( StorageFileItem.class );
+        when( file.getName() ).thenReturn( "clean-file.xtx" );
+        Assert.assertFalse( scanner.hasVirus( file ) );
+        when( file.getName() ).thenReturn( "infected-file.xtx" );
+        Assert.assertTrue( scanner.hasVirus( file ) );
+    }
+
+}
