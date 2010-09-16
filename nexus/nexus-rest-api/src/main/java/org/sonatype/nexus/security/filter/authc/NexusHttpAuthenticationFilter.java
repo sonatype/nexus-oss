305fd06c56b4fe6eef7bd75bf91097dcb4d3beed
Damian Bradicich <dbradicich@sonatype.com>
2010-09-16 17:55:56 +0000

    NEXUS-3726: re-fixed
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@7196 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-rest-api/src/main/java/org/sonatype/nexus/security/filter/authc/NexusHttpAuthenticationFilter.java
+++ nexus/nexus-rest-api/src/main/java/org/sonatype/nexus/security/filter/authc/NexusHttpAuthenticationFilter.java
@@ -433,8 +433,20 @@ public class NexusHttpAuthenticationFilter
     protected String[] getPrincipalsAndCredentials( String scheme, String encoded )
     {
         String decoded = Base64.decodeToString( encoded );
+        
+        // no credentials, no auth
+        if ( StringUtils.isEmpty( encoded ) )
+        {
+           return null;
+        }
 
         String[] parts = decoded.split( ":" );
+        
+        // invalid credentials, no auth
+        if ( parts == null || parts.length < 2 )
+        {
+            return null;
+        }
 
         return new String[] { parts[0], decoded.substring( parts[0].length() + 1 ) };
     }
