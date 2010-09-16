5788c98ded52c9dc4cf575743f2d4ba43f58eaa2
Tamas Cservenak <cstamas@sonatype.com>
2010-09-16 17:29:36 +0000

    Made the groovy plugin event inspector extend the abstract one.
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@7193 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-core-plugins/nexus-groovy-console-plugin/src/main/java/com/sonatype/nexus/plugin/groovyconsole/DefaultGroovyEventInspector.java
+++ nexus/nexus-core-plugins/nexus-groovy-console-plugin/src/main/java/com/sonatype/nexus/plugin/groovyconsole/DefaultGroovyEventInspector.java
@@ -2,12 +2,15 @@ package com.sonatype.nexus.plugin.groovyconsole;
 
 import org.codehaus.plexus.component.annotations.Component;
 import org.codehaus.plexus.component.annotations.Requirement;
+import org.sonatype.nexus.proxy.events.AbstractEventInspector;
+import org.sonatype.nexus.proxy.events.AsynchronousEventInspector;
 import org.sonatype.nexus.proxy.events.EventInspector;
 import org.sonatype.plexus.appevents.Event;
 
 @Component( role = EventInspector.class, hint = "groovy" )
 public class DefaultGroovyEventInspector
-    implements EventInspector
+    extends AbstractEventInspector
+    implements AsynchronousEventInspector
 {
     @Requirement
     private GroovyScriptManager groovyScriptManager;
