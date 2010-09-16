1739bd39c6c1b1fda1539650721fbe7cc13fac80
Marvin Froeder <velo@sonatype.com>
2010-09-16 17:38:04 +0000

    NEXUS-166
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@7195 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

---
+++ nexus/nexus-test-harness/nexus-test-harness-its/src/test/java/org/sonatype/nexus/integrationtests/nexus166/HardStartBundle.java
@@ -0,0 +1,30 @@
+package org.sonatype.nexus.integrationtests.nexus166;
+
+import java.io.File;
+
+import org.junit.Test;
+import org.sonatype.nexus.test.utils.NexusStatusUtil;
+import org.sonatype.nexus.test.utils.TestProperties;
+
+public class HardStartBundle
+{
+
+    @Test
+    public void runForkedBundle()
+        throws Exception
+    {
+        String nexus =
+            new File( TestProperties.getFile( "nexus-base-dir" ), "bin/jsw/windows-x86-32/nexus.bat" ).getCanonicalPath();
+
+        Process p = Runtime.getRuntime().exec( nexus );
+
+        while ( !new NexusStatusUtil().isNexusRunning() )
+        {
+            System.out.println( "Nexus still not started" );
+        }
+
+        // FIXME that doesn't kill and the IT never exits
+        p.destroy();
+    }
+
+}
