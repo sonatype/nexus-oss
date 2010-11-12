14c33fce37da651daba740a4a052fb7c728f83ef
Brian Demers <bdemers@sonatype.com>
2010-11-12 22:20:40 +0000

    NEXUS-3936, Added test, the fix is part of the security-rest-api project
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@7436 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

---
+++ nexus/nexus-test-harness/nexus-test-harness-its/src/test/java/org/sonatype/nexus/integrationtests/nexus3936/Nexus3936DisableSecurityStatusIT.java
@@ -0,0 +1,40 @@
+package org.sonatype.nexus.integrationtests.nexus3936;
+
+import java.util.List;
+
+import org.sonatype.nexus.integrationtests.AbstractNexusIntegrationTest;
+import org.sonatype.nexus.rest.model.NexusAuthenticationClientPermissions;
+import org.sonatype.nexus.rest.model.StatusResource;
+import org.sonatype.nexus.test.utils.NexusIllegalStateException;
+import org.sonatype.nexus.test.utils.NexusStatusUtil;
+import org.sonatype.security.rest.model.ClientPermission;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Security is already disabled for this Test, we just need to make sure the Status resource returns ALL/15, for all the
+ * permission strings.
+ */
+@Test( groups = { "security", "anonymous", "status" } )
+public class Nexus3936DisableSecurityStatusIT
+    extends AbstractNexusIntegrationTest
+{
+
+    @Test
+    public void testSecurityDisabledStatus()
+        throws NexusIllegalStateException
+    {
+
+        NexusStatusUtil statusUtil = new NexusStatusUtil();
+        StatusResource statusResource = statusUtil.getNexusStatus().getData();
+
+        List<ClientPermission> permisisons = statusResource.getClientPermissions().getPermissions();
+
+        Assert.assertTrue( permisisons.size() > 0, "Permissions are empty, expected a whole bunch, not zero." );
+        for ( ClientPermission clientPermission : permisisons )
+        {
+            Assert.assertEquals( clientPermission.getValue(), 15, "Permission '"+ clientPermission.getId() +"' should have had a value of '15', the value was" + clientPermission.getValue() );
+        }
+        // that is it, just checking the values, when security is disabled, access is WIDE open.
+    }
+}
