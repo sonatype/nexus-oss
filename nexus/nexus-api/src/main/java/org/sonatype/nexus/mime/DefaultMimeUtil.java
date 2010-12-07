c831a347b9b728a9e36f62871894c97e3f2d8410
Tamas Cservenak <cstamas@sonatype.com>
2010-12-07 17:48:54 +0000

    Added very basic support for Unix "magic" mime detection support.
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@7596 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-api/src/main/java/org/sonatype/nexus/mime/DefaultMimeUtil.java
+++ nexus/nexus-api/src/main/java/org/sonatype/nexus/mime/DefaultMimeUtil.java
@@ -12,76 +12,95 @@ import org.codehaus.plexus.component.annotations.Component;
 import eu.medsea.mimeutil.MimeType;
 import eu.medsea.mimeutil.MimeUtil2;
 import eu.medsea.mimeutil.detector.ExtensionMimeDetector;
+import eu.medsea.mimeutil.detector.MagicMimeMimeDetector;
 
 @Component( role = MimeUtil.class )
 public class DefaultMimeUtil
     implements MimeUtil
 {
-    private MimeUtil2 mimeUtil;
+    private MimeUtil2 nonTouchingMimeUtil;
+
+    private MimeUtil2 touchingMimeUtil;
 
     public DefaultMimeUtil()
     {
-        mimeUtil = new MimeUtil2();
+        // MimeUtil2 by design will start (try to) read the file/stream if some "eager" detector is registered
+        // so we follow the "private instance" pattern, and we handle two instances for now
+        
+        // uses Extension only for now (speed, no IO, but less accuracy)
+        nonTouchingMimeUtil = new MimeUtil2();
+        nonTouchingMimeUtil.registerMimeDetector( ExtensionMimeDetector.class.getName() );
 
-        // use Extension only for now (speed but less accuracy)
-        mimeUtil.registerMimeDetector( ExtensionMimeDetector.class.getName() );
+        // uses magic-mime (IO and lower speed but more accuracy)
+        touchingMimeUtil = new MimeUtil2();
+        touchingMimeUtil.registerMimeDetector( MagicMimeMimeDetector.class.getName() );
     }
 
-    public MimeUtil2 getMimeUtil2()
+    protected MimeUtil2 getNonTouchingMimeUtil2()
     {
-        return mimeUtil;
+        return nonTouchingMimeUtil;
+    }
+
+    protected MimeUtil2 getTouchingMimeUtil2()
+    {
+        return touchingMimeUtil;
     }
 
     @Override
     public String getMimeType( String fileName )
     {
-        return MimeUtil2.getMostSpecificMimeType( getMimeUtil2().getMimeTypes( fileName ) ).toString();
+        return MimeUtil2.getMostSpecificMimeType( getNonTouchingMimeUtil2().getMimeTypes( fileName ) ).toString();
     }
 
     @Override
     public String getMimeType( File file )
     {
-        return MimeUtil2.getMostSpecificMimeType( getMimeUtil2().getMimeTypes( file ) ).toString();
+        return MimeUtil2.getMostSpecificMimeType( getNonTouchingMimeUtil2().getMimeTypes( file ) ).toString();
     }
 
     @Override
     public String getMimeType( URL url )
     {
-        return MimeUtil2.getMostSpecificMimeType( getMimeUtil2().getMimeTypes( url ) ).toString();
+        return MimeUtil2.getMostSpecificMimeType( getNonTouchingMimeUtil2().getMimeTypes( url ) ).toString();
     }
-    
+
+    @Override
+    public String getMimeType( InputStream is )
+    {
+        return MimeUtil2.getMostSpecificMimeType( getTouchingMimeUtil2().getMimeTypes( is ) ).toString();
+    }
+
     @Override
     @SuppressWarnings( "unchecked" )
     public Set<String> getMimeTypes( String fileName )
     {
-        return this.toStringSet( getMimeUtil2().getMimeTypes( fileName ) );
+        return this.toStringSet( getNonTouchingMimeUtil2().getMimeTypes( fileName ) );
     }
-    
+
     @Override
     @SuppressWarnings( "unchecked" )
     public Set<String> getMimeTypes( File file )
     {
-        return this.toStringSet( getMimeUtil2().getMimeTypes( file ) );
+        return this.toStringSet( getNonTouchingMimeUtil2().getMimeTypes( file ) );
     }
-    
+
     @Override
     @SuppressWarnings( "unchecked" )
     public Set<String> getMimeTypes( URL url )
     {
-        return this.toStringSet( getMimeUtil2().getMimeTypes( url ) );
+        return this.toStringSet( getNonTouchingMimeUtil2().getMimeTypes( url ) );
     }
-    
 
     @Override
     @SuppressWarnings( "unchecked" )
     public Set<String> getMimeTypes( InputStream is )
     {
-        return toStringSet( getMimeUtil2().getMimeTypes( is ) );
+        return toStringSet( getTouchingMimeUtil2().getMimeTypes( is ) );
     }
-    
+
     // ==
-    
-    private Set<String> toStringSet( Collection<MimeType> mimeTypes)
+
+    private Set<String> toStringSet( Collection<MimeType> mimeTypes )
     {
         Set<String> result = new HashSet<String>();
         for ( MimeType mimeType : mimeTypes )
--- nexus/nexus-api/src/main/java/org/sonatype/nexus/mime/MimeUtil.java
+++ nexus/nexus-api/src/main/java/org/sonatype/nexus/mime/MimeUtil.java
@@ -5,6 +5,12 @@ import java.io.InputStream;
 import java.net.URL;
 import java.util.Set;
 
+/**
+ * A simple component that hides MIME detection code. Singular methods returns the "best" applicable MIME type, while
+ * plural methods returns all detected MIME types in ascending order.
+ * 
+ * @author cstamas
+ */
 public interface MimeUtil
 {
     String getMimeType( String fileName );
@@ -12,6 +18,8 @@ public interface MimeUtil
     String getMimeType( File file );
 
     String getMimeType( URL url );
+
+    String getMimeType( InputStream is );
 
     Set<String> getMimeTypes( String fileName );
 
