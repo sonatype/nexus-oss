1d1c373d3d70ae6570ff4b8e80becfa5d418b31a
Tamas Cservenak <cstamas@sonatype.com>
2010-12-07 17:15:51 +0000

    Mime util API extended but not yet supported
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@7594 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-api/src/main/java/org/sonatype/nexus/mime/DefaultMimeUtil.java
+++ nexus/nexus-api/src/main/java/org/sonatype/nexus/mime/DefaultMimeUtil.java
@@ -1,6 +1,7 @@
 package org.sonatype.nexus.mime;
 
 import java.io.File;
+import java.io.InputStream;
 import java.net.URL;
 import java.util.Collection;
 import java.util.HashSet;
@@ -22,7 +23,7 @@ public class DefaultMimeUtil
     {
         mimeUtil = new MimeUtil2();
 
-        // use Extenaion only for now (speed but less accuracy)
+        // use Extension only for now (speed but less accuracy)
         mimeUtil.registerMimeDetector( ExtensionMimeDetector.class.getName() );
     }
 
@@ -31,38 +32,54 @@ public class DefaultMimeUtil
         return mimeUtil;
     }
 
+    @Override
     public String getMimeType( String fileName )
     {
         return MimeUtil2.getMostSpecificMimeType( getMimeUtil2().getMimeTypes( fileName ) ).toString();
     }
 
+    @Override
     public String getMimeType( File file )
     {
         return MimeUtil2.getMostSpecificMimeType( getMimeUtil2().getMimeTypes( file ) ).toString();
     }
 
+    @Override
     public String getMimeType( URL url )
     {
         return MimeUtil2.getMostSpecificMimeType( getMimeUtil2().getMimeTypes( url ) ).toString();
     }
     
+    @Override
     @SuppressWarnings( "unchecked" )
     public Set<String> getMimeTypes( String fileName )
     {
         return this.toStringSet( getMimeUtil2().getMimeTypes( fileName ) );
     }
     
+    @Override
     @SuppressWarnings( "unchecked" )
     public Set<String> getMimeTypes( File file )
     {
         return this.toStringSet( getMimeUtil2().getMimeTypes( file ) );
     }
     
+    @Override
     @SuppressWarnings( "unchecked" )
     public Set<String> getMimeTypes( URL url )
     {
         return this.toStringSet( getMimeUtil2().getMimeTypes( url ) );
     }
+    
+
+    @Override
+    @SuppressWarnings( "unchecked" )
+    public Set<String> getMimeTypes( InputStream is )
+    {
+        return toStringSet( getMimeUtil2().getMimeTypes( is ) );
+    }
+    
+    // ==
     
     private Set<String> toStringSet( Collection<MimeType> mimeTypes)
     {
--- nexus/nexus-api/src/main/java/org/sonatype/nexus/mime/MimeUtil.java
+++ nexus/nexus-api/src/main/java/org/sonatype/nexus/mime/MimeUtil.java
@@ -1,20 +1,23 @@
 package org.sonatype.nexus.mime;
 
 import java.io.File;
+import java.io.InputStream;
 import java.net.URL;
 import java.util.Set;
 
 public interface MimeUtil
 {
     String getMimeType( String fileName );
-    
+
     String getMimeType( File file );
 
     String getMimeType( URL url );
-    
+
     Set<String> getMimeTypes( String fileName );
-    
+
     Set<String> getMimeTypes( File file );
 
     Set<String> getMimeTypes( URL url );
+
+    Set<String> getMimeTypes( InputStream is );
 }
