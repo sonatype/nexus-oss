d0130fc3f4005563c2419dc18bbb1e9c11a1c34a
Damian Bradicich <dbradicich@sonatype.com>
2010-12-09 20:17:46 +0000

    NEXUS-3979: pulled comparator out into its own class

--- security-rest-api/src/main/java/org/sonatype/security/rest/roles/RoleAndPrivilegeListPlexusResource.java
+++ security-rest-api/src/main/java/org/sonatype/security/rest/roles/RoleAndPrivilegeListPlexusResource.java
@@ -2,7 +2,6 @@ package org.sonatype.security.rest.roles;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 import javax.enterprise.inject.Typed;
@@ -56,16 +55,6 @@ public class RoleAndPrivilegeListPlexusResource
     public static final String REQUEST_START = "start";
 
     public static final String REQUEST_LIMIT = "limit";
-
-    public static final String SORT_TYPE = "type";
-
-    public static final String SORT_NAME = "name";
-
-    public static final String SORT_DESCRIPTION = "description";
-
-    public static final String DIR_ASC = "ASC";
-
-    public static final String DIR_DESC = "DESC";
 
     public RoleAndPrivilegeListPlexusResource()
     {
@@ -276,64 +265,5 @@ public class RoleAndPrivilegeListPlexusResource
         }
 
         return result;
-    }
-
-    private final class RoleAndPrivilegeListResourceComparator
-        implements Comparator<RoleAndPrivilegeListResource>
-    {
-        private final String sort;
-
-        private final String dir;
-
-        public RoleAndPrivilegeListResourceComparator( String sort, String dir )
-        {
-            this.sort = sort;
-            this.dir = dir;
-        }
-
-        public int compare( RoleAndPrivilegeListResource o1, RoleAndPrivilegeListResource o2 )
-        {
-            // always put external realms first
-            if ( o1.isExternal() && !o2.isExternal() )
-            {
-                return -1;
-            }
-            else if ( o2.isExternal() && !o1.isExternal() )
-            {
-                return 1;
-            }
-
-            if ( SORT_TYPE.equals( sort ) )
-            {
-                int compare = doCompare( o1.getType(), o2.getType(), dir );
-                if ( compare == 0 )
-                {
-                    return doCompare( o1.getName(), o2.getName(), dir );
-                }
-                return compare;
-            }
-            else if ( SORT_NAME.equals( sort ) )
-            {
-                return doCompare( o1.getName(), o2.getName(), dir );
-            }
-            else if ( SORT_DESCRIPTION.equals( sort ) )
-            {
-                return doCompare( o1.getDescription(), o2.getDescription(), dir );
-            }
-
-            return 0;
-        }
-
-        private int doCompare( String value1, String value2, String dir )
-        {
-            if ( DIR_DESC.equals( dir ) )
-            {
-                return value2.compareTo( value1 );
-            }
-            else
-            {
-                return value1.compareTo( value2 );
-            }
-        }
     }
 }
---
+++ security-rest-api/src/main/java/org/sonatype/security/rest/roles/RoleAndPrivilegeListResourceComparator.java
@@ -0,0 +1,74 @@
+package org.sonatype.security.rest.roles;
+
+import java.util.Comparator;
+
+import org.sonatype.security.rest.model.RoleAndPrivilegeListResource;
+
+public final class RoleAndPrivilegeListResourceComparator
+    implements Comparator<RoleAndPrivilegeListResource>
+{
+    private final String sort;
+
+    private final String dir;
+
+    public static final String SORT_TYPE = "type";
+
+    public static final String SORT_NAME = "name";
+
+    public static final String SORT_DESCRIPTION = "description";
+
+    public static final String DIR_ASC = "ASC";
+
+    public static final String DIR_DESC = "DESC";
+
+    public RoleAndPrivilegeListResourceComparator( String sort, String dir )
+    {
+        this.sort = sort;
+        this.dir = dir;
+    }
+
+    public int compare( RoleAndPrivilegeListResource o1, RoleAndPrivilegeListResource o2 )
+    {
+        // always put external realms first
+        if ( o1.isExternal() && !o2.isExternal() )
+        {
+            return -1;
+        }
+        else if ( o2.isExternal() && !o1.isExternal() )
+        {
+            return 1;
+        }
+
+        if ( SORT_TYPE.equals( sort ) )
+        {
+            int compare = doCompare( o1.getType(), o2.getType(), dir );
+            if ( compare == 0 )
+            {
+                return doCompare( o1.getName(), o2.getName(), dir );
+            }
+            return compare;
+        }
+        else if ( SORT_NAME.equals( sort ) )
+        {
+            return doCompare( o1.getName(), o2.getName(), dir );
+        }
+        else if ( SORT_DESCRIPTION.equals( sort ) )
+        {
+            return doCompare( o1.getDescription(), o2.getDescription(), dir );
+        }
+
+        return 0;
+    }
+
+    private int doCompare( String value1, String value2, String dir )
+    {
+        if ( DIR_DESC.equals( dir ) )
+        {
+            return value2.compareTo( value1 );
+        }
+        else
+        {
+            return value1.compareTo( value2 );
+        }
+    }
+}
