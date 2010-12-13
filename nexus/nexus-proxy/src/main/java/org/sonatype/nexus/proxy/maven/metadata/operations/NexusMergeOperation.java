72e2ba599f1e9efa419a7ec2f3eea10274b82c6c
Marvin Froeder <velo@sonatype.com>
2010-12-13 18:41:40 +0000

    Add a improvement note
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@7629 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-proxy/src/main/java/org/sonatype/nexus/proxy/maven/metadata/operations/NexusMergeOperation.java
+++ nexus/nexus-proxy/src/main/java/org/sonatype/nexus/proxy/maven/metadata/operations/NexusMergeOperation.java
@@ -26,6 +26,7 @@ public class NexusMergeOperation
     public boolean perform( Metadata targetMetadata )
         throws MetadataException
     {
+        // FIXME Metadata class has now a merge() method, we should use that probably!
         if ( sourceMetadata == null || targetMetadata == null )
         {
             return false;
