0b708cf7b01ca16b00d9d2fced68b9b78cc0fb8a
Marvin Froeder <velo@sonatype.com>
2010-12-17 17:23:36 +0000

    NEXUS-4009 "Expire cache" from "local storage" tab fails with 403 error even when user has "Clear Repository Caches" privilege
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@7658 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus/nexus-rest-api/src/main/java/org/sonatype/nexus/rest/cache/CachePlexusResource.java
+++ nexus/nexus-rest-api/src/main/java/org/sonatype/nexus/rest/cache/CachePlexusResource.java
@@ -63,7 +63,7 @@ public class CachePlexusResource
     @Override
     public PathProtectionDescriptor getResourceProtection()
     {
-        return new PathProtectionDescriptor( "/data_cache/*/*/content**", "authcBasic,perms[nexus:cache]" );
+        return new PathProtectionDescriptor( "/data_cache/*/*/content/**", "authcBasic,perms[nexus:cache]" );
     }
 
     /**
