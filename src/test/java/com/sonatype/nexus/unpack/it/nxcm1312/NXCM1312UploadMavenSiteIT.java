a1e0b3855441c947f4fc4a24bf7da49d0531d186
Brian Demers <bdemers@sonatype.com>
2011-01-04 16:27:57 +0000

    NXCM-2700, Updating license headers
    git-svn-id: https://svn.sonatype.com/repos/code/products/nexus-enterprise/trunk@17454 839f2357-842e-4662-a642-f9b39ea64065

--- src/test/java/com/sonatype/nexus/unpack/it/nxcm1312/NXCM1312UploadMavenSiteIT.java
+++ src/test/java/com/sonatype/nexus/unpack/it/nxcm1312/NXCM1312UploadMavenSiteIT.java
@@ -1,8 +1,9 @@
 /**
- * Sonatype Nexus (TM) Professional Version.
- * Copyright (c) 2008 Sonatype, Inc. All rights reserved.
- * Includes the third-party code listed at http://www.sonatype.com/products/nexus/attributions/.
- * "Sonatype" and "Sonatype Nexus" are trademarks of Sonatype, Inc.
+ * Copyright (c) 2008-2011 Sonatype, Inc.
+ *
+ * All rights reserved. Includes the third-party code listed at http://www.sonatype.com/products/nexus/attributions.
+ * Sonatype and Sonatype Nexus are trademarks of Sonatype, Inc. Apache Maven is a trademark of the Apache Foundation.
+ * M2Eclipse is a trademark of the Eclipse Foundation. All other trademarks are the property of their respective owners.
  */
 package com.sonatype.nexus.unpack.it.nxcm1312;
 
