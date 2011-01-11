0de4aa886c896fa42bc02ddf57fc4bc59e72aabd
Marvin Froeder <velo@sonatype.com>
2011-01-11 11:16:52 +0000

    NEXUS-4038 Artifact deletion events in system feeds are reported as having the user id and IP address that was used during deployment Add IT for it
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/trunk@7706 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

---
+++ nexus/nexus-test-harness/nexus-test-harness-its/src/test/java/org/sonatype/nexus/integrationtests/nexus4038/Nexus4038DeleteSystemFeedIT.java
@@ -0,0 +1,64 @@
+package org.sonatype.nexus.integrationtests.nexus4038;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.testng.Assert.assertTrue;
+
+import java.util.List;
+
+import org.apache.maven.index.artifact.Gav;
+import org.restlet.data.Method;
+import org.restlet.data.Response;
+import org.restlet.data.Status;
+import org.sonatype.nexus.integrationtests.AbstractPrivilegeTest;
+import org.sonatype.nexus.integrationtests.RequestFacade;
+import org.sonatype.nexus.integrationtests.TestContainer;
+import org.sonatype.nexus.test.utils.FeedUtil;
+import org.sonatype.nexus.test.utils.GavUtil;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.sun.syndication.feed.synd.SyndEntry;
+import com.sun.syndication.feed.synd.SyndFeed;
+
+public class Nexus4038DeleteSystemFeedIT
+    extends AbstractPrivilegeTest
+{
+
+    @Test
+    public void delete()
+        throws Exception
+    {
+        giveUserPrivilege( TEST_USER_NAME, "1000" );
+        TestContainer.getInstance().getTestContext().useAdminForRequests();
+
+        Gav gav = GavUtil.newGav( "nexus4038", "artifact", "1.0" );
+        assertTrue( Status.isSuccess( getDeployUtils().deployUsingGavWithRest( REPO_TEST_HARNESS_REPO, gav,
+            getTestFile( "artifact.jar" ) ) ) );
+
+        getEventInspectorsUtil().waitForCalmPeriod();
+
+        TestContainer.getInstance().getTestContext().setUsername( TEST_USER_NAME );
+        TestContainer.getInstance().getTestContext().setPassword( TEST_USER_PASSWORD );
+
+        String serviceURI =
+            "service/local/repositories/" + REPO_TEST_HARNESS_REPO + "/content/"
+                + GavUtil.getRelitiveArtifactPath( gav ).replace( ".jar", ".pom" );
+        Response response = RequestFacade.sendMessage( serviceURI, Method.DELETE );
+        Status status = response.getStatus();
+        Assert.assertTrue( status.isSuccess(), "Fail to delete " + gav + status );
+
+        Thread.sleep( 1000 );
+        getEventInspectorsUtil().waitForCalmPeriod();
+
+        SyndFeed feed = FeedUtil.getFeed( "recentlyChangedArtifacts" );
+
+        List<SyndEntry> entries = feed.getEntries();
+
+        Assert.assertTrue( entries.size() >= 2, "Expected more then 2 entries, but got " + entries.size() + " - "
+            + entries );
+
+        assertThat( entries.get( 0 ).getDescription().getValue(),
+            containsString( "deleted.Action was initiated by user \"" + TEST_USER_NAME + "\"" ) );
+    }
+}
