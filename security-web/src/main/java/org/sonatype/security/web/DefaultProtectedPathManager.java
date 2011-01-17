09d1db0fb9cd4c170cec73b4c290af149b67cc56
bdemers <brian.demers@gmail.com>
2011-01-17 01:06:13 -0500

    Lazy loading path protection.  Basically reimplementing what I had missed from: https://issues.sonatype.org/browse/NEXUS-3111

--- security-web/src/main/java/org/sonatype/security/web/DefaultProtectedPathManager.java
+++ security-web/src/main/java/org/sonatype/security/web/DefaultProtectedPathManager.java
@@ -1,5 +1,9 @@
 package org.sonatype.security.web;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
 import javax.enterprise.inject.Typed;
 import javax.inject.Named;
 import javax.inject.Singleton;
@@ -20,14 +24,36 @@ public class DefaultProtectedPathManager
 {
     private FilterChainManager filterChainManager;
     
+    protected Map<String, String> pseudoChains = new LinkedHashMap<String, String>();
+    
+
     public void addProtectedResource( String pathPattern, String filterExpression )
     {
-        this.filterChainManager.createChain( pathPattern, filterExpression );
+        // Only save the pathPattern and filterExpression in the pseudoChains, does not put real filters into the real
+        // chain.
+        // We can not get the real filters because this method is invoked when the application is starting, when ShiroSecurityFilter
+        // might not be located.
+     
+        if( this.filterChainManager != null )
+        {
+            this.filterChainManager.createChain( pathPattern, filterExpression );
+        }
+        else
+        {
+            this.pseudoChains.put( pathPattern, filterExpression );
+        }
     }
 
     public void setFilterChainManager( FilterChainManager filterChainManager )
     {
         this.filterChainManager = filterChainManager;
+        
+        // lazy load: see https://issues.sonatype.org/browse/NEXUS-3111
+        // which to me seems like a glassfish bug...
+        for ( Entry<String, String> entry : this.pseudoChains.entrySet() )
+        {
+            this.filterChainManager.createChain( entry.getKey(), entry.getValue() );
+        }
     }
 
 }
