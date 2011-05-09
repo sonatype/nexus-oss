d2139fc92fefab93b35c1f5de85bcc79de11bda0
Tamas Cservenak <tamas@cservenak.net>
2011-05-09 15:51:23 +0200

    Removed (unused) stuff in favor of SISU-57, which is already providing us this capability.

--- nexus/nexus-api/src/main/java/org/sonatype/nexus/logging/AbstractLoggingComponent.java
+++ nexus/nexus-api/src/main/java/org/sonatype/nexus/logging/AbstractLoggingComponent.java
@@ -23,42 +23,20 @@ import javax.inject.Inject;
 import org.codehaus.plexus.component.annotations.Requirement;
 import org.slf4j.Logger;
 
+/**
+ * Coming from Plexus, do not use this! Use {@code @Inject org.slf4j.Logger} instead!
+ * 
+ * @author cstamas
+ * @deprecated Just use @Inject org.slf4j.Logger instead!
+ */
 public abstract class AbstractLoggingComponent
 {
-    private Logger logger;
-
-    // TODO: double annos here, to be able to use this in Nexus plugins but also in Nexus Core while transitioning!
     @Inject
     @Requirement
-    private LoggerProvider loggerProvider;
-
-    /**
-     * Default constructor.
-     */
-    // TODO Drop this when switching to Guice (use constructor injection)
-    public AbstractLoggingComponent()
-    {
-
-    }
-
-    /**
-     * Constructor.
-     * 
-     * @param loggerProvider logger provider
-     */
-    @Inject
-    public AbstractLoggingComponent( final LoggerProvider loggerProvider )
-    {
-        this.loggerProvider = loggerProvider;
-    }
+    private Logger logger;
 
     protected Logger getLogger()
     {
-        if ( logger == null )
-        {
-            logger = loggerProvider.getLogger( this.getClass().getName() );
-        }
-
         return logger;
     }
 }
