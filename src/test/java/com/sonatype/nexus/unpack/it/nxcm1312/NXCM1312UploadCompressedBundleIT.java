c067315aae9f1bb2c74b0e1a677e2eac81cf3c5c
bdemers <brian.demers@gmail.com>
2011-06-07 09:12:03 -0400

    NEXUS-4352, first pass at releasing the restlet response after we are done with it.  Fixes a couple compilation problems and tests that had been broken from some core changes.

--- src/test/java/com/sonatype/nexus/unpack/it/nxcm1312/NXCM1312UploadCompressedBundleIT.java
+++ src/test/java/com/sonatype/nexus/unpack/it/nxcm1312/NXCM1312UploadCompressedBundleIT.java
@@ -12,6 +12,7 @@ import java.io.File;
 import org.restlet.data.MediaType;
 import org.restlet.data.Method;
 import org.restlet.data.Response;
+import org.restlet.data.Status;
 import org.restlet.resource.FileRepresentation;
 import org.restlet.resource.Representation;
 import org.sonatype.nexus.integrationtests.RequestFacade;
@@ -28,24 +29,28 @@ public class NXCM1312UploadCompressedBundleIT
     public void upload()
         throws Exception
     {
-        getDeployUtils().deployWithWagon( "http",
-                                          nexusBaseUrl + "service/local/repositories/" + REPO_TEST_HARNESS_REPO
-                                              + "/content-compressed", getTestFile( "bundle.zip" ), "" );
-        
+        getDeployUtils().deployWithWagon(
+            "http",
+            nexusBaseUrl + "service/local/repositories/" + REPO_TEST_HARNESS_REPO + "/content-compressed",
+            getTestFile( "bundle.zip" ),
+            "" );
+
         getEventInspectorsUtil().waitForCalmPeriod();
-        
+
         Assert.assertEquals( getSearchMessageUtil().searchForGav( "nxcm1312", "artifact", "2.0" ).size(), 1 );
-        Assert.assertEquals( getSearchMessageUtil().searchForGav( "org.nxcm1312", "maven-deploy-released", "1.0" ).size(),
-                             1 );
+        Assert.assertEquals( getSearchMessageUtil()
+            .searchForGav( "org.nxcm1312", "maven-deploy-released", "1.0" ).size(), 1 );
     }
 
     @Test( dependsOnMethods = "upload" )
     public void uploadWithPath()
         throws Exception
     {
-        getDeployUtils().deployWithWagon( "http",
-                                          nexusBaseUrl + "service/local/repositories/" + REPO_TEST_HARNESS_REPO
-                                              + "/content-compressed", getTestFile( "bundle.zip" ), "some/path" );
+        getDeployUtils().deployWithWagon(
+            "http",
+            nexusBaseUrl + "service/local/repositories/" + REPO_TEST_HARNESS_REPO + "/content-compressed",
+            getTestFile( "bundle.zip" ),
+            "some/path" );
 
         // Check for the parent folder, it should been created
         File root = new File( nexusWorkDir, "storage/nexus-test-harness-repo/some/path" );
@@ -64,32 +69,34 @@ public class NXCM1312UploadCompressedBundleIT
         // 5. then we upload bundle1.zip again, this time with delete flag
         // 6. then we validate that upload happened okay, and root directory nxcm1312 is deleted
 
-        FileRepresentation bundleRepresentation =
-            new FileRepresentation( getTestFile( "bundle.zip" ), MediaType.APPLICATION_ZIP );
-        FileRepresentation bundle1Representation =
-            new FileRepresentation( getTestFile( "bundle1.zip" ), MediaType.APPLICATION_ZIP );
+        FileRepresentation bundleRepresentation = new FileRepresentation(
+            getTestFile( "bundle.zip" ),
+            MediaType.APPLICATION_ZIP );
+        FileRepresentation bundle1Representation = new FileRepresentation(
+            getTestFile( "bundle1.zip" ),
+            MediaType.APPLICATION_ZIP );
 
         // 1. upload the bundle
-        Assert.assertTrue( uploadBundle( false, bundleRepresentation ).getStatus().isSuccess() );
+        Assert.assertTrue( uploadBundle( false, bundleRepresentation ).isSuccess() );
 
         // 2. validate all is there
         validateBundleUpload( true, "org", "nxcm1312" );
 
         // 3. upload bundle1.zip (that lacks nxcm1312 root dir but do not use delete flag)
-        Assert.assertTrue( uploadBundle( false, bundle1Representation ).getStatus().isSuccess() );
+        Assert.assertTrue( uploadBundle( false, bundle1Representation ).isSuccess() );
 
         // 4. validate, all should be still there (did not delete)
         validateBundleUpload( true, "org", "nxcm1312" );
 
         // 5. upload bundle1.zip (that lacks nxcm1312 root dir but this time DO USE delete flag)
-        Assert.assertTrue( uploadBundle( true, bundle1Representation ).getStatus().isSuccess() );
+        Assert.assertTrue( uploadBundle( true, bundle1Representation ).isSuccess() );
 
         // 4. validate, nxcm1312 should not be there anymore
         validateBundleUpload( true, "org" );
         validateBundleUpload( false, "nxcm1312" );
     }
 
-    protected Response uploadBundle( boolean useDeleteFlag, Representation bundleRepresentation )
+    protected Status uploadBundle( boolean useDeleteFlag, Representation bundleRepresentation )
         throws Exception
     {
         String serviceUrl = "service/local/repositories/" + REPO_TEST_HARNESS_REPO + "/content-compressed";
@@ -99,9 +106,17 @@ public class NXCM1312UploadCompressedBundleIT
             serviceUrl = serviceUrl + "?delete";
         }
 
-        Response response = RequestFacade.sendMessage( serviceUrl, Method.PUT, bundleRepresentation );
+        Response response = null;
+        try
+        {
+            response = RequestFacade.sendMessage( serviceUrl, Method.PUT, bundleRepresentation );
 
-        return response;
+            return response.getStatus();
+        }
+        finally
+        {
+            RequestFacade.releaseResponse( response );
+        }
     }
 
     protected void validateBundleUpload( boolean checkForPresence, String... presentRootDirectories )
@@ -113,13 +128,15 @@ public class NXCM1312UploadCompressedBundleIT
         {
             if ( checkForPresence )
             {
-                Assert.assertTrue( new File( repositoryRootDirectory, presentRootDirectory ).isDirectory(),
-                                   "Directory should exists with name: " + presentRootDirectory );
+                Assert.assertTrue(
+                    new File( repositoryRootDirectory, presentRootDirectory ).isDirectory(),
+                    "Directory should exists with name: " + presentRootDirectory );
             }
             else
             {
-                Assert.assertFalse( new File( repositoryRootDirectory, presentRootDirectory ).exists(),
-                                    "File should not exists: " + presentRootDirectory );
+                Assert.assertFalse(
+                    new File( repositoryRootDirectory, presentRootDirectory ).exists(),
+                    "File should not exists: " + presentRootDirectory );
             }
         }
     }
