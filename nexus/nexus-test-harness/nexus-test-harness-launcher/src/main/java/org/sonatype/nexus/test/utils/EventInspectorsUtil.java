25559ca55cf8f190969295915f4c514e8daf7088
Peter Lynch <peter@peterlynch.ca>
2011-06-09 00:01:05 -0400

    NEXUS-4352

--- nexus/nexus-test-harness/nexus-test-harness-launcher/src/main/java/org/sonatype/nexus/test/utils/EventInspectorsUtil.java
+++ nexus/nexus-test-harness/nexus-test-harness-launcher/src/main/java/org/sonatype/nexus/test/utils/EventInspectorsUtil.java
@@ -36,31 +36,31 @@ public class EventInspectorsUtil
     protected boolean isCalmPeriod()
         throws IOException
     {
-        final Response response = RequestFacade.doGetRequest( "service/local/eventInspectors/isCalmPeriod" );
+        final Status status = RequestFacade.doGetForStatus( "service/local/eventInspectors/isCalmPeriod" );
 
-        if ( response.getStatus().isSuccess() )
+        if ( status.isSuccess() )
         {
             // only 200 Ok means calm period,
             // otherwise 202 Accepted is returned
-            return response.getStatus().getCode() == Status.SUCCESS_OK.getCode();
+            return status.getCode() == Status.SUCCESS_OK.getCode();
         }
         else
         {
             throw new IOException( "The isCalmPeriod REST resource reported an error ("
-                + response.getStatus().toString() + "), bailing out!" );
+                + status + "), bailing out!" );
         }
     }
 
     public void waitForCalmPeriod()
         throws IOException, InterruptedException
     {
-        final Response response =
-            RequestFacade.doGetRequest( "service/local/eventInspectors/isCalmPeriod?waitForCalm=true" );
+        final Status status =
+            RequestFacade.doGetForStatus( "service/local/eventInspectors/isCalmPeriod?waitForCalm=true" );
 
-        if ( response.getStatus().getCode() != Status.SUCCESS_OK.getCode() )
+        if ( status.getCode() != Status.SUCCESS_OK.getCode() )
         {
             throw new IOException( "The isCalmPeriod REST resource reported an error ("
-                + response.getStatus().toString() + "), bailing out!" );
+                + status.toString() + "), bailing out!" );
         }
     }
 }
