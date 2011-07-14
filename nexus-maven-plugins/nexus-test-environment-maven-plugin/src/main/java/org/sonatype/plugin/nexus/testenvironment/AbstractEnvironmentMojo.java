0a39217a38f6e425146e789792431eb66db47e86
Alin Dreghiciu <adreghiciu@gmail.com>
2011-07-14 14:27:59 +0300

    Make nexus test env. plugin unpack if not present a logback conf to be used for tests, so is not necessary to have on ein each module having nexus ITs

--- nexus-maven-plugins/nexus-test-environment-maven-plugin/src/main/java/org/sonatype/plugin/nexus/testenvironment/AbstractEnvironmentMojo.java
+++ nexus-maven-plugins/nexus-test-environment-maven-plugin/src/main/java/org/sonatype/plugin/nexus/testenvironment/AbstractEnvironmentMojo.java
@@ -105,7 +105,7 @@ public class AbstractEnvironmentMojo
 
     /**
      * The maven project.
-     *
+     * 
      * @parameter expression="${project}"
      * @required
      * @readonly
@@ -114,7 +114,7 @@ public class AbstractEnvironmentMojo
 
     /**
      * Where nexus instance should be extracted
-     *
+     * 
      * @parameter default-value="${project.build.directory}/nexus"
      * @required
      */
@@ -122,7 +122,7 @@ public class AbstractEnvironmentMojo
 
     /**
      * Artifact file containing nexus bundle
-     *
+     * 
      * @parameter
      */
     protected MavenArtifact nexusBundleArtifact;
@@ -130,49 +130,49 @@ public class AbstractEnvironmentMojo
     /**
      * Name of teh directory created out of nexus artifact bundle. Default is
      * ${nexusBundleArtifactId}-${nexusBundleArtifactVersion}.
-     *
+     * 
      * @parameter
      */
     protected String nexusBundleName;
 
     /**
      * Emma used on ITs
-     *
+     * 
      * @parameter
      */
     private MavenArtifact emmaArtifact;
 
     /**
      * Nexus plugin artifacts to be installed into the Nexus instance under test.
-     *
+     * 
      * @parameter
      */
     private MavenArtifact[] nexusPluginsArtifacts;
 
     /**
      * Resources to be unpacked and then contents copied into Nexus default-configs
-     *
+     * 
      * @parameter
      */
     private MavenArtifact[] extraResourcesArtifacts;
 
     /**
      * When true setup a maven instance
-     *
+     * 
      * @parameter default-value="true"
      */
     private boolean setupMaven;
 
     /**
      * When true setup emma
-     *
+     * 
      * @parameter default-value="true"
      */
     private boolean setupEmma;
 
     /**
      * Maven used on ITs
-     *
+     * 
      * @parameter
      * @see EnvironmentMojo#setupMaven
      */
@@ -180,7 +180,7 @@ public class AbstractEnvironmentMojo
 
     /**
      * Where Maven instance should be created
-     *
+     * 
      * @parameter default-value="${project.build.directory}/maven"
      * @see EnvironmentMojo#setupMaven
      */
@@ -188,7 +188,7 @@ public class AbstractEnvironmentMojo
 
     /**
      * Resources in the test project can be added beneath this directory so that
-     *
+     * 
      * @parameter default-value="${basedir}/resources"
      */
     protected File resourcesSourceLocation;
@@ -197,7 +197,7 @@ public class AbstractEnvironmentMojo
      * This directory is where the default-configs included inside the this plugin will be extracted to BEFORE they are
      * copied into the nexus work dir. A project property 'test-resources-folder' contains the absolute path of this
      * directory.
-     *
+     * 
      * @parameter default-value="${project.build.directory}/resources"
      */
     private File resourcesDestinationLocation;
@@ -239,7 +239,7 @@ public class AbstractEnvironmentMojo
 
     /**
      * Known ports can be manually set as part of the configuration
-     *
+     * 
      * @parameter
      */
     @SuppressWarnings( "rawtypes" )
@@ -247,7 +247,7 @@ public class AbstractEnvironmentMojo
 
     /**
      * If true plugin won't include nexus-plugin dependencies with scope test
-     *
+     * 
      * @parameter
      */
     private boolean excludeTestDependencies;
@@ -255,7 +255,7 @@ public class AbstractEnvironmentMojo
     /**
      * Comma separated list of patterns to delete from an unpacked Nexus bundle. Patterns are deleted before any
      * optional plugins or other external resources are installed
-     *
+     * 
      * @parameter
      */
     private String nexusBundleExcludes;
@@ -263,7 +263,7 @@ public class AbstractEnvironmentMojo
     /**
      * If there is one or more *-webapp.zip files in runtime/apps/nexus/plugin-repository, then unpack that zip to nexus
      * base dir and delete the original file
-     *
+     * 
      * @parameter
      */
     private boolean unpackPluginWebapps;
@@ -441,6 +441,12 @@ public class AbstractEnvironmentMojo
 
         addProjectProperties( baseTestProperties );
         // end baseTest.properties
+
+        File logbackConfig = new File( testOutputDirectory, "logback.xml" );
+        if ( !logbackConfig.exists() )
+        {
+            copyUrl( "/test-config/logback.xml", logbackConfig );
+        }
 
         copyExtraResources();
 
@@ -876,8 +882,6 @@ public class AbstractEnvironmentMojo
         {
             emmaArtifact = new MavenArtifact( "emma", "emma" );
         }
-
-
 
     }
 
