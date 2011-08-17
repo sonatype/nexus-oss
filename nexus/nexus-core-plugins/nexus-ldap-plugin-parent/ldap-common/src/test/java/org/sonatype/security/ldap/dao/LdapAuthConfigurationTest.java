1d36569193be71d36e271c88d48b5daea04e046e
Peter Lynch <peter@peterlynch.ca>
2011-08-16 23:10:27 -0230

    license header fixes, remove duplicate headers, bad headers, add exclusion for nexus-migration-plugin since that is Apache licensed and we don't want to change it
    Also expect license:format to be run only from aggregator pom module and not nexus/nexus

--- nexus/nexus-core-plugins/nexus-ldap-plugin-parent/ldap-common/src/test/java/org/sonatype/security/ldap/dao/LdapAuthConfigurationTest.java
+++ nexus/nexus-core-plugins/nexus-ldap-plugin-parent/ldap-common/src/test/java/org/sonatype/security/ldap/dao/LdapAuthConfigurationTest.java
@@ -1,4 +1,4 @@
-/*
+/**
  * Copyright (c) 2008-2011 Sonatype, Inc.
  * All rights reserved. Includes the third-party code listed at http://www.sonatype.com/products/nexus/attributions.
  *
@@ -16,7 +16,6 @@
  * Sonatype, Inc. Apache Maven is a trademark of the Apache Foundation. M2Eclipse is a trademark of the Eclipse Foundation.
  * All other trademarks are the property of their respective owners.
  */
-
 package org.sonatype.security.ldap.dao;
 
 import junit.framework.Assert;
