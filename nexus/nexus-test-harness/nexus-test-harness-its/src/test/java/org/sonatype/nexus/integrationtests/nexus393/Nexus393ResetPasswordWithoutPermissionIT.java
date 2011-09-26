d8fb6cff893c2741859afad650eb09ce1ab3a47c
Alin Dreghiciu <adreghiciu@gmail.com>
2011-09-26 21:17:54 +0300

    [NEXUS-4455] Split the tests and fix the expected status code (from 401->403 Forbidden). The tests had to be splitter as Nexus393ResetPasswordPermissionIT#resetWithPermission had a side effect on #resetWithoutPermission due to the fact that #resetWithPermission  is resetting password of user used by #resetWithoutPermission so the requests made in that method were getting 401 (not authorized) because the password was reset by first method

--- nexus/nexus-test-harness/nexus-test-harness-its/src/test/java/org/sonatype/nexus/integrationtests/nexus393/Nexus393ResetPasswordWithoutPermissionIT.java
+++ nexus/nexus-test-harness/nexus-test-harness-its/src/test/java/org/sonatype/nexus/integrationtests/nexus393/Nexus393ResetPasswordWithoutPermissionIT.java
@@ -31,7 +31,7 @@ import org.testng.annotations.Test;
  * 
  * TODO Move this tests to Sonatype Security
  */
-public class Nexus393ResetPasswordPermissionIT
+public class Nexus393ResetPasswordWithoutPermissionIT
     extends AbstractPrivilegeTest
 {
 	
@@ -41,25 +41,25 @@ public class Nexus393ResetPasswordPermissionIT
     }
 
     @Test
-    public void resetWithPermission()
+    public void resetWithoutPermission()
         throws Exception
     {
-        overwriteUserRole( TEST_USER_NAME, "anonymous-with-login-reset", "1", "2" /* login */, "6", "14", "17", "19",
-                           "44", "54", "55", "57", "58", "59"/* reset */, "T1", "T2" );
+        overwriteUserRole( TEST_USER_NAME, "anonymous-with-login-but-reset", "1", "2" /* login */, "6", "14", "17",
+                           "19", "44", "54", "55", "57", "58", /* "59" reset , */"T1", "T2" );
 
         TestContainer.getInstance().getTestContext().setUsername( TEST_USER_NAME );
         TestContainer.getInstance().getTestContext().setPassword( TEST_USER_PASSWORD );
 
-        // Should be able to reset anyone password
+        // NOT Shouldn't be able to reset anyone password
         String username = "another-user";
         Response response = ResetPasswordUtils.resetPassword( username );
-        Assert.assertTrue( response.getStatus().isSuccess(), "Status: "+ response.getStatus() );
+        Assert.assertEquals(response.getStatus().getCode(), 403, "Status: "+ response.getStatus() +"\n"+ response.getEntity().getText() );
 
-        // Should be able to reset my own password
+        // NOT Should be able to reset my own password
         username = TEST_USER_NAME;
         response = ResetPasswordUtils.resetPassword( username );
-        Assert.assertTrue( response.getStatus().isSuccess(), "Status: "+ response.getStatus() );
+        Assert.assertEquals( response.getStatus().getCode(), 403, "Status: "+ response.getStatus() +"\n"+ response.getEntity().getText() );
 
     }
-
+    
 }
