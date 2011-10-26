907f59385ba8615a23feeba267f958baa0e57920
bdemers <brian.demers@gmail.com>
2011-10-26 12:34:51 -0400

    Cleaning up the connection release logic and adding better comments in Nexus4257CookieVerificationIT

--- nexus/nexus-test-harness/nexus-test-harness-its/src/test/java/org/sonatype/nexus/integrationtests/nexus4257/Nexus4257CookieVerificationIT.java
+++ nexus/nexus-test-harness/nexus-test-harness-its/src/test/java/org/sonatype/nexus/integrationtests/nexus4257/Nexus4257CookieVerificationIT.java
@@ -24,6 +24,7 @@ import org.apache.commons.httpclient.Cookie;
 import org.apache.commons.httpclient.Header;
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpException;
+import org.apache.commons.httpclient.HttpMethodBase;
 import org.apache.commons.httpclient.UsernamePasswordCredentials;
 import org.apache.commons.httpclient.auth.AuthScope;
 import org.apache.commons.httpclient.methods.GetMethod;
@@ -35,6 +36,7 @@ import org.sonatype.nexus.test.utils.SettingsMessageUtil;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
 
 public class Nexus4257CookieVerificationIT
     extends AbstractNexusIntegrationTest
@@ -72,23 +74,19 @@ public class Nexus4257CookieVerificationIT
         httpClient.getState().setCredentials( AuthScope.ANY, new UsernamePasswordCredentials( username, password ) );
 
         GetMethod getMethod = new GetMethod( url );
-        Assert.assertEquals( httpClient.executeMethod( getMethod ), 200 );
-        getMethod.releaseConnection();
-
+        Assert.assertEquals( executeAndRelease( httpClient, getMethod ), 200 );
         Cookie sessionCookie = this.getSessionCookie( httpClient.getState().getCookies() );
         Assert.assertNotNull( sessionCookie, "Session Cookie not set" );
         httpClient.getState().clear(); // remove cookies, credentials, etc
 
         // do not set the cookie, expect failure
         GetMethod failedGetMethod = new GetMethod( url );
-        Assert.assertEquals( httpClient.executeMethod( failedGetMethod ), 401 );
-        failedGetMethod.releaseConnection();
+        Assert.assertEquals( executeAndRelease( httpClient, failedGetMethod ), 401 );
 
-        // set the cookie expect greatness
+        // set the cookie expect a 200, If a cookie is set, and cannot be found on the server, the response will fail with a 401
         httpClient.getState().addCookie( sessionCookie );
         getMethod = new GetMethod( url );
-        Assert.assertEquals( httpClient.executeMethod( getMethod ), 200 );
-        getMethod.releaseConnection();
+        Assert.assertEquals( executeAndRelease( httpClient, getMethod ), 200 );
     }
     
     @Test
@@ -109,8 +107,7 @@ public class Nexus4257CookieVerificationIT
 
         GetMethod getMethod = new GetMethod( url );
         getMethod.addRequestHeader( header );
-        getMethod.releaseConnection();
-        Assert.assertEquals( httpClient.executeMethod( getMethod ), 200 );
+        Assert.assertEquals( executeAndRelease( httpClient, getMethod ), 200 );
 
         Cookie sessionCookie = this.getSessionCookie( httpClient.getState().getCookies() );
         Assert.assertNull( sessionCookie, "Session Cookie is set" );
@@ -135,19 +132,16 @@ public class Nexus4257CookieVerificationIT
         HttpClient httpClient = new HttpClient(); // anonymous access
 
         GetMethod getMethod = new GetMethod( url );
-        Assert.assertEquals( httpClient.executeMethod( getMethod ), 200 );
-        getMethod.releaseConnection();
+        Assert.assertEquals( executeAndRelease( httpClient, getMethod ), 200 );
 
         Cookie sessionCookie = this.getSessionCookie( httpClient.getState().getCookies() );
         Assert.assertNotNull( sessionCookie, "Session Cookie not set" );
-
         
         httpClient.getState().clear(); // remove cookies, credentials, etc
-        // set the cookie expect greatness
+        // set the cookie expect a 200, If a cookie is set, and cannot be found on the server, the response will fail with a 401
         httpClient.getState().addCookie( sessionCookie );
         getMethod = new GetMethod( url );
-        Assert.assertEquals( httpClient.executeMethod( getMethod ), 200 );
-        getMethod.releaseConnection();
+        Assert.assertEquals( executeAndRelease( httpClient, getMethod ), 200 );
     }
 
 
@@ -171,7 +165,7 @@ public class Nexus4257CookieVerificationIT
 
         GetMethod getMethod = new GetMethod( url );
         getMethod.addRequestHeader( header );
-        Assert.assertEquals( httpClient.executeMethod( getMethod ), 200 );
+        Assert.assertEquals( executeAndRelease( httpClient, getMethod ), 200 );
 
         Cookie sessionCookie = this.getSessionCookie( httpClient.getState().getCookies() );
         Assert.assertNull( sessionCookie, "Session Cookie is set" );
@@ -191,4 +185,19 @@ public class Nexus4257CookieVerificationIT
 
     }
 
+    private int executeAndRelease( HttpClient httpClient, HttpMethodBase method )
+        throws IOException
+    {
+        int status = 0;
+        try
+        {
+            status = httpClient.executeMethod( method );
+        }
+        finally
+        {
+            method.releaseConnection();
+        }
+
+        return status;
+    }
 }
