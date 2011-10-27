eb126cae41b815bfbfb6996367c46a3b596720e0
Tamas Cservenak <tamas@cservenak.net>
2011-10-27 14:30:11 +0200

    NEXUS-4520: added license header and doc'd what is happening here

--- nexus/nexus-test-harness/nexus-test-harness-its/src/test/java/org/sonatype/nexus/integrationtests/nexus4520/Nexus4520PlexusPrefixedVariablesArePickedUpIT.java
+++ nexus/nexus-test-harness/nexus-test-harness-its/src/test/java/org/sonatype/nexus/integrationtests/nexus4520/Nexus4520PlexusPrefixedVariablesArePickedUpIT.java
@@ -1,3 +1,21 @@
+/**
+ * Copyright (c) 2008-2011 Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions
+ *
+ * This program is free software: you can redistribute it and/or modify it only under the terms of the GNU Affero General
+ * Public License Version 3 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License Version 3
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License Version 3 along with this program.  If not, see
+ * http://www.gnu.org/licenses.
+ *
+ * Sonatype Nexus (TM) Open Source Version is available from Sonatype, Inc. Sonatype and Sonatype Nexus are trademarks of
+ * Sonatype, Inc. Apache Maven is a trademark of the Apache Foundation. M2Eclipse is a trademark of the Eclipse Foundation.
+ * All other trademarks are the property of their respective owners.
+ */
 package org.sonatype.nexus.integrationtests.nexus4520;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -16,7 +34,9 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 /**
- * TODO
+ * IT for issue NEXUS-4520, covering the "System properties" case. In short, before Nexus is booted, it sets a
+ * system propery like {@code plexus.foo=bar} (simulating option -Dplexus.foo=bar in wrapper.conf for example) and
+ * then checks for the presence of {@code foo=bar} key-value pair in AppContext.
  *
  * @author: cstamas
  */
@@ -70,7 +90,7 @@ public class Nexus4520PlexusPrefixedVariablesArePickedUpIT
     {
         final String bootLog = new String( fakeOut.toByteArray() );
 
-        // the wrapper log should contain following line:
+        // the boot log (that is NOT in log, but in wrapper.log on real instances) should contain following line:
         // "customKey"="customValue" (raw: "customValue", src: prefixRemove(prefix:plexus., filter(keyStartsWith:[plexus.], system(properties))))
         assertThat( bootLog.contains( String.format(
             "\"%s\"=\"%s\" (raw: \"%s\", src: prefixRemove(prefix:plexus., filter(keyStartsWith:[plexus.], system(properties))))",
