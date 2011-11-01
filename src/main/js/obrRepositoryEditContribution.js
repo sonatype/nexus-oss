1ed39d0a482f413bde8aa824cd61a99d915660bc
Stuart McCulloch <mcculls@gmail.com>
2011-11-01 17:54:47 +0000

    Use anonymous closure to augment behaviour instead of replacing it (helps when we want to add other custom virtual types)

--- src/main/js/obrRepositoryEditContribution.js
+++ src/main/js/obrRepositoryEditContribution.js
@@ -5,41 +5,25 @@
  * Sonatype and Sonatype Nexus are trademarks of Sonatype, Inc. Apache Maven is a trademark of the Apache Foundation.
  * M2Eclipse is a trademark of the Eclipse Foundation. All other trademarks are the property of their respective owners.
  */
-Ext.override(Sonatype.repoServer.VirtualRepositoryEditor, {
-      afterProviderSelectHandler : function(combo, rec, index) {
-        var provider = rec.data.provider;
-        var sourceRepoCombo = this.form.findField('shadowOf');
-        sourceRepoCombo.clearValue();
-        sourceRepoCombo.focus();
-        if (provider == 'obr-shadow')
-        {
-          sourceRepoCombo.store.filterBy(function fn(rec, id) {
-                if (rec.data.repoType != 'virtual' && rec.data.format != 'obr')
-                {
-                  return true;
-                }
-                return false;
-              });
-        }
-        else if (provider == 'm1-m2-shadow')
-        {
-          sourceRepoCombo.store.filterBy(function fn(rec, id) {
-                if (rec.data.repoType != 'virtual' && rec.data.format == 'maven1')
-                {
-                  return true;
-                }
-                return false;
-              });
-        }
-        else if (provider == 'm2-m1-shadow')
-        {
-          sourceRepoCombo.store.filterBy(function fn(rec, id) {
-                if (rec.data.repoType != 'virtual' && rec.data.format == 'maven2')
-                {
-                  return true;
-                }
-                return false;
-              });
-        }
-      }
-    });
+
+/*
+ * Use anonymous closure to augment the current class behaviour
+ */
+(function() {
+  var originalHandler = Sonatype.repoServer.VirtualRepositoryEditor.prototype.afterProviderSelectHandler;
+
+  Ext.override(Sonatype.repoServer.VirtualRepositoryEditor, {
+    afterProviderSelectHandler : function(combo, rec, index) {
+
+      // first invoke the original behaviour
+      originalHandler.apply(this, arguments);
+
+      // virtual OBR can be applied on top of any non-virtual, non-OBR repository
+      if (rec.data.provider == 'obr-shadow') {
+        this.form.findField('shadowOf').store.filterBy(function fn(rec, id) {
+            return rec.data.repoType != 'virtual' && rec.data.format != 'obr';
+          });}
+    }
+  });
+})();
+
