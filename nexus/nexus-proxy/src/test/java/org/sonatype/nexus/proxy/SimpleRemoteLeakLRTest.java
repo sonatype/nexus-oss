bd8f44b76d47087e40454b51493e3b5d81f5cf71
Tamas Cservenak <tamas@cservenak.net>
2011-11-03 14:24:02 +0100

    NEXUS-4521: Splitting the two test into plain UT + LRUT.
    The 2nd method was disabled, since according to comments it was running too long. Today, we
    have the LR tests, but also, on my mac, 2nd method took 8 seconds.

--- nexus/nexus-proxy/src/test/java/org/sonatype/nexus/proxy/SimpleRemoteLeakLRTest.java
+++ nexus/nexus-proxy/src/test/java/org/sonatype/nexus/proxy/SimpleRemoteLeakLRTest.java
@@ -26,12 +26,13 @@ import org.apache.commons.httpclient.HttpClient;
 import org.hamcrest.MatcherAssert;
 import org.junit.Test;
 import org.sonatype.jettytestsuite.ServletServer;
-import org.sonatype.nexus.proxy.item.StorageItem;
+import org.sonatype.nexus.proxy.item.RepositoryItemUid;
 import org.sonatype.nexus.proxy.repository.ProxyRepository;
+import org.sonatype.nexus.proxy.repository.RemoteStatus;
 import org.sonatype.nexus.proxy.storage.remote.RemoteStorageContext;
 import org.sonatype.nexus.proxy.storage.remote.commonshttpclient.CommonsHttpClientRemoteStorage;
 
-public class SimpleRemoteLeakTest
+public class SimpleRemoteLeakLRTest
     extends AbstractProxyTestEnvironment
 {
 
@@ -47,50 +48,29 @@ public class SimpleRemoteLeakTest
     }
 
     @Test
-    public void testSimplerRemoteLeak()
+    public void simplerAvailabilityCheckRemoteLeak()
         throws Exception
     {
+        // mangle one repos to have quasi different host, thus different HttpCommons HostConfig
+        // but make it fail! (unknown host, so will not be able to connect)
+        getRepositoryRegistry().getRepositoryWithFacet( "repo1", ProxyRepository.class ).setRemoteUrl(
+            getRepositoryRegistry().getRepositoryWithFacet( "repo1", ProxyRepository.class ).getRemoteUrl().replace(
+                "localhost", "1.1.1.1" ) );
+
         ProxyRepository repo1 = getRepositoryRegistry().getRepositoryWithFacet( "repo1", ProxyRepository.class );
         ProxyRepository repo2 = getRepositoryRegistry().getRepositoryWithFacet( "repo2", ProxyRepository.class );
 
-        // this test is CommonsHttpclient3x dependent, it uses it's internals in assertions!
-        // So run it only when that RRS is in use
-        if ( !( repo1.getRemoteStorage() instanceof CommonsHttpClientRemoteStorage ) )
+        // loop until we have some "sensible" result (not unknown, since this is async op)
+        // first unforced request will trigger the check, and wait until we have result
+        RemoteStatus rs1 = RemoteStatus.UNKNOWN;
+        RemoteStatus rs2 = RemoteStatus.UNKNOWN;
+
+        while ( RemoteStatus.UNKNOWN.equals( rs1 ) || RemoteStatus.UNKNOWN.equals( rs2 ) )
         {
-            System.out.println( "Test disabled, RRS implementation is not of class "
-                                    + CommonsHttpClientRemoteStorage.class.getName() + " but "
-                                    + repo1.getRemoteStorage().getClass().getName() );
+            rs1 = repo1.getRemoteStatus( new ResourceStoreRequest( RepositoryItemUid.PATH_ROOT ), false );
+            rs2 = repo2.getRemoteStatus( new ResourceStoreRequest( RepositoryItemUid.PATH_ROOT ), false );
 
-            return;
-        }
-
-        // mangle one repos to have quasi different host, thus different HttpCommons HostConfig
-        // but make it succeed! (127.0.0.1 is localhost, so will be able to connect)
-        repo1.setRemoteUrl(
-            getRepositoryRegistry().getRepositoryWithFacet( "repo1", ProxyRepository.class ).getRemoteUrl().replace(
-                "localhost", "127.0.0.1" ) );
-
-        ResourceStoreRequest req1 =
-            new ResourceStoreRequest( "/repositories/repo1/activemq/activemq-core/1.2/activemq-core-1.2.jar", false );
-        ResourceStoreRequest req2 =
-            new ResourceStoreRequest( "/repositories/repo2/xstream/xstream/1.2.2/xstream-1.2.2.pom", false );
-
-        for ( int i = 0; i < 10; i++ )
-        {
-            StorageItem item1 = getRootRouter().retrieveItem( req1 );
-            checkForFileAndMatchContents( item1 );
-
-            StorageItem item2 = getRootRouter().retrieveItem( req2 );
-            checkForFileAndMatchContents( item2 );
-
-            // to force refetch
-            getRepositoryRegistry().getRepository( item1.getRepositoryId() ).deleteItem( false,
-                                                                                         new ResourceStoreRequest(
-                                                                                             item1 ) );
-
-            getRepositoryRegistry().getRepository( item2.getRepositoryId() ).deleteItem( false,
-                                                                                         new ResourceStoreRequest(
-                                                                                             item2 ) );
+            Thread.sleep( 1000 );
         }
 
         // get the default context, since they used it
@@ -110,7 +90,4 @@ public class SimpleRemoteLeakTest
 
         MatcherAssert.assertThat( cm2.getConnectionsInPool(), is( equalTo( 1 ) ) );
     }
-
-    // NEXUS-4521: moved to SimpleRemoteLeakLRTest, it takes too long
-    // public void nonTestSimplerAvailabilityCheckRemoteLeak();
 }
