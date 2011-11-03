0c9ea08d4e28b982937558d84d27acd3146ccc55
Marvin Froeder <velo.br@gmail.com>
2011-11-03 16:12:51 -0200

    NEXUS-4539 - Initial test implementation

---
+++ nexus/nexus-test-harness/nexus-test-harness-its/src/test/java/org/sonatype/nexus/integrationtests/nexus4539/NEXUS4539ManualBlockIT.java
@@ -0,0 +1,92 @@
+/**
+ * Copyright (c) 2008-2011 Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions
+ *
+ * This program is free software: you can redistribute it and/or modify it only under the terms of the GNU Affero General
+ * Public License Version 3 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License Version 3
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License Version 3 along with this program.  If not, see
+ * http://www.gnu.org/licenses.
+ *
+ * Sonatype Nexus (TM) Open Source Version is available from Sonatype, Inc. Sonatype and Sonatype Nexus are trademarks of
+ * Sonatype, Inc. Apache Maven is a trademark of the Apache Foundation. M2Eclipse is a trademark of the Eclipse Foundation.
+ * All other trademarks are the property of their respective owners.
+ */
+package org.sonatype.nexus.integrationtests.nexus4539;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.not;
+
+import org.sonatype.nexus.proxy.repository.ProxyMode;
+import org.sonatype.nexus.proxy.repository.RemoteStatus;
+import org.sonatype.nexus.rest.model.RepositoryStatusResource;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ * while a proxy is auto-blocked move the proxy repository state to manually blocked. make sure it is still manually
+ * blocked after the auto-block thread wakes to recheck the status
+ */
+public class NEXUS4539ManualBlockIT
+    extends AutoBlockITSupport
+{
+
+    @BeforeMethod
+    public void setTimeout()
+    {
+        super.sleepTime = 100;
+    }
+
+    @Test
+    public void manualBlock()
+        throws Exception
+    {
+        // initial status, timing out
+        waitFor( RemoteStatus.UNAVAILABLE, ProxyMode.BLOCKED_AUTO );
+
+        // just make sure autoblock went remote
+        assertThat( pathsTouched, not( empty() ) );
+        pathsTouched.clear();
+
+        // enable manual block
+        repoUtil.setBlockProxy( REPO, true );
+
+        RepositoryStatusResource status = repoUtil.getStatus( REPO );
+        assertThat( status.getRemoteStatus(), equalTo( RemoteStatus.UNAVAILABLE.name() ) );
+        assertThat( status.getProxyMode(), equalTo( ProxyMode.BLOCKED_MANUAL.name() ) );
+
+        // server back to normal
+        super.sleepTime = -1;
+
+        waitPath();
+
+        status = repoUtil.getStatus( REPO );
+        // nexus shall not unblock
+        assertThat( status.getRemoteStatus(), equalTo( RemoteStatus.UNAVAILABLE.name() ) );
+        assertThat( status.getProxyMode(), equalTo( ProxyMode.BLOCKED_MANUAL.name() ) );
+    }
+
+    protected void waitPath()
+        throws Exception
+    {
+        for ( int i = 0; i < 1000; i++ )
+        {
+            // mean nexus did touch the remote server to auto unblock
+            if ( pathsTouched.contains( "repository/" ) )
+            {
+                return;
+            }
+
+            Thread.sleep( 500 );
+            System.out.println( pathsTouched );
+        }
+
+        throw new IllegalStateException();
+    }
+}
