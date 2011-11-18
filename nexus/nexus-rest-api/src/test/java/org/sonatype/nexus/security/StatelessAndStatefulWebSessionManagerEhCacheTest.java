46b303604962580c82dabc9830fbc3f9bb6f71aa
bdemers <brian.demers@gmail.com>
2011-11-18 12:49:30 -0500

    NEXUS-4630, Adding an EhCache specific test.

--- nexus/nexus-rest-api/src/test/java/org/sonatype/nexus/security/StatelessAndStatefulWebSessionManagerEhCacheTest.java
+++ nexus/nexus-rest-api/src/test/java/org/sonatype/nexus/security/StatelessAndStatefulWebSessionManagerEhCacheTest.java
@@ -24,7 +24,6 @@ import org.apache.shiro.authz.permission.RolePermissionResolver;
 import org.apache.shiro.cache.ehcache.EhCache;
 import org.apache.shiro.cache.ehcache.EhCacheManager;
 import org.apache.shiro.realm.SimpleAccountRealm;
-import org.apache.shiro.session.Session;
 import org.apache.shiro.session.UnknownSessionException;
 import org.apache.shiro.session.mgt.DefaultSessionKey;
 import org.apache.shiro.session.mgt.eis.CachingSessionDAO;
@@ -32,7 +31,6 @@ import org.apache.shiro.util.ThreadContext;
 import org.apache.shiro.web.subject.WebSubject;
 
 import org.hamcrest.MatcherAssert;
-import org.hamcrest.Matchers;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -42,7 +40,6 @@ import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.Serializable;
 import java.util.HashMap;
 
 import static org.hamcrest.Matchers.*;
@@ -50,7 +47,7 @@ import static org.hamcrest.Matchers.*;
 /**
  * Tests for StatelessAndStatefulWebSessionManager
  */
-public class StatelessAndStatefulWebSessionManagerTest
+public class StatelessAndStatefulWebSessionManagerEhCacheTest
 {
 
     private NexusWebRealmSecurityManager securityManager;
@@ -74,24 +71,12 @@ public class StatelessAndStatefulWebSessionManagerTest
         sessionManager =
             (StatelessAndStatefulWebSessionManager) securityManager.getSessionManager();
         sessionDAO = (CachingSessionDAO) sessionManager.getSessionDAO();
-
-        setupCacheManager( securityManager );
-
-        initCache( sessionDAO );
-    }
-
-    protected void setupCacheManager( NexusWebRealmSecurityManager securityManager )
-    {
-        // by default use the default implementation
-    }
-
-    protected void initCache( CachingSessionDAO sessionDAO )
-    {
         // init the cache safely by calling GetActiveSessions
         sessionDAO.getActiveSessions();
 
         // verify 0 active sessions to start
         MatcherAssert.assertThat( sessionDAO.getActiveSessions().size(), equalTo( 0 ));
+
     }
 
     /**
@@ -109,7 +94,8 @@ public class StatelessAndStatefulWebSessionManagerTest
         WebSubject subject = new WebSubject.Builder( securityManager, request, response ).buildWebSubject();
         subject.login( new UsernamePasswordToken( "user", "user123" ) );
 
-        verifyNoSessionStored();
+        // verify 0 active sessions
+        MatcherAssert.assertThat( sessionDAO.getActiveSessions().size(), equalTo( 0 ));
 
         // verify accessing the session does not blow up
         subject.getSession().getAttributeKeys();
@@ -128,8 +114,6 @@ public class StatelessAndStatefulWebSessionManagerTest
 
         // force clearing the ehcache
         sessionDAO.getActiveSessionsCache().clear();
-
-
 
         // verify accessing the session does not blow up
         subject.getSession().getAttributeKeys();
@@ -163,13 +147,17 @@ public class StatelessAndStatefulWebSessionManagerTest
         subject.login( new UsernamePasswordToken( "user", "user123" ) );
 
         // verify 1 active sessions
-        verifySingleSessionStored( subject.getSession().getId() );
+        MatcherAssert.assertThat( sessionDAO.getActiveSessions().size(), equalTo( 1 ));
 
         // verify accessing the session does not blow up
         subject.getSession().getAttributeKeys(); // directly against the subject object
+        sessionManager.getSession(
+            new DefaultSessionKey( subject.getSession().getId() ) ); // again using the sessionManager
 
         // force clearing the ehcache
         sessionDAO.getActiveSessionsCache().clear();
+
+
 
         // now the session should not be found
         try
@@ -194,18 +182,28 @@ public class StatelessAndStatefulWebSessionManagerTest
         }
     }
 
-    protected void verifyNoSessionStored()
+    /**
+     * Verifies a session is NOT stored in the when a stateless client logs in.  (uses the EhCache impl)
+     */
+    @Test
+    public void testStatelessSessionWithEhCache()
     {
-        // verify 0 active sessions
-        MatcherAssert.assertThat( sessionDAO.getActiveSessions().size(), equalTo( 0 ));
+        this.securityManager.setCacheManager( new EhCacheManager() );
+
+        // verify ehcache was set for this test
+        MatcherAssert.assertThat( this.sessionDAO.getActiveSessionsCache(), is( instanceOf( EhCache.class ) ) );
+
+        testStatelessSession();
     }
 
-    protected void verifySingleSessionStored( Serializable sessionId )
+    /**
+     * Verifies a session is NOT stored in the when a stateless client logs in.  (uses the EhCache impl)
+     */
+    @Test
+    public void testStateFullSessionWithEhCache()
     {
-        // verify 1 active sessions
-        MatcherAssert.assertThat( sessionDAO.getActiveSessions().size(), equalTo( 1 ));
-
-        sessionManager.getSession( new DefaultSessionKey( sessionId ) ); // again using the sessionManager
+        this.securityManager.setCacheManager( new EhCacheManager() );
+        testStateFullSession();
     }
 
     @After
