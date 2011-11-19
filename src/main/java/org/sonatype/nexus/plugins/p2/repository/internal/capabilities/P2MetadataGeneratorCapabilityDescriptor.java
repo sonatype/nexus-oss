56701f69f2b956b9fb9d27b1e533bb12bab51212
Alin Dreghiciu <adreghiciu@gmail.com>
2011-11-19 17:45:38 +0200

    Use the new abstract capability descriptor available in Nexus 1.10
    Signed-off-by: Alin Dreghiciu <adreghiciu@gmail.com>

--- src/main/java/org/sonatype/nexus/plugins/p2/repository/internal/capabilities/P2MetadataGeneratorCapabilityDescriptor.java
+++ src/main/java/org/sonatype/nexus/plugins/p2/repository/internal/capabilities/P2MetadataGeneratorCapabilityDescriptor.java
@@ -18,55 +18,31 @@
  */
 package org.sonatype.nexus.plugins.p2.repository.internal.capabilities;
 
-import java.util.Arrays;
-import java.util.List;
-
 import javax.inject.Named;
 import javax.inject.Singleton;
 
 import org.sonatype.nexus.formfields.FormField;
 import org.sonatype.nexus.formfields.RepoOrGroupComboFormField;
+import org.sonatype.nexus.plugins.capabilities.api.descriptor.AbstractCapabilityDescriptor;
 import org.sonatype.nexus.plugins.capabilities.api.descriptor.CapabilityDescriptor;
 import org.sonatype.nexus.plugins.p2.repository.P2MetadataGeneratorConfiguration;
 
 @Singleton
 @Named( P2MetadataGeneratorCapability.ID )
 public class P2MetadataGeneratorCapabilityDescriptor
+    extends AbstractCapabilityDescriptor
     implements CapabilityDescriptor
 {
 
     public static final String ID = P2MetadataGeneratorCapability.ID;
 
-    private final FormField repoOrGroup;
-
     public P2MetadataGeneratorCapabilityDescriptor()
     {
-        repoOrGroup =
-            new RepoOrGroupComboFormField( P2MetadataGeneratorConfiguration.REPO_OR_GROUP_ID, FormField.MANDATORY );
-    }
-
-    @Override
-    public String id()
-    {
-        return ID;
-    }
-
-    @Override
-    public String name()
-    {
-        return "P2 Metadata Generator capability";
-    }
-
-    @Override
-    public List<FormField> formFields()
-    {
-        return Arrays.asList( repoOrGroup );
-    }
-
-    @Override
-    public boolean isExposed()
-    {
-        return true;
+        super(
+            ID,
+            "P2 Metadata Generator capability",
+            new RepoOrGroupComboFormField( P2MetadataGeneratorConfiguration.REPO_OR_GROUP_ID, FormField.MANDATORY )
+        );
     }
 
 }
--- src/main/java/org/sonatype/nexus/plugins/p2/repository/internal/capabilities/P2RepositoryAggregatorCapabilityDescriptor.java
+++ src/main/java/org/sonatype/nexus/plugins/p2/repository/internal/capabilities/P2RepositoryAggregatorCapabilityDescriptor.java
@@ -18,55 +18,31 @@
  */
 package org.sonatype.nexus.plugins.p2.repository.internal.capabilities;
 
-import java.util.Arrays;
-import java.util.List;
-
 import javax.inject.Named;
 import javax.inject.Singleton;
 
 import org.sonatype.nexus.formfields.FormField;
 import org.sonatype.nexus.formfields.RepoOrGroupComboFormField;
+import org.sonatype.nexus.plugins.capabilities.api.descriptor.AbstractCapabilityDescriptor;
 import org.sonatype.nexus.plugins.capabilities.api.descriptor.CapabilityDescriptor;
 import org.sonatype.nexus.plugins.p2.repository.P2RepositoryAggregatorConfiguration;
 
 @Singleton
 @Named( P2RepositoryAggregatorCapability.ID )
 public class P2RepositoryAggregatorCapabilityDescriptor
+    extends AbstractCapabilityDescriptor
     implements CapabilityDescriptor
 {
 
     public static final String ID = P2RepositoryAggregatorCapability.ID;
 
-    private final FormField repoOrGroup;
-
     public P2RepositoryAggregatorCapabilityDescriptor()
     {
-        repoOrGroup =
-            new RepoOrGroupComboFormField( P2RepositoryAggregatorConfiguration.REPO_OR_GROUP_ID, FormField.MANDATORY );
-    }
-
-    @Override
-    public String id()
-    {
-        return ID;
-    }
-
-    @Override
-    public String name()
-    {
-        return "P2 Repository Aggregator capability";
-    }
-
-    @Override
-    public List<FormField> formFields()
-    {
-        return Arrays.asList( repoOrGroup );
-    }
-
-    @Override
-    public boolean isExposed()
-    {
-        return true;
+        super(
+            ID,
+            "P2 Repository Aggregator capability",
+            new RepoOrGroupComboFormField( P2RepositoryAggregatorConfiguration.REPO_OR_GROUP_ID, FormField.MANDATORY )
+        );
     }
 
 }
