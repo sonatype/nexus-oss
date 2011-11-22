0347bb342508655594262f7f702ea92c38bd3432
Alin Dreghiciu <adreghiciu@gmail.com>
2011-11-22 15:41:22 +0200

    IT should only verify that everything is setup correctly
    Unit tests are already testing the functionality
    
    Signed-off-by: Alin Dreghiciu <adreghiciu@gmail.com>

--- nexus/nexus-test-harness/nexus-it-helper-plugin/src/main/java/org/sonatype/nexus/plugins/capabilities/test/TouchTestCapability.java
+++ nexus/nexus-test-harness/nexus-it-helper-plugin/src/main/java/org/sonatype/nexus/plugins/capabilities/test/TouchTestCapability.java
@@ -18,61 +18,44 @@
  */
 package org.sonatype.nexus.plugins.capabilities.test;
 
-import java.io.ByteArrayInputStream;
 import java.util.Map;
 
-import org.sonatype.nexus.plugins.capabilities.api.AbstractRepositoryCapability;
+import org.sonatype.nexus.plugins.capabilities.api.AbstractCapability;
 import org.sonatype.nexus.plugins.capabilities.api.Capability;
-import org.sonatype.nexus.proxy.ResourceStoreRequest;
-import org.sonatype.nexus.proxy.registry.RepositoryRegistry;
-import org.sonatype.nexus.proxy.repository.Repository;
 
 public class TouchTestCapability
-    extends AbstractRepositoryCapability
+    extends AbstractCapability
     implements Capability
 {
 
     public static final String ID = "TouchTest";
 
-    protected TouchTestCapability( String id, RepositoryRegistry repositoryRegistry )
+    protected TouchTestCapability( String id )
     {
-        super( id, repositoryRegistry );
+        super( id );
     }
 
     @Override
     public void create( Map<String, String> properties )
     {
-        doIt( properties );
+        getLogger().info( "Create capability with id {} and properties {}", id(), properties );
     }
 
     @Override
     public void update( Map<String, String> properties )
     {
-        doIt( properties );
+        getLogger().info( "Update capability with id {} and properties {}", id(), properties );
     }
 
     @Override
     public void load( Map<String, String> properties )
     {
-        doIt( properties );
+        getLogger().info( "Load capability with id {} and properties {}", id(), properties );
     }
 
-    private void doIt( final Map<String, String> properties )
+    @Override
+    public void remove()
     {
-        final Repository repo = getRepository( TouchTestCapabilityDescriptor.FIELD_REPO_OR_GROUP_ID, properties );
-
-        try
-        {
-            repo.storeItem(
-                new ResourceStoreRequest( "/capability/test.txt" ),
-                new ByteArrayInputStream(
-                    ( "capabilities test!\n" + properties.get( TouchTestCapabilityDescriptor.FIELD_MSG_ID ) + "\n" + properties.get( TouchTestCapabilityDescriptor.FIELD_REPO_OR_GROUP_ID ) ).getBytes() ),
-                null );
-        }
-        catch ( Exception e )
-        {
-            throw new RuntimeException( e.getMessage(), e );
-        }
-
+        getLogger().info( "Remove capability with id {}", id() );
     }
 }
