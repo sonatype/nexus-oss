989bc5f804f52bf28a07293180829e2ccaafbbff
Alin Dreghiciu <adreghiciu@gmail.com>
2011-11-28 18:23:13 +0200

    Handle repository being removed

--- nexus/nexus-core-plugins/nexus-capabilities-plugin/src/main/java/org/sonatype/nexus/plugins/capabilities/internal/activation/RepositoryEventInspector.java
+++ nexus/nexus-core-plugins/nexus-capabilities-plugin/src/main/java/org/sonatype/nexus/plugins/capabilities/internal/activation/RepositoryEventInspector.java
@@ -29,6 +29,7 @@ import org.sonatype.nexus.proxy.events.EventInspector;
 import org.sonatype.nexus.proxy.events.RepositoryConfigurationUpdatedEvent;
 import org.sonatype.nexus.proxy.events.RepositoryRegistryEvent;
 import org.sonatype.nexus.proxy.events.RepositoryRegistryEventAdd;
+import org.sonatype.nexus.proxy.events.RepositoryRegistryEventRemove;
 import org.sonatype.nexus.proxy.repository.Repository;
 import org.sonatype.plexus.appevents.Event;
 
@@ -72,6 +73,10 @@ public class RepositoryEventInspector
         {
             handle( (RepositoryRegistryEventAdd) evt );
         }
+        else if ( evt instanceof RepositoryRegistryEventRemove )
+        {
+            handle( (RepositoryRegistryEventRemove) evt );
+        }
     }
 
     private void handle( final RepositoryConfigurationUpdatedEvent evt )
@@ -95,6 +100,19 @@ public class RepositoryEventInspector
             void run( final RepositoryEventsNotifier.Listener listener, final Repository repository )
             {
                 listener.onAdded( repository );
+            }
+
+        } );
+    }
+
+    private void handle( final RepositoryRegistryEventRemove evt )
+    {
+        repositoryEventsNotifier.notify( evt.getRepository(), new RepositoryEventsNotifier.Notifier( "removed" )
+        {
+            @Override
+            void run( final RepositoryEventsNotifier.Listener listener, final Repository repository )
+            {
+                listener.onRemoved( repository );
             }
 
         } );
