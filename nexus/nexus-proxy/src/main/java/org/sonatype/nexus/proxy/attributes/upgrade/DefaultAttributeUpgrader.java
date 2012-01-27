ca54f84c690ad05b0309938e8deaf77d9085df8c
Tamas Cservenak <tamas@cservenak.net>
2012-01-27 09:59:01 +0100

    Lowering default upgrade TPS to 100.

--- nexus/nexus-proxy/src/main/java/org/sonatype/nexus/proxy/attributes/upgrade/DefaultAttributeUpgrader.java
+++ nexus/nexus-proxy/src/main/java/org/sonatype/nexus/proxy/attributes/upgrade/DefaultAttributeUpgrader.java
@@ -49,17 +49,17 @@ public class DefaultAttributeUpgrader
 
     /**
      * The "switch" to enable "upgrade throttling" if needed (is critical to lessen IO bound problem with attributes).
-     * Default is to 200 UPS to for use throttling. The "measure" is "UPS": item Upgrades Per Second. Reasoning is:
+     * Default is to 100 UPS to for use throttling. The "measure" is "UPS": item Upgrades Per Second. Reasoning is:
      * Central repository is currently 300k of artifacts, this would mean in "nexus world" 6x300k items (pom, jar,
      * maven-metadata and sha1/md5 hashes for those) if all of Central would be proxied by Nexus, which is not
-     * plausible, so assume 50% of Central is present in cache (still is OVER-estimation!). Crawling 900k at 200 UPS
-     * would take exactly 1.25 hour to upgrade it. Note: this is only the value used for unattended upgrade! This is
-     * only the initial value, that is still possible to "tune" (increase, decrease) over JMX! Possible values: -1 means
-     * no throttling, will bash up to the Hardware limits, any other positive integer would mean a limit of UPS to not
+     * plausible, so assume 50% of Central is present in cache (still is OVER-estimation!). Crawling 900k at 100 UPS
+     * would take exactly 2.5 hour to upgrade it. Note: this is only the value used for unattended upgrade! This is only
+     * the initial value, that is still possible to "tune" (increase, decrease) over JMX! Possible values: -1 means no
+     * throttling, will bash up to the Hardware limits, any other positive integer would mean a limit of UPS to not
      * reach over.
      */
     private final int UPGRADE_THROTTLE_UPS = SystemPropertiesHelper.getInteger( getClass().getName() + ".throttleUps",
-        200 );
+        100 );
 
     @Requirement
     private ApplicationConfiguration applicationConfiguration;
