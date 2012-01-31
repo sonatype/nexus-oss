272c39ea4f6480fa37370664f1bc2b67dad01dbd
Sebastian Herold <sebastian.herold@immobilienscout24.de>
2012-01-31 13:03:46 +0000

    update primary xmls to be compatible with createrepo 0.9.8

--- src/test/java/de/is24/nexus/yum/repository/xml/TimeStampIgnoringDifferenceListener.java
+++ src/test/java/de/is24/nexus/yum/repository/xml/TimeStampIgnoringDifferenceListener.java
@@ -1,28 +1,38 @@
 package de.is24.nexus.yum.repository.xml;
 
 import static org.custommonkey.xmlunit.DifferenceConstants.ATTR_VALUE_ID;
+import static org.custommonkey.xmlunit.DifferenceConstants.CHILD_NODELIST_LENGTH_ID;
+import static org.custommonkey.xmlunit.DifferenceConstants.CHILD_NODELIST_SEQUENCE_ID;
 import static org.custommonkey.xmlunit.DifferenceConstants.TEXT_VALUE_ID;
+
 import org.apache.commons.lang.ArrayUtils;
 import org.custommonkey.xmlunit.Difference;
 import org.custommonkey.xmlunit.DifferenceListener;
 import org.custommonkey.xmlunit.NodeDetail;
 import org.w3c.dom.Node;
 
-
 public class TimeStampIgnoringDifferenceListener implements DifferenceListener {
+  @Override
   public void skippedComparison(Node node1, Node node2) {
   }
 
+  @Override
   public int differenceFound(Difference difference) {
-    if (difference.getTestNodeDetail().getXpathLocation().contains("/requires[") ||
-        difference.getTestNodeDetail().getXpathLocation().contains("/provides[") ||
-        difference.getTestNodeDetail().getXpathLocation().contains("/format[1]")) {
+    if (isRpmLibEntry(difference)) {
       return RETURN_IGNORE_DIFFERENCE_NODES_IDENTICAL;
     }
 
-    if (isNodeName(difference, "checksum", "timestamp", "packager", "open-checksum", "time@build", "time@file",
-          "size@package", "summary",
-          "description", "buildhost", "header-range@end")) {
+    if (difference.getTestNodeDetail().getXpathLocation().contains("/requires[")
+        || difference.getTestNodeDetail().getXpathLocation().contains("/provides[")
+        || difference.getTestNodeDetail().getXpathLocation().contains("/format[1]")
+        || difference.getTestNodeDetail().getXpathLocation().matches("^/repomd.{4,5}data.{4,5}(open-)?size.{3,4}$")
+        || difference.getTestNodeDetail().getXpathLocation().matches("^/repomd.{4,5}(data.{4,5})?text.{5,6}$")
+        || difference.getTestNodeDetail().getXpathLocation().matches("^/repomd.{4,5}revision.{3,4}$")) {
+      return RETURN_IGNORE_DIFFERENCE_NODES_IDENTICAL;
+    }
+
+    if (isNodeName(difference, "checksum", "timestamp", "packager", "open-checksum", "time@build", "time@file", "size@package", "summary",
+        "description", "buildhost", "header-range@end", "checksum@type", "open-checksum@type")) {
       return RETURN_IGNORE_DIFFERENCE_NODES_IDENTICAL;
     }
 
@@ -30,7 +40,19 @@ public class TimeStampIgnoringDifferenceListener implements DifferenceListener {
       return RETURN_IGNORE_DIFFERENCE_NODES_IDENTICAL;
     }
 
+    if ((difference.getId() == CHILD_NODELIST_LENGTH_ID || difference.getId() == CHILD_NODELIST_SEQUENCE_ID)
+        && difference.getTestNodeDetail().getXpathLocation().startsWith("/repomd[1]")) {
+      return RETURN_IGNORE_DIFFERENCE_NODES_IDENTICAL;
+    }
+
     return RETURN_ACCEPT_DIFFERENCE;
+  }
+
+  private boolean isRpmLibEntry(Difference difference) {
+    return difference.getControlNodeDetail().getXpathLocation() != null
+        && difference.getControlNodeDetail().getXpathLocation().matches("/metadata.{4,5}package.{4,5}format.{4,5}requires.{4,5}entry.{3}")
+        && controlNode(difference).getAttributes().getNamedItem("name") != null
+        && controlNode(difference).getAttributes().getNamedItem("name").getTextContent().startsWith("rpmlib(");
   }
 
   private boolean hasSameText(Difference difference) {
@@ -47,16 +69,19 @@ public class TimeStampIgnoringDifferenceListener implements DifferenceListener {
 
   private boolean isNodeName(Difference difference, String... nodeNames) {
     if (difference.getId() == ATTR_VALUE_ID) {
-      return isNodeName(attributeName(difference.getControlNodeDetail()), nodeNames) &&
-        isNodeName(attributeName(difference.getTestNodeDetail()), nodeNames);
+      return isNodeName(attributeName(difference.getControlNodeDetail()), nodeNames)
+          && isNodeName(attributeName(difference.getTestNodeDetail()), nodeNames);
     }
     if (difference.getId() == TEXT_VALUE_ID) {
-      return isNodeName(controlNode(difference).getParentNode().getLocalName(), nodeNames) &&
-        isNodeName(testNode(difference).getParentNode().getLocalName(), nodeNames);
+      return isNodeName(controlNode(difference).getParentNode().getLocalName(), nodeNames)
+          && isNodeName(testNode(difference).getParentNode().getLocalName(), nodeNames);
     }
 
-    return isNodeName(controlNode(difference).getLocalName(), nodeNames) &&
-      isNodeName(testNode(difference).getLocalName(), nodeNames);
+    return isNodeName(localName(controlNode(difference)), nodeNames) && isNodeName(localName(testNode(difference)), nodeNames);
+  }
+
+  private String localName(Node node) {
+    return node == null ? "null" : node.getLocalName();
   }
 
   private String attributeName(NodeDetail nodeDetail) {
