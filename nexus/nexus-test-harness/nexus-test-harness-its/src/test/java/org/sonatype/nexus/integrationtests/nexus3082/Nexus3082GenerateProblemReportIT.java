422bb1bbb5db1df0885df3449e0eca9329493cd7
Benjamin Hanzelmann <ben@nabcos.de>
2012-02-17 17:59:04 +0100

    o [NEXUS-4736] Set up jira mock for error reporting IT

--- nexus/nexus-test-harness/nexus-test-harness-its/src/test/java/org/sonatype/nexus/integrationtests/nexus3082/Nexus3082GenerateProblemReportIT.java
+++ nexus/nexus-test-harness/nexus-test-harness-its/src/test/java/org/sonatype/nexus/integrationtests/nexus3082/Nexus3082GenerateProblemReportIT.java
@@ -12,15 +12,61 @@
  */
 package org.sonatype.nexus.integrationtests.nexus3082;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.common.collect.Lists;
+import com.google.common.io.CharStreams;
+import com.google.common.io.Closeables;
+import com.google.common.io.Files;
+import com.google.common.io.InputSupplier;
+import com.google.common.io.Resources;
+import org.sonatype.jira.AttachmentHandler;
+import org.sonatype.jira.mock.MockAttachmentHandler;
+import org.sonatype.jira.mock.StubJira;
+import org.sonatype.jira.test.JiraXmlRpcTestServlet;
 import org.sonatype.nexus.integrationtests.AbstractNexusIntegrationTest;
 import org.sonatype.nexus.rest.model.ErrorReportResponse;
 import org.sonatype.nexus.test.utils.ErrorReportUtil;
+import org.sonatype.nexus.test.utils.TestProperties;
+import org.sonatype.tests.http.server.api.Behaviour;
+import org.sonatype.tests.http.server.fluent.Server;
+import org.sonatype.tests.http.server.jetty.impl.JettyServerProvider;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class Nexus3082GenerateProblemReportIT
     extends AbstractNexusIntegrationTest
 {
+
+    private JettyServerProvider server;
+
+    @BeforeMethod
+    public void setupJiraMock()
+        throws Exception
+    {
+        setupMockJira();
+    }
+
+    @AfterMethod
+    public void shutdownJiraMock()
+        throws Exception
+    {
+        if ( server != null )
+        {
+            server.stop();
+        }
+    }
+
     @Test
     public void generateReport()
         throws Exception
@@ -30,6 +76,37 @@ public class Nexus3082GenerateProblemReportIT
         Assert.assertNotNull( response );
 
         Assert.assertNotNull( response.getData().getJiraUrl() );
+    }
+
+    private void setupMockJira()
+        throws Exception
+    {
+
+        final int port = Integer.parseInt( TestProperties.getString( "webproxy-server-port" ) );
+
+        setupJiraMock( "jira-mock.db", port );
+    }
+
+    private void setupJiraMock( String dbName, int port )
+        throws Exception
+    {
+        final File mockDb = getTestFile( dbName );
+
+        StubJira mock = new StubJira();
+        mock.setDatabase(
+            Files.toString( mockDb, Charset.forName( "utf-8" ) )
+        );
+
+        // we have to give a real version (set in DB and here), because either nexus freaks out otherwise
+        MockAttachmentHandler handler = new MockAttachmentHandler();
+        handler.setSupportedVersion( "4.3" );
+
+        handler.setMock( mock );
+        List<AttachmentHandler> handlers = Arrays.<AttachmentHandler>asList( handler );
+        server = new JettyServerProvider();
+        server.setPort( port );
+        server.addServlet( new JiraXmlRpcTestServlet( mock, server.getUrl(), handlers ) );
+        server.start();
     }
 
     @Test
