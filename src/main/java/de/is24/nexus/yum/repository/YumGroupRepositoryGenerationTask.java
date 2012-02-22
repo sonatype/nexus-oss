ee15060f2ff3c396ba2743ba8b1e30686109bb15
Sebastian Herold <sebastian.herold@immobilienscout24.de>
2012-02-22 11:52:37 +0000

    implemented new mergerepo task

--- src/main/java/de/is24/nexus/yum/repository/YumGroupRepositoryGenerationTask.java
+++ src/main/java/de/is24/nexus/yum/repository/YumGroupRepositoryGenerationTask.java
@@ -1,15 +1,26 @@
 package de.is24.nexus.yum.repository;
 
+import static de.is24.nexus.yum.execution.ExecutionUtil.execCommand;
+import static de.is24.nexus.yum.repository.RepositoryUtils.getBaseDir;
+import static de.is24.nexus.yum.repository.YumMetadataGenerationTask.isActive;
 import static java.lang.String.format;
 
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+
 import org.codehaus.plexus.component.annotations.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.sonatype.nexus.proxy.repository.GroupRepository;
+import org.sonatype.nexus.proxy.repository.Repository;
 import org.sonatype.nexus.scheduling.AbstractNexusTask;
 import org.sonatype.scheduling.SchedulerTask;
 
 @Component(role = SchedulerTask.class, hint = YumGroupRepositoryGenerationTask.ID, instantiationStrategy = "per-lookup")
 public class YumGroupRepositoryGenerationTask extends AbstractNexusTask<YumRepository> {
 
+  private static final Logger LOG = LoggerFactory.getLogger(YumGroupRepositoryGenerationTask.class);
   public static final String ID = "YumGroupRepositoryGenerationTask";
   private GroupRepository groupRepository;
 
@@ -19,7 +30,31 @@ public class YumGroupRepositoryGenerationTask extends AbstractNexusTask<YumRepos
 
   @Override
   protected YumRepository doRun() throws Exception {
+    if (isActive() && isValidRepository(groupRepository)) {
+      LOG.info("Merging repository group {}='{}' ...", groupRepository.getId(), groupRepository.getName());
+      final File repoBaseDir = getBaseDir(groupRepository);
+      execCommand(buildCommand(repoBaseDir));
+      LOG.info("Group repository {}='{}' merged.", groupRepository.getId(), groupRepository.getName());
+      return new YumRepository(repoBaseDir, groupRepository.getId(), null);
+    }
     return null;
+  }
+
+  private boolean isValidRepository(GroupRepository groupRepository2) {
+    return groupRepository != null && !groupRepository.getMemberRepositories().isEmpty();
+  }
+
+  private String buildCommand(File repoBaseDir) throws MalformedURLException, URISyntaxException {
+    final StringBuilder repos = new StringBuilder();
+    for (Repository memberRepository : groupRepository.getMemberRepositories()) {
+      repos.append(" --repo=");
+      repos.append(getFileUrl(memberRepository));
+    }
+    return format("mergerepo --nogroups -d %s -o %s", repos.toString(), repoBaseDir.getAbsolutePath());
+  }
+
+  private String getFileUrl(Repository repository) throws URISyntaxException, MalformedURLException {
+    return getBaseDir(repository).toURI().toString();
   }
 
   @Override
@@ -29,7 +64,7 @@ public class YumGroupRepositoryGenerationTask extends AbstractNexusTask<YumRepos
 
   @Override
   protected String getMessage() {
-    return format("Generate yum metadata for group repository '%s'", groupRepository.getId());
+    return format("Generate yum metadata for group repository %s='%s'", groupRepository.getId(), groupRepository.getName());
   }
 
 }
---
+++ src/test/java/de/is24/nexus/yum/repository/YumGroupRepositoryGenerationTaskTest.java
@@ -0,0 +1,53 @@
+package de.is24.nexus.yum.repository;
+
+import static java.util.Arrays.asList;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
+import org.sonatype.nexus.proxy.repository.GroupRepository;
+import org.sonatype.nexus.proxy.repository.Repository;
+
+public class YumGroupRepositoryGenerationTaskTest {
+
+  private static final String BASE_REPO_DIR = "src/test/yum-repo";
+  private static final File REPO_DIR1 = new File(BASE_REPO_DIR + "/repo1");
+  private static final File REPO_DIR2 = new File(BASE_REPO_DIR + "/repo2");
+  private static final File GROUP_REPO_DIR = new File("target/tmp/group-repo");
+  private GroupRepository groupRepo;
+
+  @Test
+  public void shouldGenerateGroupRepo() throws Exception {
+    givenGroupRepoWith2YumRepos();
+    thenGenerateYumRepo();
+    // assertGroupRepo();
+  }
+
+  private void thenGenerateYumRepo() throws Exception {
+    YumGroupRepositoryGenerationTask task = new YumGroupRepositoryGenerationTask();
+    task.setGroupRepository(groupRepo);
+    task.doRun();
+  }
+
+  private void givenGroupRepoWith2YumRepos() throws IOException {
+    groupRepo = mock(GroupRepository.class);
+    when(groupRepo.getLocalUrl()).thenReturn(GROUP_REPO_DIR.getAbsolutePath());
+    List<Repository> repositories = asList(createRepo(REPO_DIR1), createRepo(REPO_DIR2));
+    when(groupRepo.getMemberRepositories()).thenReturn(repositories);
+    if (GROUP_REPO_DIR.exists()) {
+      FileUtils.deleteDirectory(GROUP_REPO_DIR);
+    }
+    GROUP_REPO_DIR.mkdirs();
+  }
+
+  private Repository createRepo(File repoDir) {
+    Repository repo = mock(Repository.class);
+    when(repo.getLocalUrl()).thenReturn(repoDir.getAbsolutePath());
+    return repo;
+  }
+}
