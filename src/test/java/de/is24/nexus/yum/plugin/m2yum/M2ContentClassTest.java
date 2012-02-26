2070c2156d47cd07ec06f4ed00a82e14f16f126c
Sebastian Herold <sebastian.herold@immobilienscout24.de>
2012-02-26 21:59:29 +0000

    fix content class test

--- src/test/java/de/is24/nexus/yum/plugin/m2yum/M2ContentClassTest.java
+++ src/test/java/de/is24/nexus/yum/plugin/m2yum/M2ContentClassTest.java
@@ -1,10 +1,9 @@
 package de.is24.nexus.yum.plugin.m2yum;
 
-import static org.hamcrest.Matchers.isA;
-import static org.junit.Assert.assertThat;
-
+import org.junit.Assert;
 import org.junit.Test;
 import org.sonatype.nexus.proxy.maven.maven2.M2Repository;
+import org.sonatype.nexus.proxy.registry.ContentClass;
 import org.sonatype.nexus.proxy.repository.Repository;
 
 import de.is24.nexus.yum.AbstractRepositoryTester;
@@ -13,8 +12,9 @@ public class M2ContentClassTest extends AbstractRepositoryTester {
 
   @Test
   public void shouldOverrideDefaultMaven2ContentClass() throws Exception {
+    getContainer().addComponent(new M2ContentClass(), ContentClass.class, M2ContentClass.ID);
     Repository repo = getContainer().lookup(Repository.class, M2Repository.ID);
-    assertThat((M2ContentClass) repo.getRepositoryContentClass(), isA(M2ContentClass.class));
+    Assert.assertTrue(repo.getRepositoryContentClass() instanceof M2ContentClass);
   }
 
 }
