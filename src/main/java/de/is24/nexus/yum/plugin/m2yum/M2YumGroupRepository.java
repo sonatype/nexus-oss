901590398dc1cb8d6d9f7d5423e6d2e78b8a346d
Sebastian Herold <sebastian.herold@immobilienscout24.de>
2012-02-28 22:58:45 +0000

    only regenerate group repository if a yum repository was added or removed

--- src/main/java/de/is24/nexus/yum/plugin/m2yum/M2YumGroupRepository.java
+++ src/main/java/de/is24/nexus/yum/plugin/m2yum/M2YumGroupRepository.java
@@ -1,16 +1,22 @@
 package de.is24.nexus.yum.plugin.m2yum;
 
+import static de.is24.nexus.yum.repository.RepositoryUtils.getBaseDir;
 import static java.util.Arrays.asList;
+
+import java.io.File;
 
 import org.codehaus.plexus.component.annotations.Component;
 import org.codehaus.plexus.component.annotations.Requirement;
 import org.sonatype.nexus.proxy.NoSuchRepositoryException;
 import org.sonatype.nexus.proxy.maven.MavenGroupRepository;
+import org.sonatype.nexus.proxy.maven.MavenHostedRepository;
 import org.sonatype.nexus.proxy.maven.maven2.M2GroupRepository;
 import org.sonatype.nexus.proxy.registry.ContentClass;
+import org.sonatype.nexus.proxy.registry.RepositoryRegistry;
 import org.sonatype.nexus.proxy.repository.DefaultRepositoryKind;
 import org.sonatype.nexus.proxy.repository.GroupRepository;
 import org.sonatype.nexus.proxy.repository.InvalidGroupingException;
+import org.sonatype.nexus.proxy.repository.Repository;
 import org.sonatype.nexus.proxy.repository.RepositoryKind;
 
 import de.is24.nexus.yum.service.YumService;
@@ -24,6 +30,9 @@ public class M2YumGroupRepository extends M2GroupRepository {
 
   @Requirement
   private YumService yumService;
+
+  @Requirement
+  private RepositoryRegistry repositoryRegistry;
 
   private RepositoryKind repositoryKind;
 
@@ -43,14 +52,39 @@ public class M2YumGroupRepository extends M2GroupRepository {
 
   @Override
   public void addMemberRepositoryId(String repositoryId) throws NoSuchRepositoryException, InvalidGroupingException {
+    final boolean isNewRpmRepository = isNewRepo(repositoryId) && isRpmRepo(repositoryId);
     super.addMemberRepositoryId(repositoryId);
-    yumService.createGroupRepository(this);
+    if (isNewRpmRepository) {
+      yumService.createGroupRepository(this);
+    }
+  }
+
+  private boolean isRpmRepo(String repositoryId) throws NoSuchRepositoryException {
+    final Repository repository = repositoryRegistry.getRepository(repositoryId);
+    try {
+      if (repository.getRepositoryKind().isFacetAvailable(MavenHostedRepository.class)
+          && new File(getBaseDir(repository), "repodata/repomd.xml").exists()) {
+        return true;
+      }
+    } catch (Exception e) {
+    }
+    return false;
+  }
+
+  private boolean isNewRepo(String repositoryId) {
+    return !getMemberRepositoryIds().contains(repositoryId);
   }
 
   @Override
   public void removeMemberRepositoryId(String repositoryId) {
     super.removeMemberRepositoryId(repositoryId);
-    yumService.createGroupRepository(this);
+    try {
+      if (isRpmRepo(repositoryId)) {
+        yumService.createGroupRepository(this);
+      }
+    } catch (NoSuchRepositoryException e) {
+      throw new RuntimeException("Could not detect rpm repository.", e);
+    }
   }
 
 }
