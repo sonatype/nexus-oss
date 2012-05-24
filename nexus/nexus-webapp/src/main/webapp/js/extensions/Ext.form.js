e263066a9fead208adbde1934b80afa63be0c8e2
Damian Bradicich <dbradicich@sonatype.com>
2012-05-24 14:48:27 -0400

    - Another place where we had a bad superclass call that was causing textField.setValue() to not function

--- nexus/nexus-webapp/src/main/webapp/js/extensions/Ext.form.js
+++ nexus/nexus-webapp/src/main/webapp/js/extensions/Ext.form.js
@@ -1009,23 +1009,28 @@ Ext.override(Ext.form.TextField, {
    * will encode the string again.
    */
   htmlConvert : false,
+  
+  setRawValueOrig : Ext.form.TextField.prototype.setRawValue,
+  setValueOrig : Ext.form.TextField.prototype.setValue,
+  getRawValueOrig : Ext.form.TextField.prototype.getRawValue,
+  getValueOrig : Ext.form.TextField.prototype.getValue,
 
   setRawValue : function(value) {
     if ( this.htmlDecode || this.htmlConvert )
     {
       value = Ext.util.Format.htmlDecode(value);
     }
-    Ext.form.TextField.superclass.setRawValue.call(this, value);
+    this.setRawValueOrig(value);
   },
   setValue : function(value) {
     if ( this.htmlDecode || this.htmlConvert )
     {
       value = Ext.util.Format.htmlDecode(value);
     }
-    Ext.form.TextField.superclass.setValue.call(this, value);
+    this.setValueOrig(value);
   },
   getRawValue : function() {
-    var value = Ext.form.TextField.superclass.getRawValue.call(this);
+    var value = this.getRawValueOrig();
     if ( this.htmlConvert )
     {
       value = Ext.util.Format.htmlEncode(value);
@@ -1033,7 +1038,7 @@ Ext.override(Ext.form.TextField, {
     return value;
   },
   getValue : function() {
-    var value = Ext.form.TextField.superclass.getValue.call(this);
+    var value = this.getValueOrig();
     if ( this.htmlConvert )
     {
       value = Ext.util.Format.htmlEncode(value);
