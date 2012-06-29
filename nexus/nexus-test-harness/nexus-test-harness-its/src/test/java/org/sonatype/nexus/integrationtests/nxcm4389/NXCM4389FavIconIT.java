0328e9781d25c91500f7828c53700777b320be6c
Benjamin Hanzelmann <ben@nabcos.de>
2012-06-29 13:16:47 +0200

    o enhance favicon IT

--- nexus/nexus-test-harness/nexus-test-harness-its/src/test/java/org/sonatype/nexus/integrationtests/nxcm4389/NXCM4389FavIconIT.java
+++ nexus/nexus-test-harness/nexus-test-harness-its/src/test/java/org/sonatype/nexus/integrationtests/nxcm4389/NXCM4389FavIconIT.java
@@ -10,23 +10,24 @@
  * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
  * Eclipse Foundation. All other trademarks are the property of their respective owners.
  */
-package org.sonatype.nexus.integrationtests.nexus4389;
+package org.sonatype.nexus.integrationtests.nxcm4389;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.io.IOException;
 
 import org.hamcrest.Matchers;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.sonatype.nexus.integrationtests.AbstractNexusIntegrationTest;
-import org.sonatype.nexus.integrationtests.NexusRestClient;
 import org.sonatype.nexus.integrationtests.RequestFacade;
 import org.sonatype.nexus.test.utils.NexusRequestMatchers;
 import org.testng.annotations.Test;
 
 /**
- *
+ * Check that index.html favicons are available.
  */
-public class Nexus4389FavIconIT
+public class NXCM4389FavIconIT
     extends AbstractNexusIntegrationTest
 {
 
@@ -34,12 +35,17 @@ public class Nexus4389FavIconIT
     public void testFavicons()
         throws IOException
     {
-        RequestFacade.doGetForStatus( "favicon.ico", NexusRequestMatchers.isSuccess() );
-        RequestFacade.doGetForStatus( "favicon.png", NexusRequestMatchers.isSuccess() );
-        assertThat( RequestFacade.doGetForText( "index.html" ), Matchers.allOf(
-            Matchers.containsString( "favicon.ico" ),
-            Matchers.containsString( "favicon.png" )
-        ) );
+        // assert that shortcut icon mentioned in the HTML is actually available
+        final String text = RequestFacade.doGetForText( "index.html" );
+        Document doc = Jsoup.parse( text );
+        RequestFacade.doGetForStatus( doc.select("link[rel=icon]").attr("href"), NexusRequestMatchers.isSuccess() );
+
+        // assert that IE elements are in there as well
+        final String head = doc.select( "head" ).outerHtml();
+        final int start = head.indexOf( "IE]>" );
+        final int end = head.lastIndexOf( "endif" );
+        doc = Jsoup.parse( head.substring( start + 4, end ) );
+        RequestFacade.doGetForStatus( doc.select("link[rel=shortcut icon]").attr("href"), NexusRequestMatchers.isSuccess() );
     }
 
 }
