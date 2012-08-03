61765ce93fdcf90b2832a17be3a93b831013f5cd
Benjamin Hanzelmann <ben@nabcos.de>
2012-08-03 12:24:58 +0200

    o Hide generated "Task Settings" fieldset if it's empty

--- nexus/nexus-webapp/src/main/webapp/js/repoServer/repoServer.SchedulesEditPanel.js
+++ nexus/nexus-webapp/src/main/webapp/js/repoServer/repoServer.SchedulesEditPanel.js
@@ -1657,11 +1657,13 @@ Ext.extend(Sonatype.repoServer.SchedulesEditPanel, Ext.Panel, {
                 if (item.id == id + '_' + rec.data.typeId)
                 {
                   serviceTypePanel.activeItem = i;
-                  if (item.items)
+                  if (item.items && item.items.length !== 0)
                   {
                     item.items.each(function(item) {
                           item.disabled = false;
                         });
+                  } else {
+                    serviceTypePanel.hide();
                   }
                 }
               });
@@ -1710,14 +1712,19 @@ Ext.extend(Sonatype.repoServer.SchedulesEditPanel, Ext.Panel, {
         // Then enable the fields in that card
         var formId = this.id;
         serviceTypePanel.items.each(function(item, i, len) {
-              if (item.id == formId + '_' + record.data.id)
+              if (item.id === formId + '_' + record.data.id)
               {
                 serviceTypePanel.getLayout().setActiveItem(item);
-                item.items.each(function(item) {
-                      item.enable();
-                    });
+                if (item.items.length === 0) {
+                  serviceTypePanel.hide();
+                } else  {
+                  item.items.each(function(item) {
+                    item.enable();
+                  });
+                }
               }
             }, serviceTypePanel);
+
         serviceTypePanel.doLayout();
       },
 
