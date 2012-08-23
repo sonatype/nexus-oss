68818227b49a30fc63aef5f55330d74029a114e5
sherold <sebastian.herold@immobilienscout24.de>
2012-08-23 08:59:50 +0200

    fix to test to work with 'mvn test'

--- src/test/java/org/sonatype/nexus/plugins/yum/plugin/m2yum/M2ContentClassTest.java
+++ src/test/java/org/sonatype/nexus/plugins/yum/plugin/m2yum/M2ContentClassTest.java
@@ -2,12 +2,12 @@ package org.sonatype.nexus.plugins.yum.plugin.m2yum;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.sonatype.nexus.plugins.yum.AbstractRepositoryTester;
 import org.sonatype.nexus.proxy.maven.maven2.M2Repository;
 import org.sonatype.nexus.proxy.registry.ContentClass;
 import org.sonatype.nexus.proxy.repository.Repository;
+import org.sonatype.nexus.test.NexusTestSupport;
 
-public class M2ContentClassTest extends AbstractRepositoryTester {
+public class M2ContentClassTest extends NexusTestSupport {
 
   @Test
   public void shouldOverrideDefaultMaven2ContentClass() throws Exception {
