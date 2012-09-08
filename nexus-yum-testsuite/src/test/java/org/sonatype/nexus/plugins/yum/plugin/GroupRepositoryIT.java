e93c3204ab32818fe9f057a7e27d79c44f697116
sherold <sebastian.herold@immobilienscout24.de>
2012-09-08 23:19:07 +0200

    migrated first group repository test

--- nexus-yum-testsuite/src/test/java/org/sonatype/nexus/plugins/yum/plugin/GroupRepositoryIT.java
+++ nexus-yum-testsuite/src/test/java/org/sonatype/nexus/plugins/yum/plugin/GroupRepositoryIT.java
@@ -1,8 +1,11 @@
 package org.sonatype.nexus.plugins.yum.plugin;
 
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
 import java.io.File;
-import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
 
 import org.junit.Test;
 import org.sonatype.nexus.bundle.launcher.NexusBundleConfiguration;
@@ -12,20 +15,26 @@ import org.sonatype.nexus.testsuite.support.NexusStartAndStopStrategy;
 @NexusStartAndStopStrategy(NexusStartAndStopStrategy.Strategy.EACH_TEST)
 public class GroupRepositoryIT extends NexusRunningITSupport {
 
+  private static final String TEST_REPO_NAME = "dummy-group-repo";
+
   @Test
   public void shouldCreateAGroupRepository() throws Exception {
-
+    RepositoryTestService service = new RepositoryTestService(client());
+    service.deleteGroupRepo(TEST_REPO_NAME);
+    assertThat(service.createGroupRepo(TEST_REPO_NAME, "maven2yum"), is(true));
   }
 
   @Override
   protected NexusBundleConfiguration configureNexus(NexusBundleConfiguration configuration) {
-    URI pluginFileURI;
-    try {
-      pluginFileURI = getClass().getResource("/plugin.zip").toURI();
-    } catch (URISyntaxException e) {
-      throw new RuntimeException("Could not determine plugin URI.", e);
+    URL pluginFileUrl = getClass().getResource("/plugin.zip");
+    if (pluginFileUrl == null) {
+      throw new IllegalStateException("Couldn't find /plugin.zip in classpath");
     }
-    return configuration.addPlugins(new File(pluginFileURI));
+    try {
+      return configuration.addPlugins(new File(pluginFileUrl.toURI()));
+    } catch (URISyntaxException e) {
+      throw new RuntimeException("Could not determine plugin bundle URI.", e);
+    }
   }
 
 }
