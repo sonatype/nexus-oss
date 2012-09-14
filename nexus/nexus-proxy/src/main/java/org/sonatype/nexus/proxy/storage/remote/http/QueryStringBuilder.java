b419100012d28e4134735a1616c4094381250940
Igor Fedorenko <igor@ifedorenko.com>
2012-09-14 17:03:59 -0400

    introduced QueryStringContributor component interface
    ... the idea is to allow nexus plugins contribute additional
    query string paramaters.
    
    Signed-off-by: Igor Fedorenko <igor@ifedorenko.com>

---
+++ nexus/nexus-proxy/src/main/java/org/sonatype/nexus/proxy/storage/remote/http/QueryStringBuilder.java
@@ -0,0 +1,65 @@
+/**
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2007-2012 Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
+package org.sonatype.nexus.proxy.storage.remote.http;
+
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+import org.codehaus.plexus.util.StringUtils;
+import org.sonatype.nexus.proxy.repository.ProxyRepository;
+import org.sonatype.nexus.proxy.storage.remote.RemoteStorageContext;
+
+@Named
+@Singleton
+public class QueryStringBuilder
+{
+    private final List<QueryStringContributor> queryParameterContributors;
+
+    @Inject
+    public QueryStringBuilder( List<QueryStringContributor> queryParameterContributors )
+    {
+        this.queryParameterContributors = queryParameterContributors;
+    }
+
+    public String getQueryString( RemoteStorageContext ctx, ProxyRepository repository )
+    {
+        final StringBuilder result = new StringBuilder();
+
+        final String configuredQueryString = ctx.getRemoteConnectionSettings().getQueryString();
+
+        if ( StringUtils.isNotBlank( configuredQueryString ) )
+        {
+            result.append( configuredQueryString );
+        }
+
+        for ( QueryStringContributor contributor : queryParameterContributors )
+        {
+            String contributedQueryString = contributor.getQueryString( ctx, repository );
+            if ( StringUtils.isNotBlank( contributedQueryString ) )
+            {
+                if ( StringUtils.isNotBlank( result.toString() ) )
+                {
+                    result.append( '&' );
+                }
+                result.append( contributedQueryString );
+            }
+        }
+
+        final String resultStr = result.toString();
+
+        return StringUtils.isNotBlank( resultStr ) ? resultStr : null;
+    }
+}
