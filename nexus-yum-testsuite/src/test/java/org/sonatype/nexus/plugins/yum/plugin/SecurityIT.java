05a780518efb8d75244a0b073530f59c5f6207be
sherold <sebastian.herold@immobilienscout24.de>
2012-09-21 11:13:26 +0200

    implemented test with yum-admin-user

--- nexus-yum-testsuite/src/test/java/org/sonatype/nexus/plugins/yum/plugin/SecurityIT.java
+++ nexus-yum-testsuite/src/test/java/org/sonatype/nexus/plugins/yum/plugin/SecurityIT.java
@@ -12,29 +12,68 @@
  */
 package org.sonatype.nexus.plugins.yum.plugin;
 
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
 import org.junit.Test;
+import org.sonatype.nexus.client.core.subsystem.security.Users;
 import org.sonatype.nexus.plugins.yum.plugin.client.subsystem.YumClient;
+import org.sonatype.security.rest.model.UserResource;
 
 import com.sun.jersey.api.client.UniformInterfaceException;
 
 public class SecurityIT extends AbstractIntegrationTestCase {
-  @Test(expected = UniformInterfaceException.class)
+  private static final String ANOTHER_VERSION = "4.3.1";
+private static final String VERSION = "1.2.3";
+private static final String REPO = "releases";
+private static final String PASSWORD = "yum123";
+
+@Test(expected = UniformInterfaceException.class)
   public void shouldNotHaveReadAccessToAliasesForAnonymous() throws Exception {
     final String alias = uniqueName();
-    yum().createOrUpdateAlias("releases", alias, "1.2.3");
-    createNexusClientForAnonymous(nexus()).getSubsystem(YumClient.class).getAliasVersion("releases", alias);
+    yum().createOrUpdateAlias(REPO, alias, VERSION);
+    createNexusClientForAnonymous(nexus()).getSubsystem(YumClient.class).getAliasVersion(REPO, alias);
   }
 
   @Test(expected = UniformInterfaceException.class)
   public void shouldNotCreateAliasForAnonymous() throws Exception {
-    createNexusClientForAnonymous(nexus()).getSubsystem(YumClient.class).createOrUpdateAlias("releases", uniqueName(), "1.2.3");
+    createNexusClientForAnonymous(nexus()).getSubsystem(YumClient.class).createOrUpdateAlias(REPO, uniqueName(), VERSION);
   }
 
   @Test(expected = UniformInterfaceException.class)
   public void shouldNotHaveUpdateAccessToAliasesForAnonymous() throws Exception {
     final String alias = uniqueName();
-    yum().createOrUpdateAlias("releases", alias, "1.2.3");
-    createNexusClientForAnonymous(nexus()).getSubsystem(YumClient.class).createOrUpdateAlias("releases", alias, "3.2.1");
+    yum().createOrUpdateAlias(REPO, alias, VERSION);
+    createNexusClientForAnonymous(nexus()).getSubsystem(YumClient.class).createOrUpdateAlias(REPO, alias, "3.2.1");
   }
 
+    @Test
+    public void shouldAllowAccessForYumAdmin()
+        throws Exception
+    {
+        final UserResource user = givenYumAdminUser();
+        final YumClient yum = createNexusClient( nexus(), user.getUserId(), PASSWORD ).getSubsystem( YumClient.class );
+        final String alias = uniqueName();
+        yum.createOrUpdateAlias( REPO, alias, VERSION );
+        assertThat( yum.getAliasVersion( REPO, alias ), is( VERSION ) );
+        yum.createOrUpdateAlias( REPO, alias, ANOTHER_VERSION );
+        assertThat( yum.getAliasVersion( REPO, alias ), is( ANOTHER_VERSION ) );
+    }
+
+    private UserResource givenYumAdminUser()
+    {
+        final String username = uniqueName();
+        final Users users = client().getSubsystem( Users.class );
+        final UserResource user = new UserResource();
+
+        user.setUserId( username );
+        user.setEmail( username + "@sonatype.org" );
+        user.setFirstName( "bar" );
+        user.setLastName( "foo" );
+        user.setPassword( PASSWORD );
+        user.setStatus( "active" );
+        user.addRole( "anonymous" );
+        user.addRole( "nexus-yum-admin" );
+        return users.create( user );
+    }
 }
