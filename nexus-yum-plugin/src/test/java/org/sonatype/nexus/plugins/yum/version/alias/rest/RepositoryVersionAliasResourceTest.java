5374e4eeb0cd8f15bc0d0c52b152c238d9f656a0
sherold <sebastian.herold@immobilienscout24.de>
2012-10-04 11:33:30 +0200

    remove UT because security feature is tested via IT test

--- nexus-yum-plugin/src/test/java/org/sonatype/nexus/plugins/yum/version/alias/rest/RepositoryVersionAliasResourceTest.java
+++ nexus-yum-plugin/src/test/java/org/sonatype/nexus/plugins/yum/version/alias/rest/RepositoryVersionAliasResourceTest.java
@@ -26,109 +26,140 @@ import org.sonatype.nexus.plugins.yum.AbstractYumNexusTestCase;
 import org.sonatype.nexus.plugins.yum.config.YumConfiguration;
 import org.sonatype.plexus.rest.resource.PlexusResource;
 
-
 /**
  * @author BVoss
- *
  */
-public class RepositoryVersionAliasResourceTest extends AbstractYumNexusTestCase {
-  private static final String EXISTING_VERSION = "trunk";
-  private static final String RELEASES = "releases";
-  private static final String TRUNK_VERSION = "5.1.15-2";
-  private static final String NOT_EXISTING_REPOSITORY = "blablup-repo";
-  private static final String VERSION_TO_CREATE = "new-version";
-  private static final String ALIAS_TO_CREATE = "alias-to-create";
+public class RepositoryVersionAliasResourceTest
+    extends AbstractYumNexusTestCase
+{
+    private static final String EXISTING_VERSION = "trunk";
 
-  @Requirement(hint = "RepositoryVersionAliasResource")
-  private PlexusResource resource;
+    private static final String RELEASES = "releases";
 
-  @Inject
-  private YumConfiguration yumConfiguration;
+    private static final String TRUNK_VERSION = "5.1.15-2";
 
-  @Before
-  public void loadYumConfig() {
-    yumConfiguration.load();
-  }
+    private static final String NOT_EXISTING_REPOSITORY = "blablup-repo";
 
-  @Test
-  public void requestedAliasNotfound() throws Exception {
-    assert404(new Request());
-  }
+    private static final String VERSION_TO_CREATE = "new-version";
 
-  @Test
-  public void requestedAliasNotfoundNoParameters() throws Exception {
-    assert404(createRequest(RELEASES, "bla"));
-  }
+    private static final String ALIAS_TO_CREATE = "alias-to-create";
 
-  @Test
-  public void requestedAliasReturnedVersionString() throws Exception {
-    final Request request = createRequest(RELEASES, EXISTING_VERSION);
-    final StringRepresentation version = (StringRepresentation) resource.get(null, request, null, null);
-    Assert.assertEquals(TRUNK_VERSION, version.getText());
-  }
+    @Requirement( hint = "RepositoryVersionAliasResource" )
+    private PlexusResource resource;
 
-  @Test
-  public void shouldReturn404ForInvalidRepository() throws Exception {
-    assert404(createRequest(NOT_EXISTING_REPOSITORY, EXISTING_VERSION));
-  }
+    @Inject
+    private YumConfiguration yumConfiguration;
 
-  @Test
-  public void shouldReturn404ForInvalidRepositoryRpm() throws Exception {
-    assert404(createRequest(NOT_EXISTING_REPOSITORY, EXISTING_VERSION + ".rpm"));
-  }
-
-  @Test
-  public void shouldRetrieveRestRequirements() throws Exception {
-    assertThat(resource.getResourceUri(), is(RESOURCE_URI));
-    assertThat(resource.getPayloadInstance(), nullValue());
-    assertThat(resource.getPayloadInstance(GET), nullValue());
-    assertThat(resource.getPayloadInstance(POST), instanceOf(String.class));
-    assertThat(resource.getResourceProtection().getPathPattern(), is("/yum/alias/*"));
-    assertThat(resource.getResourceProtection().getFilterExpression(), is("anon"));
-  }
-
-  @Test
-  public void shouldRejectEmptyPayload() throws Exception {
-    check400ForPayload(null);
-  }
-
-  @Test
-  public void shouldRejectObjectPayload() throws Exception {
-    check400ForPayload(new Object());
-  }
-
-  @Test
-  public void shouldSetVersion() throws Exception {
-    final Request request = createRequest(NOT_EXISTING_REPOSITORY, ALIAS_TO_CREATE);
-    StringRepresentation result = (StringRepresentation) resource.post(null, request, null, VERSION_TO_CREATE);
-    assertThat(result.getText(), is(VERSION_TO_CREATE));
-    result = (StringRepresentation) resource.get(null, request, null, null);
-    assertThat(result.getText(), is(VERSION_TO_CREATE));
-  }
-
-  private void check400ForPayload(Object payload) {
-    try {
-      resource.post(null, createRequest(NOT_EXISTING_REPOSITORY, RELEASES), null, payload);
-      Assert.fail();
-    } catch (ResourceException e) {
-      assertThat(e.getStatus(), is(CLIENT_ERROR_BAD_REQUEST));
+    @Before
+    public void loadYumConfig()
+    {
+        yumConfiguration.load();
     }
-  }
 
-  private void assert404(final Request request) {
-    try {
-      resource.get(null, request, null, null);
-      Assert.fail(ResourceException.class + " expected");
-    } catch (ResourceException e) {
-      Assert.assertEquals(Status.CLIENT_ERROR_NOT_FOUND, e.getStatus());
+    @Test
+    public void requestedAliasNotfound()
+        throws Exception
+    {
+        assert404( new Request() );
     }
-  }
 
-  private Request createRequest(final String repoValue, final String aliasValue) {
-    final Request request = new Request();
-    request.getAttributes().put(RepositoryVersionAliasResource.REPOSITORY_ID_PARAM, repoValue);
-    request.getAttributes().put(RepositoryVersionAliasResource.ALIAS_PARAM, aliasValue);
-    return request;
-  }
+    @Test
+    public void requestedAliasNotfoundNoParameters()
+        throws Exception
+    {
+        assert404( createRequest( RELEASES, "bla" ) );
+    }
+
+    @Test
+    public void requestedAliasReturnedVersionString()
+        throws Exception
+    {
+        final Request request = createRequest( RELEASES, EXISTING_VERSION );
+        final StringRepresentation version = (StringRepresentation) resource.get( null, request, null, null );
+        Assert.assertEquals( TRUNK_VERSION, version.getText() );
+    }
+
+    @Test
+    public void shouldReturn404ForInvalidRepository()
+        throws Exception
+    {
+        assert404( createRequest( NOT_EXISTING_REPOSITORY, EXISTING_VERSION ) );
+    }
+
+    @Test
+    public void shouldReturn404ForInvalidRepositoryRpm()
+        throws Exception
+    {
+        assert404( createRequest( NOT_EXISTING_REPOSITORY, EXISTING_VERSION + ".rpm" ) );
+    }
+
+    @Test
+    public void shouldRetrieveRestRequirements()
+        throws Exception
+    {
+        assertThat( resource.getResourceUri(), is( RESOURCE_URI ) );
+        assertThat( resource.getPayloadInstance(), nullValue() );
+        assertThat( resource.getPayloadInstance( GET ), nullValue() );
+        assertThat( resource.getPayloadInstance( POST ), instanceOf( String.class ) );
+    }
+
+    @Test
+    public void shouldRejectEmptyPayload()
+        throws Exception
+    {
+        check400ForPayload( null );
+    }
+
+    @Test
+    public void shouldRejectObjectPayload()
+        throws Exception
+    {
+        check400ForPayload( new Object() );
+    }
+
+    @Test
+    public void shouldSetVersion()
+        throws Exception
+    {
+        final Request request = createRequest( NOT_EXISTING_REPOSITORY, ALIAS_TO_CREATE );
+        StringRepresentation result = (StringRepresentation) resource.post( null, request, null, VERSION_TO_CREATE );
+        assertThat( result.getText(), is( VERSION_TO_CREATE ) );
+        result = (StringRepresentation) resource.get( null, request, null, null );
+        assertThat( result.getText(), is( VERSION_TO_CREATE ) );
+    }
+
+    private void check400ForPayload( Object payload )
+    {
+        try
+        {
+            resource.post( null, createRequest( NOT_EXISTING_REPOSITORY, RELEASES ), null, payload );
+            Assert.fail();
+        }
+        catch ( ResourceException e )
+        {
+            assertThat( e.getStatus(), is( CLIENT_ERROR_BAD_REQUEST ) );
+        }
+    }
+
+    private void assert404( final Request request )
+    {
+        try
+        {
+            resource.get( null, request, null, null );
+            Assert.fail( ResourceException.class + " expected" );
+        }
+        catch ( ResourceException e )
+        {
+            Assert.assertEquals( Status.CLIENT_ERROR_NOT_FOUND, e.getStatus() );
+        }
+    }
+
+    private Request createRequest( final String repoValue, final String aliasValue )
+    {
+        final Request request = new Request();
+        request.getAttributes().put( RepositoryVersionAliasResource.REPOSITORY_ID_PARAM, repoValue );
+        request.getAttributes().put( RepositoryVersionAliasResource.ALIAS_PARAM, aliasValue );
+        return request;
+    }
 
 }
--- nexus-yum-plugin/src/test/java/org/sonatype/nexus/plugins/yum/version/rest/VersionizedYumRepositoryResourceTest.java
+++ nexus-yum-plugin/src/test/java/org/sonatype/nexus/plugins/yum/version/rest/VersionizedYumRepositoryResourceTest.java
@@ -10,7 +10,7 @@
  * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
  * Eclipse Foundation. All other trademarks are the property of their respective owners.
  */
- package org.sonatype.nexus.plugins.yum.version.rest;
+package org.sonatype.nexus.plugins.yum.version.rest;
 
 import static org.easymock.EasyMock.createMock;
 import static org.easymock.EasyMock.expect;
@@ -43,138 +43,161 @@ import org.sonatype.plexus.rest.resource.PlexusResource;
 import com.google.code.tempusfugit.temporal.Condition;
 import com.noelios.restlet.http.HttpResponse;
 
+public class VersionizedYumRepositoryResourceTest
+    extends AbstractYumNexusTestCase
+{
+    private static final String VERSION = "2.2-1";
 
-public class VersionizedYumRepositoryResourceTest extends AbstractYumNexusTestCase {
-  private static final String VERSION = "2.2-1";
-  private static final String TESTREPO = "conflict-artifact";
-  private static final String ALIAS = "myAlias";
+    private static final String TESTREPO = "conflict-artifact";
 
-  @Requirement(hint = "VersionizedYumRepositoryResource")
-  private PlexusResource resource;
+    private static final String ALIAS = "myAlias";
 
-  @Inject
-  private RepositoryRegistry repositoryRegistry;
+    @Requirement( hint = "VersionizedYumRepositoryResource" )
+    private PlexusResource resource;
 
-  @Inject
-  private YumConfiguration aliasMapper;
+    @Inject
+    private RepositoryRegistry repositoryRegistry;
 
-  private MavenRepository repository;
+    @Inject
+    private YumConfiguration aliasMapper;
 
-  @Before
-  public void registerRepository() throws Exception {
-    if (repository == null) {
-      repository = createRepository(TESTREPO);
-    }
-    repositoryRegistry.registerRepository(repository);
-    waitFor(new Condition() {
-        @Override
-        public boolean isSatisfied() {
-          return repositoryRegistry.findRepositoryInfoForId(TESTREPO).getVersions().size() == 5;
+    private MavenRepository repository;
+
+    @Before
+    public void registerRepository()
+        throws Exception
+    {
+        if ( repository == null )
+        {
+            repository = createRepository( TESTREPO );
         }
-      });
-    aliasMapper.setAlias(TESTREPO, ALIAS, VERSION);
-  }
+        repositoryRegistry.registerRepository( repository );
+        waitFor( new Condition()
+        {
+            @Override
+            public boolean isSatisfied()
+            {
+                return repositoryRegistry.findRepositoryInfoForId( TESTREPO ).getVersions().size() == 5;
+            }
+        } );
+        aliasMapper.setAlias( TESTREPO, ALIAS, VERSION );
+    }
 
-  @Test
-  public void shouldAllowResourceAccess() throws Exception {
-    Assert.assertEquals("/yum/repos/{repository}/{version}", resource.getResourceUri());
-    Assert.assertEquals("/yum/*", resource.getResourceProtection().getPathPattern());
-    Assert.assertEquals("anon", resource.getResourceProtection().getFilterExpression());
-    Assert.assertNull(resource.getPayloadInstance());
-  }
+    @Test( expected = ResourceException.class )
+    public void shouldThrowNotFound()
+        throws Exception
+    {
+        resource.get( null, createRequest( "/", "blabla", "80.02.2" ), null, null );
+    }
 
-  @Test(expected = ResourceException.class)
-  public void shouldThrowNotFound() throws Exception {
-    resource.get(null, createRequest("/", "blabla", "80.02.2"), null, null);
-  }
+    @Test( expected = ResourceException.class )
+    public void shouldThrowNotFoundForVersion()
+        throws Exception
+    {
+        resource.get( null, createRequest( "/", TESTREPO, "not-present" ), null, null );
+    }
 
-  @Test(expected = ResourceException.class)
-  public void shouldThrowNotFoundForVersion() throws Exception {
-    resource.get(null, createRequest("/", TESTREPO, "not-present"), null, null);
-  }
+    @Test
+    public void shouldRedirectIndex()
+        throws Exception
+    {
+        Request request = createRequest( "", TESTREPO, VERSION );
+        Response response = createResponse( request );
+        resource.get( null, request, response, null );
+        Assert.assertEquals( Status.REDIRECTION_PERMANENT, response.getStatus() );
+        Assert.assertEquals( "http://localhost:8081/nexus/service/local/yum/repos/" + TESTREPO + "/" + VERSION + "/",
+            response.getLocationRef().getIdentifier() );
+    }
 
-  @Test
-  public void shouldRedirectIndex() throws Exception {
-    Request request = createRequest("", TESTREPO, VERSION);
-    Response response = createResponse(request);
-    resource.get(null, request, response, null);
-    Assert.assertEquals(Status.REDIRECTION_PERMANENT, response.getStatus());
-    Assert.assertEquals("http://localhost:8081/nexus/service/local/yum/repos/" + TESTREPO + "/" + VERSION + "/",
-      response.getLocationRef().getIdentifier());
-  }
+    @Test
+    public void shouldProvideFile()
+        throws Exception
+    {
+        Request request = createRequest( "/repodata/repomd.xml", TESTREPO, VERSION );
+        System.out.println( repositoryRegistry.findRepositoryInfoForId( TESTREPO ).getVersions() );
 
-  @Test
-  public void shouldProvideFile() throws Exception {
-    Request request = createRequest("/repodata/repomd.xml", TESTREPO, VERSION);
-    System.out.println(repositoryRegistry.findRepositoryInfoForId(TESTREPO).getVersions());
+        FileRepresentation representation = (FileRepresentation) resource.get( null, request, null, null );
+        Assert.assertTrue( representation.getFile().exists() );
+    }
 
-    FileRepresentation representation = (FileRepresentation) resource.get(null, request, null, null);
-    Assert.assertTrue(representation.getFile().exists());
-  }
+    @Test
+    public void shouldGenerateFileIndex()
+        throws Exception
+    {
+        Request request = createRequest( "/repodata/", TESTREPO, VERSION );
+        StringRepresentation representation = (StringRepresentation) resource.get( null, request, null, null );
+        Assert.assertEquals( MediaType.TEXT_HTML, representation.getMediaType() );
+        Assert.assertTrue( representation.getText().contains( "repomd.xml" ) );
+    }
 
-  @Test
-  public void shouldGenerateFileIndex() throws Exception {
-    Request request = createRequest("/repodata/", TESTREPO, VERSION);
-    StringRepresentation representation = (StringRepresentation) resource.get(null, request, null, null);
-    Assert.assertEquals(MediaType.TEXT_HTML, representation.getMediaType());
-    Assert.assertTrue(representation.getText().contains("repomd.xml"));
-  }
+    @Test
+    public void shouldGenerateDirectoryIndex()
+        throws Exception
+    {
+        shouldGenerateDirectoryIndexForVersionAndRepo( VERSION, TESTREPO );
+    }
 
-  @Test
-  public void shouldGenerateDirectoryIndex() throws Exception {
-    shouldGenerateDirectoryIndexForVersionAndRepo(VERSION, TESTREPO);
-  }
+    @Test
+    public void shouldGenerateDirectoryIndexForAlias()
+        throws Exception
+    {
+        shouldGenerateDirectoryIndexForVersionAndRepo( ALIAS, TESTREPO );
+    }
 
-  @Test
-  public void shouldGenerateDirectoryIndexForAlias() throws Exception {
-    shouldGenerateDirectoryIndexForVersionAndRepo(ALIAS, TESTREPO);
-  }
+    @Test
+    public void shouldNotFindFilesOutsideTheRepository()
+        throws Exception
+    {
+        Request request = createRequest( "/any-rpm.rpm", TESTREPO, VERSION );
+        FileRepresentation representation = (FileRepresentation) resource.get( null, request, null, null );
+        Assert.assertFalse( representation.getFile().exists() );
+    }
 
-  @Test
-  public void shouldNotFindFilesOutsideTheRepository() throws Exception {
-    Request request = createRequest("/any-rpm.rpm", TESTREPO, VERSION);
-    FileRepresentation representation = (FileRepresentation) resource.get(null, request, null, null);
-    Assert.assertFalse(representation.getFile().exists());
-  }
+    @Test( expected = ResourceException.class )
+    public void shouldThrowExceptionForAccessToParentDirectory()
+        throws Exception
+    {
+        Request request = createRequest( "/../any-rpm.rpm", TESTREPO, VERSION );
+        resource.get( null, request, null, null );
+    }
 
-  @Test(expected = ResourceException.class)
-  public void shouldThrowExceptionForAccessToParentDirectory() throws Exception {
-    Request request = createRequest("/../any-rpm.rpm", TESTREPO, VERSION);
-    resource.get(null, request, null, null);
-  }
+    private void shouldGenerateDirectoryIndexForVersionAndRepo( final String version, final String repo )
+        throws ResourceException
+    {
+        Request request = createRequest( "/", repo, version );
+        StringRepresentation representation = (StringRepresentation) resource.get( null, request, null, null );
+        Assert.assertEquals( MediaType.TEXT_HTML, representation.getMediaType() );
+        Assert.assertTrue( representation.getText().contains( "repodata/" ) );
+    }
 
-  private void shouldGenerateDirectoryIndexForVersionAndRepo(final String version, final String repo)
-    throws ResourceException {
-    Request request = createRequest("/", repo, version);
-    StringRepresentation representation = (StringRepresentation) resource.get(null, request, null, null);
-    Assert.assertEquals(MediaType.TEXT_HTML, representation.getMediaType());
-    Assert.assertTrue(representation.getText().contains("repodata/"));
-  }
+    private Response createResponse( Request request )
+    {
+        return new HttpResponse( null, request );
+    }
 
-  private Response createResponse(Request request) {
-    return new HttpResponse(null, request);
-  }
+    private Request createRequest( String urlSuffix, String repository, String version )
+    {
+        Request request =
+            new Request( Method.GET, "http://localhost:8081/nexus/service/local/yum/repos/" + repository + "/"
+                + version + urlSuffix );
+        request.setAttributes( createAttributes( repository, version ) );
+        return request;
+    }
 
-  private Request createRequest(String urlSuffix, String repository, String version) {
-    Request request = new Request(Method.GET,
-      "http://localhost:8081/nexus/service/local/yum/repos/" + repository + "/" + version +
-      urlSuffix);
-    request.setAttributes(createAttributes(repository, version));
-    return request;
-  }
+    private Map<String, Object> createAttributes( String repository, String version )
+    {
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put( "repository", repository );
+        map.put( "version", version );
+        return map;
+    }
 
-  private Map<String, Object> createAttributes(String repository, String version) {
-    Map<String, Object> map = new HashMap<String, Object>();
-    map.put("repository", repository);
-    map.put("version", version);
-    return map;
-  }
-
-  public MavenRepository createRepository(String id) {
-    MavenRepository repo = createMock(MavenRepository.class);
-    expect(repo.getId()).andReturn(id).anyTimes();
-    expect(repo.getLocalUrl()).andReturn("file:" + RepositoryTestUtils.RPM_BASE_FILE.getAbsolutePath()).anyTimes();
-    replay(repo);
-    return repo;
-  }
+    public MavenRepository createRepository( String id )
+    {
+        MavenRepository repo = createMock( MavenRepository.class );
+        expect( repo.getId() ).andReturn( id ).anyTimes();
+        expect( repo.getLocalUrl() ).andReturn( "file:" + RepositoryTestUtils.RPM_BASE_FILE.getAbsolutePath() ).anyTimes();
+        replay( repo );
+        return repo;
+    }
 }
