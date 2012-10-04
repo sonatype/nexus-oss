5ac3a62a44798e768cc99a78f0da9bf20ae69a97
sherold <sebastian.herold@immobilienscout24.de>
2012-10-04 11:34:17 +0200

    extract url from request not via GlobalRestApiSettings

--- nexus-yum-plugin/src/main/java/org/sonatype/nexus/plugins/yum/rest/AbstractYumRepositoryResource.java
+++ nexus-yum-plugin/src/main/java/org/sonatype/nexus/plugins/yum/rest/AbstractYumRepositoryResource.java
@@ -10,9 +10,7 @@
  * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
  * Eclipse Foundation. All other trademarks are the property of their respective owners.
  */
- package org.sonatype.nexus.plugins.yum.rest;
-
-import javax.inject.Inject;
+package org.sonatype.nexus.plugins.yum.rest;
 
 import org.restlet.Context;
 import org.restlet.data.Request;
@@ -21,65 +19,66 @@ import org.restlet.data.Status;
 import org.restlet.resource.Representation;
 import org.restlet.resource.ResourceException;
 import org.restlet.resource.Variant;
-import org.sonatype.nexus.configuration.application.GlobalRestApiSettings;
 import org.sonatype.nexus.plugins.yum.repository.FileDirectoryStructure;
 import org.sonatype.nexus.plugins.yum.rest.domain.IndexRepresentation;
 import org.sonatype.nexus.plugins.yum.rest.domain.UrlPathInterpretation;
 import org.sonatype.nexus.plugins.yum.rest.domain.YumFileRepresentation;
 import org.sonatype.plexus.rest.resource.AbstractPlexusResource;
 
+public abstract class AbstractYumRepositoryResource
+    extends AbstractPlexusResource
+{
+    private final UrlPathParser requestSegmentInterpetor;
 
-public abstract class AbstractYumRepositoryResource extends AbstractPlexusResource {
-  private UrlPathParser requestSegmentInterpetor;
-
-  private GlobalRestApiSettings restApiSettings;
-
-  @Inject
-  public void initialize(GlobalRestApiSettings restApiSettings) {
-    this.restApiSettings = restApiSettings;
-    this.requestSegmentInterpetor = new UrlPathParser(getVersionedRepositoryBaseUrl(), getUrlPrefixName(),
-      getSegmentCountAfterPrefix());
-  }
-
-  @Override
-  public Object getPayloadInstance() {
-    // if you allow PUT or POST you would need to return your object.
-    return null;
-  }
-
-  @Override
-  public Object get(Context context, Request request, Response response, Variant variant) throws ResourceException {
-    try {
-      UrlPathInterpretation interpretation = requestSegmentInterpetor.interprete(request);
-
-      if (interpretation.isRedirect()) {
-        response.redirectPermanent(interpretation.getRedirectUri());
-        return null;
-      }
-
-      return createRepresentation(interpretation, getFileStructure(request, interpretation));
-    } catch (ResourceException e) {
-      throw e;
-    } catch (Exception e) {
-      throw new ResourceException(Status.SERVER_ERROR_INTERNAL, e);
+    public AbstractYumRepositoryResource()
+    {
+        this.requestSegmentInterpetor = new UrlPathParser( getUrlPrefixName(), getSegmentCountAfterPrefix() );
     }
-  }
 
-  private String getVersionedRepositoryBaseUrl() {
-    return restApiSettings.getBaseUrl() + "/service/local";
-  }
+    @Override
+    public Object getPayloadInstance()
+    {
+        // if you allow PUT or POST you would need to return your object.
+        return null;
+    }
 
-  private Representation createRepresentation(UrlPathInterpretation interpretation,
-    FileDirectoryStructure fileDirectoryStructure) {
-    return interpretation.isIndex() ? new IndexRepresentation(interpretation, fileDirectoryStructure)
-                                    : new YumFileRepresentation(
-        interpretation, fileDirectoryStructure);
-  }
+    @Override
+    public Object get( Context context, Request request, Response response, Variant variant )
+        throws ResourceException
+    {
+        try
+        {
+            UrlPathInterpretation interpretation = requestSegmentInterpetor.interprete( request );
 
-  protected abstract String getUrlPrefixName();
+            if ( interpretation.isRedirect() )
+            {
+                response.redirectPermanent( interpretation.getRedirectUri() );
+                return null;
+            }
 
-  protected abstract FileDirectoryStructure getFileStructure(Request request, UrlPathInterpretation interpretation)
-    throws Exception;
+            return createRepresentation( interpretation, getFileStructure( request, interpretation ) );
+        }
+        catch ( ResourceException e )
+        {
+            throw e;
+        }
+        catch ( Exception e )
+        {
+            throw new ResourceException( Status.SERVER_ERROR_INTERNAL, e );
+        }
+    }
 
-  protected abstract int getSegmentCountAfterPrefix();
+    private Representation createRepresentation( UrlPathInterpretation interpretation,
+                                                 FileDirectoryStructure fileDirectoryStructure )
+    {
+        return interpretation.isIndex() ? new IndexRepresentation( interpretation, fileDirectoryStructure )
+            : new YumFileRepresentation( interpretation, fileDirectoryStructure );
+    }
+
+    protected abstract String getUrlPrefixName();
+
+    protected abstract FileDirectoryStructure getFileStructure( Request request, UrlPathInterpretation interpretation )
+        throws Exception;
+
+    protected abstract int getSegmentCountAfterPrefix();
 }
--- nexus-yum-plugin/src/main/java/org/sonatype/nexus/plugins/yum/rest/UrlPathParser.java
+++ nexus-yum-plugin/src/main/java/org/sonatype/nexus/plugins/yum/rest/UrlPathParser.java
@@ -10,7 +10,7 @@
  * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
  * Eclipse Foundation. All other trademarks are the property of their respective owners.
  */
- package org.sonatype.nexus.plugins.yum.rest;
+package org.sonatype.nexus.plugins.yum.rest;
 
 import static org.apache.commons.lang.StringUtils.join;
 import static org.restlet.data.Status.CLIENT_ERROR_BAD_REQUEST;
@@ -25,69 +25,87 @@ import org.restlet.resource.ResourceException;
 import org.sonatype.nexus.plugins.yum.repository.YumRepository;
 import org.sonatype.nexus.plugins.yum.rest.domain.UrlPathInterpretation;
 
+public class UrlPathParser
+{
+    private static final int FIRST_PARAM = 0;
 
-public class UrlPathParser {
-  private static final int FIRST_PARAM = 0;
-  private static final int SECOND_PARAM = 1;
-  private final String segmentPrefix;
-  private final int segmentsAfterPrefix;
-  private final String yumBaseUrl;
+    private static final int SECOND_PARAM = 1;
 
-  public UrlPathParser(String yumBaseUrl, String yumRepoPrefixName, int segmentsAfterPrefix) {
-    this.yumBaseUrl = yumBaseUrl;
-    this.segmentPrefix = yumRepoPrefixName;
-    this.segmentsAfterPrefix = segmentsAfterPrefix;
-  }
+    private final String segmentPrefix;
 
-  public UrlPathInterpretation interprete(Request request) throws ResourceException {
-    List<String> segments = request.getResourceRef().getSegments();
+    private final int segmentsAfterPrefix;
 
-    int yumIndex = segments.indexOf(segmentPrefix);
-    if (yumIndex < 0) {
-      throw new ResourceException(SERVER_ERROR_INTERNAL, "Prefix '" + segmentPrefix + "' not found.");
+    public UrlPathParser( String yumRepoPrefixName, int segmentsAfterPrefix )
+    {
+        this.segmentPrefix = yumRepoPrefixName;
+        this.segmentsAfterPrefix = segmentsAfterPrefix;
     }
 
-    URL repoUrl;
-    try {
-      repoUrl = new URL(yumBaseUrl + "/" + join(segments.subList(yumIndex, yumIndex + segmentsAfterPrefix + 1), "/"));
-    } catch (MalformedURLException e) {
-      throw new ResourceException(SERVER_ERROR_INTERNAL, e);
+    public UrlPathInterpretation interprete( Request request )
+        throws ResourceException
+    {
+        final List<String> segments = request.getResourceRef().getSegments();
+        final int yumIndex = segments.indexOf( segmentPrefix );
+        if ( yumIndex < 0 )
+        {
+            throw new ResourceException( SERVER_ERROR_INTERNAL, "Prefix '" + segmentPrefix + "' not found." );
+        }
+
+        final URL repoUrl;
+        try
+        {
+            repoUrl =
+                new URL( request.getResourceRef().getHostIdentifier() + "/"
+                    + join( segments.subList( 0, yumIndex + segmentsAfterPrefix + 1 ), "/" ) );
+        }
+        catch ( MalformedURLException e )
+        {
+            throw new ResourceException( SERVER_ERROR_INTERNAL, e );
+        }
+
+        final List<String> lastSegments = segments.subList( yumIndex + segmentsAfterPrefix + 1, segments.size() );
+
+        if ( lastSegments.contains( ".." ) )
+        {
+            throw new ResourceException( CLIENT_ERROR_BAD_REQUEST, "Requests with '..' are not allowed" );
+        }
+
+        if ( lastSegments.isEmpty() )
+        {
+            return new UrlPathInterpretation( repoUrl, null, true, true, pathToIndex( segments ) );
+        }
+
+        if ( lastSegments.get( FIRST_PARAM ).length() == 0 )
+        {
+            return new UrlPathInterpretation( repoUrl, null, true );
+        }
+
+        if ( YumRepository.YUM_REPOSITORY_DIR_NAME.equals( lastSegments.get( FIRST_PARAM ) ) )
+        {
+            if ( lastSegments.size() == 1 )
+            {
+                return new UrlPathInterpretation( repoUrl, YumRepository.YUM_REPOSITORY_DIR_NAME, true, true,
+                    pathToIndex( segments ) );
+            }
+
+            if ( lastSegments.get( SECOND_PARAM ).length() == 0 )
+            {
+                return new UrlPathInterpretation( repoUrl, YumRepository.YUM_REPOSITORY_DIR_NAME, true );
+            }
+
+            if ( lastSegments.size() == 2 )
+            {
+                return new UrlPathInterpretation( repoUrl, lastSegments.get( FIRST_PARAM ) + "/"
+                    + lastSegments.get( SECOND_PARAM ), false );
+            }
+        }
+
+        return new UrlPathInterpretation( repoUrl, join( lastSegments, "/" ), false, false, null );
     }
 
-    List<String> lastSegments = segments.subList(yumIndex + segmentsAfterPrefix + 1, segments.size());
-
-    if (lastSegments.contains("..")) {
-      throw new ResourceException(CLIENT_ERROR_BAD_REQUEST, "Requests with '..' are not allowed");
+    private String pathToIndex( List<String> segments )
+    {
+        return "/" + join( segments, "/" ) + "/";
     }
-
-    if (lastSegments.isEmpty()) {
-      return new UrlPathInterpretation(repoUrl, null, true, true, pathToIndex(segments));
-    }
-
-    if (lastSegments.get(FIRST_PARAM).length() == 0) {
-      return new UrlPathInterpretation(repoUrl, null, true);
-    }
-
-    if (YumRepository.YUM_REPOSITORY_DIR_NAME.equals(lastSegments.get(FIRST_PARAM))) {
-      if (lastSegments.size() == 1) {
-        return new UrlPathInterpretation(repoUrl, YumRepository.YUM_REPOSITORY_DIR_NAME, true, true, pathToIndex(segments));
-      }
-
-      if (lastSegments.get(SECOND_PARAM).length() == 0) {
-        return new UrlPathInterpretation(repoUrl, YumRepository.YUM_REPOSITORY_DIR_NAME, true);
-      }
-
-      if (lastSegments.size() == 2) {
-        return new UrlPathInterpretation(repoUrl, lastSegments.get(FIRST_PARAM) + "/" + lastSegments.get(SECOND_PARAM),
-          false);
-      }
-    }
-
-    return new UrlPathInterpretation(repoUrl, join(lastSegments, "/"), false, false, null);
-  }
-
-  private String pathToIndex(List<String> segments) {
-    return "/" + join(segments, "/") + "/";
-  }
 
 }
