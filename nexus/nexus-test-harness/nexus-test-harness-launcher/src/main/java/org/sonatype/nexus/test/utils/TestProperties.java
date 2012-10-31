31512665691d0d06cdd5981d9eab6ec30655dd4b
Jason Dillon <jason@planet57.com>
2012-10-31 12:32:48 -0700

    Lazy init bundle, note about how this class sucks

--- nexus/nexus-test-harness/nexus-test-harness-launcher/src/main/java/org/sonatype/nexus/test/utils/TestProperties.java
+++ nexus/nexus-test-harness/nexus-test-harness-launcher/src/main/java/org/sonatype/nexus/test/utils/TestProperties.java
@@ -18,36 +18,41 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.ResourceBundle;
 
+//
+// FIXME: This class is used in many static context in tests, and can fail to locate the "baseTest" bundle (which is installed by the maven-test-environment-maven-plugin).
+// FIXME: Use of this class in a static context should be avoided.
+//
+
 public class TestProperties
 {
-
     private static ResourceBundle bundle;
 
-    // i hate doing this, but its really easy, and this is for tests. this class can be replaced easy, if we need to...
-    static
-    {
-        bundle = ResourceBundle.getBundle( "baseTest" );
+    private static synchronized ResourceBundle getBundle() {
+        if (bundle == null) {
+            bundle = ResourceBundle.getBundle( "baseTest" );
+        }
+        return bundle;
     }
 
     public static String getString( String key )
     {
-        return bundle.getString( key );
+        return getBundle().getString( key );
     }
 
     public static Integer getInteger( String key )
     {
-        String value = bundle.getString( key );
+        String value = getBundle().getString( key );
         return new Integer( value );
     }
 
     public static Map<String, String> getAll()
     {
         Map<String, String> properties = new LinkedHashMap<String, String>();
-        Enumeration<String> keys = bundle.getKeys();
+        Enumeration<String> keys = getBundle().getKeys();
         while ( keys.hasMoreElements() )
         {
             String key = keys.nextElement();
-            properties.put( key, bundle.getString( key ) );
+            properties.put( key, getBundle().getString( key ) );
         }
         return properties;
     }
@@ -56,5 +61,4 @@ public class TestProperties
     {
         return new File( getString( key ) );
     }
-
 }
