263b2743397b655fb0ec838a06d7b38e473cb25e
Alin Dreghiciu <adreghiciu@gmail.com>
2012-11-16 03:25:46 +0200

    Fix remove member from group IT
    Signed-off-by: Alin Dreghiciu <adreghiciu@gmail.com>

--- nexus-yum-testsuite/src/test/java/org/sonatype/nexus/repository/yum/testsuite/YumGroupRepositoryIT.java
+++ nexus-yum-testsuite/src/test/java/org/sonatype/nexus/repository/yum/testsuite/YumGroupRepositoryIT.java
@@ -19,8 +19,12 @@ import static org.junit.Assert.assertThat;
 import static org.sonatype.nexus.client.core.subsystem.content.Location.repositoryLocation;
 import static org.sonatype.nexus.repository.yum.client.MetadataType.PRIMARY_XML;
 
+import java.io.IOException;
+
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.sonatype.nexus.client.core.exception.NexusClientNotFoundException;
 import org.sonatype.nexus.client.core.subsystem.repository.GroupRepository;
 import org.sonatype.nexus.client.core.subsystem.repository.maven.MavenHostedRepository;
 import org.sonatype.nexus.repository.yum.client.YumGroupRepository;
@@ -34,6 +38,9 @@ public class YumGroupRepositoryIT
     @Rule
     public OsTestRule osTestRule = new OsTestRule();
 
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
     public YumGroupRepositoryIT( final String nexusBundleCoordinates )
     {
         super( nexusBundleCoordinates );
@@ -44,7 +51,7 @@ public class YumGroupRepositoryIT
     public void shouldRegenerateRepoAfterUpload()
         throws Exception
     {
-        final GroupRepository groupRepo = givenGroupRepoWith2RPMs();
+        final GroupRepository groupRepo = givenAYumGroupRepoWith2RPMs();
 
         final String primaryXml = getPrimaryXmlOf( groupRepo );
         assertThat( primaryXml, containsString( "test-artifact" ) );
@@ -56,8 +63,8 @@ public class YumGroupRepositoryIT
     public void shouldRegenerateGroupRepoWhenMemberRepoIsRemoved()
         throws Exception
     {
-        final GroupRepository groupRepo = givenGroupRepoWith2RPMs();
-        groupRepo.removeMember( groupRepo.memberRepositories().get( 0 ).toString() ).save();
+        final GroupRepository groupRepo = givenAYumGroupRepoWith2RPMs();
+        groupRepo.removeMember( repositoryIdForTest( "2" ) ).save();
 
         sleep( 5, SECONDS );
 
@@ -68,10 +75,25 @@ public class YumGroupRepositoryIT
 
     @Test
     @IgnoreOn( "mac" )
+    public void removeYumRepositoryWhenOnlyOneMember()
+        throws Exception
+    {
+        final GroupRepository groupRepo = givenAYumGroupRepoWith2RPMs();
+        groupRepo.removeMember( repositoryIdForTest( "1" ) ).save();
+        groupRepo.removeMember( repositoryIdForTest( "2" ) ).save();
+
+        sleep( 5, SECONDS );
+
+        thrown.expect( NexusClientNotFoundException.class );
+        getPrimaryXmlOf( groupRepo );
+    }
+
+    @Test
+    @IgnoreOn( "mac" )
     public void shouldRegenerateGroupRepoWhenMemberRepoIsAdded()
         throws Exception
     {
-        final GroupRepository groupRepo = givenGroupRepoWith2RPMs();
+        final GroupRepository groupRepo = givenAYumGroupRepoWith2RPMs();
 
         final MavenHostedRepository repo3 = repositories().create(
             MavenHostedRepository.class, repositoryIdForTest( "3" )
@@ -96,11 +118,12 @@ public class YumGroupRepositoryIT
     }
 
     private String getPrimaryXmlOf( final GroupRepository groupRepo )
+        throws IOException
     {
         return yum().getMetadata( groupRepo.id(), PRIMARY_XML, String.class );
     }
 
-    private YumGroupRepository givenGroupRepoWith2RPMs()
+    private YumGroupRepository givenAYumGroupRepoWith2RPMs()
         throws Exception
     {
         final MavenHostedRepository repo1 = repositories().create(
@@ -111,8 +134,12 @@ public class YumGroupRepositoryIT
             MavenHostedRepository.class, repositoryIdForTest( "2" )
         ).excludeFromSearchResults().save();
 
+        final MavenHostedRepository repoX = repositories().create(
+            MavenHostedRepository.class, repositoryIdForTest( "X" )
+        ).excludeFromSearchResults().save();
+
         final YumGroupRepository groupRepo = repositories().create( YumGroupRepository.class, repositoryIdForTest() )
-            .ofRepositories( repo1.id(), repo2.id() )
+            .ofRepositories( repo1.id(), repo2.id(), repoX.id() )
             .save();
 
         sleep( 5, SECONDS );
@@ -123,7 +150,7 @@ public class YumGroupRepositoryIT
         );
 
         content().upload(
-            repositoryLocation( repo1.id(), "a_group2/an_artifact2/2.0/an_artifact2-2.0.rpm" ),
+            repositoryLocation( repo2.id(), "a_group2/an_artifact2/2.0/an_artifact2-2.0.rpm" ),
             testData.resolveFile( "/rpms/test-rpm-5.6.7-1.noarch.rpm" )
         );
 
