c983df427499ec5fea4b3bacd8cef43fb2c529e8
Benjamin Hanzelmann <ben@nabcos.de>
2012-11-16 21:22:04 +0100

    Fix requirejs dependencies

--- nexus/nexus-webapp/src/main/webapp/js/sonatype/all.js
+++ nexus/nexus-webapp/src/main/webapp/js/sonatype/all.js
@@ -10,6 +10,6 @@
  * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
  * Eclipse Foundation. All other trademarks are the property of their respective owners.
  */
-define('sonatype/all',['sonatype', 'sonatype/lib', 'sonatype/utils', 'sonatype/view', 'nexus/panels'], function(Sonatype) {
+define('sonatype/all',['sonatype', 'sonatype/lib', 'sonatype/utils', 'sonatype/view', 'nexus/panels', 'nexus/ext'], function(Sonatype) {
   return Sonatype;
 });
