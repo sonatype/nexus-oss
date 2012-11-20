0322d9789715b7e49f25babe044e2121d7c7a6fa
Alin Dreghiciu <adreghiciu@gmail.com>
2012-11-21 00:31:36 +0200

    Allow Yum group capability on any group repository
    Signed-off-by: Alin Dreghiciu <adreghiciu@gmail.com>

--- nexus-yum-plugin/src/main/java/org/sonatype/nexus/repository/yum/internal/capabilities/YumGroupRepositoryCapabilityDescriptor.java
+++ nexus-yum-plugin/src/main/java/org/sonatype/nexus/repository/yum/internal/capabilities/YumGroupRepositoryCapabilityDescriptor.java
@@ -27,7 +27,7 @@ import org.sonatype.nexus.plugins.capabilities.CapabilityType;
 import org.sonatype.nexus.plugins.capabilities.Validator;
 import org.sonatype.nexus.plugins.capabilities.support.CapabilityDescriptorSupport;
 import org.sonatype.nexus.plugins.capabilities.support.validator.Validators;
-import org.sonatype.nexus.proxy.maven.MavenGroupRepository;
+import org.sonatype.nexus.proxy.repository.GroupRepository;
 
 @Singleton
 @Named( YumGroupRepositoryCapabilityDescriptor.TYPE_ID )
@@ -58,7 +58,7 @@ public class YumGroupRepositoryCapabilityDescriptor
     public Validator validator()
     {
         return validators.logical().and(
-            validators.repository().repositoryOfType( TYPE, REPOSITORY_ID, MavenGroupRepository.class ),
+            validators.repository().repositoryOfType( TYPE, REPOSITORY_ID, GroupRepository.class ),
             validators.capability().uniquePer( TYPE, REPOSITORY_ID )
         );
     }
@@ -67,7 +67,7 @@ public class YumGroupRepositoryCapabilityDescriptor
     public Validator validator( final CapabilityIdentity id )
     {
         return validators.logical().and(
-            validators.repository().repositoryOfType( TYPE, REPOSITORY_ID, MavenGroupRepository.class ),
+            validators.repository().repositoryOfType( TYPE, REPOSITORY_ID, GroupRepository.class ),
             validators.capability().uniquePerExcluding( id, TYPE, REPOSITORY_ID )
         );
     }
