500195e61e2be7248061daec42bf9a4330e171a4
Stuart McCulloch <mcculls@gmail.com>
2012-11-27 20:42:16 +0000

    More RemoteIPFinder fixes: respect XFF spec and return left-most resolvable IP, which should be the client's.
    Also restlet1x appears to reverse the order of XFF entries when it stores them in ClientInfo.getAddresses,
    so we now reverse this list to get it back to XFF order before resolving. (Out of interest, restlet2x has
    fixed this and maintains the same order as XFF - it also has a clearer API for working with addresses.)

--- nexus/plugins/restlet1x/nexus-restlet1x-plugin/src/test/java/org/sonatype/nexus/rest/RemoteIPFinderTest.java
+++ nexus/plugins/restlet1x/nexus-restlet1x-plugin/src/test/java/org/sonatype/nexus/rest/RemoteIPFinderTest.java
@@ -12,23 +12,100 @@
  */
 package org.sonatype.nexus.rest;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 
 import junit.framework.Assert;
 
 import org.junit.Test;
 import org.mockito.Mockito;
+import org.restlet.data.ClientInfo;
+import org.restlet.data.Form;
+import org.restlet.data.Request;
 
+@SuppressWarnings( "unchecked" )
 public class RemoteIPFinderTest
 {
     @Test
     public void testResolveIP()
     {
+        HttpServletRequest http = Mockito.mock( HttpServletRequest.class );
+
+        Request restlet = Mockito.mock( Request.class );
+        Map<String, Object> attributes = Mockito.mock( Map.class );
+        Form form = Mockito.mock( Form.class );
+        ClientInfo clientInfo = new ClientInfo();
+
+        Mockito.doReturn( attributes ).when( restlet ).getAttributes();
+        Mockito.doReturn( form ).when( attributes ).get( "org.restlet.http.headers" );
+        Mockito.doReturn( clientInfo ).when( restlet ).getClientInfo();
+
         /*
-         * Broken resolveIP method always returned first element, check it now skips non-resolvable IPs:
+         * Verify that we respect the X-Forwarded-For spec and return the left-most resolvable IP:
          */
-        HttpServletRequest request = Mockito.mock( HttpServletRequest.class );
-        Mockito.doReturn( "[missing],127.0.0.1,{unknown}" ).when( request ).getHeader( "X-Forwarded-For" );
-        Assert.assertEquals( "127.0.0.1", RemoteIPFinder.findIP( request ) );
+
+        // HTTP
+
+        Mockito.doReturn( "" ).when( http ).getHeader( RemoteIPFinder.FORWARD_HEADER );
+        Assert.assertEquals( null, RemoteIPFinder.findIP( http ) );
+
+        Mockito.doReturn( null ).when( http ).getHeader( RemoteIPFinder.FORWARD_HEADER );
+        Assert.assertEquals( null, RemoteIPFinder.findIP( http ) );
+
+        Mockito.doReturn( "127.0.0.1" ).when( http ).getHeader( RemoteIPFinder.FORWARD_HEADER );
+        Assert.assertEquals( "127.0.0.1", RemoteIPFinder.findIP( http ) );
+
+        Mockito.doReturn( "missing, 127.0.0.2, unknown, 127.0.0.1" ).when( http ).getHeader( RemoteIPFinder.FORWARD_HEADER );
+        Assert.assertEquals( "127.0.0.2", RemoteIPFinder.findIP( http ) );
+
+        Mockito.doReturn( "127.0.0.3, 127.0.0.2, 127.0.0.1" ).when( http ).getHeader( RemoteIPFinder.FORWARD_HEADER );
+        Assert.assertEquals( "127.0.0.3", RemoteIPFinder.findIP( http ) );
+
+        Mockito.doReturn( "localhost" ).when( http ).getHeader( RemoteIPFinder.FORWARD_HEADER );
+        Assert.assertEquals( "localhost", RemoteIPFinder.findIP( http ) );
+
+        Mockito.doReturn( "client, proxy1, proxy2" ).when( http ).getHeader( RemoteIPFinder.FORWARD_HEADER );
+        Mockito.doReturn( null ).when( http ).getRemoteAddr();
+        Assert.assertEquals( null, RemoteIPFinder.findIP( http ) );
+
+        Mockito.doReturn( "client, proxy1, proxy2" ).when( http ).getHeader( RemoteIPFinder.FORWARD_HEADER );
+        Mockito.doReturn( "upstream" ).when( http ).getRemoteAddr();
+        Assert.assertEquals( "upstream", RemoteIPFinder.findIP( http ) );
+
+        // RESTLET
+
+        Mockito.doReturn( "" ).when( form ).getFirstValue( RemoteIPFinder.FORWARD_HEADER );
+        Assert.assertEquals( null, RemoteIPFinder.findIP( restlet ) );
+
+        Mockito.doReturn( null ).when( form ).getFirstValue( RemoteIPFinder.FORWARD_HEADER );
+        Assert.assertEquals( null, RemoteIPFinder.findIP( restlet ) );
+
+        Mockito.doReturn( "127.0.0.1" ).when( form ).getFirstValue( RemoteIPFinder.FORWARD_HEADER );
+        Assert.assertEquals( "127.0.0.1", RemoteIPFinder.findIP( restlet ) );
+
+        Mockito.doReturn( "missing, 127.0.0.2, unknown, 127.0.0.1" ).when( form ).getFirstValue( RemoteIPFinder.FORWARD_HEADER );
+        Assert.assertEquals( "127.0.0.2", RemoteIPFinder.findIP( restlet ) );
+
+        Mockito.doReturn( "127.0.0.3, 127.0.0.2, 127.0.0.1" ).when( form ).getFirstValue( RemoteIPFinder.FORWARD_HEADER );
+        Assert.assertEquals( "127.0.0.3", RemoteIPFinder.findIP( restlet ) );
+
+        Mockito.doReturn( "localhost" ).when( form ).getFirstValue( RemoteIPFinder.FORWARD_HEADER );
+        Assert.assertEquals( "localhost", RemoteIPFinder.findIP( restlet ) );
+
+        clientInfo.setAddresses( null );
+        Mockito.doReturn( "client, proxy1, proxy2" ).when( form ).getFirstValue( RemoteIPFinder.FORWARD_HEADER );
+        Assert.assertEquals( null, RemoteIPFinder.findIP( restlet ) );
+
+        clientInfo.setAddresses( Collections.<String> emptyList() );
+        Mockito.doReturn( "client, proxy1, proxy2" ).when( form ).getFirstValue( RemoteIPFinder.FORWARD_HEADER );
+        Assert.assertEquals( null, RemoteIPFinder.findIP( restlet ) );
+
+        // restlet1x clientInfo addresses are the last known upstream address + reverse of XFF
+        clientInfo.setAddresses( Arrays.asList( "upstream", "proxy2", "proxy1", "client" ) );
+        Mockito.doReturn( "client, proxy1, proxy2" ).when( form ).getFirstValue( RemoteIPFinder.FORWARD_HEADER );
+        Assert.assertEquals( "upstream", RemoteIPFinder.findIP( restlet ) );
     }
 }
