a63f7bd12088c48badaab1dedd0e414097c46c88
Jason Dillon <jason@planet57.com>
2012-11-28 12:15:19 -0800

    add a dummy test to allow build automation configuration to be uniform

---
+++ src/test/java/org/sonatype/nexus/plugins/p2bridge/internal/DummyTest.java
@@ -0,0 +1,11 @@
+package org.sonatype.nexus.plugins.p2bridge.internal;
+
+import org.junit.Test;
+
+public class DummyTest
+{
+    @Test
+    public void dummy() {
+        // FIXMe: This is here to allow build automation configuration to be unform, really should put some real tests here.
+    }
+}
