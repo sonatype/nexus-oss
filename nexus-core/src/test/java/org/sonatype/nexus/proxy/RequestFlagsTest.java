62c8e28ef7c321850fb0061878c9bb3568e0312e
Tamas Cservenak <tamas@cservenak.net>
2013-01-14 23:29:04 +0100

    Added last-modified header to responses.
    It turns out that test server was simply omitting last-modified
    headers, so now explicitly setting it to be able to simulate
    cases like expired but remote not newer and expired and
    remote is newer.

--- nexus-core/src/test/java/org/sonatype/nexus/proxy/RequestFlagsTest.java
+++ nexus-core/src/test/java/org/sonatype/nexus/proxy/RequestFlagsTest.java
@@ -1,6 +1,26 @@
+/*
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2007-2012 Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
 package org.sonatype.nexus.proxy;
 
 import static org.sonatype.tests.http.server.fluent.Behaviours.content;
+
+import java.util.Date;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import junit.framework.Assert;
 
 import org.apache.commons.io.IOUtils;
@@ -22,6 +42,7 @@ import org.sonatype.nexus.proxy.repository.ProxyRepository;
 import org.sonatype.nexus.templates.repository.RepositoryTemplate;
 import org.sonatype.nexus.test.NexusTestSupport;
 import org.sonatype.security.guice.SecurityModule;
+import org.sonatype.tests.http.server.api.Behaviour;
 import org.sonatype.tests.http.server.fluent.Behaviours;
 import org.sonatype.tests.http.server.fluent.Proxy;
 import org.sonatype.tests.http.server.fluent.Server;
@@ -29,18 +50,6 @@ import org.sonatype.tests.http.server.jetty.behaviour.Record;
 
 import com.google.inject.Module;
 
-/*
- * Sonatype Nexus (TM) Open Source Version
- * Copyright (c) 2007-2012 Sonatype, Inc.
- * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
- *
- * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
- * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
- *
- * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
- * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
- * Eclipse Foundation. All other trademarks are the property of their respective owners.
- */
 public class RequestFlagsTest
     extends NexusTestSupport
 {
@@ -58,6 +67,8 @@ public class RequestFlagsTest
 
     private Record recordedRequests;
 
+    private LastModifiedSender lastModifiedSender;
+
     private Nexus nexus;
 
     private ProxyRepository proxyRepository;
@@ -66,8 +77,15 @@ public class RequestFlagsTest
     public void prepare()
         throws Exception
     {
+        HttpServletResponse resp;
+
         recordedRequests = new Record();
-        server = Proxy.withPort( 0 ).serve( PATH ).withBehaviours( recordedRequests, content( CONTENT ) ).start();
+        // somewhere in near past
+        lastModifiedSender =
+            new LastModifiedSender( new Date( System.currentTimeMillis() - TimeUnit.DAYS.toMillis( 3 ) ) );
+
+        server =
+            Proxy.withPort( 0 ).serve( PATH ).withBehaviours( recordedRequests, lastModifiedSender, content( CONTENT ) ).start();
         nexus = lookup( Nexus.class );
         proxyRepository = createProxyRepository();
 
@@ -137,6 +155,7 @@ public class RequestFlagsTest
         final StorageItem item = proxyRepository.retrieveItem( request );
 
         MatcherAssert.assertThat( recordedRequests.getRequests().size(), Matchers.equalTo( 1 ) );
+        MatcherAssert.assertThat( recordedRequests.getRequests().get( 0 ), Matchers.startsWith( "GET" ) );
         MatcherAssert.assertThat( item, Matchers.instanceOf( StorageFileItem.class ) );
 
         final String content = IOUtils.toString( ( (StorageFileItem) item ).getContentLocator().getContent() );
@@ -159,6 +178,7 @@ public class RequestFlagsTest
         final StorageItem item = proxyRepository.retrieveItem( request );
 
         MatcherAssert.assertThat( recordedRequests.getRequests().size(), Matchers.equalTo( 1 ) );
+        MatcherAssert.assertThat( recordedRequests.getRequests().get( 0 ), Matchers.startsWith( "GET" ) );
         MatcherAssert.assertThat( item, Matchers.instanceOf( StorageFileItem.class ) );
 
         final String content = IOUtils.toString( ( (StorageFileItem) item ).getContentLocator().getContent() );
@@ -176,6 +196,7 @@ public class RequestFlagsTest
         final StorageItem item = proxyRepository.retrieveItem( request );
 
         MatcherAssert.assertThat( recordedRequests.getRequests().size(), Matchers.equalTo( 1 ) );
+        MatcherAssert.assertThat( recordedRequests.getRequests().get( 0 ), Matchers.startsWith( "GET" ) );
         MatcherAssert.assertThat( item, Matchers.instanceOf( StorageFileItem.class ) );
 
         final String content = IOUtils.toString( ( (StorageFileItem) item ).getContentLocator().getContent() );
@@ -198,6 +219,9 @@ public class RequestFlagsTest
 
         // BOTH requests will go to remote server!
         MatcherAssert.assertThat( recordedRequests.getRequests().size(), Matchers.equalTo( 2 ) );
+        // BOTH requests were GETs
+        MatcherAssert.assertThat( recordedRequests.getRequests().get( 0 ), Matchers.startsWith( "GET" ) );
+        MatcherAssert.assertThat( recordedRequests.getRequests().get( 1 ), Matchers.startsWith( "GET" ) );
         MatcherAssert.assertThat( item, Matchers.instanceOf( StorageFileItem.class ) );
 
         final String content = IOUtils.toString( ( (StorageFileItem) item ).getContentLocator().getContent() );
@@ -222,8 +246,7 @@ public class RequestFlagsTest
         final int port = server.getPort();
         server.stop();
         server =
-            Proxy.withPort( port ).serve( PATH ).withBehaviours( recordedRequests,
-                Behaviours.error( 404, "Not found" ) ).start();
+            Proxy.withPort( port ).serve( PATH ).withBehaviours( recordedRequests, Behaviours.error( 404, "Not found" ) ).start();
 
         final ResourceStoreRequest request = new ResourceStoreRequest( PATH );
         request.setRequestRemoteOnly( true );
@@ -239,6 +262,9 @@ public class RequestFlagsTest
 
         // BOTH requests will go to remote server!
         MatcherAssert.assertThat( recordedRequests.getRequests().size(), Matchers.equalTo( 2 ) );
+        // BOTH requests were GETs
+        MatcherAssert.assertThat( recordedRequests.getRequests().get( 0 ), Matchers.startsWith( "GET" ) );
+        MatcherAssert.assertThat( recordedRequests.getRequests().get( 1 ), Matchers.startsWith( "GET" ) );
 
         // cache got removed coz of 404!
         MatcherAssert.assertThat(
@@ -283,6 +309,7 @@ public class RequestFlagsTest
         final StorageItem item = proxyRepository.retrieveItem( request );
 
         MatcherAssert.assertThat( recordedRequests.getRequests().size(), Matchers.equalTo( 1 ) );
+        MatcherAssert.assertThat( recordedRequests.getRequests().get( 0 ), Matchers.startsWith( "GET" ) );
         MatcherAssert.assertThat( item, Matchers.instanceOf( StorageFileItem.class ) );
 
         final String content = IOUtils.toString( ( (StorageFileItem) item ).getContentLocator().getContent() );
@@ -303,7 +330,35 @@ public class RequestFlagsTest
         request.setRequestAsExpired( true );
         final StorageItem item = proxyRepository.retrieveItem( request );
 
-        // BOTH requests will go to remote server (but 2nd will do TWO HTTP requests)!
+        // BOTH requests will go to remote server
+        MatcherAssert.assertThat( recordedRequests.getRequests().size(), Matchers.equalTo( 2 ) );
+        // But, requests are GET and HEAD (1st is for "prime", 2nd is checking for remote)
+        MatcherAssert.assertThat( recordedRequests.getRequests().get( 0 ), Matchers.startsWith( "GET" ) );
+        MatcherAssert.assertThat( recordedRequests.getRequests().get( 1 ), Matchers.startsWith( "HEAD" ) );
+        MatcherAssert.assertThat( recordedRequests.getRequests().get( 2 ), Matchers.startsWith( "GET" ) );
+        MatcherAssert.assertThat( item, Matchers.instanceOf( StorageFileItem.class ) );
+
+        final String content = IOUtils.toString( ( (StorageFileItem) item ).getContentLocator().getContent() );
+        MatcherAssert.assertThat( content, Matchers.equalTo( CONTENT ) );
+    }
+
+    @Test
+    public void asExpiredFlagWithPrimedCacheGoesRemoteAndIsServedWithRemoteNewer()
+        throws Exception
+    {
+        // prime the cache
+        {
+            final ResourceStoreRequest request = new ResourceStoreRequest( PATH );
+            proxyRepository.retrieveItem( request );
+        }
+        
+        lastModifiedSender.setLastModified( new Date() );
+
+        final ResourceStoreRequest request = new ResourceStoreRequest( PATH );
+        request.setRequestAsExpired( true );
+        final StorageItem item = proxyRepository.retrieveItem( request );
+
+        // BOTH requests will go to remote server (but 2nd will do HEAD only request)!
         MatcherAssert.assertThat( recordedRequests.getRequests().size(), Matchers.equalTo( 3 ) );
         // But, requests are GET, HEAD and GET (1st is for "prime", 2nd is checking for remote, and 3rd one actually
         // GETs it
@@ -331,6 +386,34 @@ public class RequestFlagsTest
         request.setRequestAsExpired( true );
         final StorageItem item = proxyRepository.retrieveItem( request );
 
+        // BOTH requests will go to remote server
+        MatcherAssert.assertThat( recordedRequests.getRequests().size(), Matchers.equalTo( 2 ) );
+        // But, requests are GET and HEAD (1st is for "prime", 2nd is checking for remote)
+        MatcherAssert.assertThat( recordedRequests.getRequests().get( 0 ), Matchers.startsWith( "GET" ) );
+        MatcherAssert.assertThat( recordedRequests.getRequests().get( 1 ), Matchers.startsWith( "HEAD" ) );
+        MatcherAssert.assertThat( item, Matchers.instanceOf( StorageFileItem.class ) );
+
+        final String content = IOUtils.toString( ( (StorageFileItem) item ).getContentLocator().getContent() );
+        MatcherAssert.assertThat( content, Matchers.equalTo( CONTENT ) );
+    }
+
+    @Test
+    public void asExpiredFlagWithExpiredCacheGoesRemoteAndIsServedWithRemoteNewer()
+        throws Exception
+    {
+        // prime the cache and make it expired
+        {
+            final ResourceStoreRequest request = new ResourceStoreRequest( PATH );
+            proxyRepository.retrieveItem( request );
+            proxyRepository.expireCaches( new ResourceStoreRequest( "/" ) );
+        }
+        
+        lastModifiedSender.setLastModified( new Date() );
+
+        final ResourceStoreRequest request = new ResourceStoreRequest( PATH );
+        request.setRequestAsExpired( true );
+        final StorageItem item = proxyRepository.retrieveItem( request );
+
         // BOTH requests will go to remote server (but 2nd will do TWO HTTP requests)!
         MatcherAssert.assertThat( recordedRequests.getRequests().size(), Matchers.equalTo( 3 ) );
         // But, requests are GET, HEAD and GET (1st is for "prime", 2nd is checking for remote, and 3rd one actually
@@ -344,4 +427,29 @@ public class RequestFlagsTest
         MatcherAssert.assertThat( content, Matchers.equalTo( CONTENT ) );
     }
 
+    // ==
+
+    public class LastModifiedSender
+        implements Behaviour
+    {
+        private Date lastModified;
+
+        public LastModifiedSender( final Date date )
+        {
+            setLastModified( date );
+        }
+
+        public void setLastModified( final Date when )
+        {
+            lastModified = when;
+        }
+
+        @Override
+        public boolean execute( HttpServletRequest request, HttpServletResponse response, Map<Object, Object> ctx )
+            throws Exception
+        {
+            response.setDateHeader( "last-modified", lastModified.getTime() );
+            return true;
+        }
+    }
 }
