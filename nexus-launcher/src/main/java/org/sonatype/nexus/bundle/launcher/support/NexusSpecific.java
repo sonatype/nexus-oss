3b6a3b44ba2c354ce3a8fad5b28db144fd842019
Jason Dillon <jason@planet57.com>
2013-01-18 12:30:49 -0800

    Use javax.inject.Qualifier

--- nexus-launcher/src/main/java/org/sonatype/nexus/bundle/launcher/support/NexusSpecific.java
+++ nexus-launcher/src/main/java/org/sonatype/nexus/bundle/launcher/support/NexusSpecific.java
@@ -21,13 +21,13 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-import com.google.inject.BindingAnnotation;
+import javax.inject.Qualifier;
 
-@BindingAnnotation
+@Qualifier
 @Target( { TYPE, FIELD, PARAMETER, METHOD } )
 @Retention( RUNTIME )
 public @interface NexusSpecific
 {
-
+    // empty
 }
 
