110a6fa362dea59aeb7c65bea2f935566b249532
Tamas Cservenak <tamas@cservenak.net>
2013-02-07 16:56:21 +0100

    NEXUS-5472: Java7 class in unused import (wtf?)

--- nexus-core/src/test/java/org/sonatype/nexus/proxy/maven/wl/internal/scrape/AmazonS3IndexScraperTest.java
+++ nexus-core/src/test/java/org/sonatype/nexus/proxy/maven/wl/internal/scrape/AmazonS3IndexScraperTest.java
@@ -19,7 +19,6 @@ import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 
 import java.io.IOException;
-import java.nio.channels.SeekableByteChannel;
 import java.util.List;
 import java.util.Map;
 
