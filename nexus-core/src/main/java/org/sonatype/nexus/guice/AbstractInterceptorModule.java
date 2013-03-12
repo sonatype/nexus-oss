b695d93b3a4f2a7811fd1989eac9c5f349d3d1ab
Stuart McCulloch <mcculls@gmail.com>
2013-03-12 03:35:05 +0000

    Workaround to automatically share method interceptors via explicitly bound module sub-types while the Sisu feature to do this properly is implemented

---
+++ nexus-core/src/main/java/org/sonatype/nexus/guice/AbstractInterceptorModule.java
@@ -0,0 +1,50 @@
+/*
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2007-2012 Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
+
+package org.sonatype.nexus.guice;
+
+import java.lang.reflect.Method;
+
+import org.aopalliance.intercept.MethodInterceptor;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Key;
+import com.google.inject.matcher.Matcher;
+import com.google.inject.name.Names;
+
+/**
+ * Workaround to automatically share method interceptors while Sisu feature <i>'jdillon-to-create'</i> is implemented.
+ * <p>
+ * This module is only bound once in its originating realm, when the bindInterceptor method is first called. The Nexus
+ * Plugin Manager can then see this module via the injected dynamic list of AbstractInterceptorModules and will install
+ * it in any plugins registered after this point. Note: you can't contribute interceptors to earlier plugins or from a
+ * plugin to core, but the other direction works fine.
+ */
+public abstract class AbstractInterceptorModule
+    extends AbstractModule
+{
+    private boolean bound;
+
+    @Override
+    protected void bindInterceptor( Matcher<? super Class<?>> classMatcher, Matcher<? super Method> methodMatcher,
+                                    MethodInterceptor... interceptors )
+    {
+        if ( !bound )
+        {
+            // Explicitly bind module instance under a specific sub-type (not Module as Guice forbids that)
+            bind( Key.get( AbstractInterceptorModule.class, Names.named( getClass().getName() ) ) ).toInstance( this );
+            bound = true;
+        }
+        super.bindInterceptor( classMatcher, methodMatcher, interceptors );
+    }
+}
