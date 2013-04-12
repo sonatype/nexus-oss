0e29db7fc4900c942f64d75cf3699a71da1ae58b
Benjamin Hanzelmann <ben@nabcos.de>
2013-04-12 17:13:33 +0200

    Redirect /images to /static/images
    Exported location for resoures in plugins is /static. References to
    /images are scattered around all plugins. Redirect for compatibility.

---
+++ plugins/nexus-ui-extjs3-plugin/src/main/java/org/sonatype/nexus/plugins/ui/RedirectingNexusApplicationCustomizer.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2008-2012 Sonatype, Inc.
+ *
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/pro/attributions
+ * Sonatype and Sonatype Nexus are trademarks of Sonatype, Inc. Apache Maven is a trademark of the Apache Foundation.
+ * M2Eclipse is a trademark of the Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
+package org.sonatype.nexus.plugins.ui;
+
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+import org.codehaus.plexus.logging.AbstractLogEnabled;
+import org.restlet.data.Request;
+import org.restlet.data.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.sonatype.nexus.rest.NexusApplication;
+import org.sonatype.nexus.rest.NexusApplicationCustomizer;
+import org.sonatype.plexus.rest.RetargetableRestlet;
+
+/**
+ * Installs a filter before the NexusApplication root that will redirect /images to /static/images.
+ *
+ * @since 2.5
+ */
+@Named("ui-extjs3-redirector")
+@Singleton
+public class RedirectingNexusApplicationCustomizer
+    extends AbstractLogEnabled
+    implements NexusApplicationCustomizer
+{
+    @Override
+    public void customize( final NexusApplication app, final RetargetableRestlet root )
+    {
+        app.setRoot( new Redirector( app ) );
+    }
+
+    private class Redirector
+        extends RetargetableRestlet
+    {
+
+
+        public Redirector( final NexusApplication app )
+        {
+            super( app.getContext() );
+            setNext( app.getRoot() );
+        }
+
+        @Override
+        protected int beforeHandle( final Request request, final Response response )
+        {
+            final String path = request.getResourceRef().getPath();
+            final String ctxPath = request.getResourceRef().getBaseRef().getPath();
+
+            if (path.startsWith( ctxPath + "/images/" )) {
+                response.redirectPermanent( path.replace( ctxPath + "/images/", ctxPath + "/static/images/" ));
+                return STOP;
+            }
+
+            return super.beforeHandle( request, response );
+        }
+    }
+}
