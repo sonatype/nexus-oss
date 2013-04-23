cfd5953922c30e3f045d56ef9e9446312ddba887
Tamas Cservenak <tamas@cservenak.net>
2013-04-23 12:21:47 +0200

    Nexus Kenai Realm Plugin.
    While this realm is not shipped with any of distros
    it's OSS plugin for Nexus, hence it built with Nexus OSS.

---
+++ plugins/kenai/nexus-kenai-testsuite/src/test/java/org/sonatype/nexus/kenai/testsuite/KenaiITSupport.java
@@ -0,0 +1,75 @@
+/*
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2007-2012 Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
+package org.sonatype.nexus.kenai.testsuite;
+
+import static org.sonatype.nexus.testsuite.support.NexusStartAndStopStrategy.Strategy.EACH_TEST;
+import static org.sonatype.nexus.testsuite.support.ParametersLoaders.firstAvailableTestParameters;
+import static org.sonatype.nexus.testsuite.support.ParametersLoaders.systemTestParameters;
+import static org.sonatype.nexus.testsuite.support.ParametersLoaders.testParameters;
+import static org.sonatype.sisu.goodies.common.Varargs.$;
+
+import java.text.SimpleDateFormat;
+import java.util.Collection;
+import java.util.Date;
+
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+import org.junit.runners.Parameterized;
+import org.sonatype.nexus.bundle.launcher.NexusBundleConfiguration;
+import org.sonatype.nexus.client.core.subsystem.content.Content;
+import org.sonatype.nexus.testsuite.support.NexusRunningParametrizedITSupport;
+import org.sonatype.nexus.testsuite.support.NexusStartAndStopStrategy;
+
+/**
+ * Support for LDAP integration tests.
+ * 
+ * @since 2.4
+ */
+@NexusStartAndStopStrategy( EACH_TEST )
+public class KenaiITSupport
+    extends NexusRunningParametrizedITSupport
+{
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data()
+    {
+        return firstAvailableTestParameters( systemTestParameters(),
+            testParameters( $( "${it.nexus.bundle.groupId}:${it.nexus.bundle.artifactId}:zip:bundle" ) ) ).load();
+    }
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    public KenaiITSupport( final String nexusBundleCoordinates )
+    {
+        super( nexusBundleCoordinates );
+    }
+
+    @Override
+    protected NexusBundleConfiguration configureNexus( final NexusBundleConfiguration configuration )
+    {
+        return configuration.setLogLevel( "org.sonatype.nexus.plugins.kenai", "DEBUG" ).addPlugins(
+            artifactResolver().resolvePluginFromDependencyManagement( "org.sonatype.nexus.plugins",
+                "nexus-kenai-plugin" ) );
+    }
+
+    public static String uniqueName( final String prefix )
+    {
+        return prefix + "-" + new SimpleDateFormat( "yyyyMMdd-HHmmss-SSS" ).format( new Date() );
+    }
+
+    public Content content()
+    {
+        return client().getSubsystem( Content.class );
+    }
+}
