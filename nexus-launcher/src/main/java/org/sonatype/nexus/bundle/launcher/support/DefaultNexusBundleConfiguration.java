c4c745329032d94891a8d4383c303717f039d494
Jason Dillon <jason@planet57.com>
2013-04-25 14:32:01 -0700

    Add custom InstrumentedSelectChannelConnector impl which sets up metrics on doStart() instead of CTOR so that configuration of the connector can be same as other standard connector impls.

--- nexus-launcher/src/main/java/org/sonatype/nexus/bundle/launcher/support/DefaultNexusBundleConfiguration.java
+++ nexus-launcher/src/main/java/org/sonatype/nexus/bundle/launcher/support/DefaultNexusBundleConfiguration.java
@@ -269,7 +269,7 @@ public class DefaultNexusBundleConfiguration
                 fileTaskBuilder.replace()
                     .inFile( path( "nexus/conf/jetty.xml" ) )
                     .replace(
-                        "com.yammer.metrics.jetty.InstrumentedSelectChannelConnector",
+                        "org.sonatype.nexus.bootstrap.jetty.InstrumentedSelectChannelConnector",
                         "org.eclipse.jetty.server.nio.BlockingChannelConnector"
                     )
                     .failIfFileDoesNotExist()
