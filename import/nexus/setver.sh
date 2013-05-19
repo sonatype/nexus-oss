356321dd04b450108f5e8b0e7334e14aae84d2b3
Jason Dillon <jason@planet57.com>
2013-05-19 12:38:46 -0700

    move appcontext, base-conf*, security, sisu-* to components/

--- import/nexus/setver.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# This will update all pom.xml files to use version specified as first parameter of this script
-
-mvn -Dtycho.mode=maven org.eclipse.tycho:tycho-versions-plugin:0.18.0-SNAPSHOT:set-version \
-  -Dartifacts=nexus-plugin-parent,nexus,nexus-runtime-platform -Dproperties=nexus.version \
-  -DnewVersion=$1
