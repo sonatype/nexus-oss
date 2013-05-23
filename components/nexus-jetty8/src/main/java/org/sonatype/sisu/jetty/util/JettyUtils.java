04e7e5b8f7bbae7a1faa062c205b4517afe75146
Jason Dillon <jason@planet57.com>
2013-05-22 22:55:07 -0700

    merge update-license-headers, deal with damn 2 files with bunk line endings Conflicts: 	plugins/restlet1x/nexus-restlet-bridge/src/test/java/org/sonatype/plexus/rest/xstream/xml/InvalidXMLTest.java 	plugins/restlet1x/nexus-restlet-bridge/src/test/java/org/sonatype/plexus/rest/xstream/xml/SimpleTestObject.java

--- components/nexus-jetty8/src/main/java/org/sonatype/sisu/jetty/util/JettyUtils.java
+++ components/nexus-jetty8/src/main/java/org/sonatype/sisu/jetty/util/JettyUtils.java
@@ -1,14 +1,14 @@
 /*
- * Copyright (c) 2012 Sonatype, Inc. All rights reserved.
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2007-2013 Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
  *
- * This program is licensed to you under the Apache License Version 2.0,
- * and you may not use this file except in compliance with the Apache License Version 2.0.
- * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the Apache License Version 2.0 is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
  */
 package org.sonatype.sisu.jetty.util;
 
