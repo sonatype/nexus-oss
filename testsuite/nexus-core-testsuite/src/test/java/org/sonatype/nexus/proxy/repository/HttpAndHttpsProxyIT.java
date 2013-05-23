3f7b0d2d5d55bc8f9a68db622aae1032186d4ea1
Alin Dreghiciu <adreghiciu@gmail.com>
2013-05-23 16:15:08 +0300

    Fix remaining ITs packages
    Signed-off-by: Alin Dreghiciu <adreghiciu@gmail.com>

--- testsuite/nexus-core-testsuite/src/test/java/org/sonatype/nexus/proxy/repository/HttpAndHttpsProxyIT.java
+++ testsuite/nexus-core-testsuite/src/test/java/org/sonatype/nexus/proxy/repository/HttpAndHttpsProxyIT.java
@@ -10,7 +10,7 @@
  * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
  * Eclipse Foundation. All other trademarks are the property of their respective owners.
  */
-package core.webproxy;
+package org.sonatype.nexus.proxy.repository;
 
 import static org.sonatype.nexus.client.core.subsystem.content.Location.repositoryLocation;
 import static org.sonatype.nexus.testsuite.support.ParametersLoaders.firstAvailableTestParameters;
