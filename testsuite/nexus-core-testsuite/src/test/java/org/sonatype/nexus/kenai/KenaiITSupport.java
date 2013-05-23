b622c00a8127012f40b88b4d3c10a9433d922adc
Alin Dreghiciu <adreghiciu@gmail.com>
2013-05-23 11:06:30 +0300

    Merge kenai plugin ITs into nexus-core-testsuite module
    Signed-off-by: Alin Dreghiciu <adreghiciu@gmail.com>

--- testsuite/nexus-core-testsuite/src/test/java/org/sonatype/nexus/kenai/KenaiITSupport.java
+++ testsuite/nexus-core-testsuite/src/test/java/org/sonatype/nexus/kenai/KenaiITSupport.java
@@ -10,7 +10,7 @@
  * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
  * Eclipse Foundation. All other trademarks are the property of their respective owners.
  */
-package org.sonatype.nexus.kenai.testsuite;
+package org.sonatype.nexus.kenai;
 
 import static org.sonatype.nexus.testsuite.support.NexusStartAndStopStrategy.Strategy.EACH_TEST;
 import static org.sonatype.nexus.testsuite.support.ParametersLoaders.firstAvailableTestParameters;
