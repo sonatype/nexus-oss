aff2534c05493eaf3151bde7599fd81569f512bb
Tamas Cservenak <tamas@cservenak.net>
2013-07-16 14:52:26 +0200

    NXCM-5406: Make nx-staging-plugin mvn3.1 friendly
    It turned out that the plugin itself is okay
    with mvn3.1, but it's the nexus-core-client that
    used old Aether for some version comparisions.
    
    Luckily, these ops are well hidden behind the Condition
    iface, so for now copy paste of relevant
    Aether classes happened into the client
    (and removal of Aether dependency).
    
    This makes nexus-staging-maven-plugin happy in
    mvn 3.1

---
+++ components/nexus-client-core/src/main/java/org/sonatype/nexus/client/core/condition/internal/InvalidVersionSpecificationException.java
@@ -0,0 +1,54 @@
+/*
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2007-2013 Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
+package org.sonatype.nexus.client.core.condition.internal;
+
+/*******************************************************************************
+ * Copyright (c) 2010-2011 Sonatype, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ *   http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
+/******
+ * Copied from org.sonatype.aether:aether-api:1.13.1
+ ******/
+/**
+ * Thrown when a version or version range could not be parsed.
+ * 
+ * @author Benjamin Bentmann
+ */
+public class InvalidVersionSpecificationException
+    extends RuntimeException //RepositoryException
+{
+
+    private final String version;
+
+    public InvalidVersionSpecificationException( String version, String message )
+    {
+        super( message );
+        this.version = version;
+    }
+
+    public InvalidVersionSpecificationException( String version, Throwable cause )
+    {
+        super( "Could not parse version specification " + version, cause );
+        this.version = version;
+    }
+
+    public String getVersion()
+    {
+        return version;
+    }
+
+}
