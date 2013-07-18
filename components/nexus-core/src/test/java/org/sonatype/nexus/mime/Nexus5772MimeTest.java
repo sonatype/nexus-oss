9bfc3f6d6558cdca798d87f7680e51de1d4e7a42
Tamas Cservenak <tamas@cservenak.net>
2013-07-18 12:27:29 +0200

    NEXUS-5772: Fix license hdr on new UT

--- components/nexus-core/src/test/java/org/sonatype/nexus/mime/Nexus5772MimeTest.java
+++ components/nexus-core/src/test/java/org/sonatype/nexus/mime/Nexus5772MimeTest.java
@@ -1,3 +1,15 @@
+/*
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2007-2013 Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
 package org.sonatype.nexus.mime;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -16,7 +28,6 @@ import org.sonatype.nexus.proxy.item.ContentLocator;
 import org.sonatype.nexus.proxy.item.FileContentLocator;
 import org.sonatype.sisu.litmus.testsupport.TestSupport;
 
-import eu.medsea.mimeutil.MimeException;
 import eu.medsea.mimeutil.MimeUtil;
 import eu.medsea.mimeutil.MimeUtil2;
 
