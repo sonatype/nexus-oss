089d07c55f14e264aaa6aaf30a3ef6155fd23653
Alin Dreghiciu <adreghiciu@gmail.com>
2013-07-22 18:13:24 +0300

    [NEXUS-5797] Add UnusedSnapshotRemoverTask/Descriptor

---
+++ components/nexus-core/src/main/java/org/sonatype/nexus/maven/tasks/UnusedSnapshotRemoverTask.java
@@ -0,0 +1,88 @@
+/*
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2007-2013 Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
+package org.sonatype.nexus.maven.tasks;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import static org.sonatype.nexus.maven.tasks.descriptors.UnusedSnapshotRemovalTaskDescriptor.DAYS_SINCE_LAST_REQUESTED_FIELD_ID;
+import static org.sonatype.nexus.maven.tasks.descriptors.UnusedSnapshotRemovalTaskDescriptor.REPO_OR_GROUP_FIELD_ID;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.sonatype.nexus.maven.tasks.descriptors.UnusedSnapshotRemovalTaskDescriptor;
+import org.sonatype.nexus.scheduling.AbstractNexusRepositoriesTask;
+
+/**
+ * Unused Snapshot Remover Task.
+ *
+ * @since 2.6.1
+ */
+@Named( UnusedSnapshotRemovalTaskDescriptor.ID )
+public class UnusedSnapshotRemoverTask
+    extends AbstractNexusRepositoriesTask<SnapshotRemovalResult>
+{
+
+    public static final String SYSTEM_REMOVE_SNAPSHOTS_ACTION = "REMOVESNAPSHOTS";
+
+    private final SnapshotRemover snapshotRemover;
+
+    @Inject
+    public UnusedSnapshotRemoverTask( final SnapshotRemover snapshotRemover )
+    {
+        this.snapshotRemover = checkNotNull( snapshotRemover );
+    }
+
+    @Override
+    protected String getRepositoryFieldId()
+    {
+        return REPO_OR_GROUP_FIELD_ID;
+    }
+
+    public int getDaysSinceLastRequested()
+    {
+        final String param = getParameters().get( DAYS_SINCE_LAST_REQUESTED_FIELD_ID );
+        return Integer.parseInt( checkNotNull( param, DAYS_SINCE_LAST_REQUESTED_FIELD_ID ) );
+    }
+
+    public void setDaysSinceLastRequested( int daysSinceLastRequested )
+    {
+        getParameters().put( DAYS_SINCE_LAST_REQUESTED_FIELD_ID, Integer.toString( daysSinceLastRequested ) );
+    }
+
+    @Override
+    public SnapshotRemovalResult doRun()
+        throws Exception
+    {
+        return null;
+    }
+
+    @Override
+    protected String getAction()
+    {
+        return SYSTEM_REMOVE_SNAPSHOTS_ACTION;
+    }
+
+    @Override
+    protected String getMessage()
+    {
+        if ( getRepositoryId() != null )
+        {
+            return "Removing unused snapshots from repository " + getRepositoryName();
+        }
+        else
+        {
+            return "Removing unused snapshots from all registered repositories";
+        }
+    }
+
+}
