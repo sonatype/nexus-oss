d696b18cbf702a8a63931e7584be2d69cb3ce63e
Tamas Cservenak <tamas@cservenak.com>
2013-09-02 06:30:46 -0700

    Merge pull request #107 from sonatype/nexus-5849-artifactory-autorouting
    NEXUS-5849: Central prefix file is served through Artifactory, which results in non-central artifacts being blocked
    Conflicts:
    	components/nexus-core/src/main/java/org/sonatype/nexus/proxy/maven/routing/internal/PropfileDiscoveryStatusSource.java
    	components/nexus-core/src/main/java/org/sonatype/nexus/proxy/maven/routing/internal/RemotePrefixFileStrategy.java
    	components/nexus-core/src/main/java/org/sonatype/nexus/proxy/maven/routing/internal/RemoteScrapeStrategy.java
    	components/nexus-core/src/test/java/org/sonatype/nexus/proxy/maven/routing/internal/RemoteContentDiscovererImplTest.java
    	components/nexus-core/src/test/java/org/sonatype/nexus/proxy/maven/routing/internal/RemoteScrapeStrategyTest.java

--- components/nexus-core/src/test/java/org/sonatype/nexus/proxy/maven/routing/internal/RemoteScrapeStrategyTest.java
+++ components/nexus-core/src/test/java/org/sonatype/nexus/proxy/maven/routing/internal/RemoteScrapeStrategyTest.java
@@ -10,47 +10,48 @@
  * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
  * Eclipse Foundation. All other trademarks are the property of their respective owners.
  */
+
 package org.sonatype.nexus.proxy.maven.routing.internal;
 
-import static org.mockito.Mockito.when;
+import org.sonatype.nexus.NexusAppTestSupport;
+import org.sonatype.nexus.proxy.maven.MavenProxyRepository;
+import org.sonatype.nexus.proxy.maven.routing.discovery.RemoteStrategy;
+import org.sonatype.nexus.proxy.maven.routing.discovery.StrategyFailedException;
+import org.sonatype.nexus.proxy.storage.remote.DefaultRemoteStorageContext;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.sonatype.nexus.proxy.maven.MavenProxyRepository;
-import org.sonatype.nexus.proxy.maven.routing.discovery.RemoteStrategy;
-import org.sonatype.nexus.proxy.maven.routing.discovery.StrategyFailedException;
-import org.sonatype.nexus.proxy.maven.routing.internal.RemoteScrapeStrategy;
-import org.sonatype.nexus.test.NexusTestSupport;
+import static org.mockito.Mockito.when;
 
 public class RemoteScrapeStrategyTest
-    extends NexusTestSupport
+    extends NexusAppTestSupport
 {
-    @Mock
-    private MavenProxyRepository mavenProxyRepository;
+  @Mock
+  private MavenProxyRepository mavenProxyRepository;
 
-    @Before
-    public void initMocks()
-    {
-        MockitoAnnotations.initMocks( this );
-    }
+  @Before
+  public void initMocks() {
+    MockitoAnnotations.initMocks(this);
+    when(mavenProxyRepository.getRemoteStorageContext()).thenReturn(new DefaultRemoteStorageContext(null));
+  }
 
-    @Test( expected = StrategyFailedException.class )
-    public void checkNoFtpIsScraped()
-        throws Exception
-    {
-        when( mavenProxyRepository.getRemoteUrl() ).thenReturn( "ftp://host.com/repo/" );
-        final RemoteScrapeStrategy rss = (RemoteScrapeStrategy) lookup( RemoteStrategy.class, RemoteScrapeStrategy.ID );
-        rss.discover( mavenProxyRepository );
-    }
+  @Test(expected = StrategyFailedException.class)
+  public void checkNoFtpIsScraped()
+      throws Exception
+  {
+    when(mavenProxyRepository.getRemoteUrl()).thenReturn("ftp://host.com/repo/");
+    final RemoteScrapeStrategy rss = (RemoteScrapeStrategy) lookup(RemoteStrategy.class, RemoteScrapeStrategy.ID);
+    rss.discover(mavenProxyRepository);
+  }
 
-    @Test( expected = StrategyFailedException.class )
-    public void checkOnlyHttpIsScraped()
-        throws Exception
-    {
-        when( mavenProxyRepository.getRemoteUrl() ).thenReturn( "groups:thegroup/" );
-        final RemoteScrapeStrategy rss = (RemoteScrapeStrategy) lookup( RemoteStrategy.class, RemoteScrapeStrategy.ID );
-        rss.discover( mavenProxyRepository );
-    }
+  @Test(expected = StrategyFailedException.class)
+  public void checkOnlyHttpIsScraped()
+      throws Exception
+  {
+    when(mavenProxyRepository.getRemoteUrl()).thenReturn("groups:thegroup/");
+    final RemoteScrapeStrategy rss = (RemoteScrapeStrategy) lookup(RemoteStrategy.class, RemoteScrapeStrategy.ID);
+    rss.discover(mavenProxyRepository);
+  }
 }
