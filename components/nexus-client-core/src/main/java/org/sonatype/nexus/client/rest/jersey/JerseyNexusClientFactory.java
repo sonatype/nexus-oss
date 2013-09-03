a12ef191e1b5be9e53cfa640c74f25756ece0d7d
Alin Dreghiciu <adreghiciu@gmail.com>
2013-09-03 12:20:22 +0300

    Introduce an adapter between SystemFactory and SystemProvider
    Signed-off-by: Alin Dreghiciu <adreghiciu@gmail.com>

--- components/nexus-client-core/src/main/java/org/sonatype/nexus/client/rest/jersey/JerseyNexusClientFactory.java
+++ components/nexus-client-core/src/main/java/org/sonatype/nexus/client/rest/jersey/JerseyNexusClientFactory.java
@@ -14,20 +14,18 @@
 package org.sonatype.nexus.client.rest.jersey;
 
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
-import javax.annotation.Nullable;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.inject.Singleton;
 
 import org.sonatype.nexus.client.core.Condition;
-import org.sonatype.nexus.client.core.NexusClient;
 import org.sonatype.nexus.client.core.spi.SubsystemFactory;
 import org.sonatype.nexus.client.core.spi.SubsystemProvider;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 
 /**
  * @since 2.1
@@ -39,77 +37,38 @@ public class JerseyNexusClientFactory
 {
 
   public JerseyNexusClientFactory(final SubsystemFactory<?, JerseyNexusClient>... subsystemFactories) {
-    super(adapt(subsystemFactories));
+    super(
+        Lists.<SubsystemProvider>newArrayList(
+            new SubsystemFactoriesSubsystemProvider(Sets.newHashSet(subsystemFactories))
+        )
+    );
   }
 
   public JerseyNexusClientFactory(final Condition connectionCondition,
                                   final SubsystemFactory<?, JerseyNexusClient>... subsystemFactories)
   {
-    super(connectionCondition, adapt(subsystemFactories));
+    super(
+        connectionCondition,
+        Lists.<SubsystemProvider>newArrayList(
+            new SubsystemFactoriesSubsystemProvider(Sets.newHashSet(subsystemFactories))
+        )
+    );
   }
-
 
   public JerseyNexusClientFactory(final Set<SubsystemFactory<?, JerseyNexusClient>> subsystemFactories) {
-    super(adapt(subsystemFactories.toArray(new SubsystemFactory[subsystemFactories.size()])));
+    super(
+        Lists.<SubsystemProvider>newArrayList(
+            new SubsystemFactoriesSubsystemProvider(subsystemFactories)
+        )
+    );
   }
 
+  /**
+   * @since 2.7
+   */
   @Inject
-  public JerseyNexusClientFactory(final Set<SubsystemFactory<?, JerseyNexusClient>> subsystemFactories,
-                                  final List<SubsystemProvider> subsystemProviders)
-  {
-    super(combine(subsystemFactories, subsystemProviders));
-  }
-
-  /**
-   * Adapts a set of {@link SubsystemFactory}s to a list of {@link SubsystemProvider}s.
-   *
-   * @param subsystemFactories to adapt (can be null)
-   * @return adapted (never null, can be empty if provided subsystem factories is empty)
-   */
-  private static List<SubsystemProvider> adapt(
-      @Nullable final SubsystemFactory<?, JerseyNexusClient>[] subsystemFactories)
-  {
-    final List<SubsystemProvider> providers = Lists.newArrayList();
-    if (subsystemFactories != null) {
-      for (final SubsystemFactory<?, JerseyNexusClient> subsystemFactory : subsystemFactories) {
-        providers.add(
-            new SubsystemProvider()
-            {
-              @Override
-              public Object get(final Class type, final Map<Object, Object> context) {
-                if (type.equals(subsystemFactory.getType())) {
-                  return subsystemFactory.create((JerseyNexusClient) context.get(NexusClient.class));
-                }
-                return null;
-              }
-            }
-        );
-      }
-    }
-    return providers;
-  }
-
-  /**
-   * Combines a set of {@link SubsystemFactory}s and a list of {@link SubsystemProvider}s in a list of
-   * {@link SubsystemProvider}s, adapting the {@link SubsystemFactory}s as {@link SubsystemProvider}s.
-   *
-   * @param subsystemFactories to combine (can be null)
-   * @param subsystemProviders to combine (can be null)
-   * @return combined list (never null)
-   */
-  private static List<SubsystemProvider> combine(
-      @Nullable final Set<SubsystemFactory<?, JerseyNexusClient>> subsystemFactories,
-      @Nullable final List<SubsystemProvider> subsystemProviders)
-  {
-
-    final List<SubsystemProvider> providers = Lists.newArrayList();
-    if (subsystemFactories != null) {
-      providers.addAll(adapt(subsystemFactories.toArray(new SubsystemFactory[subsystemFactories.size()])));
-    }
-    if (subsystemProviders != null) {
-      providers.addAll(subsystemProviders);
-    }
-    return providers;
+  public JerseyNexusClientFactory(final List<SubsystemProvider> subsystemProviders) {
+    super(subsystemProviders);
   }
 
 }
