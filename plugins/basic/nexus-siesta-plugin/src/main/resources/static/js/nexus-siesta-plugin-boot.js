900427bf15669b47416c1f78bf0f3a2bca251fbe
Jason Dillon <jason@planet57.com>
2013-09-24 13:56:57 -0700

    tidy, fix case in boot

--- plugins/basic/nexus-siesta-plugin/src/main/resources/static/js/nexus-siesta-plugin-boot.js
+++ plugins/basic/nexus-siesta-plugin/src/main/resources/static/js/nexus-siesta-plugin-boot.js
@@ -10,7 +10,9 @@
  * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
  * Eclipse Foundation. All other trademarks are the property of their respective owners.
  */
-/*global Ext, Sonatype, Nexus*/
+
+/*global define*/
+
 define('nexus-siesta-plugin-boot', [
-  'nexus/siesta'
+  'Nexus/siesta'
 ]);
