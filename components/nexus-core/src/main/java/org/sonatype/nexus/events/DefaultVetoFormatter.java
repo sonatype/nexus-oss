5e5a5f8b92f3bcfe6b4229499714f745d1bec98f
Tamas Cservenak <tamas@cservenak.net>
2013-10-07 21:57:06 +0200

    Remove deprecated classes.

--- components/nexus-core/src/main/java/org/sonatype/nexus/events/DefaultVetoFormatter.java
+++ components/nexus-core/src/main/java/org/sonatype/nexus/events/DefaultVetoFormatter.java
@@ -19,7 +19,6 @@ import javax.inject.Singleton;
 import org.sonatype.nexus.proxy.events.Veto;
 import org.sonatype.nexus.proxy.events.VetoFormatter;
 import org.sonatype.nexus.proxy.events.VetoFormatterRequest;
-import org.sonatype.nexus.util.StringDigester;
 
 import org.codehaus.plexus.util.ExceptionUtils;
 
@@ -28,6 +27,8 @@ import org.codehaus.plexus.util.ExceptionUtils;
 public class DefaultVetoFormatter
     implements VetoFormatter
 {
+  private static final String LINE_SEPERATOR = System.getProperty("line.separator");
+
   public String format(VetoFormatterRequest request) {
     StringBuilder sb = new StringBuilder();
 
@@ -37,14 +38,14 @@ public class DefaultVetoFormatter
       sb.append("Event " + request.getEvent().toString() + " has been vetoed by one or more components.");
 
       if (request.isDetailed()) {
-        sb.append(StringDigester.LINE_SEPERATOR);
+        sb.append(LINE_SEPERATOR);
 
         for (Veto veto : request.getEvent().getVetos()) {
           sb.append("vetoer: " + veto.getVetoer().toString());
           sb.append("cause:");
-          sb.append(StringDigester.LINE_SEPERATOR);
+          sb.append(LINE_SEPERATOR);
           sb.append(ExceptionUtils.getFullStackTrace(veto.getReason()));
-          sb.append(StringDigester.LINE_SEPERATOR);
+          sb.append(LINE_SEPERATOR);
         }
       }
     }
