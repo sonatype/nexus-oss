1e084ce1a8ce62e935ff7b58391d337ee32043a7
Alin Dreghiciu <adreghiciu@gmail.com>
2013-10-11 12:44:11 +0300

    [NX-19] Add basic nexus-logging-plugin structure
    Signed-off-by: Alin Dreghiciu <adreghiciu@gmail.com>

---
+++ plugins/basic/nexus-logging-plugin/src/main/resources/static/js/Nexus/logging/LoggingPanel.js
@@ -0,0 +1,57 @@
+/*
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2007-2013 Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
+/*global NX, Ext, Nexus, Sonatype*/
+
+/**
+ * Logging panel.
+ *
+ * @since 2.7
+ */
+NX.define('Nexus.logging.LoggingPanel', {
+  extend: 'Ext.Panel',
+
+  mixins: [
+    'Nexus.LogAwareMixin'
+  ],
+
+  requires: [
+  ],
+
+  /**
+   * @override
+   */
+  initComponent: function () {
+    var self = this;
+
+    self.constructor.superclass.initComponent.apply(self, arguments);
+  }
+
+}, function () {
+  var type = this,
+      sp = Sonatype.lib.Permissions;
+
+  NX.log.debug('Adding global view: ' + type.$className);
+
+  // install panel into main NX navigation
+  Sonatype.Events.on('nexusNavigationInit', function (panel) {
+    panel.add({
+      enabled: sp.checkPermission('nexus:logging', sp.READ),
+      sectionId: 'st-nexus-config',
+      title: 'Logging',
+      tabId: 'logging',
+      tabCode: type
+    });
+
+    NX.log.debug('Registered global view: ' + type.$className);
+  });
+});
