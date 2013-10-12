1b6544569ae7800bc8bb7ad7e69477c75596095e
Jason Dillon <jason@planet57.com>
2013-10-11 19:32:01 -0700

    Add basic template for rendering simple key-value stuff, extjs 3.4 xtemplate blows can't deal with objects

--- plugins/basic/nexus-atlas-plugin/src/main/resources/static/js/Nexus/atlas/SystemInformationPanel.js
+++ plugins/basic/nexus-atlas-plugin/src/main/resources/static/js/Nexus/atlas/SystemInformationPanel.js
@@ -17,10 +17,14 @@
  * @since 2.7
  */
 NX.define('Nexus.atlas.SystemInformationPanel', {
-  extend: 'Ext.Container',
+  extend: 'Ext.Panel',
 
   mixins: [
     'Nexus.LogAwareMixin'
+  ],
+
+  requires: [
+    'Nexus.siesta'
   ],
 
   /**
@@ -32,10 +36,96 @@ NX.define('Nexus.atlas.SystemInformationPanel', {
     Ext.apply(self, {
       cls: 'nx-atlas-SystemInformationPanel',
       title: 'System Information',
-      html: 'TODO'
+      layout: 'fit',
+      autoScroll: true,
+      tbar: [
+        {
+          xtype: 'button',
+          text: 'Refresh',
+          scope: self,
+          handler: self.refresh
+        }
+      ]
     });
 
-    self.constructor.superclass.initComponent.apply(self, arguments);
-  }
+    // TODO: Automatically refresh when visible
 
+    self.sectionTpl = NX.create('Ext.XTemplate',
+        '<div>',
+        '<h2>{name}</h2>',
+        '<table>',
+        '<tpl for="props">',
+        '<tr>',
+        '<td>{name}</td>',
+        '<td>{value}</td>',
+        '</tr>',
+        '</tpl>',
+        '</table>',
+        '</div>',
+        {
+          compiled: true
+        }
+    );
+
+    self.mainTpl = NX.create('Ext.XTemplate',
+        '<div>',
+        '{[ this.section("system-time", values) ]}',
+        '{[ this.section("system-properties", values) ]}',
+        '{[ this.section("system-environment", values) ]}',
+        '{[ this.section("system-runtime", values) ]}',
+        '{[ this.section("system-threads", values) ]}',
+        // FIXME: filestores is a complex structure, need to have a separate handler for it
+        //'{[ this.section("system-filestores", values) ]}',
+        '{[ this.section("nexus-configuration", values) ]}',
+        '{[ this.section("nexus-properties", values) ]}',
+        // FIXME: plugins is a complex structure ...
+        //'{[ this.section("nexus-plugins", values) ]}',
+        '</div>',
+        {
+          compiled: true,
+
+          section: function (name, values) {
+            // pull off the section of data we want to render
+            var data = values[name];
+
+            // convert object into array of name/value objects for xtemplate to render
+            var props = [];
+            Ext.iterate(data, function(key, value) {
+              props.push({
+                name: key,
+                value: value
+              })
+            });
+
+            return self.sectionTpl.apply({
+              name: name,
+              props: props
+            });
+          }
+        }
+    );
+
+    self.constructor.superclass.initComponent.apply(self, arguments);
+  },
+
+  refresh: function () {
+    var self = this,
+        mask = NX.create('Ext.LoadMask', self.getEl(), { msg: 'Loading...' });
+
+    self.logDebug('Refreshing');
+
+    mask.show();
+    Ext.Ajax.request({
+      url: Nexus.siesta.basePath + '/atlas/system-information',
+
+      scope: self,
+      callback: function () {
+        mask.hide()
+      },
+      success: function (response, opts) {
+        var obj = Ext.decode(response.responseText);
+        self.mainTpl.overwrite(self.body, obj);
+      }
+    });
+  }
 });
