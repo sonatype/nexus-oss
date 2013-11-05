499e765da844ae10e0d2d8b091e6a1254f633d07
Tamas Cservenak <tamas@cservenak.net>
2013-11-05 13:24:40 +0100

    Using component support

--- components/nexus-core/src/main/java/org/sonatype/nexus/proxy/cache/EhCacheCacheManager.java
+++ components/nexus-core/src/main/java/org/sonatype/nexus/proxy/cache/EhCacheCacheManager.java
@@ -20,6 +20,7 @@ import javax.inject.Singleton;
 import org.sonatype.nexus.logging.AbstractLoggingComponent;
 import org.sonatype.nexus.proxy.events.NexusStoppedEvent;
 import org.sonatype.sisu.ehcache.CacheManagerComponent;
+import org.sonatype.sisu.goodies.common.ComponentSupport;
 import org.sonatype.sisu.goodies.eventbus.EventBus;
 
 import static com.google.common.base.Preconditions.checkNotNull;
@@ -34,7 +35,7 @@ import com.google.common.eventbus.Subscribe;
 @Named
 @Singleton
 public class EhCacheCacheManager
-    extends AbstractLoggingComponent
+    extends ComponentSupport
     implements CacheManager
 {
   private final CacheManagerComponent cacheManagerComponent;
