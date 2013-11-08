3d05efcde9c1534455a900d4326728dd0b6b293f
Jason Dillon <jason@planet57.com>
2013-11-08 10:07:43 -0800

    [NEXUS-6107] Use plexus-utils forceMkdir() to remain compatible with java6

--- components/nexus-client-core/src/main/java/org/sonatype/nexus/client/internal/rest/jersey/subsystem/JerseyUtilities.java
+++ components/nexus-client-core/src/main/java/org/sonatype/nexus/client/internal/rest/jersey/subsystem/JerseyUtilities.java
@@ -17,7 +17,6 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.nio.file.Files;
 import java.util.Date;
 
 import javax.ws.rs.core.Response;
@@ -29,6 +28,7 @@ import org.sonatype.nexus.client.rest.jersey.JerseyNexusClient;
 
 import com.sun.jersey.api.client.ClientHandlerException;
 import com.sun.jersey.api.client.ClientResponse;
+import org.codehaus.plexus.util.FileUtils;
 import org.codehaus.plexus.util.IOUtil;
 
 import static com.google.common.base.Preconditions.checkState;
@@ -68,7 +68,8 @@ public class JerseyUtilities
   {
     if (!target.exists()) {
       final File targetDir = target.getParentFile();
-      Files.createDirectories(targetDir.toPath());
+      // NOTE: can not use java.nio.Files here as this module needs to remain Java6 compatible
+      FileUtils.forceMkdir(targetDir);
     }
     else {
       checkState(target.isFile() && target.canWrite(), "File '%s' is not a file or could not be written",
