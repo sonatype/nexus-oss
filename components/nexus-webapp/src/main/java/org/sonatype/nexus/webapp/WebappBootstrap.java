1646db4db1fa5bb5a5fa6f51a307c82210a5e1fe
Jason Dillon <jason@planet57.com>
2013-11-13 11:07:02 -0800

    Need to have "nexus-app" property set, or no plugins will load. Added warning when system plugin dir is invalid. Add more logging in webapp bootstrap. Re-hookup JUL configuration.

--- components/nexus-webapp/src/main/java/org/sonatype/nexus/webapp/WebappBootstrap.java
+++ components/nexus-webapp/src/main/java/org/sonatype/nexus/webapp/WebappBootstrap.java
@@ -18,6 +18,7 @@ import java.net.URL;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
@@ -32,7 +33,9 @@ import org.sonatype.nexus.web.NexusWebModule;
 
 import com.google.common.base.Throwables;
 import com.google.common.collect.Lists;
+import com.google.inject.Injector;
 import com.google.inject.Module;
+import com.google.inject.servlet.GuiceServletContextListener;
 import org.codehaus.plexus.ContainerConfiguration;
 import org.codehaus.plexus.DefaultContainerConfiguration;
 import org.codehaus.plexus.DefaultPlexusContainer;
@@ -40,18 +43,19 @@ import org.codehaus.plexus.PlexusConstants;
 import org.codehaus.plexus.PlexusContainer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.bridge.SLF4JBridgeHandler;
 
 import static com.google.common.base.Preconditions.checkState;
 
 /**
- * Nexus bootstrap {@link ServletContextListener}.
+ * Web application bootstrap {@link ServletContextListener}.
  *
  * @since 2.8
  */
-public class NexusBootstrap
-    implements ServletContextListener
+public class WebappBootstrap
+    extends GuiceServletContextListener
 {
-  private static final Logger log = LoggerFactory.getLogger(NexusBootstrap.class);
+  private static final Logger log = LoggerFactory.getLogger(WebappBootstrap.class);
 
   private static final String CUSTOM_MODULES = "customModules";
 
@@ -59,19 +63,27 @@ public class NexusBootstrap
 
   private PlexusContainer container;
 
+  private Injector injector;
+
   private NxApplication application;
 
   private LogManager logManager;
 
+  @Override
   public void contextInitialized(final ServletContextEvent event) {
     log.info("Initializing");
 
     ServletContext context = event.getServletContext();
 
+    // FIXME: Why is this here?  for legacy test shit?
     if (context.getAttribute(PlexusConstants.PLEXUS_KEY) != null) {
       log.info("Plexus container already exists; skipping");
       return;
     }
+
+    // FIXME: JUL handler should be handled by container or bootstrap
+    SLF4JBridgeHandler.removeHandlersForRootLogger();
+    SLF4JBridgeHandler.install();
 
     try {
       // Use bootstrap configuration if it exists, else load it
@@ -81,19 +93,23 @@ public class NexusBootstrap
         config = new Configuration();
         config.load();
         Configuration.HOLDER.set(config);
+
+        log.info("Properties:");
+        for (Entry<String,String> entry : config.getProperties().entrySet()) {
+          log.info("  {}='{}'", entry.getKey(), entry.getValue());
+        }
       }
       Map<String, String> properties = config.getProperties();
 
       // lock the work directory
       File workDir = new File(properties.get("nexus-work")).getCanonicalFile();
       lockFile = new LockFile(new File(workDir, "nexus.lock"));
-      if (!lockFile.lock()) {
-        throw new IllegalStateException("Nexus work directory already is use: " + workDir);
-      }
+      checkState(lockFile.lock(), "Nexus work directory already in use: %s", workDir);
 
       // setup plexus configuration
       URL plexusXml = getClass().getResource("/plexus.xml");
       checkState(plexusXml != null, "Missing plexus.xml");
+      log.debug("Plexus XML: {}", plexusXml);
 
       @SuppressWarnings("unchecked")
       ContainerConfiguration plexusConfiguration = new DefaultContainerConfiguration()
@@ -105,7 +121,7 @@ public class NexusBootstrap
           .setComponentVisibility(PlexusConstants.GLOBAL_VISIBILITY);
 
       List<Module> modules = Lists.newArrayList(
-          new NexusWebModule(event.getServletContext()),
+          new NexusWebModule(context),
           new CoreModule()
       );
 
@@ -118,13 +134,21 @@ public class NexusBootstrap
       // create the container
       container = new DefaultPlexusContainer(plexusConfiguration, modules.toArray(new Module[modules.size()]));
       context.setAttribute(PlexusConstants.PLEXUS_KEY, container);
+      log.debug("Container: {}", container);
+
+      // configure guice servlet
+      injector = container.lookup(Injector.class);
+      log.debug("Injector: {}", injector);
+      super.contextInitialized(event);
 
       // configure logging
       logManager = container.lookup(LogManager.class);
+      log.debug("Log manager: {}", logManager);
       logManager.configure();
 
       // start the application
       application = container.lookup(NxApplication.class);
+      log.debug("Application: {}", application);
       application.start();
     }
     catch (Exception e) {
@@ -135,6 +159,7 @@ public class NexusBootstrap
     log.info("Initialized");
   }
 
+  @Override
   public void contextDestroyed(final ServletContextEvent event) {
     log.info("Destroying");
 
@@ -155,6 +180,8 @@ public class NexusBootstrap
       logManager = null;
     }
 
+    super.contextDestroyed(event);
+
     // cleanup the container
     if (container != null) {
       container.dispose();
@@ -168,5 +195,11 @@ public class NexusBootstrap
     }
 
     log.info("Destroyed");
+  }
+
+  @Override
+  protected Injector getInjector() {
+    checkState(injector != null, "Missing injector reference");
+    return injector;
   }
 }
