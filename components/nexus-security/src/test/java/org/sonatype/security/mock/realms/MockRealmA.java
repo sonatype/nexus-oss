e3c356055d861d8649f04d4633ca04eedd22dca1
Alin Dreghiciu <adreghiciu@gmail.com>
2013-11-29 09:23:05 +0200

    Remove unused code from nexus-security

--- components/nexus-security/src/test/java/org/sonatype/security/mock/realms/MockRealmA.java
+++ components/nexus-security/src/test/java/org/sonatype/security/mock/realms/MockRealmA.java
@@ -13,29 +13,20 @@
 
 package org.sonatype.security.mock.realms;
 
-import java.util.Collection;
-import java.util.List;
-
 import javax.enterprise.inject.Typed;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.inject.Singleton;
 
-import org.sonatype.security.usermanagement.RoleIdentifier;
-import org.sonatype.security.usermanagement.User;
 import org.sonatype.security.usermanagement.UserManager;
-import org.sonatype.security.usermanagement.UserNotFoundException;
 
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
 import org.apache.shiro.authc.SimpleAuthenticationInfo;
 import org.apache.shiro.authc.UsernamePasswordToken;
-import org.apache.shiro.authz.AuthorizationException;
-import org.apache.shiro.authz.Permission;
 import org.apache.shiro.realm.AuthenticatingRealm;
 import org.apache.shiro.realm.Realm;
-import org.apache.shiro.subject.PrincipalCollection;
 
 @Singleton
 @Typed(Realm.class)
@@ -43,11 +34,9 @@ import org.apache.shiro.subject.PrincipalCollection;
 public class MockRealmA
     extends AuthenticatingRealm
 {
-  private final UserManager userManager;
 
   @Inject
   public MockRealmA(@Named("MockUserManagerA") UserManager userManager) {
-    this.userManager = userManager;
     this.setAuthenticationTokenClass(UsernamePasswordToken.class);
   }
 
@@ -72,111 +61,4 @@ public class MockRealmA
     return "MockRealmA";
   }
 
-  public void checkPermission(PrincipalCollection subjectPrincipal, String permission)
-      throws AuthorizationException
-  {
-    // TODO Auto-generated method stub
-
-  }
-
-  public void checkPermission(PrincipalCollection subjectPrincipal, Permission permission)
-      throws AuthorizationException
-  {
-    // TODO Auto-generated method stub
-
-  }
-
-  public void checkPermissions(PrincipalCollection subjectPrincipal, String... permissions)
-      throws AuthorizationException
-  {
-    // TODO Auto-generated method stub
-
-  }
-
-  public void checkPermissions(PrincipalCollection subjectPrincipal, Collection<Permission> permissions)
-      throws AuthorizationException
-  {
-    // TODO Auto-generated method stub
-
-  }
-
-  public void checkRole(PrincipalCollection subjectPrincipal, String roleIdentifier)
-      throws AuthorizationException
-  {
-    // TODO Auto-generated method stub
-
-  }
-
-  public void checkRoles(PrincipalCollection subjectPrincipal, Collection<String> roleIdentifiers)
-      throws AuthorizationException
-  {
-    // TODO Auto-generated method stub
-
-  }
-
-  public boolean hasAllRoles(PrincipalCollection subjectPrincipal, Collection<String> roleIdentifiers) {
-    // TODO Auto-generated method stub
-    return false;
-  }
-
-  public boolean hasRole(PrincipalCollection subjectPrincipal, String roleIdentifier) {
-    // mock this one out using the user manager
-
-    try {
-      User user = this.userManager.getUser(subjectPrincipal.oneByType(String.class));
-      for (RoleIdentifier eachRoleIdentifier : user.getRoles()) {
-        if (eachRoleIdentifier.getRoleId().equals(roleIdentifier)) {
-          return true;
-        }
-      }
-    }
-    catch (UserNotFoundException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
-
-    return false;
-  }
-
-  public boolean[] hasRoles(PrincipalCollection subjectPrincipal, List<String> roleIdentifiers) {
-    // TODO Auto-generated method stub
-    return null;
-  }
-
-  public boolean isPermitted(PrincipalCollection principals, String permission) {
-    // TODO Auto-generated method stub
-    return false;
-  }
-
-  public boolean isPermitted(PrincipalCollection subjectPrincipal, Permission permission) {
-    // TODO Auto-generated method stub
-    return false;
-  }
-
-  public boolean[] isPermitted(PrincipalCollection subjectPrincipal, String... permissions) {
-    // TODO Auto-generated method stub
-    return null;
-  }
-
-  public boolean[] isPermitted(PrincipalCollection subjectPrincipal, List<Permission> permissions) {
-    // TODO Auto-generated method stub
-    return null;
-  }
-
-  public boolean isPermittedAll(PrincipalCollection subjectPrincipal, String... permissions) {
-    // TODO Auto-generated method stub
-    return false;
-  }
-
-  public boolean isPermittedAll(PrincipalCollection subjectPrincipal, Collection<Permission> permissions) {
-    // TODO Auto-generated method stub
-    return false;
-  }
-
-  public void checkRoles(PrincipalCollection subjectPrincipal, String... roleIdentifiers)
-      throws AuthorizationException
-  {
-    // TODO Auto-generated method stub
-
-  }
 }
