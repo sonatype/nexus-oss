aa3ea0e28e70207950a311eee982d39800c65e53
Jason Dillon <jason@planet57.com>
2013-12-13 10:25:39 -0800

    ServletModule handles binding ServletContext already we don't need to bind it again

--- components/nexus-core/src/main/java/org/sonatype/nexus/guice/NexusModules.java
+++ components/nexus-core/src/main/java/org/sonatype/nexus/guice/NexusModules.java
@@ -65,8 +65,6 @@ public class NexusModules
 
     @Override
     protected void configure() {
-      bind(ServletContext.class).toInstance(servletContext);
-
       install(new CommonModule());
 
       install(new ServletModule()
