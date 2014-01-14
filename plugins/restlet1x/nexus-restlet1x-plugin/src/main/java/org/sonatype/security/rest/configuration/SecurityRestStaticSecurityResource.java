6d7725e40d743832bc7dec76cf87468bc3b4af15
Stuart McCulloch <mcculls@gmail.com>
2014-01-14 13:00:51 +0000

    NX-156: fix REST-related split packages

--- plugins/restlet1x/nexus-restlet1x-plugin/src/main/java/org/sonatype/security/rest/configuration/SecurityRestStaticSecurityResource.java
+++ plugins/restlet1x/nexus-restlet1x-plugin/src/main/java/org/sonatype/security/rest/configuration/SecurityRestStaticSecurityResource.java
@@ -11,7 +11,7 @@
  * Eclipse Foundation. All other trademarks are the property of their respective owners.
  */
 
-package org.sonatype.security.configuration;
+package org.sonatype.security.rest.configuration;
 
 import javax.enterprise.inject.Typed;
 import javax.inject.Named;
