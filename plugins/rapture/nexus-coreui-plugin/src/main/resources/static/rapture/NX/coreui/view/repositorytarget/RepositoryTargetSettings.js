2d5e419eca9b20846e3e0be6facb89dbb925a69d
Alin Dreghiciu <adreghiciu@gmail.com>
2014-03-18 07:41:18 +0200

    Make format not editable

--- plugins/rapture/nexus-coreui-plugin/src/main/resources/static/rapture/NX/coreui/view/repositorytarget/RepositoryTargetSettings.js
+++ plugins/rapture/nexus-coreui-plugin/src/main/resources/static/rapture/NX/coreui/view/repositorytarget/RepositoryTargetSettings.js
@@ -48,6 +48,7 @@ Ext.define('NX.coreui.view.repositorytarget.RepositoryTargetSettings', {
         name: 'format',
         fieldLabel: 'Repository Type',
         emptyText: 'select a repository type',
+        editable: false,
         store: 'RepositoryFormat',
         queryMode: 'local',
         displayField: 'name',
