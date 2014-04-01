fca5175dd8171e46c0d72c58719f6723a4a99435
Jason Dillon <jason@planet57.com>
2014-04-01 15:40:26 -0700

    drop redundant modifiers

--- testsupport/nexus-test-common/src/main/java/org/sonatype/nexus/test/Names.java
+++ testsupport/nexus-test-common/src/main/java/org/sonatype/nexus/test/Names.java
@@ -15,8 +15,8 @@ package org.sonatype.nexus.test;
 public interface Names
 {
 
-  static final String BASE_DIR = "basedir";
+  String BASE_DIR = "basedir";
 
-  static final String BASE_DIR_CONF = "${" + BASE_DIR + ":-.}";
+  String BASE_DIR_CONF = "${" + BASE_DIR + ":-.}";
 
 }
