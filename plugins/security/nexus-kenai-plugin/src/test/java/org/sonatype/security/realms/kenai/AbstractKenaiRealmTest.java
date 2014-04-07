f214b816d0e17790d4ef84adb91abb317c169694
Tamas Cservenak <tamas@cservenak.net>
2014-04-07 20:53:46 +0200

    Cleaned Kenai realm
    Uses HTH now to emulate Kenai API

--- plugins/security/nexus-kenai-plugin/src/test/java/org/sonatype/security/realms/kenai/AbstractKenaiRealmTest.java
+++ plugins/security/nexus-kenai-plugin/src/test/java/org/sonatype/security/realms/kenai/AbstractKenaiRealmTest.java
@@ -12,17 +12,14 @@
  */
 package org.sonatype.security.realms.kenai;
 
-import java.net.ServerSocket;
-import java.util.Arrays;
-import java.util.Properties;
-
 import com.sonatype.security.realms.kenai.config.model.Configuration;
 
-import org.sonatype.jettytestsuite.ServletInfo;
-import org.sonatype.jettytestsuite.ServletServer;
-import org.sonatype.jettytestsuite.WebappContext;
 import org.sonatype.nexus.NexusAppTestSupport;
 import org.sonatype.security.realms.kenai.config.KenaiRealmConfiguration;
+import org.sonatype.tests.http.runner.junit.ServerResource;
+import org.sonatype.tests.http.server.fluent.Server;
+
+import org.junit.Rule;
 
 public abstract class AbstractKenaiRealmTest
     extends NexusAppTestSupport
@@ -36,36 +33,8 @@ public abstract class AbstractKenaiRealmTest
 
   protected static final String AUTH_APP_NAME = "auth_app";
 
-  protected ServletServer server;
-
-  protected ServletServer getServletServer()
-      throws Exception
-  {
-    ServletServer server = new ServletServer();
-
-    ServerSocket socket = new ServerSocket(0);
-    int freePort = socket.getLocalPort();
-    socket.close();
-
-    server.setPort(freePort);
-
-    WebappContext webapp = new WebappContext();
-    server.setWebappContexts(Arrays.asList(webapp));
-
-    webapp.setName("auth_app");
-
-    ServletInfo servletInfoAuthc = new ServletInfo();
-    servletInfoAuthc.setName("authc");
-    servletInfoAuthc.setMapping("/api/login/*");
-    servletInfoAuthc.setServletClass(KenaiMockAuthcServlet.class.getName());
-    servletInfoAuthc.setParameters(new Properties());
-
-    webapp.setServletInfos(Arrays.asList(servletInfoAuthc));
-
-    server.initialize();
-
-    return server;
-  }
+  @Rule
+  public ServerResource server = new ServerResource(Server.server().serve("/api/login/*").withServlet(new KenaiMockAuthcServlet()).getServerProvider());
 
   protected KenaiRealmConfiguration getKenaiRealmConfiguration()
       throws Exception
@@ -75,31 +44,8 @@ public abstract class AbstractKenaiRealmTest
     Configuration configuration = kenaiRealmConfiguration.getConfiguration();
     configuration.setDefaultRole(DEFAULT_ROLE);
     configuration.setEmailDomain("sonatype.org");
-    configuration.setBaseUrl(server.getUrl(AUTH_APP_NAME) + "/"); // add the '/' to the end
+    configuration.setBaseUrl(server.getServerProvider().getUrl() + AUTH_APP_NAME + "/"); // add the '/' to the end
     // kenaiRealmConfiguration.updateConfiguration( configuration );
     return kenaiRealmConfiguration;
-  }
-
-  @Override
-  protected void setUp()
-      throws Exception
-  {
-    super.setUp();
-
-    // start the kenai mock server
-    server = getServletServer();
-    server.start();
-  }
-
-  @Override
-  protected void tearDown()
-      throws Exception
-  {
-    try {
-      server.stop();
-    }
-    finally {
-      super.tearDown();
-    }
   }
 }
