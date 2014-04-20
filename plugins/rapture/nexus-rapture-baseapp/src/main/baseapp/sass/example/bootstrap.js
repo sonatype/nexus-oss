aed4dd1a08ec1283c38a44ddedc541791ebc43ac
Jason Dillon <jason@planet57.com>
2014-04-20 15:04:22 -0700

    [NX-180] Refine quicktip style to confluence use confluence {info} colors

--- plugins/rapture/nexus-rapture-baseapp/src/main/baseapp/sass/example/bootstrap.js
+++ plugins/rapture/nexus-rapture-baseapp/src/main/baseapp/sass/example/bootstrap.js
@@ -413,7 +413,6 @@ Ext.ClassManager.addNameAlternateMappings({
   ],
   "Ext.grid.RowEditor": [],
   "Ext.grid.RowEditorButtons": [],
-  "Ext.grid.Scroller": [],
   "Ext.grid.View": [],
   "Ext.grid.ViewDropZone": [],
   "Ext.grid.column.Action": [
@@ -1295,7 +1294,6 @@ Ext.ClassManager.addNameAliasMappings({
   "Ext.grid.RowEditorButtons": [
     "widget.roweditorbuttons"
   ],
-  "Ext.grid.Scroller": [],
   "Ext.grid.View": [
     "widget.gridview"
   ],
