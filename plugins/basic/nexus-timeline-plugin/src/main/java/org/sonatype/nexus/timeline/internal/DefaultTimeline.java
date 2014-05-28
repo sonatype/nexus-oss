e69d53c98c6c3dc22011790c91ba658c2581b8ec
Tamas Cservenak <tamas@cservenak.net>
2014-05-28 18:04:07 +0200

    Implement proper partitioning by day and purge

--- plugins/basic/nexus-timeline-plugin/src/main/java/org/sonatype/nexus/timeline/internal/DefaultTimeline.java
+++ plugins/basic/nexus-timeline-plugin/src/main/java/org/sonatype/nexus/timeline/internal/DefaultTimeline.java
@@ -15,9 +15,12 @@ package org.sonatype.nexus.timeline.internal;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.EmptyStackException;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.Stack;
+import java.util.concurrent.TimeUnit;
 
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -61,11 +64,17 @@ public class DefaultTimeline
     extends LifecycleSupport
     implements Timeline
 {
+  private static final long A_DAY_IN_MILLIS = TimeUnit.DAYS.toMillis(1);
+
   private final Lifecycle lifecycle;
 
   private final JournalStore journalStore;
 
   private final SchemaStore schemaStore;
+
+  private Object runningDayMonitor = new Object();
+
+  private Long runningDay;
 
   @Inject
   public DefaultTimeline(final EventBus eventBus,
@@ -113,7 +122,7 @@ public class DefaultTimeline
       log.info("Creating schema for {} type", EntryRecord.SCHEMA_NAME);
 
       Schema schema = new Schema.Builder().
-          addAttribute("timestamp", Type.U64, false).
+          addAttribute("timestamp", Type.I64, false).
           addAttribute("type", Type.UTF8_SMALLSTRING, false).
           addAttribute("subType", Type.UTF8_SMALLSTRING, false).
           addAttribute("data", Type.MAP, false).
@@ -163,6 +172,16 @@ public class DefaultTimeline
   private void addEntryRecord(final List<EntryRecord> records) {
     try {
       for (EntryRecord record : records) {
+        // Long and not primitive, to avoid NPE due to auto-unboxing, since runningDay is null initially
+        final Long currentEntryRunningDay = record.getTimestamp() / A_DAY_IN_MILLIS;
+        synchronized (runningDayMonitor) {
+          if (runningDay != currentEntryRunningDay) {
+            if (runningDay != null) {
+              journalStore.closeActivePartition();
+            }
+            runningDay = currentEntryRunningDay;
+          }
+        }
         journalStore.append(EntryRecord.SCHEMA_NAME, EntryRecord.class, record, TypeValidation.STRICT);
       }
     }
@@ -191,6 +210,7 @@ public class DefaultTimeline
               null)) {
         for (KeyValuePair<EntryRecord> kv : kvs) {
           final EntryRecord record = kv.getValue();
+          // TODO: secondary indexes for types and subTypes?
           if (types != null && !types.contains(record.getType())) {
             continue; // skip it
           }
@@ -204,7 +224,9 @@ public class DefaultTimeline
           if (count < currentCount) {
             break;
           }
-          callback.processNext(record);
+          if (!callback.processNext(record)) {
+            break;
+          }
         }
       }
     }
@@ -218,22 +240,31 @@ public class DefaultTimeline
     if (!isStarted()) {
       return;
     }
-    // NOTE: This is merely a hack, we need to come up with some general partitioning strategy
-    // Could we partition per-day? Or just modify the semantics of the input maybe, as purge
-    // was painfully needed for Lucene backed Timeline, but with Kazuki it might get non-issue?
     try {
       if (days == 0) {
-        // today included, lose the current partition, so that any new events are separate from those that exist already
+        // today included, close current partition too, so that any new events are separate from those that exist already
         journalStore.closeActivePartition();
       }
+      final Stack<String> partitionIds = new Stack<>();
       try (KeyValueIterable<PartitionInfoSnapshot> partitions = journalStore.getAllPartitions()) {
+        // gather all closed partitions
         for (PartitionInfo partition : partitions) {
-          if (!partition.isClosed()) {
-            continue;
+          if (partition.isClosed()) {
+            partitionIds.push(partition.getPartitionId());
           }
-          journalStore.dropPartition(partition.getPartitionId());
         }
       }
+      // keep "days" partition
+      for (int i = 0; i > days; i++) {
+        partitionIds.pop();
+      }
+      // drop others
+      while (!partitionIds.isEmpty()) {
+        journalStore.dropPartition(partitionIds.pop());
+      }
+    }
+    catch (EmptyStackException e) {
+      // we wanted to keep more than we have, so it's fine
     }
     catch (KazukiException e) {
       log.warn("Failed to purge Timeline store", e);
