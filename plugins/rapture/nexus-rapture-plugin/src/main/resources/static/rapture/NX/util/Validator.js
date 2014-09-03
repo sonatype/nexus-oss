8dadbd2a8e7c409b9c6ca390e040583f83f577bb
Alin Dreghiciu <adreghiciu@gmail.com>
2014-09-03 15:46:08 +0300

    Allow only one letter in top level domain in url validator (e.g. http://policy.s)

--- plugins/rapture/nexus-rapture-plugin/src/main/resources/static/rapture/NX/util/Validator.js
+++ plugins/rapture/nexus-rapture-plugin/src/main/resources/static/rapture/NX/util/Validator.js
@@ -51,7 +51,7 @@ Ext.define('NX.util.Validator', {
         (options.require_protocol ? '' : '?') +
         '(?:\\S+(?::\\S*)?@)?(?:(?:(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[0-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:www.)?)?(?:(?:[a-z\\u00a1-\\uffff0-9]+' +
         separators + ')*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]+' + separators +
-        ')*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,}))' + (options.require_tld ? '' : '?') +
+        ')*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{1,}))' + (options.require_tld ? '' : '?') +
         ')|localhost)(?::(\\d{1,5}))?(?:(?:/|\\?|#)[^\\s]*)?$', 'i');
 
     var match = str.match(url),
