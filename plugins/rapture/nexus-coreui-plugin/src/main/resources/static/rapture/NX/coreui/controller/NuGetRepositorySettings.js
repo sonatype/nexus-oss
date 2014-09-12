f94bc218f2c45e10f4a4472f1228b40a04665ac0
Alin Dreghiciu <adreghiciu@gmail.com>
2014-09-12 21:19:20 +0300

    [NEXUS-6849] Move NuGet ui code to coreui plugin
    Signed-off-by: Alin Dreghiciu <adreghiciu@gmail.com>

---
+++ plugins/rapture/nexus-coreui-plugin/src/main/resources/static/rapture/NX/coreui/controller/NuGetRepositorySettings.js
@@ -0,0 +1,172 @@
+/*
+ * Copyright (c) 2008-2014 Sonatype, Inc.
+ *
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/pro/attributions
+ * Sonatype and Sonatype Nexus are trademarks of Sonatype, Inc. Apache Maven is a trademark of the Apache Foundation.
+ * M2Eclipse is a trademark of the Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
+/*global Ext, NX*/
+
+/**
+ * NuGet repository settings controller.
+ *
+ * @since 3.0
+ */
+Ext.define('NX.coreui.controller.NuGetRepositorySettings', {
+  extend: 'Ext.app.Controller',
+  requires: [
+    'NX.Conditions',
+    'NX.Permissions',
+    'NX.Security'
+  ],
+
+  views: [
+    'nuget.NuGetApiKeyDetails',
+    'nuget.NuGetRepositorySettings'
+  ],
+  refs: [
+    { ref: 'feature', selector: 'nx-coreui-repository-feature' },
+    { ref: 'panel', selector: 'nx-coreui-nuget-repository-settings' }
+  ],
+
+  /**
+   * @override
+   */
+  init: function() {
+    var me = this;
+
+    me.getApplication().getIconController().addIcons({
+      'nuget-default': {
+        file: 'key.png',
+        variants: ['x16', 'x32']
+      }
+    });
+
+    me.listen({
+      component: {
+        'nx-coreui-repository-list': {
+          selection: me.onSelection
+        },
+        'nx-coreui-nuget-repository-settings button[action=access]': {
+          click: me.accessApiKey,
+          afterrender: me.bindAccessApiKeyButton
+        },
+        'nx-coreui-nuget-repository-settings button[action=reset]': {
+          click: me.resetApiKey,
+          afterrender: me.bindResetApiKeyButton
+        }
+      }
+    });
+  },
+
+  /**
+   * @private
+   * Add "NuGet" panel to repository tabs, if not already present and/or load NuGet settings into the panel.
+   * @param {NX.coreui.view.repository.RepositoryList} grid repository grid
+   * @param {NX.coreui.model.Repository} model selected repository
+   */
+  onSelection: function(grid, model) {
+    var me = this,
+        panel = me.getPanel();
+
+    if (model && NX.Permissions.check('apikey:access', 'read') && model.get('format') === 'nuget') {
+      if (!panel) {
+        me.getFeature().addTab({ xtype: 'nx-coreui-nuget-repository-settings', title: 'NuGet' });
+        panel = me.getPanel();
+      }
+      panel.setRepository(model);
+    }
+    else {
+      if (panel) {
+        me.getFeature().removeTab(panel);
+      }
+    }
+  },
+
+  /**
+   * @private
+   * Authenticate & show API Key.
+   */
+  accessApiKey: function() {
+    var me = this;
+
+    NX.Security.doWithAuthenticationToken(
+        'Accessing NuGet API Key requires validation of your credentials.',
+        {
+          success: function(authToken) {
+            NX.direct.nuget_NuGet.readKey(authToken, function(response) {
+              if (Ext.isDefined(response) && response.success) {
+                me.showApiKey(response.data);
+              }
+            });
+          }
+        }
+    );
+  },
+
+  /**
+   * @private
+   * Authenticate & reset API Key.
+   */
+  resetApiKey: function() {
+    var me = this;
+
+    NX.Security.doWithAuthenticationToken(
+        'Resetting NuGet API Key requires validation of your credentials.',
+        {
+          success: function(authToken) {
+            NX.direct.nuget_NuGet.resetKey(authToken, function(response) {
+              if (Ext.isDefined(response) && response.success) {
+                me.showApiKey(response.data);
+              }
+            });
+          }
+        }
+    );
+  },
+
+  /**
+   * @private
+   * Show API Key details window.
+   * @param {String} apiKey to show
+   */
+  showApiKey: function(apiKey) {
+    var me = this,
+        repositoryId = me.getPanel().getRepository().getId();
+
+    Ext.widget('nx-coreui-nuget-apikeydetails', { repositoryId: repositoryId, apiKey: apiKey });
+  },
+
+  /**
+   * @private
+   * Enable 'Access API Key' when user has 'apikey:access:read' permission.
+   * @param {Ext.Button} button access API Key button
+   */
+  bindAccessApiKeyButton: function(button) {
+    button.mon(
+        NX.Conditions.isPermitted('apikey:access', 'read'),
+        {
+          satisfied: button.enable,
+          unsatisfied: button.disable,
+          scope: button
+        }
+    );
+  },
+
+  /**
+   * @private
+   * Enable 'Reset API Key' when user has 'apikey:access:delete' permission.
+   * @param {Ext.Button} button reset API Key button
+   */
+  bindResetApiKeyButton: function(button) {
+    button.mon(
+        NX.Conditions.isPermitted('apikey:access', 'delete'),
+        {
+          satisfied: button.enable,
+          unsatisfied: button.disable,
+          scope: button
+        }
+    );
+  }
+
+});
---
+++ plugins/rapture/nexus-coreui-plugin/src/main/resources/static/rapture/NX/coreui/view/nuget/NuGetApiKeyDetails.js
@@ -0,0 +1,113 @@
+/*
+ * Copyright (c) 2008-2014 Sonatype, Inc.
+ *
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/pro/attributions
+ * Sonatype and Sonatype Nexus are trademarks of Sonatype, Inc. Apache Maven is a trademark of the Apache Foundation.
+ * M2Eclipse is a trademark of the Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
+/*global Ext, NX*/
+
+/**
+ * Dialog to display a NuGet API Key details.
+ *
+ * @since 3.0
+ */
+Ext.define('NX.coreui.view.nuget.NuGetApiKeyDetails', {
+  extend: 'Ext.window.Window',
+  alias: 'widget.nx-coreui-nuget-apikeydetails',
+  requires: [
+    'NX.Icons',
+    'NX.Messages'
+  ],
+
+  title: 'NuGet API Key',
+  autoShow: true,
+  modal: true,
+  constrain: true,
+  width: 640,
+  resizable: false,
+
+  /**
+   * @cfg {String} repository id
+   */
+  repositoryId: undefined,
+
+  /**
+   * @cfg {String} NuGet API Key
+   */
+  apiKey: undefined,
+
+  /**
+   * @override
+   */
+  initComponent: function() {
+    var me = this;
+
+    me.items = {
+      xtype: 'form',
+      bodyPadding: 10,
+      items: [
+        {
+          xtype: 'panel',
+          layout: 'hbox',
+          items: [
+            { xtype: 'component', html: NX.Icons.img('nuget-default', 'x32') },
+            { xtype: 'label', margin: '0 0 0 5',
+              html: 'NuGet API Key enables pushing packages using NuGet.exe.<br/>' +
+                  '<span style="font-weight: bold;">Keep this key secret!</span>'
+            }
+          ]
+        },
+        {
+          xtype: 'label',
+          html: '<p>Your NuGet API Key is:</p>'
+        },
+        {
+          xtype: 'textfield',
+          value: me.apiKey,
+          readOnly: true,
+          selectOnFocus: true,
+          fieldStyle: {
+            padding: '2px',
+            'font-family': 'monospace'
+          }
+        },
+        {
+          xtype: 'label',
+          html: '<p>You can register this key with the following command:</p>'
+        },
+        {
+          xtype: 'textfield',
+          value: 'nuget setapikey ' + me.apiKey + ' -source ' + me.repositoryId,
+          readOnly: true,
+          selectOnFocus: true,
+          fieldStyle: {
+            padding: '2px',
+            'font-family': 'monospace'
+          }
+        },
+        {
+          xtype: 'label',
+          html: '<p>This window will automatically close after one minute.</p>'
+        }
+      ],
+      buttonAlign: 'left',
+      buttons: [
+        { text: 'Cancel', handler: function() {
+          this.up('window').close();
+        }}
+      ]
+    };
+
+    me.callParent(arguments);
+
+    // Automatically close the window
+    Ext.defer(function() {
+      if (me.isVisible()) { // ignore if already closed
+        NX.Messages.add({ text: 'Automatically closing NuGet API Key details due to timeout' });
+        me.close();
+      }
+    }, 1 * 60 * 1000); // 1 minute
+  }
+
+});
