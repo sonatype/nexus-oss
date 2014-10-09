f8fd15d311e28e941f0c654317a5aeb3fefd776b
Jason Dillon <jason@planet57.com>
2014-10-09 12:41:06 -0700

    Revert "[NEXUS-6905] merge ruby gems plugin"
    This reverts commit c2e57a0b1cbfd3a581524b08ca522d63dfdd92c7.

--- plugins/rubygem/nexus-gem/lib/rubygems_plugin.rb
+++
@@ -1,6 +0,0 @@
-require 'rubygems/command_manager'
-require 'commands/abstract_command'
-
-require "commands/nexus"
-Gem::CommandManager.instance.register_command :nexus
-
--- plugins/rubygem/nexus-ruby-tools/src/test/resources/rubygems/gems/zip-2.0.2/lib/zip/version.rb
+++
@@ -1,3 +0,0 @@
-module Zip
-  VERSION = '2.0.2'
-end
