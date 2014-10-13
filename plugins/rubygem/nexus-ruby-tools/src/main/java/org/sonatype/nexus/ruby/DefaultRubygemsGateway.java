e18fecf922da39186c9179813abfe0bd3b6ac95b
Christian Meier <m.kristian@web.de>
2014-10-13 12:33:27 +0100

    * reduce RubygemsGateway to a factory of ruby classes * refactored repository repair methods into own helper class * follow the same code pattern of ruby clases for DependencyDataImpl

--- plugins/rubygem/nexus-ruby-tools/src/main/java/org/sonatype/nexus/ruby/DefaultRubygemsGateway.java
+++ plugins/rubygem/nexus-ruby-tools/src/main/java/org/sonatype/nexus/ruby/DefaultRubygemsGateway.java
@@ -13,73 +13,74 @@
 package org.sonatype.nexus.ruby;
 
 import java.io.InputStream;
-import java.util.List;
 
-import org.jruby.embed.PathType;
 import org.jruby.embed.ScriptingContainer;
-import org.jruby.runtime.builtin.IRubyObject;
 
 public class DefaultRubygemsGateway
-    extends ScriptWrapper
     implements RubygemsGateway
 {
+
+  private final Object dependencyHelperImplClass;
+  private final Object gemspecHelperImplClass;
+  private final Object specsHelperImplClass;
+  private final Object mergeSpecsHelperImplClass;
+  private final Object repairHelperImplClass;
+  private final Object dependencyDataImplClass;
+  private final ScriptingContainer container;
+
   /**
    * Ctor that accepts prepared scripting container, as usually the container is or should be
    * managed (ie. by calling {@link ScriptingContainer#terminate()} when application is shut down.
    */
   public DefaultRubygemsGateway(ScriptingContainer container) {
-    super(container, newScript(container));
-  }
-
-  private static Object newScript(final ScriptingContainer scriptingContainer) {
-    IRubyObject nexusRubygemsClass = scriptingContainer.parse(PathType.CLASSPATH, "nexus/rubygems.rb").run();
-    return scriptingContainer.callMethod(nexusRubygemsClass, "new", Object.class);
-  }
-
-  @Override
-  public DependencyHelper newDependencyHelper() {
-    return callMethod("new_dependency_helper", DependencyHelper.class);
+    this.container = container;
+    dependencyHelperImplClass = container.runScriptlet("require 'nexus/dependency_helper_impl';"
+        + "Nexus::DependencyHelperImpl");
+    gemspecHelperImplClass = container.runScriptlet("require 'nexus/gemspec_helper_impl';"
+        + "Nexus::GemspecHelperImpl");
+    specsHelperImplClass = container.runScriptlet("require 'nexus/specs_helper_impl';"
+        + "Nexus::SpecsHelperImpl");
+    mergeSpecsHelperImplClass = container.runScriptlet("require 'nexus/merge_specs_helper_impl';"
+        + "Nexus::MergeSpecsHelperImpl");
+    repairHelperImplClass = container.runScriptlet("require 'nexus/repair_helper_impl';"
+        + "Nexus::RepairHelperImpl");
+    dependencyDataImplClass = container.runScriptlet("require 'nexus/dependency_data_impl';"
+        + "Nexus::DependencyDataImpl");
   }
 
   @Override
   public SpecsHelper newSpecsHelper() {
-    return callMethod("new_specs_helper", SpecsHelper.class);
+    return container.callMethod(specsHelperImplClass, "new", SpecsHelper.class);
   }
 
   @Override
   public MergeSpecsHelper newMergeSpecsHelper() {
-    return callMethod("new_merge_specs_helper", MergeSpecsHelper.class);
+    return container.callMethod(mergeSpecsHelperImplClass, "new", MergeSpecsHelper.class);
   }
 
   @Override
-  public DependencyData dependencies(InputStream is, String name, long modified) {
-    return new DependencyDataImpl(scriptingContainer,
-        callMethod("dependencies", new Object[]{name, is}, Object.class),
-        modified);
-  }
-
-  @Override
-  public void recreateRubygemsIndex(String directory) {
-    callMethod("recreate_rubygems_index", directory, Void.class);
-  }
-
-  @Override
-  public void purgeBrokenDepencencyFiles(String directory) {
-    callMethod("purge_broken_depencency_files", directory, Void.class);
-  }
-
-  @Override
-  public void purgeBrokenGemspecFiles(String directory) {
-    callMethod("purge_broken_gemspec_files", directory, Void.class);
+  public DependencyHelper newDependencyHelper() {
+    return container.callMethod(dependencyHelperImplClass, "new", DependencyHelper.class);
   }
 
   @Override
   public GemspecHelper newGemspecHelper(InputStream gemspec) {
-    return callMethod("new_gemspec_helper", gemspec, GemspecHelper.class);
+    return container.callMethod(gemspecHelperImplClass, "from_gemspec_rz", gemspec, GemspecHelper.class);
   }
 
   @Override
   public GemspecHelper newGemspecHelperFromGem(InputStream gem) {
-    return callMethod("new_gemspec_helper_from_gem", gem, GemspecHelper.class);
+    return container.callMethod(gemspecHelperImplClass, "from_gem", gem, GemspecHelper.class);
+  }
+
+  @Override
+  public RepairHelper newRepairHelper() {
+    return container.callMethod(repairHelperImplClass, "new", RepairHelper.class);
+  }
+
+  @Override
+  public DependencyData newDependencyData(InputStream dependency, String name, long modified) {
+    return container.callMethod(dependencyDataImplClass, "new", new Object[]{dependency, name, modified},
+        DependencyData.class);
   }
 }
