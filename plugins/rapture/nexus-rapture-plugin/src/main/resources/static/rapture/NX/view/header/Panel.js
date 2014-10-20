9320615892a40b93184e57d21ddf32b4bf707177
Jason Dillon <jason@planet57.com>
2014-10-20 15:27:44 -0700

    set height of header

--- plugins/rapture/nexus-rapture-plugin/src/main/resources/static/rapture/NX/view/header/Panel.js
+++ plugins/rapture/nexus-rapture-plugin/src/main/resources/static/rapture/NX/view/header/Panel.js
@@ -41,6 +41,9 @@ Ext.define('NX.view.header.Panel', {
       {
         xtype: 'toolbar',
 
+        // set height to ensure we have uniform size and not depend on what is in the toolbar
+        height: 40,
+
         style: {
           backgroundColor: '#000000'
         },
