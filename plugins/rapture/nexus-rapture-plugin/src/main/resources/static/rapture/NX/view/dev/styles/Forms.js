1f273b2df1208e8b1b44acafb21a45c8f8a48769
Jason Dillon <jason@planet57.com>
2014-11-13 17:04:26 -0800

    Add more form elements from examples/themes

--- plugins/rapture/nexus-rapture-plugin/src/main/resources/static/rapture/NX/view/dev/styles/Forms.js
+++ plugins/rapture/nexus-rapture-plugin/src/main/resources/static/rapture/NX/view/dev/styles/Forms.js
@@ -33,6 +33,7 @@ Ext.define('NX.view.dev.styles.Forms', {
     var me = this;
 
     me.items = [
+      // basic form layout
       {
         xtype: 'form',
         items: [
@@ -44,6 +45,134 @@ Ext.define('NX.view.dev.styles.Forms', {
         buttons: [
           { text: 'Submit', ui: 'primary' },
           { text: 'Discard' }
+        ]
+      },
+
+      // form example from extjs example/themes
+      {
+        xtype: 'form',
+        items: [
+          {
+            xtype: 'label',
+            text: 'Plain Label'
+          },
+          {
+            fieldLabel: 'TextField',
+            xtype: 'textfield',
+            name: 'someField',
+            emptyText: 'Enter a value'
+          },
+          {
+            fieldLabel: 'ComboBox',
+            xtype: 'combo',
+            store: ['Foo', 'Bar']
+          },
+          {
+            fieldLabel: 'DateField',
+            xtype: 'datefield',
+            name: 'date'
+          },
+          {
+            fieldLabel: 'TimeField',
+            name: 'time',
+            xtype: 'timefield'
+          },
+          {
+            fieldLabel: 'NumberField',
+            xtype: 'numberfield',
+            name: 'number',
+            emptyText: '(This field is optional)',
+            allowBlank: true
+          },
+          {
+            fieldLabel: 'TextArea',
+            xtype: 'textareafield',
+            name: 'message',
+            cls: 'x-form-valid',
+            value: 'This field is hard-coded to have the "valid" style (it will require some code changes to add/remove this style dynamically)'
+          },
+          {
+            fieldLabel: 'Checkboxes',
+            xtype: 'checkboxgroup',
+            columns: [100, 100],
+            items: [
+              {boxLabel: 'Foo', checked: true, id: 'fooChk', inputId: 'fooChkInput'},
+              {boxLabel: 'Bar'}
+            ]
+          },
+          {
+            fieldLabel: 'Radios',
+            xtype: 'radiogroup',
+            columns: [100, 100],
+            items: [
+              {boxLabel: 'Foo', checked: true, name: 'radios'},
+              {boxLabel: 'Bar', name: 'radios'}
+            ]
+          },
+          {
+            hideLabel: true,
+            id: 'htmleditor',
+            xtype: 'htmleditor',
+            name: 'html',
+            enableColors: false,
+            value: 'Mouse over toolbar for tooltips.<br /><br />The HTMLEditor IFrame requires a refresh between a stylesheet switch to get accurate colors.',
+            height: 110
+          },
+          {
+            xtype: 'fieldset',
+            title: 'Plain Fieldset',
+            items: [
+              {
+                hideLabel: true,
+                xtype: 'radiogroup',
+                items: [
+                  {boxLabel: 'Radio A', checked: true, name: 'radiogrp2'},
+                  {boxLabel: 'Radio B', name: 'radiogrp2'}
+                ]
+              }
+            ]
+          },
+          {
+            xtype: 'fieldset',
+            title: 'Collapsible Fieldset',
+            collapsible: true,
+            items: [
+              { xtype: 'checkbox', boxLabel: 'Checkbox 1' },
+              { xtype: 'checkbox', boxLabel: 'Checkbox 2' }
+            ]
+          },
+          {
+            xtype: 'fieldset',
+            title: 'Checkbox Fieldset',
+            checkboxToggle: true,
+            items: [
+              { xtype: 'radio', boxLabel: 'Radio 1', name: 'radiongrp1' },
+              { xtype: 'radio', boxLabel: 'Radio 2', name: 'radiongrp1' }
+            ]
+          }
+        ],
+
+        buttons: [
+          {
+            text: 'Toggle Enabled',
+            handler: function () {
+              this.up('form').items.each(function (item) {
+                item.setDisabled(!item.disabled);
+              });
+            }
+          },
+          {
+            text: 'Reset Form',
+            handler: function () {
+              this.up('form').getForm().reset();
+            }
+          },
+          {
+            text: 'Validate',
+            handler: function () {
+              this.up('form').getForm().isValid();
+            }
+          }
         ]
       }
     ];
