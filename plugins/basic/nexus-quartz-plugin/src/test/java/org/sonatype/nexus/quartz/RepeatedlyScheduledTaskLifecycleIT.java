5450afac45072e426df47e6a5855f166d921a0b2
Tamas Cservenak <tamas@cservenak.net>
2014-11-29 11:07:46 +0100

    Stabilize new IT As the expectation was not quite true. Let's give some chance for pool to do it's homework before we assert
    Same fix as in fb65a64063e7fe954cf9aa4c3b4a21780d2ddef8 for
    the other new IT (diff is the ending state, DONE vs WAITING)

--- plugins/basic/nexus-quartz-plugin/src/test/java/org/sonatype/nexus/quartz/RepeatedlyScheduledTaskLifecycleIT.java
+++ plugins/basic/nexus-quartz-plugin/src/test/java/org/sonatype/nexus/quartz/RepeatedlyScheduledTaskLifecycleIT.java
@@ -91,8 +91,14 @@ public class RepeatedlyScheduledTaskLifecycleIT
       // and block for the result
       final String result = future.get();
       assertThat(result, equalTo(RESULT));
+      // taskInfo for DONE task is terminal
+      assertThat(taskInfo.getCurrentState().getState(), equalTo(State.WAITING));
     }
 
+    // the fact that future.get returned still does not mean that the pool is done
+    // pool maintenance might not be done yet
+    // so let's sleep for some
+    Thread.sleep(500);
     // done
     assertThat(nexusTaskScheduler.getRunningTaskCount(), equalTo(0));
 
