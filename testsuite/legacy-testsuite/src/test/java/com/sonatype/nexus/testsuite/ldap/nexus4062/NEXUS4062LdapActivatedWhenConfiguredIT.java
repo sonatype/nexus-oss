821b4b963a1529324562924e02f243c63c4dbc70
Tamas Cservenak <tamas@cservenak.net>
2014-12-15 17:12:19 +0100

    NEXUS-7769: IT related fixes
    As pw encoding is now not using legacy anymore, and Realm name
    changed.

--- testsuite/legacy-testsuite/src/test/java/com/sonatype/nexus/testsuite/ldap/nexus4062/NEXUS4062LdapActivatedWhenConfiguredIT.java
+++ testsuite/legacy-testsuite/src/test/java/com/sonatype/nexus/testsuite/ldap/nexus4062/NEXUS4062LdapActivatedWhenConfiguredIT.java
@@ -69,9 +69,9 @@ public class NEXUS4062LdapActivatedWhenConfiguredIT
     final List<String> activeRealmsAfterConf = SettingsMessageUtil.getCurrentSettings().getSecurityRealms();
 
     // at starting point LDAP Realm was not active
-    assertThat(activeRealmsBeforeConf, not(hasItem("LdapAuthenticatingRealm")));
+    assertThat(activeRealmsBeforeConf, not(hasItem("LdapRealm")));
     // when configured, LDAP Realm was automatically added to active Realms
-    assertThat(activeRealmsAfterConf, hasItem("LdapAuthenticatingRealm"));
+    assertThat(activeRealmsAfterConf, hasItem("LdapRealm"));
   }
 
   private void removeServers() throws Exception {
