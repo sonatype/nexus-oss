025475ead571ad82ee89a8b4bc8ca795e1a8382a
Jason Dillon <jason@planet57.com>
2015-01-05 15:23:35 -0800

    Drop TM from footer, GC states this is not required here (we have TM details in detailed about/copyright)

--- plugins/rapture/nexus-rapture-plugin/src/main/resources/static/rapture/NX/app/PluginStrings.js
+++ plugins/rapture/nexus-rapture-plugin/src/main/resources/static/rapture/NX/app/PluginStrings.js
@@ -57,7 +57,7 @@ Ext.define('NX.app.PluginStrings', {
     GLOBAL_HEADER_HELP_SUPPORT: 'Support',
 
     // Footer
-    GLOBAL_FOOTER_COPYRIGHT: 'Sonatype Nexus™ Copyright © 2008-2015, Sonatype Inc. All rights reserved.',
+    GLOBAL_FOOTER_COPYRIGHT: 'Copyright © 2008-2015, Sonatype Inc. All rights reserved.',
 
     // Sign in
     GLOBAL_SIGN_IN_TITLE: 'Sign In',
