e7fb9c3ec644761044e9ec08e3fd42e8bdde5533
Stuart McCulloch <mcculls@gmail.com>
2015-01-12 15:01:12 +0000

    NEXUS-7903: fix content-range algorithm
    * a range of [0..0] should return one byte
    * implicit range boundary should be length-1
    * satisfiable range should be [0..length-1]

--- plugins/basic/nexus-content-plugin/src/main/java/org/sonatype/nexus/content/internal/ContentServlet.java
+++ plugins/basic/nexus-content-plugin/src/main/java/org/sonatype/nexus/content/internal/ContentServlet.java
@@ -521,7 +521,7 @@ public class ContentServlet
           response.setHeader("Content-Range", "bytes */" + file.getLength());
           return;
         }
-        final long bodySize = range.upperEndpoint() - range.lowerEndpoint();
+        final long bodySize = 1 + range.upperEndpoint() - range.lowerEndpoint();
         response.setStatus(SC_PARTIAL_CONTENT);
         response.setHeader("Content-Length", String.valueOf(bodySize));
         response.setHeader("Content-Range",
@@ -638,7 +638,7 @@ public class ContentServlet
           }
           else if (rangeValue.endsWith("-")) {
             return Collections.singletonList(Range.closed(
-                Long.parseLong(rangeValue.substring(0, rangeValue.length() - 1)), contentLength));
+                Long.parseLong(rangeValue.substring(0, rangeValue.length() - 1)), contentLength - 1));
           }
           else if (rangeValue.contains("-")) {
             final String[] parts = rangeValue.split("-");
@@ -670,6 +670,6 @@ public class ContentServlet
    * Returns {@code true} if the {@link Range} is applicable to file (file full closed range encloses passed in range).
    */
   protected boolean isRequestedRangeSatisfiable(final StorageFileItem file, final Range<Long> range) {
-    return Range.closed(0L, file.getLength()).encloses(range);
+    return Range.closed(0L, file.getLength() - 1).encloses(range);
   }
 }
