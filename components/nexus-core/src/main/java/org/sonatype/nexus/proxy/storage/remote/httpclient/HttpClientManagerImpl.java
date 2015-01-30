57e24fdf85071067d6c8dcd7d7049bcae97129c5
Tamas Cservenak <tamas@cservenak.net>
2015-01-30 12:52:46 +0100

    NEXUS-7915: Expose circular redirection and cookie settings via properties

--- components/nexus-core/src/main/java/org/sonatype/nexus/proxy/storage/remote/httpclient/HttpClientManagerImpl.java
+++ components/nexus-core/src/main/java/org/sonatype/nexus/proxy/storage/remote/httpclient/HttpClientManagerImpl.java
@@ -13,8 +13,10 @@
 package org.sonatype.nexus.proxy.storage.remote.httpclient;
 
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Locale;
 import java.util.Objects;
+import java.util.Set;
 
 import javax.annotation.Nullable;
 import javax.inject.Inject;
@@ -27,16 +29,22 @@ import org.sonatype.nexus.proxy.repository.ProxyRepository;
 import org.sonatype.nexus.proxy.storage.remote.RemoteItemNotFoundException;
 import org.sonatype.nexus.proxy.storage.remote.RemoteStorageContext;
 import org.sonatype.nexus.proxy.utils.UserAgentBuilder;
+import org.sonatype.nexus.util.SystemPropertiesHelper;
 import org.sonatype.sisu.goodies.common.ComponentSupport;
 
-import com.google.common.base.Preconditions;
+import com.google.common.base.Function;
+import com.google.common.base.Splitter;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Sets;
 import org.apache.http.Header;
 import org.apache.http.HttpRequest;
 import org.apache.http.HttpResponse;
 import org.apache.http.ProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.RedirectStrategy;
+import org.apache.http.client.config.CookieSpecs;
 import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.client.DefaultRedirectStrategy;
 import org.apache.http.protocol.HttpContext;
 import org.slf4j.Logger;
@@ -55,9 +63,17 @@ public class HttpClientManagerImpl
     extends ComponentSupport
     implements HttpClientManager
 {
+  private static final String NX_REMOTE_ENABLE_CIRCULAR_REDIRECTS_KEY = "nexus.remoteStorage.enableCircularRedirectsForHosts";
+
+  private static final String NX_REMOTE_USE_COOKIES_KEY = "nexus.remoteStorage.useCookiesForHosts";
+
   private final Hc4Provider hc4Provider;
 
   private final UserAgentBuilder userAgentBuilder;
+
+  private final Set<String> enableCircularRedirectsForHosts;
+
+  private final Set<String> useCookiesForHosts;
 
   /**
    * Constructor.
@@ -69,6 +85,8 @@ public class HttpClientManagerImpl
   public HttpClientManagerImpl(final Hc4Provider hc4Provider, final UserAgentBuilder userAgentBuilder) {
     this.hc4Provider = checkNotNull(hc4Provider);
     this.userAgentBuilder = checkNotNull(userAgentBuilder);
+    this.enableCircularRedirectsForHosts = parseAndNormalizeCsvProperty(NX_REMOTE_ENABLE_CIRCULAR_REDIRECTS_KEY);
+    this.useCookiesForHosts = parseAndNormalizeCsvProperty(NX_REMOTE_USE_COOKIES_KEY);
   }
 
   @Override
@@ -88,6 +106,57 @@ public class HttpClientManagerImpl
   // ==
 
   /**
+   * Normalizes proxy repository's hostname extracted from it's {@link ProxyRepository#getRemoteUrl()} method. Never
+   * returns {@code null}.
+   *
+   * @see #normalizeHostname(String)
+   */
+  private String normalizeHostname(final ProxyRepository proxyRepository) {
+    try {
+      final URI uri = new URI(proxyRepository.getRemoteUrl());
+      return normalizeHostname(uri.getHost());
+    }
+    catch (URISyntaxException e) {
+      // ignore
+    }
+    return "";
+  }
+
+  /**
+   * Normalizes passed in host name string by lower casing it. Never returns {@code null} even if input was {@code
+   * null}.
+   */
+  private String normalizeHostname(final String hostName) {
+    if (hostName == null) {
+      return "";
+    } else {
+      return hostName.toLowerCase(Locale.US).trim();
+    }
+  }
+
+  /**
+   * Parses and normalizes (by lower-casing) CSV of host names under given property key. Never returns {@code null}.
+   * Never returns a set that contains {@code null} or empty strings.
+   */
+  private Set<String> parseAndNormalizeCsvProperty(final String systemPropertyKey) {
+    return Sets.newHashSet(
+        Iterables.transform(
+            Splitter.on(",")
+                .trimResults()
+                .omitEmptyStrings()
+                .split(SystemPropertiesHelper.getString(systemPropertyKey, "")),
+            new Function<String, String>()
+            {
+              @Override
+              public String apply(final String input) {
+                return normalizeHostname(input);
+              }
+            }
+        )
+    );
+  }
+
+  /**
    * Configures the fresh instance of HttpClient for given proxy repository specific needs. Right now it sets
    * appropriate redirect strategy only.
    */
@@ -99,6 +168,18 @@ public class HttpClientManagerImpl
 
     // set proxy redirect strategy
     builder.getHttpClientBuilder().setRedirectStrategy(getProxyRepositoryRedirectStrategy(proxyRepository, ctx));
+
+    final String proxyHostName = normalizeHostname(proxyRepository);
+    if (enableCircularRedirectsForHosts.contains(proxyHostName)) {
+      log.info("Allowing circular redirects in proxy {}", proxyRepository);
+      builder.getRequestConfigBuilder().setCircularRedirectsAllowed(true); // allow circular redirects
+      builder.getRequestConfigBuilder().setMaxRedirects(10); // lessen max redirects from default 50
+    }
+    if (useCookiesForHosts.contains(proxyHostName)) {
+      log.info("Allowing cookie use in proxy {}", proxyRepository);
+      builder.getHttpClientBuilder().setDefaultCookieStore(new BasicCookieStore()); // in memory only
+      builder.getRequestConfigBuilder().setCookieSpec(CookieSpecs.BROWSER_COMPATIBILITY); // emulate browsers
+    }
   }
 
   /**
--- components/nexus-core/src/test/java/org/sonatype/nexus/proxy/storage/remote/httpclient/HttpClientManagerTest.java
+++ components/nexus-core/src/test/java/org/sonatype/nexus/proxy/storage/remote/httpclient/HttpClientManagerTest.java
@@ -12,7 +12,10 @@
  */
 package org.sonatype.nexus.proxy.storage.remote.httpclient;
 
+import java.lang.reflect.Field;
+
 import org.sonatype.nexus.apachehttpclient.Hc4Provider;
+import org.sonatype.nexus.apachehttpclient.Hc4Provider.Builder;
 import org.sonatype.nexus.apachehttpclient.Hc4ProviderImpl;
 import org.sonatype.nexus.apachehttpclient.PoolingClientConnectionManagerMBeanInstaller;
 import org.sonatype.nexus.configuration.application.ApplicationConfiguration;
@@ -21,20 +24,27 @@ import org.sonatype.nexus.proxy.repository.ProxyRepository;
 import org.sonatype.nexus.proxy.repository.RemoteProxySettings;
 import org.sonatype.nexus.proxy.storage.remote.RemoteStorageContext;
 import org.sonatype.nexus.proxy.utils.UserAgentBuilder;
+import org.sonatype.nexus.util.SystemPropertiesHelper;
 import org.sonatype.sisu.goodies.eventbus.EventBus;
 import org.sonatype.sisu.litmus.testsupport.TestSupport;
 
+import com.google.common.base.Throwables;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.ProtocolException;
 import org.apache.http.StatusLine;
+import org.apache.http.client.CookieStore;
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.RedirectStrategy;
+import org.apache.http.client.config.CookieSpecs;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.cookie.CookieSpec;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.client.StandardHttpRequestRetryHandler;
 import org.apache.http.message.BasicHeader;
 import org.apache.http.protocol.BasicHttpContext;
 import org.apache.http.protocol.HttpContext;
+import org.hamcrest.Matcher;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Ignore;
@@ -42,7 +52,10 @@ import org.junit.Test;
 import org.mockito.Mock;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.nullValue;
 import static org.mockito.Mockito.when;
 
 /**
@@ -84,8 +97,6 @@ public class HttpClientManagerTest
 
   private Hc4Provider hc4Provider;
 
-  private HttpClientManagerImpl httpClientManager;
-
   @Before
   public void before() {
     final DefaultRemoteConnectionSettings rcs = new DefaultRemoteConnectionSettings();
@@ -98,14 +109,13 @@ public class HttpClientManagerTest
 
     when(proxyRepository.getId()).thenReturn("central");
     when(response.getStatusLine()).thenReturn(statusLine);
-
-    httpClientManager = new HttpClientManagerImpl(hc4Provider, userAgentBuilder);
   }
 
   @Test
   public void doNotFollowRedirectsToDirIndex()
       throws ProtocolException
   {
+    final HttpClientManagerImpl httpClientManager = new HttpClientManagerImpl(hc4Provider, userAgentBuilder);
     final RedirectStrategy underTest =
         httpClientManager.getProxyRepositoryRedirectStrategy(proxyRepository, globalRemoteStorageContext);
     HttpContext httpContext;
@@ -139,6 +149,7 @@ public class HttpClientManagerTest
   public void doFollowCrossSiteRedirects()
       throws ProtocolException
   {
+    final HttpClientManagerImpl httpClientManager = new HttpClientManagerImpl(hc4Provider, userAgentBuilder);
     final RedirectStrategy underTest =
         httpClientManager.getProxyRepositoryRedirectStrategy(proxyRepository, globalRemoteStorageContext);
 
@@ -154,6 +165,106 @@ public class HttpClientManagerTest
     when(statusLine.getStatusCode()).thenReturn(HttpStatus.SC_MOVED_TEMPORARILY);
     when(response.getFirstHeader("location")).thenReturn(new BasicHeader("location", "http://hostB/dir/"));
     assertThat(underTest.isRedirected(request, response, new BasicHttpContext()), is(true));
+  }
+
+  @Test
+  public void enableCircularRedirectsForHosts()
+  {
+    System.setProperty("nexus.remoteStorage.enableCircularRedirectsForHosts", "Other.org,someDomain.com");
+    try {
+      final HttpClientManagerImpl httpClientManager = new HttpClientManagerImpl(hc4Provider, userAgentBuilder);
+      Builder builder;
+
+      // not listed hostname, all as before
+      when(proxyRepository.getRemoteUrl()).thenReturn("http://repo1.central.org/maven2/");
+      builder = hc4Provider.prepareHttpClient(globalRemoteStorageContext);
+      httpClientManager.configure(proxyRepository, globalRemoteStorageContext, builder);
+      fieldEqualsReflection(builder.getRequestConfigBuilder(), "circularRedirectsAllowed", false);
+      fieldEqualsReflection(builder.getRequestConfigBuilder(), "maxRedirects", 50);
+
+      // not listed hostname, all as before (non-standard url, like procurement)
+      when(proxyRepository.getRemoteUrl()).thenReturn("foo:bar");
+      builder = hc4Provider.prepareHttpClient(globalRemoteStorageContext);
+      httpClientManager.configure(proxyRepository, globalRemoteStorageContext, builder);
+      fieldEqualsReflection(builder.getRequestConfigBuilder(), "circularRedirectsAllowed", false);
+      fieldEqualsReflection(builder.getRequestConfigBuilder(), "maxRedirects", 50);
+
+      // listed, changes should be applied
+      when(proxyRepository.getRemoteUrl()).thenReturn("http://other.org/maven2/");
+      builder = hc4Provider.prepareHttpClient(globalRemoteStorageContext);
+      httpClientManager.configure(proxyRepository, globalRemoteStorageContext, builder);
+      fieldEqualsReflection(builder.getRequestConfigBuilder(), "circularRedirectsAllowed", true);
+      fieldEqualsReflection(builder.getRequestConfigBuilder(), "maxRedirects", 10);
+
+      // listed, changes should be applied
+      when(proxyRepository.getRemoteUrl()).thenReturn("https://SOMEDOMAIN.COM/mavenrepo/");
+      builder = hc4Provider.prepareHttpClient(globalRemoteStorageContext);
+      httpClientManager.configure(proxyRepository, globalRemoteStorageContext, builder);
+      fieldEqualsReflection(builder.getRequestConfigBuilder(), "circularRedirectsAllowed", true);
+      fieldEqualsReflection(builder.getRequestConfigBuilder(), "maxRedirects", 10);
+    } finally {
+      System.clearProperty("nexus.remoteStorage.enableCircularRedirectsForHosts");
+    }
+  }
+
+  @Test
+  public void useCookiesForHosts()
+  {
+    System.setProperty("nexus.remoteStorage.useCookiesForHosts", "Other.org,someDomain.com");
+    try {
+      final HttpClientManagerImpl httpClientManager = new HttpClientManagerImpl(hc4Provider, userAgentBuilder);
+      Builder builder;
+
+      // not listed hostname, all as before
+      when(proxyRepository.getRemoteUrl()).thenReturn("http://repo1.central.org/maven2/");
+      builder = hc4Provider.prepareHttpClient(globalRemoteStorageContext);
+      httpClientManager.configure(proxyRepository, globalRemoteStorageContext, builder);
+      fieldEqualsReflection(builder.getHttpClientBuilder(), "cookieStore", nullValue());
+      fieldEqualsReflection(builder.getRequestConfigBuilder(), "cookieSpec", CookieSpecs.IGNORE_COOKIES);
+
+      // not listed hostname, all as before (non-standard url, like procurement)
+      when(proxyRepository.getRemoteUrl()).thenReturn("foo:bar");
+      builder = hc4Provider.prepareHttpClient(globalRemoteStorageContext);
+      httpClientManager.configure(proxyRepository, globalRemoteStorageContext, builder);
+      fieldEqualsReflection(builder.getHttpClientBuilder(), "cookieStore", nullValue());
+      fieldEqualsReflection(builder.getRequestConfigBuilder(), "cookieSpec", CookieSpecs.IGNORE_COOKIES);
+
+      // listed, changes should be applied
+      when(proxyRepository.getRemoteUrl()).thenReturn("http://other.org/maven2/");
+      builder = hc4Provider.prepareHttpClient(globalRemoteStorageContext);
+      httpClientManager.configure(proxyRepository, globalRemoteStorageContext, builder);
+      fieldEqualsReflection(builder.getHttpClientBuilder(), "cookieStore", notNullValue());
+      fieldEqualsReflection(builder.getRequestConfigBuilder(), "cookieSpec", CookieSpecs.BROWSER_COMPATIBILITY);
+
+      // listed, changes should be applied
+      when(proxyRepository.getRemoteUrl()).thenReturn("https://SOMEDOMAIN.COM/mavenrepo/");
+      builder = hc4Provider.prepareHttpClient(globalRemoteStorageContext);
+      httpClientManager.configure(proxyRepository, globalRemoteStorageContext, builder);
+      fieldEqualsReflection(builder.getHttpClientBuilder(), "cookieStore", notNullValue());
+      fieldEqualsReflection(builder.getRequestConfigBuilder(), "cookieSpec", CookieSpecs.BROWSER_COMPATIBILITY);
+    } finally {
+      System.clearProperty("nexus.remoteStorage.useCookiesForHosts");
+    }
+  }
+
+  private void fieldEqualsReflection(final Object instance, final String fieldName, final Object expected) {
+    if (expected == null) {
+      fieldEqualsReflection(instance, fieldName, nullValue());
+    } else {
+      fieldEqualsReflection(instance, fieldName, equalTo(expected));
+    }
+  }
+
+  private void fieldEqualsReflection(final Object instance, final String fieldName, final Matcher<? super Object> matcher) {
+    try {
+      final Field field = instance.getClass().getDeclaredField(fieldName);
+      field.setAccessible(true);
+      final Object value = field.get(instance);
+      assertThat("Class " + instance.getClass().getSimpleName() + " field " + fieldName, value, matcher);
+    }
+    catch (Exception e) {
+      throw new RuntimeException("Class " + instance.getClass().getSimpleName() + " field " + fieldName, e);
+    }
   }
 
   @Test
