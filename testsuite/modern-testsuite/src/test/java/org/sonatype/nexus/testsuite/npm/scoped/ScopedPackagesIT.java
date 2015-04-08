a33d05eb4842c6b13823b18642f76f484fc3e989
Tamas Cservenak <tamas@cservenak.net>
2015-04-08 13:59:21 +0200

    Fix IT

--- testsuite/modern-testsuite/src/test/java/org/sonatype/nexus/testsuite/npm/scoped/ScopedPackagesIT.java
+++ testsuite/modern-testsuite/src/test/java/org/sonatype/nexus/testsuite/npm/scoped/ScopedPackagesIT.java
@@ -124,9 +124,9 @@ public class ScopedPackagesIT
     assertThat(registry1Paths, hasSize(1));
     assertThat(registry1Paths, containsInAnyOrder("/testproject"));
 
-    // registry2 should been asked for metadata
+    // registry2 should been asked for metadata 2 times (scoped req did not touch reg1)
     final List<String> registry2Paths = mockNpmRegistry2.getPathRecorder().getPathsForVerb("GET");
-    assertThat(registry2Paths, hasSize(1));
-    assertThat(registry2Paths, containsInAnyOrder("/@registry2/testproject"));
+    assertThat(registry2Paths, hasSize(2));
+    assertThat(registry2Paths, containsInAnyOrder("/testproject", "/@registry2/testproject"));
   }
 }
