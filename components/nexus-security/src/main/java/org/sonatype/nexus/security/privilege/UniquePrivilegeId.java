adaf006947ebf18336ee73afa95d5f90c0eb8a4f
Jason Dillon <jason@planet57.com>
2015-05-19 11:54:07 -0700

    fix UniquePrivilegeId validtor (oops)

--- components/nexus-security/src/main/java/org/sonatype/nexus/security/privilege/UniquePrivilegeId.java
+++ components/nexus-security/src/main/java/org/sonatype/nexus/security/privilege/UniquePrivilegeId.java
@@ -18,8 +18,6 @@ import java.lang.annotation.Target;
 import javax.validation.Constraint;
 import javax.validation.Payload;
 
-import org.sonatype.nexus.security.role.UniqueRoleIdValidator;
-
 import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
 import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.METHOD;
@@ -33,7 +31,7 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
  */
 @Target({FIELD, METHOD, PARAMETER, ANNOTATION_TYPE})
 @Retention(RUNTIME)
-@Constraint(validatedBy = UniqueRoleIdValidator.class)
+@Constraint(validatedBy = UniquePrivilegeIdValidator.class)
 public @interface UniquePrivilegeId
 {
   String message() default "Privilege-ID must be unique";
