e0647fca85f6c8e4898c025e7726851bb89406d1
Daniel Sauble <djsauble@gmail.com>
2015-06-01 18:15:45 +0000

    Renames 'Select Provider' to 'Select Recipe'

--- plugins/rapture/nexus-coreui-plugin/src/main/resources/static/rapture/NX/coreui/app/PluginStrings.js
+++ plugins/rapture/nexus-coreui-plugin/src/main/resources/static/rapture/NX/coreui/app/PluginStrings.js
@@ -147,7 +147,7 @@ Ext.define('NX.coreui.app.PluginStrings', {
     ADMIN_REPOSITORIES_TITLE: 'Repositories',
     ADMIN_REPOSITORIES_SUBTITLE: 'Manage repositories',
     ADMIN_REPOSITORIES_CREATE_TITLE: 'Create Repository: {0}',
-    ADMIN_REPOSITORIES_SELECT_TITLE: 'Select Provider',
+    ADMIN_REPOSITORIES_SELECT_TITLE: 'Select Recipe',
     ADMIN_REPOSITORIES_CREATE_SUCCESS: 'Repository created: ',
     ADMIN_REPOSITORIES_CREATE_ERROR: 'You do not have permission to create repositories',
     ADMIN_REPOSITORIES_UPDATE_SUCCESS: 'Repository updated: ',
